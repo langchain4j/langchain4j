9baced45694f87ef42f3d468d467d58d89675d6e
deep-learning-dynamo <deep-learning-dynamo@gmail.com>
2023-07-12 17:18:44 +0200

    added an option to split multiple documents at once

--- langchain4j-core/src/main/java/dev/langchain4j/data/document/DocumentSplitter.java
+++ langchain4j-core/src/main/java/dev/langchain4j/data/document/DocumentSplitter.java
@@ -2,7 +2,15 @@ package dev.langchain4j.data.document;
 
 import java.util.List;
 
+import static java.util.stream.Collectors.toList;
+
 public interface DocumentSplitter {
 
     List<DocumentSegment> split(Document document);
+
+    default List<DocumentSegment> split(List<Document> documents) {
+        return documents.stream()
+                .flatMap(document -> split(document).stream())
+                .collect(toList());
+    }
 }
