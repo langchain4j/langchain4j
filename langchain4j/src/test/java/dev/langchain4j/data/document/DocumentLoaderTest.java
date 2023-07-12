f76b9ff1a06a6e65b601f293073fef481403a578
deep-learning-dynamo <deep-learning-dynamo@gmail.com>
2023-07-12 18:12:45 +0200

    improved DocumentLoaderTest a bit

--- langchain4j/src/test/java/dev/langchain4j/data/document/DocumentLoaderTest.java
+++ langchain4j/src/test/java/dev/langchain4j/data/document/DocumentLoaderTest.java
@@ -6,54 +6,43 @@ import java.net.MalformedURLException;
 import java.net.URL;
 import java.nio.file.Paths;
 
-import static dev.langchain4j.data.document.DocumentType.PDF;
-import static dev.langchain4j.data.document.DocumentType.TEXT;
 import static org.assertj.core.api.Assertions.assertThat;
 
 class DocumentLoaderTest {
 
     @Test
     void should_load_text_document_from_file_system() {
-        DocumentLoader loader = DocumentLoader.from(Paths.get("src/test/resources/test-file-utf8.txt"), TEXT);
-
+        DocumentLoader loader = DocumentLoader.from(Paths.get("src/test/resources/test-file-utf8.txt"));
 
         Document document = loader.load();
 
-
         assertThat(document.text()).isEqualTo("test\ncontent");
-
         Metadata metadata = document.metadata();
         assertThat(metadata.get("file_name")).isEqualTo("test-file-utf8.txt");
-        assertThat(Paths.get(metadata.get("absolute_directory_path").toString())).isAbsolute();
+        assertThat(Paths.get(metadata.get("absolute_directory_path"))).isAbsolute();
     }
 
     @Test
     void should_load_text_document_from_url() throws MalformedURLException {
-        String url = "https://raw.githubusercontent.com/ai-for-java/ai4j/master/src/test/java/test-file.txt";
-        DocumentLoader loader = DocumentLoader.from(new URL(url), TEXT);
-
+        String url = "https://raw.githubusercontent.com/langchain4j/langchain4j/main/langchain4j/src/test/resources/test-file-utf8.txt";
+        DocumentLoader loader = DocumentLoader.from(new URL(url));
 
         Document document = loader.load();
 
-
-        assertThat(document.text()).isEqualToIgnoringWhitespace("test\ncontent");
-
+        assertThat(document.text()).isEqualTo("test\ncontent");
         Metadata metadata = document.metadata();
         assertThat(metadata.get("url")).isEqualTo(url);
     }
 
     @Test
     void should_load_pdf_document_from_file_system() {
-        DocumentLoader loader = DocumentLoader.from(Paths.get("src/test/resources/test-file.pdf"), PDF);
-
+        DocumentLoader loader = DocumentLoader.from(Paths.get("src/test/resources/test-file.pdf"));
 
         Document document = loader.load();
 
-
         assertThat(document.text()).isEqualToIgnoringWhitespace("test\ncontent");
-
         Metadata metadata = document.metadata();
         assertThat(metadata.get("file_name")).isEqualTo("test-file.pdf");
-        assertThat(Paths.get(metadata.get("absolute_directory_path").toString())).isAbsolute();
+        assertThat(Paths.get(metadata.get("absolute_directory_path"))).isAbsolute();
     }
 }
