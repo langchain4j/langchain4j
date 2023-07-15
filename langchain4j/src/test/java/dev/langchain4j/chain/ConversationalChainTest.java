5ac5858bd285cd1b05eb1d91c40b830c86e8871e
deep-learning-dynamo <deep-learning-dynamo@gmail.com>
2023-07-15 17:17:57 +0200

    refactored ConversationalChainTest

--- langchain4j/src/test/java/dev/langchain4j/chain/ConversationalChainTest.java
+++ langchain4j/src/test/java/dev/langchain4j/chain/ConversationalChainTest.java
@@ -1,114 +1,61 @@
 package dev.langchain4j.chain;
 
-import dev.langchain4j.data.message.AiMessage;
-import dev.langchain4j.data.message.UserMessage;
 import dev.langchain4j.memory.ChatMemory;
 import dev.langchain4j.memory.chat.MessageWindowChatMemory;
 import dev.langchain4j.model.chat.ChatLanguageModel;
 import dev.langchain4j.model.output.Result;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.NullSource;
+import org.junit.jupiter.params.provider.ValueSource;
 
-import java.util.Collections;
-import java.util.List;
-
+import static dev.langchain4j.data.message.AiMessage.aiMessage;
+import static dev.langchain4j.data.message.UserMessage.userMessage;
+import static java.util.Collections.singletonList;
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.assertj.core.api.Assertions.assertThatExceptionOfType;
-import static org.mockito.Mockito.any;
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.times;
-import static org.mockito.Mockito.verify;
-import static org.mockito.Mockito.when;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.mockito.Mockito.*;
 
 public class ConversationalChainTest {
 
     @Test
-    public void testExecute_PositiveCase() {
+    public void should_store_user_and_ai_messages_in_chat_memory() {
         // Given
         ChatLanguageModel chatLanguageModel = mock(ChatLanguageModel.class);
-        ChatMemory chatMemory = MessageWindowChatMemory.withCapacity(5);
-        UserMessage userMessage = UserMessage.from("hello Ai.");
-        chatMemory.add(userMessage);
-        AiMessage aiMessage = AiMessage.from("hello human.");
-        chatMemory.add(aiMessage);
-        AiMessage result = new AiMessage("Result");
-        Result<AiMessage> aiMessageResult = new Result<>(result);
-        String userMessage2 = "how are you doing?";
-        when(chatLanguageModel.sendMessages(any(List.class))).thenReturn(aiMessageResult);
+        String aiMessage = "Hi there";
+        when(chatLanguageModel.sendMessages(anyList())).thenReturn(Result.from(aiMessage(aiMessage)));
+
+        ChatMemory chatMemory = spy(MessageWindowChatMemory.withCapacity(10));
 
         ConversationalChain chain = ConversationalChain.builder()
                 .chatLanguageModel(chatLanguageModel)
                 .chatMemory(chatMemory)
                 .build();
 
-        // When
-
-        String response = chain.execute(userMessage2);
-
-        // Then
-        assertThat(response).isEqualTo("Result");
-    }
-
-    @Test
-    public void testExecute_NegativeCase_EmptyInput() {
-        // Given
-        ChatLanguageModel chatLanguageModel = mock(ChatLanguageModel.class);
-        ChatMemory chatMemory = mock(ChatMemory.class);
-
-        ConversationalChain chain = ConversationalChain.builder()
-                .chatLanguageModel(chatLanguageModel)
-                .chatMemory(chatMemory)
-                .build();
-
-        // When & Then
-        assertThatExceptionOfType(IllegalArgumentException.class)
-                .isThrownBy(() -> chain.execute(""));
-    }
-
-    @Test
-    public void testExecute_EdgeCase_WhitespaceInput() {
-        // Given
-        ChatLanguageModel chatLanguageModel = mock(ChatLanguageModel.class);
-        ChatMemory chatMemory = mock(ChatMemory.class);
-
-        ConversationalChain chain = ConversationalChain.builder()
-                .chatLanguageModel(chatLanguageModel)
-                .chatMemory(chatMemory)
-                .build();
-
-        // When & Then
-        assertThatExceptionOfType(IllegalArgumentException.class)
-                .isThrownBy(() -> chain.execute("   "));
-    }
-
-    @Test
-    public void testExecute_CornerCase_LongStringInput() {
-        // Given
-        ChatLanguageModel chatLanguageModel = mock(ChatLanguageModel.class);
-        ChatMemory chatMemory = mock(ChatMemory.class);
-        String userMessage = String.join("", Collections.nCopies(100, "Hello AI"));
-        Result<AiMessage> outputAiMessage = new Result<>(new AiMessage("Result"));
-        when(chatLanguageModel.sendMessages(any(List.class))).thenReturn(outputAiMessage);
-
-        ConversationalChain chain = ConversationalChain.builder()
-                .chatLanguageModel(chatLanguageModel)
-                .chatMemory(chatMemory)
-                .build();
+        String userMessage = "Hello";
 
         // When
         String response = chain.execute(userMessage);
 
         // Then
-        assertThat(response).isEqualTo("Result");
-        verify(chatMemory, times(2)).add(any());
+        assertThat(response).isEqualTo(aiMessage);
+
+        verify(chatMemory).add(userMessage(userMessage));
+        verify(chatMemory).messages();
+        verify(chatMemory).add(aiMessage(aiMessage));
+        verifyNoMoreInteractions(chatMemory);
+
+        verify(chatLanguageModel).sendMessages(singletonList(userMessage(userMessage)));
+        verifyNoMoreInteractions(chatLanguageModel);
     }
 
-    @Test
-    public void testExecute_NegativeCase_NullResponse() {
+    @ParameterizedTest
+    @NullSource
+    @ValueSource(strings = {"", " "})
+    public void should_fail_when_user_message_is_null_or_blank(String userMessage) {
         // Given
         ChatLanguageModel chatLanguageModel = mock(ChatLanguageModel.class);
         ChatMemory chatMemory = mock(ChatMemory.class);
-        String userMessage = "Hello AI";
-        when(chatLanguageModel.sendMessages(any(List.class))).thenReturn(null);
 
         ConversationalChain chain = ConversationalChain.builder()
                 .chatLanguageModel(chatLanguageModel)
@@ -116,8 +63,8 @@ public class ConversationalChainTest {
                 .build();
 
         // When & Then
-        assertThatExceptionOfType(NullPointerException.class)
-                .isThrownBy(() -> chain.execute(userMessage));
+        assertThatThrownBy(() -> chain.execute(userMessage))
+                .isExactlyInstanceOf(IllegalArgumentException.class)
+                .hasMessage("userMessage cannot be null or blank");
     }
-
 }
