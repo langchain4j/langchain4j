ca96e99f57dba76ca2019c9d318cbcfd3a24df81
LangChain4j <langchain4j@gmail.com>
2023-07-17 22:09:23 +0200

    improved retry mechanism (#32)
    - do not retry in case of 401
    - wait for 1 second before retrying in case of 429

--- langchain4j/src/test/java/dev/langchain4j/internal/RetryUtilsTest.java
+++ langchain4j/src/test/java/dev/langchain4j/internal/RetryUtilsTest.java
@@ -1,5 +1,6 @@
 package dev.langchain4j.internal;
 
+import dev.ai4j.openai4j.OpenAiHttpException;
 import org.junit.jupiter.api.Test;
 
 import java.util.concurrent.Callable;
@@ -20,17 +21,21 @@ class RetryUtilsTest {
 
         assertThat(result).isEqualTo("Success");
         verify(mockAction).call();
+        verifyNoMoreInteractions(mockAction);
     }
 
     @Test
     void testRetryThenSuccess() throws Exception {
         Callable<String> mockAction = mock(Callable.class);
-        when(mockAction.call()).thenThrow(new RuntimeException()).thenReturn("Success");
+        when(mockAction.call())
+                .thenThrow(new RuntimeException())
+                .thenReturn("Success");
 
         String result = withRetry(mockAction, 3);
 
         assertThat(result).isEqualTo("Success");
         verify(mockAction, times(2)).call();
+        verifyNoMoreInteractions(mockAction);
     }
 
     @Test
@@ -41,5 +46,41 @@ class RetryUtilsTest {
         assertThatThrownBy(() -> withRetry(mockAction, 3))
                 .isInstanceOf(RuntimeException.class);
         verify(mockAction, times(3)).call();
+        verifyNoMoreInteractions(mockAction);
+    }
+
+    @Test
+    void should_not_retry_401_unauthorized() throws Exception {
+        Callable<String> mockAction = mock(Callable.class);
+        when(mockAction.call()).thenThrow(new OpenAiHttpException(401, "Unauthorized"));
+
+        assertThatThrownBy(() -> withRetry(mockAction, 3))
+                .isInstanceOf(RuntimeException.class)
+                .hasCauseInstanceOf(OpenAiHttpException.class)
+                .hasRootCauseMessage("Unauthorized");
+
+        verify(mockAction).call();
+        verifyNoMoreInteractions(mockAction);
+    }
+
+    @Test
+    void should_wait_1_second_before_retry_when_429_too_many_requests() throws Exception {
+        Callable<String> mockAction = mock(Callable.class);
+        when(mockAction.call())
+                .thenThrow(new OpenAiHttpException(429, "Too many requests"))
+                .thenReturn("Success");
+
+        long startTime = System.currentTimeMillis();
+
+        String result = withRetry(mockAction, 3);
+
+        long endTime = System.currentTimeMillis();
+        long duration = endTime - startTime;
+
+        assertThat(result).isEqualTo("Success");
+        verify(mockAction, times(2)).call();
+        verifyNoMoreInteractions(mockAction);
+
+        assertThat(duration).isGreaterThanOrEqualTo(1000);
     }
 }
