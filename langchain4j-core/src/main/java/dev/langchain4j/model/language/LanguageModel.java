95d5b2cbac78a9dd4699c46caba4e707132ec848
deep-learning-dynamo <deep-learning-dynamo@gmail.com>
2023-07-23 18:05:34 +0200

    added support for streaming tool execution requests from LLM

--- langchain4j-core/src/main/java/dev/langchain4j/model/language/LanguageModel.java
+++ langchain4j-core/src/main/java/dev/langchain4j/model/language/LanguageModel.java
@@ -5,6 +5,7 @@ import dev.langchain4j.model.input.Prompt;
 /**
  * Represents a LLM with a simple text interface.
  * It is recommended to use the ChatLanguageModel instead, as it offers greater capabilities.
+ * More details: https://openai.com/blog/gpt-4-api-general-availability
  */
 public interface LanguageModel {
 
