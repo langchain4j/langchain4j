71770ef7d5c92360d33307fdbcffa6b19890b36b
deep-learning-dynamo <deep-learning-dynamo@gmail.com>
2023-07-24 19:46:13 +0200

    we are out of free Git LFS quota. temporarily disabled in-process embedding model tests

--- langchain4j-embeddings-all-minilm-l6-v2/src/test/java/dev/langchain4j/model/embedding/ALL_MINILM_L6_V2_EmbeddingModelTest.java
+++ langchain4j-embeddings-all-minilm-l6-v2/src/test/java/dev/langchain4j/model/embedding/ALL_MINILM_L6_V2_EmbeddingModelTest.java
@@ -1,6 +1,7 @@
 package dev.langchain4j.model.embedding;
 
 import dev.langchain4j.data.embedding.Embedding;
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -8,6 +9,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 class ALL_MINILM_L6_V2_EmbeddingModelTest {
 
     @Test
+    @Disabled("Temporary disabling. This test should run only when this or used (e.g. langchain4j-embeddings) module(s) change")
     void should_embed() {
 
         EmbeddingModel model = new ALL_MINILM_L6_V2_EmbeddingModel();
