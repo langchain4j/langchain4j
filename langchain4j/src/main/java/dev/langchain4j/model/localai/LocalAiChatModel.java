d2c972311f34aaa5e880d3510b2ab8d669cb455c
kuraleta <digital.kuraleta@gmail.com>
2023-07-25 16:30:51 +0200

    LocalAI integration

--- langchain4j/src/main/java/dev/langchain4j/model/localai/LocalAiChatModel.java
+++ langchain4j/src/main/java/dev/langchain4j/model/localai/LocalAiChatModel.java
@@ -1,4 +1,4 @@
-package dev.langchain4j.model.openai;
+package dev.langchain4j.model.localai;
 
 import dev.ai4j.openai4j.OpenAiClient;
 import dev.ai4j.openai4j.chat.ChatCompletionRequest;
@@ -7,54 +7,43 @@ import dev.langchain4j.agent.tool.ToolSpecification;
 import dev.langchain4j.data.message.AiMessage;
 import dev.langchain4j.data.message.ChatMessage;
 import dev.langchain4j.model.chat.ChatLanguageModel;
-import dev.langchain4j.model.chat.TokenCountEstimator;
 import lombok.Builder;
 
 import java.time.Duration;
 import java.util.List;
 
 import static dev.langchain4j.internal.RetryUtils.withRetry;
+import static dev.langchain4j.internal.ValidationUtils.ensureNotBlank;
 import static dev.langchain4j.model.openai.InternalOpenAiHelper.*;
-import static dev.langchain4j.model.openai.OpenAiModelName.GPT_3_5_TURBO;
+import static java.time.Duration.ofSeconds;
 
-public class OpenAiChatModel implements ChatLanguageModel, TokenCountEstimator {
+public class LocalAiChatModel implements ChatLanguageModel {
 
     private final OpenAiClient client;
     private final String modelName;
     private final Double temperature;
     private final Double topP;
     private final Integer maxTokens;
-    private final Double presencePenalty;
-    private final Double frequencyPenalty;
     private final Integer maxRetries;
-    private final OpenAiTokenizer tokenizer;
 
     @Builder
-    public OpenAiChatModel(String apiKey,
-                           String modelName,
-                           Double temperature,
-                           Double topP,
-                           Integer maxTokens,
-                           Double presencePenalty,
-                           Double frequencyPenalty,
-                           Duration timeout,
-                           Integer maxRetries,
-                           Boolean logRequests,
-                           Boolean logResponses) {
+    public LocalAiChatModel(String baseUrl,
+                            String modelName,
+                            Double temperature,
+                            Double topP,
+                            Integer maxTokens,
+                            Duration timeout,
+                            Integer maxRetries,
+                            Boolean logRequests,
+                            Boolean logResponses) {
 
-        modelName = modelName == null ? GPT_3_5_TURBO : modelName;
         temperature = temperature == null ? 0.7 : temperature;
-        timeout = timeout == null ? defaultTimeoutFor(modelName) : timeout;
+        timeout = timeout == null ? ofSeconds(60) : timeout;
         maxRetries = maxRetries == null ? 3 : maxRetries;
 
-        String url = OPENAI_URL;
-        if (OPENAI_DEMO_API_KEY.equals(apiKey)) {
-            url = OPENAI_DEMO_URL;
-        }
-
         this.client = OpenAiClient.builder()
-                .apiKey(apiKey)
-                .url(url)
+                .apiKey("ignored")
+                .url(ensureNotBlank(baseUrl, "baseUrl"))
                 .callTimeout(timeout)
                 .connectTimeout(timeout)
                 .readTimeout(timeout)
@@ -62,14 +51,11 @@ public class OpenAiChatModel implements ChatLanguageModel, TokenCountEstimator {
                 .logRequests(logRequests)
                 .logResponses(logResponses)
                 .build();
-        this.modelName = modelName;
+        this.modelName = ensureNotBlank(modelName, "modelName");
         this.temperature = temperature;
         this.topP = topP;
         this.maxTokens = maxTokens;
-        this.presencePenalty = presencePenalty;
-        this.frequencyPenalty = frequencyPenalty;
         this.maxRetries = maxRetries;
-        this.tokenizer = new OpenAiTokenizer(this.modelName);
     }
 
     @Override
@@ -87,21 +73,10 @@ public class OpenAiChatModel implements ChatLanguageModel, TokenCountEstimator {
                 .temperature(temperature)
                 .topP(topP)
                 .maxTokens(maxTokens)
-                .presencePenalty(presencePenalty)
-                .frequencyPenalty(frequencyPenalty)
                 .build();
 
         ChatCompletionResponse response = withRetry(() -> client.chatCompletion(request).execute(), maxRetries);
 
         return aiMessageFrom(response);
-    }
-
-    @Override
-    public int estimateTokenCount(List<ChatMessage> messages) {
-        return tokenizer.countTokens(messages);
-    }
-
-    public static OpenAiChatModel withApiKey(String apiKey) {
-        return builder().apiKey(apiKey).build();
     }
 }
