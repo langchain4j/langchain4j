b9930e834567c07d5b9f895df87531c7b2136db7
LangChain4j <langchain4j@gmail.com>
2023-07-26 20:50:50 +0200

    Added LocalAI support to Spring Boot starter (#51)
    Also added missing OpenAI configuration properties (topP, maxTokens, presencePenalty, frequencyPenalty, maxRetries)

---
+++ langchain4j-spring-boot-starter/src/main/java/dev/langchain4j/EmbeddingModel.java
@@ -0,0 +1,41 @@
+package dev.langchain4j;
+
+class EmbeddingModel {
+
+    private ModelProvider provider;
+    private OpenAi openAi;
+    private HuggingFace huggingFace;
+    private LocalAi localAi;
+
+    public ModelProvider getProvider() {
+        return provider;
+    }
+
+    public void setProvider(ModelProvider provider) {
+        this.provider = provider;
+    }
+
+    public OpenAi getOpenAi() {
+        return openAi;
+    }
+
+    public void setOpenAi(OpenAi openAi) {
+        this.openAi = openAi;
+    }
+
+    public HuggingFace getHuggingFace() {
+        return huggingFace;
+    }
+
+    public void setHuggingFace(HuggingFace huggingFace) {
+        this.huggingFace = huggingFace;
+    }
+
+    public LocalAi getLocalAi() {
+        return localAi;
+    }
+
+    public void setLocalAi(LocalAi localAi) {
+        this.localAi = localAi;
+    }
+}
--- langchain4j-spring-boot-starter/src/main/java/dev/langchain4j/LangChain4jAutoConfiguration.java
+++ langchain4j-spring-boot-starter/src/main/java/dev/langchain4j/LangChain4jAutoConfiguration.java
@@ -1,13 +1,14 @@
 package dev.langchain4j;
 
-import dev.langchain4j.LangChain4jProperties.HuggingFace;
-import dev.langchain4j.LangChain4jProperties.OpenAi;
 import dev.langchain4j.model.chat.ChatLanguageModel;
 import dev.langchain4j.model.embedding.EmbeddingModel;
 import dev.langchain4j.model.huggingface.HuggingFaceChatModel;
 import dev.langchain4j.model.huggingface.HuggingFaceEmbeddingModel;
 import dev.langchain4j.model.huggingface.HuggingFaceLanguageModel;
 import dev.langchain4j.model.language.LanguageModel;
+import dev.langchain4j.model.localai.LocalAiChatModel;
+import dev.langchain4j.model.localai.LocalAiEmbeddingModel;
+import dev.langchain4j.model.localai.LocalAiLanguageModel;
 import dev.langchain4j.model.moderation.ModerationModel;
 import dev.langchain4j.model.openai.OpenAiChatModel;
 import dev.langchain4j.model.openai.OpenAiEmbeddingModel;
@@ -19,7 +20,7 @@ import org.springframework.boot.context.properties.EnableConfigurationProperties
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Lazy;
 
-import static dev.langchain4j.LangChain4jProperties.ModelProvider.OPEN_AI;
+import static dev.langchain4j.ModelProvider.OPEN_AI;
 import static dev.langchain4j.exception.IllegalConfigurationException.illegalConfiguration;
 import static dev.langchain4j.internal.Utils.isNullOrBlank;
 
@@ -48,7 +49,12 @@ public class LangChain4jAutoConfiguration {
                         .apiKey(openAi.getApiKey())
                         .modelName(openAi.getModelName())
                         .temperature(openAi.getTemperature())
+                        .topP(openAi.getTopP())
+                        .maxTokens(openAi.getMaxTokens())
+                        .presencePenalty(openAi.getPresencePenalty())
+                        .frequencyPenalty(openAi.getFrequencyPenalty())
                         .timeout(openAi.getTimeout())
+                        .maxRetries(openAi.getMaxRetries())
                         .logRequests(openAi.getLogRequests())
                         .logResponses(openAi.getLogResponses())
                         .build();
@@ -66,6 +72,26 @@ public class LangChain4jAutoConfiguration {
                         .maxNewTokens(huggingFace.getMaxNewTokens())
                         .returnFullText(huggingFace.getReturnFullText())
                         .waitForModel(huggingFace.getWaitForModel())
+                        .build();
+
+            case LOCAL_AI:
+                LocalAi localAi = properties.getChatModel().getLocalAi();
+                if (localAi == null || isNullOrBlank(localAi.getBaseUrl())) {
+                    throw illegalConfiguration("\n\nPlease define 'langchain4j.chat-model.localai.base-url' property");
+                }
+                if (isNullOrBlank(localAi.getModelName())) {
+                    throw illegalConfiguration("\n\nPlease define 'langchain4j.chat-model.localai.model-name' property");
+                }
+                return LocalAiChatModel.builder()
+                        .baseUrl(localAi.getBaseUrl())
+                        .modelName(localAi.getModelName())
+                        .temperature(localAi.getTemperature())
+                        .topP(localAi.getTopP())
+                        .maxTokens(localAi.getMaxTokens())
+                        .timeout(localAi.getTimeout())
+                        .maxRetries(localAi.getMaxRetries())
+                        .logRequests(localAi.getLogRequests())
+                        .logResponses(localAi.getLogResponses())
                         .build();
 
             default:
@@ -95,6 +121,7 @@ public class LangChain4jAutoConfiguration {
                         .modelName(openAi.getModelName())
                         .temperature(openAi.getTemperature())
                         .timeout(openAi.getTimeout())
+                        .maxRetries(openAi.getMaxRetries())
                         .logRequests(openAi.getLogRequests())
                         .logResponses(openAi.getLogResponses())
                         .build();
@@ -112,6 +139,26 @@ public class LangChain4jAutoConfiguration {
                         .maxNewTokens(huggingFace.getMaxNewTokens())
                         .returnFullText(huggingFace.getReturnFullText())
                         .waitForModel(huggingFace.getWaitForModel())
+                        .build();
+
+            case LOCAL_AI:
+                LocalAi localAi = properties.getLanguageModel().getLocalAi();
+                if (localAi == null || isNullOrBlank(localAi.getBaseUrl())) {
+                    throw illegalConfiguration("\n\nPlease define 'langchain4j.language-model.localai.base-url' property");
+                }
+                if (isNullOrBlank(localAi.getModelName())) {
+                    throw illegalConfiguration("\n\nPlease define 'langchain4j.language-model.localai.model-name' property");
+                }
+                return LocalAiLanguageModel.builder()
+                        .baseUrl(localAi.getBaseUrl())
+                        .modelName(localAi.getModelName())
+                        .temperature(localAi.getTemperature())
+                        .topP(localAi.getTopP())
+                        .maxTokens(localAi.getMaxTokens())
+                        .timeout(localAi.getTimeout())
+                        .maxRetries(localAi.getMaxRetries())
+                        .logRequests(localAi.getLogRequests())
+                        .logResponses(localAi.getLogResponses())
                         .build();
 
             default:
@@ -136,11 +183,11 @@ public class LangChain4jAutoConfiguration {
                 if (openAi == null || isNullOrBlank(openAi.getApiKey())) {
                     throw illegalConfiguration("\n\nPlease define 'langchain4j.embedding-model.openai.api-key' property"); // TODO exception type
                 }
-
                 return OpenAiEmbeddingModel.builder()
                         .apiKey(openAi.getApiKey())
                         .modelName(openAi.getModelName())
                         .timeout(openAi.getTimeout())
+                        .maxRetries(openAi.getMaxRetries())
                         .logRequests(openAi.getLogRequests())
                         .logResponses(openAi.getLogResponses())
                         .build();
@@ -150,13 +197,30 @@ public class LangChain4jAutoConfiguration {
                 if (huggingFace == null || isNullOrBlank(huggingFace.getAccessToken())) {
                     throw illegalConfiguration("\n\nPlease define 'langchain4j.embedding-model.huggingface.access-token' property");
                 }
-
                 return HuggingFaceEmbeddingModel.builder()
                         .accessToken(huggingFace.getAccessToken())
                         .modelId(huggingFace.getModelId())
                         .waitForModel(huggingFace.getWaitForModel())
                         .timeout(huggingFace.getTimeout())
                         .build();
+
+            case LOCAL_AI:
+                LocalAi localAi = properties.getEmbeddingModel().getLocalAi();
+                if (localAi == null || isNullOrBlank(localAi.getBaseUrl())) {
+                    throw illegalConfiguration("\n\nPlease define 'langchain4j.embedding-model.localai.base-url' property");
+                }
+                if (isNullOrBlank(localAi.getModelName())) {
+                    throw illegalConfiguration("\n\nPlease define 'langchain4j.embedding-model.localai.model-name' property");
+                }
+                return LocalAiEmbeddingModel.builder()
+                        .baseUrl(localAi.getBaseUrl())
+                        .modelName(localAi.getModelName())
+                        .timeout(localAi.getTimeout())
+                        .maxRetries(localAi.getMaxRetries())
+                        .logRequests(localAi.getLogRequests())
+                        .logResponses(localAi.getLogResponses())
+                        .build();
+
             default:
                 throw illegalConfiguration("Unsupported embedding model provider: %s", properties.getEmbeddingModel().getProvider());
         }
@@ -185,6 +249,7 @@ public class LangChain4jAutoConfiguration {
                 .apiKey(openAi.getApiKey())
                 .modelName(openAi.getModelName())
                 .timeout(openAi.getTimeout())
+                .maxRetries(openAi.getMaxRetries())
                 .logRequests(openAi.getLogRequests())
                 .logResponses(openAi.getLogResponses())
                 .build();
