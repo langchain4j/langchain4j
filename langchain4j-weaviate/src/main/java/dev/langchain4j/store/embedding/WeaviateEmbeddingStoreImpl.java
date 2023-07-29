c42af344bbc1bf6f12198426e1a517676b92cc20
Titov, Alexey <alexey.titov@adesso.de>
2023-07-29 22:13:21 +0200

    next draft

--- langchain4j-weaviate/src/main/java/dev/langchain4j/store/embedding/WeaviateEmbeddingStoreImpl.java
+++ langchain4j-weaviate/src/main/java/dev/langchain4j/store/embedding/WeaviateEmbeddingStoreImpl.java
@@ -1,5 +1,9 @@
 package dev.langchain4j.store.embedding;
 
+import static java.nio.charset.StandardCharsets.UTF_8;
+import static java.util.Collections.emptyList;
+import static java.util.Collections.singletonList;
+
 import dev.langchain4j.data.embedding.Embedding;
 import dev.langchain4j.data.segment.TextSegment;
 import io.weaviate.client.Config;
@@ -9,157 +13,179 @@ import io.weaviate.client.v1.data.model.WeaviateObject;
 import io.weaviate.client.v1.data.replication.model.ConsistencyLevel;
 import io.weaviate.client.v1.graphql.query.argument.NearVectorArgument;
 import io.weaviate.client.v1.graphql.query.fields.Field;
+import java.security.MessageDigest;
+import java.util.*;
+import java.util.stream.Collectors;
 import lombok.Builder;
 import lombok.SneakyThrows;
 import lombok.val;
 
-import java.nio.charset.StandardCharsets;
-import java.security.MessageDigest;
-import java.util.*;
-import java.util.stream.Collectors;
-
 public class WeaviateEmbeddingStoreImpl implements EmbeddingStore<TextSegment> {
 
-    private static final String DEFAULT_CLASS = "Default";
-    private static final String METADATA_TEXT_SEGMENT = "text";
-    private static final String ADDITIONALS = "_additional";
+  private static final String DEFAULT_CLASS = "Default";
+  private static final String METADATA_TEXT_SEGMENT = "text";
+  private static final String ADDITIONALS = "_additional";
 
-    private final WeaviateClient client;
+  private final WeaviateClient client;
+  private final String objectClass;
 
-    @Builder
-    @SneakyThrows
-    public WeaviateEmbeddingStoreImpl(String apiKey, String scheme, String host) {
-        client = WeaviateAuthClient.apiKey(new Config(scheme, host), apiKey);
+  @Builder
+  @SneakyThrows
+  public WeaviateEmbeddingStoreImpl(String apiKey, String scheme, String host, String objectClass) {
+    client = WeaviateAuthClient.apiKey(new Config(scheme, host), apiKey);
+    this.objectClass = objectClass != null ? objectClass : DEFAULT_CLASS;
+  }
 
-        val meta = client.misc().metaGetter().run();
-        if (meta.getError() == null) {
-            System.out.printf("meta.hostname: %s\n", meta.getResult().getHostname());
-            System.out.printf("meta.version: %s\n", meta.getResult().getVersion());
-            System.out.printf("meta.modules: %s\n", meta.getResult().getModules());
-        } else {
-            System.out.printf("Error: %s\n", meta.getError().getMessages());
-        }
+  @Override
+  public String add(Embedding embedding) {
+    val id = generateRandomId();
+    add(id, embedding);
+    return id;
+  }
+
+  @Override
+  public void add(String id, Embedding embedding) {
+    addAll(singletonList(id), singletonList(embedding), null);
+  }
+
+  @Override
+  public String add(Embedding embedding, TextSegment textSegment) {
+    return addAll(singletonList(embedding), textSegment == null ? null : singletonList(textSegment))
+      .stream()
+      .findFirst()
+      .orElse(null);
+  }
+
+  @Override
+  public List<String> addAll(List<Embedding> embeddings) {
+    return addAll(embeddings, null);
+  }
+
+  @Override
+  public List<String> addAll(List<Embedding> embeddings, List<TextSegment> embedded) {
+    return addAll(null, embeddings, embedded);
+  }
+
+  @Override
+  public List<EmbeddingMatch<TextSegment>> findRelevant(Embedding referenceEmbedding, int maxResults) {
+    return null;
+  }
+
+  @Override
+  public List<EmbeddingMatch<TextSegment>> findRelevant(
+    Embedding referenceEmbedding,
+    int maxResults,
+    double minCertainty
+  ) {
+    val result = client
+      .graphQL()
+      .get()
+      .withClassName(objectClass)
+      .withFields(
+        Field.builder().name(METADATA_TEXT_SEGMENT).build(),
+        Field
+          .builder()
+          .name(ADDITIONALS)
+          .fields(
+            Field.builder().name("id").build(),
+            Field.builder().name("certainty").build(),
+            Field.builder().name("vector").build()
+          )
+          .build()
+      )
+      .withNearVector(
+        NearVectorArgument
+          .builder()
+          .vector(referenceEmbedding.vectorAsList().toArray(new Float[0]))
+          .certainty((float) minCertainty)
+          .build()
+      )
+      .withLimit(maxResults)
+      .run();
+
+    if (result.hasErrors()) {
+      return emptyList();
     }
 
-    @SneakyThrows
-    private static String generateUUI(String input) {
-        MessageDigest sha256 = MessageDigest.getInstance("SHA-256");
-        byte[] hashBytes = sha256.digest(input.getBytes(StandardCharsets.UTF_8));
-        StringBuilder sb = new StringBuilder();
-        for (byte b : hashBytes) sb.append(String.format("%02x", b));
-        String sha256Hash = sb.toString();
-        return UUID.nameUUIDFromBytes(sha256Hash.getBytes(StandardCharsets.UTF_8)).toString();
+    val resGetPart = ((Map<String, Map>) result.getResult().getData()).entrySet().stream().findFirst();
+    if (!resGetPart.isPresent()) {
+      return emptyList();
     }
 
-    private static EmbeddingMatch<TextSegment> toEmbeddingMatch(Map<String, ?> item) {
-        val additional = (Map<String, ?>) item.get(ADDITIONALS);
-
-        return new EmbeddingMatch<>(
-                (String) additional.get("id"),
-                Embedding.from(((List<Double>) additional.get("vector")).stream().map(Double::floatValue).collect(Collectors.toList())),
-                TextSegment.from((String) item.get(METADATA_TEXT_SEGMENT)),
-                (Double) additional.get("certainty"));
+    val resItemsPart = resGetPart.get().getValue().entrySet().stream().findFirst();
+    if (!resItemsPart.isPresent()) {
+      return emptyList();
     }
 
-    @Override
-    public String add(Embedding embedding) {
-        return null;
+    val resItems = ((Map.Entry<String, List<Map<String, ?>>>) resItemsPart.get()).getValue();
+
+    return resItems.stream().map(WeaviateEmbeddingStoreImpl::toEmbeddingMatch).collect(Collectors.toList());
+  }
+
+  private List<String> addAll(List<String> ids, List<Embedding> embeddings, List<TextSegment> embedded) {
+    if (embedded != null && embeddings.size() != embedded.size()) {
+      throw new IllegalArgumentException("The list of embeddings and embedded must have the same size");
     }
 
-    @Override
-    public void add(String id, Embedding embedding) {
-
+    val resIds = new ArrayList<String>();
+    val objects = new ArrayList<WeaviateObject>();
+    for (int i = 0; i < embeddings.size(); i++) {
+      val id = ids != null ? ids.get(i) : embedded != null ? generateUUI(embedded.get(i).text()) : generateRandomId();
+      resIds.add(id);
+      objects.add(buildObject(id, embeddings.get(i), embedded != null ? embedded.get(i).text() : null));
     }
 
-    @Override
-    public String add(Embedding embedding, TextSegment textSegment) {
-        return null;
+    client
+      .batch()
+      .objectsBatcher()
+      .withObjects(objects.toArray(new WeaviateObject[0]))
+      .withConsistencyLevel(ConsistencyLevel.ALL)
+      .run();
+
+    return resIds;
+  }
+
+  private WeaviateObject buildObject(String id, Embedding embedding, String text) {
+    val builder = WeaviateObject
+      .builder()
+      .className(objectClass)
+      .id(id)
+      .vector(embedding.vectorAsList().toArray(new Float[0]));
+
+    if (text != null) {
+      val props = new HashMap<String, Object>();
+      props.put(METADATA_TEXT_SEGMENT, text);
+
+      builder.properties(props);
     }
 
-    @Override
-    public List<String> addAll(List<Embedding> embeddings) {
-        return null;
-    }
+    return builder.build();
+  }
 
-    @Override
-    public List<String> addAll(List<Embedding> embeddings, List<TextSegment> embedded) {
-        if (embeddings.size() != embedded.size()) {
-            throw new IllegalArgumentException("The list of embeddings and embedded must have the same size");
-        }
+  private static EmbeddingMatch<TextSegment> toEmbeddingMatch(Map<String, ?> item) {
+    val additional = (Map<String, ?>) item.get(ADDITIONALS);
 
-        List<String> ids = new ArrayList<>();
-        for (int i = 0; i < embeddings.size(); i++) {
-//            ids.add(add(embeddings.get(i), embedded.get(i)));
+    return new EmbeddingMatch<>(
+      (String) additional.get("id"),
+      Embedding.from(
+        ((List<Double>) additional.get("vector")).stream().map(Double::floatValue).collect(Collectors.toList())
+      ),
+      TextSegment.from((String) item.get(METADATA_TEXT_SEGMENT)),
+      (Double) additional.get("certainty")
+    );
+  }
 
-            val props = new HashMap<String, Object>();
-            props.put(METADATA_TEXT_SEGMENT, embedded.get(i).text());
+  // TODO this shall be migrated to some common place
+  @SneakyThrows
+  private static String generateUUI(String input) {
+    val hashBytes = MessageDigest.getInstance("SHA-256").digest(input.getBytes(UTF_8));
+    val sb = new StringBuilder();
+    for (byte b : hashBytes) sb.append(String.format("%02x", b));
+    return UUID.nameUUIDFromBytes(sb.toString().getBytes(UTF_8)).toString();
+  }
 
-            val id = generateUUI(embedded.get(i).text());
-            ids.add(id);
-
-            val object = WeaviateObject.builder()
-                    .className(DEFAULT_CLASS)
-                    .id(id)
-                    .properties(props)
-                    .vector(embeddings.get(i).vectorAsList().toArray(new Float[0]))
-                    .build();
-
-            client.batch().objectsBatcher()
-                    .withObject(object)
-                    .withConsistencyLevel(ConsistencyLevel.ALL)  // default QUORUM
-                    .run();
-
-//        if (result.hasErrors()) {
-//            System.out.println(result.getError());
-//        }
-//        System.out.println(result.getResult());
-
-        }
-
-        val getResult = client.data().objectsGetter()
-                .withClassName(DEFAULT_CLASS)
-                .withConsistencyLevel(ConsistencyLevel.ALL)
-                .run();
-
-        if (getResult.hasErrors()) {
-            System.out.println(getResult.getError());
-        }
-        System.out.println(getResult.getResult().size());
-        return ids;
-    }
-
-    @Override
-    public List<EmbeddingMatch<TextSegment>> findRelevant(Embedding referenceEmbedding, int maxResults) {
-        return null;
-    }
-
-    @Override
-    public List<EmbeddingMatch<TextSegment>> findRelevant(Embedding referenceEmbedding, int maxResults, double minCertainty) {
-        val result = client.graphQL().get()
-                .withClassName(DEFAULT_CLASS)
-                .withFields(Field.builder().name(METADATA_TEXT_SEGMENT).build(), Field.builder()
-                        .name(ADDITIONALS)
-                        .fields(
-                                Field.builder().name("id").build(),
-                                Field.builder().name("certainty").build(),
-                                Field.builder().name("vector").build()
-                        ).build())
-                .withNearVector(NearVectorArgument.builder()
-                        .vector(referenceEmbedding.vectorAsList().toArray(new Float[0]))
-                        .certainty((float) minCertainty)
-                        .build())
-                .withLimit(maxResults)
-                .run();
-
-        if (result.hasErrors()) {
-            System.out.println(result.getError());
-            return null;
-        }
-
-        val resGetPart = ((Map<String, Map>) result.getResult().getData()).entrySet().stream().findFirst().get().getValue();
-        val resItems = ((Map.Entry<String, List<Map<String, ?>>>) resGetPart.entrySet().stream().findFirst().get()).getValue();
-
-        return resItems.stream().map(WeaviateEmbeddingStoreImpl::toEmbeddingMatch).collect(Collectors.toList());
-    }
-
+  // TODO this shall be migrated to some common place
+  private static String generateRandomId() {
+    return UUID.randomUUID().toString();
+  }
 }
--- langchain4j/src/main/java/dev/langchain4j/store/embedding/WeaviateEmbeddingStore.java
+++ langchain4j/src/main/java/dev/langchain4j/store/embedding/WeaviateEmbeddingStore.java
@@ -2,78 +2,97 @@ package dev.langchain4j.store.embedding;
 
 import dev.langchain4j.data.embedding.Embedding;
 import dev.langchain4j.data.segment.TextSegment;
-import lombok.Builder;
-
 import java.lang.reflect.Constructor;
 import java.lang.reflect.InvocationTargetException;
 import java.util.List;
+import lombok.Builder;
 
 public class WeaviateEmbeddingStore implements EmbeddingStore<TextSegment> {
 
-    private final EmbeddingStore<TextSegment> implementation;
+  private final EmbeddingStore<TextSegment> implementation;
 
-    @Builder
-    public WeaviateEmbeddingStore(String apiKey, String scheme, String host) {
-        try {
-            implementation = loadDynamically("dev.langchain4j.store.embedding.WeaviateEmbeddingStoreImpl", apiKey, scheme, host);
-        } catch (ClassNotFoundException e) {
-            throw new RuntimeException(getMessage(), e);
-        } catch (Exception e) {
-            throw new RuntimeException(e);
-        }
+  @Builder
+  public WeaviateEmbeddingStore(String apiKey, String scheme, String host, String objectClass) {
+    try {
+      implementation =
+        loadDynamically(
+          "dev.langchain4j.store.embedding.WeaviateEmbeddingStoreImpl",
+          apiKey,
+          scheme,
+          host,
+          objectClass
+        );
+    } catch (ClassNotFoundException e) {
+      throw new RuntimeException(getMessage(), e);
+    } catch (Exception e) {
+      throw new RuntimeException(e);
     }
+  }
 
-    private static String getMessage() {
-        return "To use WeaviateEmbeddingStore, please add the following dependency to your project:\n\n"
-                + "Maven:\n"
-                + "<dependency>\n" +
-                "    <groupId>dev.langchain4j</groupId>\n" +
-                "    <artifactId>langchain4j-weaviate</artifactId>\n" +
-                "    <version>0.16.0</version>\n" +
-                "</dependency>\n\n"
-                + "Gradle:\n"
-                + "implementation 'dev.langchain4j:langchain4j-weaviate:0.16.0'\n";
-    }
+  private static String getMessage() {
+    return (
+      "To use WeaviateEmbeddingStore, please add the following dependency to your project:\n\n" +
+      "Maven:\n" +
+      "<dependency>\n" +
+      "    <groupId>dev.langchain4j</groupId>\n" +
+      "    <artifactId>langchain4j-weaviate</artifactId>\n" +
+      "    <version>0.18.0</version>\n" +
+      "</dependency>\n\n" +
+      "Gradle:\n" +
+      "implementation 'dev.langchain4j:langchain4j-weaviate:0.18.0'\n"
+    );
+  }
 
-    private static EmbeddingStore<TextSegment> loadDynamically(String implementationClassName, String apiKey, String scheme, String host) throws ClassNotFoundException, NoSuchMethodException, InstantiationException, IllegalAccessException, InvocationTargetException {
-        Class<?> implementationClass = Class.forName(implementationClassName);
-        Class<?>[] constructorParameterTypes = new Class<?>[]{String.class, String.class, String.class};
-        Constructor<?> constructor = implementationClass.getConstructor(constructorParameterTypes);
-        return (EmbeddingStore<TextSegment>) constructor.newInstance(apiKey, scheme, host);
-    }
+  private static EmbeddingStore<TextSegment> loadDynamically(
+    String implementationClassName,
+    String apiKey,
+    String scheme,
+    String host,
+    String objectClass
+  )
+    throws ClassNotFoundException, NoSuchMethodException, InstantiationException, IllegalAccessException, InvocationTargetException {
+    Class<?> implementationClass = Class.forName(implementationClassName);
+    Class<?>[] constructorParameterTypes = new Class<?>[] { String.class, String.class, String.class, String.class };
+    Constructor<?> constructor = implementationClass.getConstructor(constructorParameterTypes);
+    return (EmbeddingStore<TextSegment>) constructor.newInstance(apiKey, scheme, host, objectClass);
+  }
 
-    @Override
-    public String add(Embedding embedding) {
-        return implementation.add(embedding);
-    }
+  @Override
+  public String add(Embedding embedding) {
+    return implementation.add(embedding);
+  }
 
-    @Override
-    public void add(String id, Embedding embedding) {
-        implementation.add(id, embedding);
-    }
+  @Override
+  public void add(String id, Embedding embedding) {
+    implementation.add(id, embedding);
+  }
 
-    @Override
-    public String add(Embedding embedding, TextSegment textSegment) {
-        return implementation.add(embedding, textSegment);
-    }
+  @Override
+  public String add(Embedding embedding, TextSegment textSegment) {
+    return implementation.add(embedding, textSegment);
+  }
 
-    @Override
-    public List<String> addAll(List<Embedding> embeddings) {
-        return implementation.addAll(embeddings);
-    }
+  @Override
+  public List<String> addAll(List<Embedding> embeddings) {
+    return implementation.addAll(embeddings);
+  }
 
-    @Override
-    public List<String> addAll(List<Embedding> embeddings, List<TextSegment> textSegments) {
-        return implementation.addAll(embeddings, textSegments);
-    }
+  @Override
+  public List<String> addAll(List<Embedding> embeddings, List<TextSegment> textSegments) {
+    return implementation.addAll(embeddings, textSegments);
+  }
 
-    @Override
-    public List<EmbeddingMatch<TextSegment>> findRelevant(Embedding referenceEmbedding, int maxResults) {
-        return implementation.findRelevant(referenceEmbedding, maxResults);
-    }
+  @Override
+  public List<EmbeddingMatch<TextSegment>> findRelevant(Embedding referenceEmbedding, int maxResults) {
+    return implementation.findRelevant(referenceEmbedding, maxResults);
+  }
 
-    @Override
-    public List<EmbeddingMatch<TextSegment>> findRelevant(Embedding referenceEmbedding, int maxResults, double minSimilarity) {
-        return implementation.findRelevant(referenceEmbedding, maxResults, minSimilarity);
-    }
+  @Override
+  public List<EmbeddingMatch<TextSegment>> findRelevant(
+    Embedding referenceEmbedding,
+    int maxResults,
+    double minSimilarity
+  ) {
+    return implementation.findRelevant(referenceEmbedding, maxResults, minSimilarity);
+  }
 }
