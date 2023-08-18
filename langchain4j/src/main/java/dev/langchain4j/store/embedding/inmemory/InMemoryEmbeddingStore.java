6f41f03681393189ef8676a35823adf674a92be0
Dmytro Liubarskyi <dmytro.liubarskyi@genedata.com>
2023-08-18 17:52:51 +0200

    wip

--- langchain4j/src/main/java/dev/langchain4j/store/embedding/inmemory/InMemoryEmbeddingStore.java
+++ langchain4j/src/main/java/dev/langchain4j/store/embedding/inmemory/InMemoryEmbeddingStore.java
@@ -1,13 +1,22 @@
 package dev.langchain4j.store.embedding.inmemory;
 
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
 import dev.langchain4j.data.embedding.Embedding;
+import dev.langchain4j.data.segment.TextSegment;
 import dev.langchain4j.store.embedding.EmbeddingMatch;
 import dev.langchain4j.store.embedding.EmbeddingStore;
 import dev.langchain4j.store.embedding.RelevanceScore;
 
+import java.io.IOException;
+import java.lang.reflect.Type;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.util.*;
 
 import static dev.langchain4j.internal.Utils.randomUUID;
+import static java.nio.file.StandardOpenOption.CREATE;
+import static java.nio.file.StandardOpenOption.TRUNCATE_EXISTING;
 import static java.util.Comparator.comparingDouble;
 
 public class InMemoryEmbeddingStore<Embedded> implements EmbeddingStore<Embedded> {
@@ -22,6 +31,21 @@ public class InMemoryEmbeddingStore<Embedded> implements EmbeddingStore<Embedded
             this.id = id;
             this.embedding = embedding;
             this.embedded = embedded;
+        }
+
+        @Override
+        public boolean equals(Object o) { // TODO remove
+            if (this == o) return true;
+            if (o == null || getClass() != o.getClass()) return false;
+            Entry that = (Entry) o;
+            return Objects.equals(this.id, that.id)
+                    && Objects.equals(this.embedding, that.embedding)
+                    && Objects.equals(this.embedded, that.embedded);
+        }
+
+        @Override
+        public int hashCode() { // TODO remove
+            return Objects.hash(id, embedding, embedded);
         }
     }
 
@@ -92,5 +116,25 @@ public class InMemoryEmbeddingStore<Embedded> implements EmbeddingStore<Embedded
         result.sort(comparator);
         Collections.reverse(result);
         return result;
+    }
+
+    public void toFile(String filePath) { //  TODO better name
+        try {
+            String json = new Gson().toJson(this);
+            Files.write(Paths.get(filePath), json.getBytes(), CREATE, TRUNCATE_EXISTING);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public static InMemoryEmbeddingStore<TextSegment> fromFile(String filePath) { //  TODO better name
+        try {
+            String json = new String(Files.readAllBytes(Paths.get(filePath)));
+            Type type = new TypeToken<InMemoryEmbeddingStore<TextSegment>>() {
+            }.getType();
+            return new Gson().fromJson(json, type);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
