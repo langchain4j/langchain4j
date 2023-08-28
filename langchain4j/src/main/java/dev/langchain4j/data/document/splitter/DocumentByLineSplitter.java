bebfc78ee1529403d81fe003b40b5217800b07e9
LangChain4j <langchain4j@gmail.com>
2023-08-28 21:33:48 +0200

    Re-implemented document splitters (#130)

---
+++ langchain4j/src/main/java/dev/langchain4j/data/document/splitter/DocumentByLineSplitter.java
@@ -0,0 +1,61 @@
+package dev.langchain4j.data.document.splitter;
+
+import dev.langchain4j.data.document.Document;
+import dev.langchain4j.data.document.DocumentSplitter;
+import dev.langchain4j.data.segment.TextSegment;
+import dev.langchain4j.model.Tokenizer;
+
+/**
+ * Splits the provided {@link Document} into lines and attempts to fit as many lines as possible
+ * into a single {@link TextSegment}, adhering to the limit set by {@code maxSegmentSize}.
+ * <p>
+ * The {@code maxSegmentSize} can be defined in terms of characters (default) or tokens.
+ * For token-based limit, a {@link Tokenizer} must be provided.
+ * <p>
+ * Line boundaries are detected by a minimum of one newline character ("\n").
+ * Any additional whitespaces before or after are ignored.
+ * So, the following examples are all valid line separators: "\n", "\n\n", " \n", "\n " and so on.
+ * <p>
+ * If multiple lines fit within {@code maxSegmentSize}, they are joined together using a newline ("\n").
+ * <p>
+ * If a single line is too long and exceeds {@code maxSegmentSize},
+ * the {@code subSplitter} ({@link DocumentBySentenceSplitter} by default) is used to split it into smaller parts and
+ * place them into multiple segments.
+ * Such segments contain only the parts of the split long line.
+ * <p>
+ * Each {@link TextSegment} inherits all metadata from the {@link Document} and includes an "index" metadata key
+ * representing its position within the document (starting from 0).
+ */
+public class DocumentByLineSplitter extends HierarchicalDocumentSplitter {
+
+    public DocumentByLineSplitter(int maxSegmentSizeInChars) {
+        super(maxSegmentSizeInChars, null, null);
+    }
+
+    public DocumentByLineSplitter(int maxSegmentSizeInChars, DocumentSplitter subSplitter) {
+        super(maxSegmentSizeInChars, null, subSplitter);
+    }
+
+    public DocumentByLineSplitter(int maxSegmentSizeInTokens, Tokenizer tokenizer) {
+        super(maxSegmentSizeInTokens, tokenizer, null);
+    }
+
+    public DocumentByLineSplitter(int maxSegmentSizeInTokens, Tokenizer tokenizer, DocumentSplitter subSplitter) {
+        super(maxSegmentSizeInTokens, tokenizer, subSplitter);
+    }
+
+    @Override
+    public String[] split(String text) {
+        return text.split("\\s*\\R\\s*"); // additional whitespaces are ignored
+    }
+
+    @Override
+    public String joinDelimiter() {
+        return "\n";
+    }
+
+    @Override
+    protected DocumentSplitter defaultSubSplitter() {
+        return new DocumentBySentenceSplitter(maxSegmentSize, tokenizer);
+    }
+}
---
+++ langchain4j/src/test/java/dev/langchain4j/data/document/splitter/DocumentByRegexSplitterTest.java
@@ -0,0 +1,83 @@
+package dev.langchain4j.data.document.splitter;
+
+import dev.langchain4j.data.document.Document;
+import dev.langchain4j.data.document.DocumentSplitter;
+import dev.langchain4j.data.segment.TextSegment;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import java.util.List;
+
+import static dev.langchain4j.data.document.Metadata.metadata;
+import static dev.langchain4j.data.segment.TextSegment.textSegment;
+import static java.lang.String.format;
+import static org.assertj.core.api.Assertions.assertThat;
+
+class DocumentByRegexSplitterTest {
+
+    @ParameterizedTest
+    @ValueSource(strings = {" ", ",", "\n", "\n\n"})
+    void should_split_by(String separator) {
+
+        String text = format("one%stwo%sthree", separator, separator);
+        Document document = Document.from(text, metadata("document", "0"));
+
+        int maxSegmentSize = 5;
+        DocumentSplitter splitter = new DocumentByRegexSplitter(separator, separator, maxSegmentSize);
+
+        List<TextSegment> segments = splitter.split(document);
+
+        segments.forEach(segment ->
+                assertThat(segment.text().length()).isLessThanOrEqualTo(maxSegmentSize));
+        assertThat(segments).containsExactly(
+                textSegment("one", metadata("index", "0").add("document", "0")),
+                textSegment("two", metadata("index", "1").add("document", "0")),
+                textSegment("three", metadata("index", "2").add("document", "0"))
+        );
+    }
+
+    @Test
+    void should_fit_multiple_parts_into_the_same_segment() {
+
+        Document document = Document.from("one two three", metadata("document", "0"));
+
+        int maxSegmentSize = 10;
+        DocumentSplitter splitter = new DocumentByRegexSplitter(" ", "\n", maxSegmentSize);
+
+        List<TextSegment> segments = splitter.split(document);
+
+        segments.forEach(segment ->
+                assertThat(segment.text().length()).isLessThanOrEqualTo(maxSegmentSize));
+        assertThat(segments).containsExactly(
+                textSegment("one\ntwo", metadata("index", "0").add("document", "0")),
+                textSegment("three", metadata("index", "1").add("document", "0"))
+        );
+    }
+
+    @Test
+    void should_split_part_into_sub_parts_if_it_does_not_fit_into_segment() {
+
+        Document document = Document.from(
+                "This is a first line.\nThis is a second line.\n\nThis is a third line.",
+                metadata("document", "0")
+        );
+
+        int maxSegmentSize = 15;
+        DocumentSplitter subSplitter = new DocumentByWordSplitter(maxSegmentSize);
+        DocumentSplitter splitter = new DocumentByRegexSplitter("\n", "\n", maxSegmentSize, subSplitter);
+
+        List<TextSegment> segments = splitter.split(document);
+
+        segments.forEach(segment ->
+                assertThat(segment.text().length()).isLessThanOrEqualTo(maxSegmentSize));
+        assertThat(segments).containsExactly(
+                textSegment("This is a first", metadata("index", "0").add("document", "0")),
+                textSegment("line.", metadata("index", "1").add("document", "0")),
+                textSegment("This is a", metadata("index", "2").add("document", "0")),
+                textSegment("second line.", metadata("index", "3").add("document", "0")),
+                textSegment("This is a third", metadata("index", "4").add("document", "0")),
+                textSegment("line.", metadata("index", "5").add("document", "0"))
+        );
+    }
+}
