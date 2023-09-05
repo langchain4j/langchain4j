57d8693af0f98db84593aef38b1743f36063d8ca
kuraleta <digital.kuraleta@gmail.com>
2023-09-05 21:10:00 +0200

    Changed all models to return Result<T> instead of T to enable returning additional metadata such as used tokens, finish reason, etc.

--- langchain4j-core/src/main/java/dev/langchain4j/model/chat/StreamingChatLanguageModel.java
+++ langchain4j-core/src/main/java/dev/langchain4j/model/chat/StreamingChatLanguageModel.java
@@ -4,12 +4,10 @@ import dev.langchain4j.agent.tool.ToolSpecification;
 import dev.langchain4j.data.message.ChatMessage;
 import dev.langchain4j.data.message.UserMessage;
 import dev.langchain4j.model.StreamingResponseHandler;
-import dev.langchain4j.model.input.Prompt;
 
 import java.util.List;
 
 import static dev.langchain4j.data.message.UserMessage.userMessage;
-import static dev.langchain4j.model.input.structured.StructuredPromptProcessor.toPrompt;
 import static java.util.Collections.singletonList;
 
 /**
@@ -23,11 +21,6 @@ public interface StreamingChatLanguageModel {
 
     default void generate(UserMessage userMessage, StreamingResponseHandler handler) {
         generate(singletonList(userMessage), handler);
-    }
-
-    default void generate(Object structuredPrompt, StreamingResponseHandler handler) {
-        Prompt prompt = toPrompt(structuredPrompt);
-        generate(prompt.toUserMessage(), handler);
     }
 
     void generate(List<ChatMessage> messages, StreamingResponseHandler handler);
--- langchain4j-core/src/main/java/dev/langchain4j/model/language/StreamingLanguageModel.java
+++ langchain4j-core/src/main/java/dev/langchain4j/model/language/StreamingLanguageModel.java
@@ -3,8 +3,6 @@ package dev.langchain4j.model.language;
 import dev.langchain4j.model.StreamingResponseHandler;
 import dev.langchain4j.model.input.Prompt;
 
-import static dev.langchain4j.model.input.structured.StructuredPromptProcessor.toPrompt;
-
 /**
  * Represents a language model that has a simple text interface (as opposed to a chat interface)
  * and can stream a response one token at a time.
@@ -17,9 +15,5 @@ public interface StreamingLanguageModel {
 
     default void generate(Prompt prompt, StreamingResponseHandler handler) {
         generate(prompt.text(), handler);
-    }
-
-    default void generate(Object structuredPrompt, StreamingResponseHandler handler) {
-        generate(toPrompt(structuredPrompt), handler);
     }
 }
