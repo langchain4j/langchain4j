7801eca701e805dd3380604892212f4cc3f3fc4b
deep-learning-dynamo <deep-learning-dynamo@gmail.com>
2023-09-26 15:51:23 +0200

    Milvus: create collection and index automatically if it does not exist + refactoring

--- langchain4j-milvus/src/main/java/dev/langchain4j/store/embedding/milvus/Mapper.java
+++ langchain4j-milvus/src/main/java/dev/langchain4j/store/embedding/milvus/Mapper.java
@@ -4,23 +4,27 @@ import dev.langchain4j.data.embedding.Embedding;
 import dev.langchain4j.data.segment.TextSegment;
 import dev.langchain4j.store.embedding.EmbeddingMatch;
 import io.milvus.client.MilvusServiceClient;
-import io.milvus.param.MetricType;
+import io.milvus.common.clientenum.ConsistencyLevelEnum;
 import io.milvus.response.QueryResultsWrapper;
 import io.milvus.response.SearchResultsWrapper;
 
-import java.util.*;
-import java.util.function.Predicate;
-import java.util.stream.Collectors;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
+import static dev.langchain4j.internal.Utils.isNullOrBlank;
 import static dev.langchain4j.store.embedding.milvus.CollectionOperationsExecutor.queryForVectors;
 import static dev.langchain4j.store.embedding.milvus.Generator.generateEmptyScalars;
+import static dev.langchain4j.store.embedding.milvus.MilvusEmbeddingStore.*;
+import static java.util.stream.Collectors.toList;
 
 class Mapper {
 
     static List<List<Float>> toVectors(List<Embedding> embeddings) {
         return embeddings.stream()
                 .map(Embedding::vectorAsList)
-                .collect(Collectors.toList());
+                .collect(toList());
     }
 
     static List<String> toScalars(List<TextSegment> textSegments, int size) {
@@ -32,77 +36,53 @@ class Mapper {
     static List<String> textSegmentsToScalars(List<TextSegment> textSegments) {
         return textSegments.stream()
                 .map(TextSegment::text)
-                .collect(Collectors.toList());
+                .collect(toList());
     }
 
     static List<EmbeddingMatch<TextSegment>> toEmbeddingMatches(MilvusServiceClient milvusClient,
                                                                 SearchResultsWrapper resultsWrapper,
-                                                                MilvusCollectionDescription collectionDescription,
-                                                                MilvusOperationsParams operationsParams,
-                                                                double minSimilarity) {
-        List<EmbeddingMatch<TextSegment>> result = new ArrayList<>();
-        List<String> rowIds = (List<String>) resultsWrapper.getFieldWrapper(collectionDescription.idFieldName()).getFieldData();
-        Map<String, List<Float>> idsAndVectors = getVectors(milvusClient, collectionDescription, rowIds, operationsParams);
+                                                                String collectionName,
+                                                                ConsistencyLevelEnum consistencyLevel,
+                                                                boolean queryForVectorOnSearch) {
+        List<EmbeddingMatch<TextSegment>> matches = new ArrayList<>();
 
+        List<String> rowIds = (List<String>) resultsWrapper.getFieldWrapper(ID_FIELD_NAME).getFieldData();
+        Map<String, Embedding> idToEmbedding = new HashMap<>();
+        if (queryForVectorOnSearch) {
+            idToEmbedding.putAll(queryEmbeddings(milvusClient, collectionName, rowIds, consistencyLevel));
+        }
 
         for (int i = 0; i < resultsWrapper.getRowRecords().size(); i++) {
             String rowId = resultsWrapper.getIDScore(0).get(i).getStrID();
             double score = resultsWrapper.getIDScore(0).get(i).getScore();
-            String text = String.valueOf(resultsWrapper.getFieldData(collectionDescription.scalarFieldName(), 0).get(i));
-            Embedding embedding = Embedding.from(idsAndVectors.getOrDefault(rowId, Collections.emptyList()));
-            TextSegment textSegment = TextSegment.from(text);
+            String text = String.valueOf(resultsWrapper.getFieldData(TEXT_FIELD_NAME, 0).get(i));
+            Embedding embedding = idToEmbedding.get(rowId);
+            TextSegment textSegment = isNullOrBlank(text) ? null : TextSegment.from(text);
             EmbeddingMatch<TextSegment> embeddingMatch = new EmbeddingMatch<>(score, rowId, embedding, textSegment);
-            result.add(embeddingMatch);
+            matches.add(embeddingMatch);
         }
 
-        return filterByMinSimilarity(result, minSimilarity, operationsParams.metricType().name());
+        return matches;
     }
 
-    private static List<EmbeddingMatch<TextSegment>> filterByMinSimilarity(List<EmbeddingMatch<TextSegment>> matches,
-                                                                           double minSimilarity,
-                                                                           String metricType) {
+    private static Map<String, Embedding> queryEmbeddings(MilvusServiceClient milvusClient,
+                                                          String collectionName,
+                                                          List<String> rowIds,
+                                                          ConsistencyLevelEnum consistencyLevel) {
+        QueryResultsWrapper queryResultsWrapper = queryForVectors(
+                milvusClient,
+                collectionName,
+                rowIds,
+                consistencyLevel
+        );
 
-        Predicate<EmbeddingMatch<TextSegment>> predicate = getPredicate(metricType, minSimilarity);
-
-        return matches.stream()
-                .filter(predicate)
-                .collect(Collectors.toList());
-    }
-
-    private static Predicate<EmbeddingMatch<TextSegment>> getPredicate(String metricType, double minSimilarity) {
-        Predicate<EmbeddingMatch<TextSegment>> l2Predicate = (em) -> em.score() <= minSimilarity;
-        Predicate<EmbeddingMatch<TextSegment>> ipPredicate = (em) -> em.score() >= minSimilarity;
-
-        if (MetricType.L2.equals(MetricType.valueOf(metricType))) {
-            return l2Predicate;
-        } else if (MetricType.IP.equals(MetricType.valueOf(metricType))) {
-            return ipPredicate;
-        } else {
-            throw new IllegalArgumentException(String.format("Unsupported metricType: '%s'.%n", metricType));
+        Map<String, Embedding> idToEmbedding = new HashMap<>();
+        for (QueryResultsWrapper.RowRecord row : queryResultsWrapper.getRowRecords()) {
+            String id = row.get(ID_FIELD_NAME).toString();
+            List<Float> vector = (List<Float>) row.get(VECTOR_FIELD_NAME);
+            idToEmbedding.put(id, Embedding.from(vector));
         }
-    }
 
-    private static Map<String, List<Float>> getVectors(MilvusServiceClient milvusClient,
-                                                       MilvusCollectionDescription collectionDescription,
-                                                       List<String> rowIds,
-                                                       MilvusOperationsParams operationsParams) {
-
-        if (operationsParams.queryForVectorOnSearch()) {
-            QueryResultsWrapper queryResultsWrapper = queryForVectors(milvusClient,
-                    collectionDescription,
-                    rowIds,
-                    operationsParams.consistencyLevel().name());
-
-            Map<String, List<Float>> idsAndVectors = new HashMap<>();
-            for (QueryResultsWrapper.RowRecord row : queryResultsWrapper.getRowRecords()) {
-                String id = row.get(collectionDescription.idFieldName()).toString();
-                List<Float> vector = (List<Float>) row.get(collectionDescription.vectorFieldName());
-                idsAndVectors.put(id, vector);
-            }
-
-            return idsAndVectors;
-        } else {
-            return Collections.emptyMap();
-        }
+        return idToEmbedding;
     }
 }
