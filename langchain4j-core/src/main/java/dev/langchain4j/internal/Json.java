4fde8b3ba39b8b538e00632b6880538500ac3b6f
Georgios Andrianakis <geoand@gmail.com>
2023-10-20 11:50:17 +0300

    Introduce Json SPI

--- langchain4j-core/src/main/java/dev/langchain4j/internal/Json.java
+++ langchain4j-core/src/main/java/dev/langchain4j/internal/Json.java
@@ -1,57 +1,44 @@
 package dev.langchain4j.internal;
 
-import static java.time.format.DateTimeFormatter.ISO_LOCAL_DATE;
-import static java.time.format.DateTimeFormatter.ISO_LOCAL_DATE_TIME;
-
-import com.google.gson.*;
-import com.google.gson.stream.JsonWriter;
-import java.io.*;
-import java.nio.charset.StandardCharsets;
-import java.time.LocalDate;
-import java.time.LocalDateTime;
+import dev.langchain4j.spi.json.JsonCodecFactory;
+import dev.langchain4j.spi.ServiceHelper;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Collection;
 
 public class Json {
 
-  private static final Gson GSON = new GsonBuilder()
-    .setPrettyPrinting()
-    .registerTypeAdapter(
-      LocalDate.class,
-      (JsonSerializer<LocalDate>) (localDate, type, context) -> new JsonPrimitive(localDate.format(ISO_LOCAL_DATE))
-    )
-    .registerTypeAdapter(
-      LocalDate.class,
-      (JsonDeserializer<LocalDate>) (json, type, context) -> LocalDate.parse(json.getAsString(), ISO_LOCAL_DATE)
-    )
-    .registerTypeAdapter(
-      LocalDateTime.class,
-      (JsonSerializer<LocalDateTime>) (localDateTime, type, context) ->
-        new JsonPrimitive(localDateTime.format(ISO_LOCAL_DATE_TIME))
-    )
-    .registerTypeAdapter(
-      LocalDateTime.class,
-      (JsonDeserializer<LocalDateTime>) (json, type, context) ->
-        LocalDateTime.parse(json.getAsString(), ISO_LOCAL_DATE_TIME)
-    )
-    .create();
+  private static final JsonCodec CODEC = loadCodec();
+
+  private static JsonCodec loadCodec() {
+    Collection<JsonCodecFactory> factories = ServiceHelper.loadFactories(JsonCodecFactory.class);
+    for (JsonCodecFactory factory : factories) {
+      return factory.create();
+    }
+    // fallback to default
+    return new GsonJsonCodec();
+  }
+
+
 
   public static String toJson(Object o) {
-    return GSON.toJson(o);
+    return CODEC.toJson(o);
   }
 
   public static <T> T fromJson(String json, Class<T> type) {
-    return GSON.fromJson(json, type);
+    return CODEC.fromJson(json, type);
   }
 
-  public static InputStream toInputStream(Object o, Class type) throws IOException {
-    try (
-      ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
-      OutputStreamWriter outputStreamWriter = new OutputStreamWriter(byteArrayOutputStream, StandardCharsets.UTF_8);
-      JsonWriter jsonWriter = new JsonWriter(outputStreamWriter)
-    ) {
-      GSON.toJson(o, type, jsonWriter);
-      jsonWriter.flush();
-
-      return new ByteArrayInputStream(byteArrayOutputStream.toByteArray());
-    }
+  public static InputStream toInputStream(Object o, Class<?> type) throws IOException {
+    return CODEC.toInputStream(o, type);
   }
+
+  public interface JsonCodec {
+    String toJson(Object o);
+
+    <T> T fromJson(String json, Class<T> type);
+    InputStream toInputStream(Object o, Class<?> type) throws IOException;
+
+  }
+
 }
