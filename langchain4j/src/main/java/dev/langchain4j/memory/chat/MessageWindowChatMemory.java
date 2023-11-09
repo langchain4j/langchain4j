e85694155de360b236eed37d7c9920a15f85091e
jiangsier-xyz <126842484+jiangsier-xyz@users.noreply.github.com>
2023-11-09 12:41:49 +0800

    Optimize import statements.

--- langchain4j/src/main/java/dev/langchain4j/memory/chat/MessageWindowChatMemory.java
+++ langchain4j/src/main/java/dev/langchain4j/memory/chat/MessageWindowChatMemory.java
@@ -8,7 +8,6 @@ import dev.langchain4j.store.memory.chat.InMemoryChatMemoryStore;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Optional;
