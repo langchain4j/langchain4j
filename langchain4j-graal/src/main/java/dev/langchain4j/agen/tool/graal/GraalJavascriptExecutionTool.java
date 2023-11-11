10e31a2d397c1d4eabeaa3ed0af83aca01651bd5
Joan Anglada <janglada@gmail.com>
2023-11-11 15:18:33 +0100

    Add sandbox policy

--- langchain4j-graal/src/main/java/dev/langchain4j/agen/tool/graal/GraalJavascriptExecutionTool.java
+++ langchain4j-graal/src/main/java/dev/langchain4j/agen/tool/graal/GraalJavascriptExecutionTool.java
@@ -3,7 +3,10 @@ package dev.langchain4j.agen.tool.graal;
 import dev.langchain4j.agent.tool.P;
 import dev.langchain4j.agent.tool.Tool;
 import org.graalvm.polyglot.Context;
+import org.graalvm.polyglot.HostAccess;
 import org.graalvm.polyglot.SandboxPolicy;
+
+import java.io.ByteArrayOutputStream;
 
 /**
  * A tool that executes JS code using Graal JS
@@ -18,10 +21,19 @@ public class GraalJavascriptExecutionTool {
             "You might know the answer without running any code, but you should still run the code to get the answer.\n" +
             "If it does not seem like you can write code to answer the question, just return \"I don't know\" as the answer")
     public String executeJavaScriptCode(
-            @P("JavaScript code to execute, result MUST be printed to console")
+            @P("JavaScript code to execute, result MUST be returned by the code")
             String javaScriptCode
     ) {
-        try (Context context = Context.newBuilder("js").sandbox(SandboxPolicy.UNTRUSTED).build()) {
+        ByteArrayOutputStream out =  new ByteArrayOutputStream();
+        ByteArrayOutputStream err =  new ByteArrayOutputStream();
+
+        // GraalVM community edition supports only sandbox policy TRUSTED or CONSTRAINED.
+        try (Context context = Context.newBuilder("js")
+                .sandbox(SandboxPolicy.CONSTRAINED)
+                .allowHostAccess(HostAccess.UNTRUSTED)
+                .out(new ByteArrayOutputStream())
+                .err(new ByteArrayOutputStream())
+                .build()) {
             return String.valueOf(context.eval("js", javaScriptCode).as(Object.class));
         }
     }
--- langchain4j-graal/src/main/java/dev/langchain4j/agen/tool/graal/GraalPythonExecutionTool.java
+++ langchain4j-graal/src/main/java/dev/langchain4j/agen/tool/graal/GraalPythonExecutionTool.java
@@ -3,7 +3,10 @@ package dev.langchain4j.agen.tool.graal;
 import dev.langchain4j.agent.tool.P;
 import dev.langchain4j.agent.tool.Tool;
 import org.graalvm.polyglot.Context;
+import org.graalvm.polyglot.HostAccess;
 import org.graalvm.polyglot.SandboxPolicy;
+
+import java.io.ByteArrayOutputStream;
 
 /**
  * A tool that executes Python code using GraalPython
@@ -18,11 +21,21 @@ public class GraalPythonExecutionTool {
             "You might know the answer without running any code, but you should still run the code to get the answer.\n" +
             "If it does not seem like you can write code to answer the question, just return \"I don't know\" as the answer")
     public String executePythonCode(
-            @P("Python code to execute, result MUST be printed to console")
+            @P("Python code to execute, result MUST be returned by the code")
             String pythonCode
     ) {
-        try (Context context = Context.newBuilder("python").sandbox(SandboxPolicy.UNTRUSTED).build()) {
+        ByteArrayOutputStream out =  new ByteArrayOutputStream();
+        ByteArrayOutputStream err =  new ByteArrayOutputStream();
 
+        // The language python can only be used up to the TRUSTED sandbox policy.
+        try (Context context = Context.
+                newBuilder("python")
+                .sandbox(SandboxPolicy.CONSTRAINED)
+                .out(out)
+                .err(err)
+                .allowHostAccess(HostAccess.UNTRUSTED)
+                .build())
+        {
             return String.valueOf(context.eval("python", pythonCode).as(Object.class));
         }
     }
