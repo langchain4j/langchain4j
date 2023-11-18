f42b294bd15af545132bbc71503d2c69dca98411
Titov, Alexey <a@koeg.de>
2023-11-18 18:20:36 +0100

    next changes

--- langchain4j-open-ai-dall-e/src/main/java/AuthorizationHeaderInjector.java
+++ langchain4j-open-ai-dall-e/src/main/java/AuthorizationHeaderInjector.java
@@ -2,6 +2,7 @@ import java.io.IOException;
 import okhttp3.Interceptor;
 import okhttp3.Request;
 import okhttp3.Response;
+import org.jetbrains.annotations.NotNull;
 
 class AuthorizationHeaderInjector implements Interceptor {
     private final String apiKey;
@@ -10,6 +11,7 @@ class AuthorizationHeaderInjector implements Interceptor {
         this.apiKey = apiKey;
     }
 
+    @NotNull
     public Response intercept(Interceptor.Chain chain) throws IOException {
         Request request = chain.request().newBuilder().addHeader("Authorization", "Bearer " + this.apiKey).build();
         return chain.proceed(request);
