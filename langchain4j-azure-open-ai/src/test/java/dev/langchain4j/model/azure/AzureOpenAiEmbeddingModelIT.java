8050b836c92b26d84d36dca650ac3bc2cff33ff0
Julien Dubois <julien.dubois@gmail.com>
2023-12-07 15:50:08 +0100

    Add test should_embed_in_batches()

--- langchain4j-azure-open-ai/src/test/java/dev/langchain4j/model/azure/AzureOpenAiEmbeddingModelIT.java
+++ langchain4j-azure-open-ai/src/test/java/dev/langchain4j/model/azure/AzureOpenAiEmbeddingModelIT.java
@@ -1,12 +1,17 @@
 package dev.langchain4j.model.azure;
 
 import dev.langchain4j.data.embedding.Embedding;
+import dev.langchain4j.data.segment.TextSegment;
 import dev.langchain4j.model.embedding.EmbeddingModel;
+import dev.langchain4j.model.openai.OpenAiTokenizer;
 import dev.langchain4j.model.output.Response;
 import dev.langchain4j.model.output.TokenUsage;
 import org.junit.jupiter.api.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -34,6 +39,31 @@ public class AzureOpenAiEmbeddingModelIT {
         assertThat(tokenUsage.inputTokenCount()).isEqualTo(2);
         assertThat(tokenUsage.outputTokenCount()).isNull();
         assertThat(tokenUsage.totalTokenCount()).isEqualTo(2);
+
+        assertThat(response.finishReason()).isNull();
+    }
+
+    @Test
+    void should_embed_in_batches() {
+
+        int batchSize = 16;
+        int numberOfSegments = batchSize + 1;
+
+        List<TextSegment> segments = new ArrayList<>();
+        for (int i = 0; i < numberOfSegments; i++) {
+            segments.add(TextSegment.from("text " + i));
+        }
+
+        Response<List<Embedding>> response = model.embedAll(segments);
+        System.out.println(response);
+
+        assertThat(response.content()).hasSize(numberOfSegments);
+        assertThat(response.content().get(0).dimension()).isEqualTo(1536);
+
+        TokenUsage tokenUsage = response.tokenUsage();
+        assertThat(tokenUsage.inputTokenCount()).isEqualTo(numberOfSegments * 3);
+        assertThat(tokenUsage.outputTokenCount()).isNull();
+        assertThat(tokenUsage.totalTokenCount()).isEqualTo(numberOfSegments * 3);
 
         assertThat(response.finishReason()).isNull();
     }
