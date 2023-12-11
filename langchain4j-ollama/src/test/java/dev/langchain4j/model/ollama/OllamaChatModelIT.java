54ed06aa1b1581260e843e96c177997c56eecefa
fintanmm <fintan.macmahon@gmail.com>
2023-12-11 15:18:52 +0000

    Refactor OllamaChatModel for improved AbstractOllamaInfrastructure integration

--- langchain4j-ollama/src/test/java/dev/langchain4j/model/ollama/OllamaChatModelIT.java
+++ langchain4j-ollama/src/test/java/dev/langchain4j/model/ollama/OllamaChatModelIT.java
@@ -3,7 +3,6 @@ package dev.langchain4j.model.ollama;
 import dev.langchain4j.data.message.AiMessage;
 import dev.langchain4j.data.message.ChatMessage;
 import dev.langchain4j.model.output.Response;
-import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 
 import java.util.ArrayList;
@@ -13,19 +12,16 @@ import static dev.langchain4j.data.message.SystemMessage.systemMessage;
 import static dev.langchain4j.data.message.UserMessage.userMessage;
 import static org.assertj.core.api.Assertions.assertThat;
 
-@Disabled("needs Ollama running locally")
-class OllamaChatModelIT {
+class OllamaChatModelIT extends AbstractOllamaInfrastructure {
 
     OllamaChatModel model = OllamaChatModel.builder()
-            .baseUrl("http://localhost:11434")
-            .modelName("orca-mini")
-            .temperature(0.5)
+            .baseUrl(getBaseUrl())
+            .modelName(ORCA_MINI_MODEL)
             .build();
 
     @Test
     void should_send_messages_and_receive_response() {
 
-        // create a list but without using the of method
         List<ChatMessage> chatMessages = new ArrayList<>();
         chatMessages.add(systemMessage("You are a good friend of mine, who likes to answer with jokes"));
         chatMessages.add(userMessage("Hey Bro, what are you doing?"));
