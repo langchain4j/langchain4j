89ed8ba5aed940c70499fedccebfa098a020bfbb
fintanmm <fintan.macmahon@gmail.com>
2023-12-15 12:51:42 +0000

    Added support classes for Ollama chat model implementation. The Message class represents a chat message. The ChatRequest class represents a request to the Ollama. The ChatResponse class represents a response from the Ollama. The Role enum represents the role of a chat message sender. It also provides a method to convert from ChatMessageType to Role.

---
+++ langchain4j-ollama/src/main/java/dev/langchain4j/model/ollama/Message.java
@@ -0,0 +1,15 @@
+package dev.langchain4j.model.ollama;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Message {
+    private Role role;
+    private String content;
+}
