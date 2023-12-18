77d69b266135c3f8fe9850c6f0d327ac1cb657be
Julien Dubois <julien.dubois@gmail.com>
2023-12-18 10:15:53 +0100

    Update the API usage following code review from @Heezer

--- langchain4j-azure-open-ai/src/test/java/dev/langchain4j/model/azure/AzureOpenAiImageModelIT.java
+++ langchain4j-azure-open-ai/src/test/java/dev/langchain4j/model/azure/AzureOpenAiImageModelIT.java
@@ -6,7 +6,10 @@ import org.junit.jupiter.api.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.util.List;
+
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 public class AzureOpenAiImageModelIT {
 
@@ -20,11 +23,15 @@ public class AzureOpenAiImageModelIT {
                 .deploymentName(System.getenv("AZURE_OPENAI_DEPLOYMENT_NAME"))
                 .apiKey(System.getenv("AZURE_OPENAI_KEY"))
                 .logRequestsAndResponses(true)
+                .setN(1)
                 .build();
 
-        Response<Image> response = model.generate("A coffee mug in Paris, France");
+        Response<List<Image>> response = model.generate("A coffee mug in Paris, France");
 
         logger.info(response.toString());
-        assertThat(response.content().url()).isNotBlank();
+
+        Image image = response.content().get(0);
+        assertNotNull(image.revisedPrompt());
+        assertNotNull(image.url());
     }
 }
