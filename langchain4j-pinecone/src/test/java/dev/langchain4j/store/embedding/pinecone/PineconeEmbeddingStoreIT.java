b51f5f99da63b84ead1a40cfa7209a0917a83699
LangChain4j <langchain4j@gmail.com>
2023-12-22 14:02:40 +0100

    fixed pinecone client CVEs (#409)

--- langchain4j-pinecone/src/test/java/dev/langchain4j/store/embedding/pinecone/PineconeEmbeddingStoreIT.java
+++ langchain4j-pinecone/src/test/java/dev/langchain4j/store/embedding/pinecone/PineconeEmbeddingStoreIT.java
@@ -5,10 +5,7 @@ import dev.langchain4j.model.embedding.AllMiniLmL6V2QuantizedEmbeddingModel;
 import dev.langchain4j.model.embedding.EmbeddingModel;
 import dev.langchain4j.store.embedding.EmbeddingStore;
 import dev.langchain4j.store.embedding.EmbeddingStoreWithoutMetadataIT;
-import lombok.SneakyThrows;
 import org.junit.jupiter.api.condition.EnabledIfEnvironmentVariable;
-
-import java.util.concurrent.TimeUnit;
 
 import static dev.langchain4j.internal.Utils.randomUUID;
 
@@ -17,8 +14,8 @@ class PineconeEmbeddingStoreIT extends EmbeddingStoreWithoutMetadataIT {
 
     EmbeddingStore<TextSegment> embeddingStore = PineconeEmbeddingStore.builder()
             .apiKey(System.getenv("PINECONE_API_KEY"))
-            .environment("gcp-starter")
-            .projectId("6furl0x")
+            .environment("northamerica-northeast1-gcp")
+            .projectId("19a129b")
             .index("test")
             .nameSpace(randomUUID())
             .build();
@@ -33,11 +30,5 @@ class PineconeEmbeddingStoreIT extends EmbeddingStoreWithoutMetadataIT {
     @Override
     protected EmbeddingModel embeddingModel() {
         return embeddingModel;
-    }
-
-    @Override
-    @SneakyThrows
-    protected void awaitUntilPersisted() {
-        TimeUnit.SECONDS.sleep(20);
     }
 }
