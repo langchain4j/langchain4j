128e30a0ba3d802e7e34de87ac23f77649c76ff4
Crutcher Dunnavant <crutcher@gmail.com>
2023-12-27 03:26:17 -0800

    Add DocumentLoaderTest class and update DocumentLoader (#379)
    A new test class, DocumentLoaderTest, was added to ensure the correct
    operation of the DocumentLoader. Additionally, comments were added to
    the load method in the DocumentLoader class to clarify its purpose and
    usage.

--- langchain4j-core/src/main/java/dev/langchain4j/data/document/DocumentLoader.java
+++ langchain4j-core/src/main/java/dev/langchain4j/data/document/DocumentLoader.java
@@ -4,6 +4,15 @@ import java.io.InputStream;
 
 public class DocumentLoader {
 
+    /**
+     * Loads a document from the given source using the given parser.
+     *
+     * <p>Forwards the source Metadata to the parsed Document.
+     *
+     * @param source The source from which the document will be loaded.
+     * @param parser The parser that will be used to parse the document.
+     * @return The loaded document.
+     */
     public static Document load(DocumentSource source, DocumentParser parser) {
         try (InputStream inputStream = source.inputStream()) {
             Document document = parser.parse(inputStream);
