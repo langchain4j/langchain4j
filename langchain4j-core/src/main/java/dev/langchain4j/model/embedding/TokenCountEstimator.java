9b94aada9867504405cc24940d684baba45038e5
Crutcher Dunnavant <crutcher@gmail.com>
2024-01-05 12:02:55 -0800

    Added @Deprecated link because AbstractInProcessEmbeddingModel depends on the interface and still uses it.

---
+++ langchain4j-core/src/main/java/dev/langchain4j/model/embedding/TokenCountEstimator.java
@@ -0,0 +1,8 @@
+package dev.langchain4j.model.embedding;
+
+/**
+ * @deprecated Use {@link dev.langchain4j.model.language.TokenCountEstimator} instead.
+ */
+@Deprecated
+public interface TokenCountEstimator extends dev.langchain4j.model.language.TokenCountEstimator{
+}
