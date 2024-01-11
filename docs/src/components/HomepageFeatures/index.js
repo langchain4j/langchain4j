ef972019839e59c8b2d401d692c7a41dfcaa35b2
Amith Koujalgi <koujalgi.amith@gmail.com>
2024-01-11 19:51:12 +0530

    updated images for the docs home page

--- docs/src/components/HomepageFeatures/index.js
+++ docs/src/components/HomepageFeatures/index.js
@@ -5,7 +5,7 @@ import styles from './styles.module.css';
 const FeatureList = [
     {
         title: 'Easy interaction with LLMs and AI',
-        Png: require('@site/static/img/llm-logos.png').default,
+        Svg: require('@site/static/img/llm-logos.svg').default,
         description: (
             <>
                 All major commercial and open source models are easily accessible via a streamlined <code>API</code>, allowing you to build Chatbots, Assistants, Data Classifiers, Autonomous Agents, ...
@@ -14,7 +14,7 @@ const FeatureList = [
     },
     {
         title: 'Tailored for Java',
-        Png: require('@site/static/img/framework-logos.png').default,
+        Svg: require('@site/static/img/framework-logos.svg').default,
         description: (
             <>
                 Smooth integration in your java applications thanks to Quarkus and Spring Boot integrations, converse with LLMs in POJOs and have the LLM call Java methods
@@ -23,7 +23,7 @@ const FeatureList = [
     },
     {
         title: 'Tools, AI Services, Chains, RAG',
-        Png: require('@site/static/img/functionality-logos.png').default,
+        Svg: require('@site/static/img/functionality-logos.svg').default,
         description: (
             <>
                 Provides an extensive toolbox of common AI LLM operations, made easy thanks to various layers of abstraction
