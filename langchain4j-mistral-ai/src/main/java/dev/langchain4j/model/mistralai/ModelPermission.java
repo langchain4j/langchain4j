62be0b265b0173a5f1020cb697ba48aa99bc32ed
czelabueno <c.zelabueno@gmail.com>
2024-01-15 17:32:46 -0500

    Mistral AI models req/resp

---
+++ langchain4j-mistral-ai/src/main/java/dev/langchain4j/model/mistralai/ModelPermission.java
@@ -0,0 +1,28 @@
+package dev.langchain4j.model.mistralai;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+class ModelPermission{
+
+    private String id;
+    private String object;
+    private Integer created;
+    private Boolean allowCreateEngine;
+    private Boolean allowSampling;
+    private Boolean allowLogprobs;
+    private Boolean allowSearchIndices;
+    private Boolean allowView;
+    private Boolean allowFineTuning;
+    private String organization;
+    private String group;
+    private Boolean isBlocking;
+
+}
