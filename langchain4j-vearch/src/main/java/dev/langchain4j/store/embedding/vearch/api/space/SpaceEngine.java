cd20253a006379edd7d1174ee55821c141f36425
Martin7-1 <1754350460@qq.com>
2024-01-19 16:06:07 +0800

    fix constraint check

--- langchain4j-vearch/src/main/java/dev/langchain4j/store/embedding/vearch/api/space/SpaceEngine.java
+++ langchain4j-vearch/src/main/java/dev/langchain4j/store/embedding/vearch/api/space/SpaceEngine.java
@@ -1,18 +1,27 @@
 package dev.langchain4j.store.embedding.vearch.api.space;
 
-import lombok.Builder;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
-@Builder
 public class SpaceEngine {
 
     private String name;
     private Long indexSize;
     private RetrievalType retrievalType;
     private RetrievalParam retrievalParam;
+
+    public SpaceEngine() {
+
+    }
+
+    public SpaceEngine(String name, Long indexSize, RetrievalType retrievalType, RetrievalParam retrievalParam) {
+        setName(name);
+        setIndexSize(indexSize);
+        setRetrievalType(retrievalType);
+        setRetrievalParam(retrievalParam);
+    }
 
     public void setRetrievalParam(RetrievalParam retrievalParam) {
         // do some constraint check
@@ -23,5 +32,41 @@ public class SpaceEngine {
                             retrievalType.name(), clazz.getSimpleName()));
         }
         this.retrievalParam = retrievalParam;
+    }
+
+    public static Builder builder() {
+        return new Builder();
+    }
+
+    public static class Builder {
+
+        private String name;
+        private Long indexSize;
+        private RetrievalType retrievalType;
+        private RetrievalParam retrievalParam;
+
+        public Builder name(String name) {
+            this.name = name;
+            return this;
+        }
+
+        public Builder indexSize(Long indexSize) {
+            this.indexSize = indexSize;
+            return this;
+        }
+
+        public Builder retrievalType(RetrievalType retrievalType) {
+            this.retrievalType = retrievalType;
+            return this;
+        }
+
+        public Builder retrievalParam(RetrievalParam retrievalParam) {
+            this.retrievalParam = retrievalParam;
+            return this;
+        }
+
+        public SpaceEngine build() {
+            return new SpaceEngine(name, indexSize, retrievalType, retrievalParam);
+        }
     }
 }
