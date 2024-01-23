121484d564fdb7f2709ea35963b67c7be89c28ff
LangChain4j <info@langchain4j.dev>
2024-01-23 18:34:55 +0100

    removed outdated tests

--- langchain4j/src/test/java/dev/langchain4j/chain/ConversationalChainTest.java
+++ langchain4j/src/test/java/dev/langchain4j/chain/ConversationalChainTest.java
@@ -1,15 +1,11 @@
 package dev.langchain4j.chain;
 
-import dev.langchain4j.data.document.Metadata;
-import dev.langchain4j.data.segment.TextSegment;
 import dev.langchain4j.memory.ChatMemory;
 import dev.langchain4j.memory.chat.MessageWindowChatMemory;
 import dev.langchain4j.model.chat.ChatLanguageModel;
-import dev.langchain4j.model.input.Prompt;
 import dev.langchain4j.model.input.PromptTemplate;
 import dev.langchain4j.model.output.Response;
 import dev.langchain4j.retriever.EmbeddingStoreRetriever;
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.junit.jupiter.params.ParameterizedTest;
@@ -19,8 +15,6 @@ import org.mockito.ArgumentCaptor;
 import org.mockito.Captor;
 import org.mockito.junit.jupiter.MockitoExtension;
 
-import java.util.ArrayList;
-import java.util.List;
 import java.util.Map;
 
 import static dev.langchain4j.data.message.AiMessage.aiMessage;
@@ -47,36 +41,6 @@ class ConversationalChainTest {
 
     @Captor
     ArgumentCaptor<Map<String, Object>> variablesCaptor;
-
-    @Test
-    void should_not_include_metadata_keys_in_chat_memory_when_metadata_null() {
-        List<TextSegment> segments = new ArrayList<>();
-        segments.add(TextSegment.from("Segment 1", Metadata.metadata("title", "title_1.pdf")));
-        segments.add(TextSegment.from("Segment 2", Metadata.metadata("title", "title_2.pdf")));
-
-        ConversationalRetrievalChain chain = ConversationalRetrievalChain.builder()
-                .chatLanguageModel(chatLanguageModel)
-                .promptTemplate(promptTemplate)
-                .chatMemory(chatMemory)
-                .retriever(retriever)
-                .build();
-
-        when(chatLanguageModel.generate(anyList())).thenReturn(Response.from(aiMessage(aiMessage)));
-        when(retriever.findRelevant(question)).thenReturn(segments);
-        when(promptTemplate.apply(any())).thenReturn(Prompt.from("Generated prompt"));
-
-        String result = chain.execute(question);
-
-        verify(promptTemplate).apply(variablesCaptor.capture());
-        verify(chatMemory, times(2)).add(any());
-        verify(chatLanguageModel).generate(anyList());
-
-        Map<String, Object> capturedVariables = variablesCaptor.getValue();
-        String capturedInformation = (String) capturedVariables.get("information");
-
-        Assertions.assertEquals("Hi there", result);
-        Assertions.assertEquals("...Segment 1...\n\n...Segment 2...", capturedInformation);
-    }
 
     @Test
     void should_store_user_and_ai_messages_in_chat_memory() {
