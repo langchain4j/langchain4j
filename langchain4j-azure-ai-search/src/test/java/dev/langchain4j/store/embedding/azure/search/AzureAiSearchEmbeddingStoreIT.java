d424fa80f7d6d401def19f3e3af90b9d882caa47
Julien Dubois <julien.dubois@gmail.com>
2024-01-24 15:15:56 +0100

    First vector search tests passing

--- langchain4j-azure-ai-search/src/test/java/dev/langchain4j/store/embedding/azure/search/AzureAiSearchEmbeddingStoreIT.java
+++ langchain4j-azure-ai-search/src/test/java/dev/langchain4j/store/embedding/azure/search/AzureAiSearchEmbeddingStoreIT.java
@@ -7,7 +7,6 @@ import dev.langchain4j.model.embedding.EmbeddingModel;
 import dev.langchain4j.store.embedding.EmbeddingMatch;
 import dev.langchain4j.store.embedding.EmbeddingStore;
 import dev.langchain4j.store.embedding.EmbeddingStoreIT;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.condition.EnabledIfEnvironmentVariable;
 
@@ -17,7 +16,7 @@ import static java.util.Arrays.asList;
 import static org.assertj.core.api.Assertions.assertThat;
 
 @EnabledIfEnvironmentVariable(named = "AZURE_SEARCH_ENDPOINT", matches = ".+")
-public class AzureAiSearchEmbeddingStoreIT {//extends EmbeddingStoreIT {
+public class AzureAiSearchEmbeddingStoreIT extends EmbeddingStoreIT {
 
     private EmbeddingModel embeddingModel;
 
@@ -31,44 +30,59 @@ public class AzureAiSearchEmbeddingStoreIT {//extends EmbeddingStoreIT {
                 .deploymentName(System.getenv("AZURE_OPENAI_DEPLOYMENT_NAME"))
                 .logRequestsAndResponses(true)
                 .build();
-    }
 
-    @BeforeEach
-    void cleanUp() {
-        // Delete and recreate the index before each test, so they are idempotent
-        AzureAiSearchEmbeddingStore azureAiSearchEmbeddingStore = AzureAiSearchEmbeddingStore.builder()
+        embeddingStore =  AzureAiSearchEmbeddingStore.builder()
                 .endpoint(System.getenv("AZURE_SEARCH_ENDPOINT"))
                 .apiKey(System.getenv("AZURE_SEARCH_KEY"))
                 .embeddingModel(embeddingModel)
                 .build();
-
-        azureAiSearchEmbeddingStore.cleanUpIndex();
-        azureAiSearchEmbeddingStore.createOrUpdateIndex(embeddingModel);
-        embeddingStore = azureAiSearchEmbeddingStore;
     }
 
     @Test
-    void testAddEmbeddingAndFindRelevant() {
-        Embedding firstEmbedding = embeddingModel.embed("Chihuahua").content();
-        Embedding secondEmbedding = embeddingModel.embed("Pet").content();
-        Embedding thirdEmbedding = embeddingModel.embed("Dog").content();
-        Embedding fourthEmbedding = embeddingModel.embed("Chocolate cookie").content();
-        embeddingStore.addAll(asList(firstEmbedding, secondEmbedding, thirdEmbedding, fourthEmbedding));
+    void testAddEmbeddingsAndFindRelevant() {
+        String content1 = "Chihuahua";
+        String content2 = "Poodle";
+        String content3 = "Golden retriever";
+        String content4 = "Chocolate cookie";
+        List<String> contents = asList(content1, content2, content3, content4);
 
-        List<EmbeddingMatch<TextSegment>> relevant = embeddingStore.findRelevant(firstEmbedding, 10);
+        for (String content : contents) {
+            TextSegment textSegment = TextSegment.from(content);
+            Embedding embedding = embeddingModel.embed(content).content();
+            embeddingStore.add(embedding, textSegment);
+        }
+
+        Embedding relevantEmbedding = embeddingModel.embed("Cake").content();
+        List<EmbeddingMatch<TextSegment>> relevant = embeddingStore.findRelevant(relevantEmbedding, 10);
         assertThat(relevant).hasSize(3);
         assertThat(relevant.get(0).embedding()).isNotNull();
         assertThat(relevant.get(1).embedding()).isNotNull();
         assertThat(relevant.get(2).embedding()).isNotNull();
     }
 
-    //@Override
+    @Override
     protected EmbeddingStore<TextSegment> embeddingStore() {
         return embeddingStore;
     }
 
-    //@Override
+    @Override
     protected EmbeddingModel embeddingModel() {
         return embeddingModel;
+    }
+
+    @Override
+    protected void clearStore() {
+        AzureAiSearchEmbeddingStore azureAiSearchEmbeddingStore = (AzureAiSearchEmbeddingStore) embeddingStore;
+        azureAiSearchEmbeddingStore.deleteIndex();
+        azureAiSearchEmbeddingStore.createOrUpdateIndex(embeddingModel);
+    }
+
+    @Override
+    protected void awaitUntilPersisted() {
+        try {
+            Thread.sleep(1_000);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
