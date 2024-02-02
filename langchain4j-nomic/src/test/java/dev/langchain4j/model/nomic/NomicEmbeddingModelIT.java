ba4409640aab142e45e7e10b6604aefda4d46762
LangChain4j <info@langchain4j.dev>
2024-02-02 08:49:22 +0100

    IT naming, api kay in release build

--- langchain4j-nomic/src/test/java/dev/langchain4j/model/nomic/NomicEmbeddingModelIT.java
+++ langchain4j-nomic/src/test/java/dev/langchain4j/model/nomic/NomicEmbeddingModelIT.java
@@ -13,7 +13,7 @@ import static java.time.Duration.ofSeconds;
 import static java.util.Arrays.asList;
 import static org.assertj.core.api.Assertions.assertThat;
 
-class NomicEmbeddingModelTest {
+class NomicEmbeddingModelIT {
 
     @Test
     void should_embed_single_text() {
