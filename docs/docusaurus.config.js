386457bdd695b9a576b810a75c45deb54bc93ec4
Amith Koujalgi <amith.koujalgi@razorthink.com>
2024-02-04 01:06:38 +0530

    Reverted the base url from / to /langchain4j/ as this is a limitation from GitHub to use the repository name as a base URL for GitHub pages deployment

--- docs/docusaurus.config.js
+++ docs/docusaurus.config.js
@@ -20,7 +20,7 @@ const config = {
     url: 'https://langchain4j.github.io/',
     // Set the /<baseUrl>/ pathname under which your site is served
     // For GitHub pages deployment, it is often '/<projectName>/'
-    baseUrl: '/',
+    baseUrl: '/langchain4j/',
 
     // GitHub pages deployment config.
     // If you aren't using GitHub pages, you don't need these.
