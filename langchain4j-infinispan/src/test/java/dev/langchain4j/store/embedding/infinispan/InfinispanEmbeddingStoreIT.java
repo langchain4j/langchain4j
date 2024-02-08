7d89ea229ee1ba25cfea55d3a08e33cd0c99b5c2
Katia Aresti <katiaaresti@gmail.com>
2024-02-08 11:15:07 +0100

    First Infinispan Integration / Langchain (#552)
    This is the first implementation of Infinispan as Embeddings Store for
    langchain
    The current version is 15.0.0.Dev07, which will be in a final release in
    a few weeks time
    
    closes #553
    
    Example https://github.com/langchain4j/langchain4j-examples/pull/42

---
+++ langchain4j-infinispan/src/test/java/dev/langchain4j/store/embedding/infinispan/InfinispanEmbeddingStoreIT.java
@@ -0,0 +1,62 @@
+package dev.langchain4j.store.embedding.infinispan;
+
+import dev.langchain4j.data.segment.TextSegment;
+import dev.langchain4j.model.embedding.AllMiniLmL6V2QuantizedEmbeddingModel;
+import dev.langchain4j.model.embedding.EmbeddingModel;
+import dev.langchain4j.store.embedding.EmbeddingStore;
+import dev.langchain4j.store.embedding.EmbeddingStoreIT;
+import org.infinispan.client.hotrod.configuration.ClientIntelligence;
+import org.infinispan.client.hotrod.configuration.ConfigurationBuilder;
+import org.infinispan.server.test.core.InfinispanContainer;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+
+import static org.infinispan.server.test.core.InfinispanContainer.DEFAULT_PASSWORD;
+import static org.infinispan.server.test.core.InfinispanContainer.DEFAULT_USERNAME;
+
+class InfinispanEmbeddingStoreIT extends EmbeddingStoreIT {
+    static InfinispanContainer infinispan = new InfinispanContainer();
+    EmbeddingStore<TextSegment> embeddingStore;
+    EmbeddingModel embeddingModel = new AllMiniLmL6V2QuantizedEmbeddingModel();
+
+    @BeforeAll
+    static void beforeAll() {
+        infinispan.start();
+    }
+
+    @AfterAll
+    static void afterAll() {
+        infinispan.stop();
+    }
+
+    @Override
+    protected void clearStore() {
+        ConfigurationBuilder builder = new ConfigurationBuilder();
+        builder.addServer().host(infinispan.getHost())
+              .port(infinispan.getFirstMappedPort())
+       .security()
+              .authentication()
+              .username(DEFAULT_USERNAME)
+              .password(DEFAULT_PASSWORD);
+        // jut to avoid docker 4 mac issues, don't use in production
+        builder.clientIntelligence(ClientIntelligence.BASIC);
+
+        InfinispanEmbeddingStore embeddingStoreInf = InfinispanEmbeddingStore.builder()
+              .cacheName("my-cache")
+              .dimension(384)
+              .infinispanConfigBuilder(builder)
+              .build();
+        embeddingStoreInf.clearCache();
+        embeddingStore = embeddingStoreInf;
+    }
+
+    @Override
+    protected EmbeddingStore<TextSegment> embeddingStore() {
+        return embeddingStore;
+    }
+
+    @Override
+    protected EmbeddingModel embeddingModel() {
+        return embeddingModel;
+        }
+}
