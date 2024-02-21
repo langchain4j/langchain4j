20ca3dbe02bb9e2294ae49072f1953a45108d4db
LangChain4j <info@langchain4j.dev>
2024-02-21 14:39:13 +0100

    docu: updated baseUrl

--- docs/docusaurus.config.js
+++ docs/docusaurus.config.js
@@ -20,7 +20,7 @@ const config = {
     url: 'https://langchain4j.github.io/',
     // Set the /<baseUrl>/ pathname under which your site is served
     // For GitHub pages deployment, it is often '/<projectName>/'
-    baseUrl: '/langchain4j/',
+    baseUrl: '/',
 
     // GitHub pages deployment config.
     // If you aren't using GitHub pages, you don't need these.
