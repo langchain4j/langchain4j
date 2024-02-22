a29f341ea66f036e391b4fa9cacd8b7a0434ae15
LangChain4j <info@langchain4j.dev>
2024-02-22 21:48:00 +0100

    FileSystemDocumentLoader: add possibility to load recursively and with glob/regex filtering

--- langchain4j/src/main/java/dev/langchain4j/data/document/loader/FileSystemDocumentLoader.java
+++ langchain4j/src/main/java/dev/langchain4j/data/document/loader/FileSystemDocumentLoader.java
@@ -9,6 +9,7 @@ import org.slf4j.LoggerFactory;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.nio.file.PathMatcher;
 import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.List;
@@ -20,9 +21,11 @@ import static java.nio.file.Files.isDirectory;
 import static java.nio.file.Files.isRegularFile;
 
 public class FileSystemDocumentLoader {
-    private FileSystemDocumentLoader() {}
 
     private static final Logger log = LoggerFactory.getLogger(FileSystemDocumentLoader.class);
+
+    private FileSystemDocumentLoader() {
+    }
 
     /**
      * Loads a document from the specified file.
@@ -68,23 +71,11 @@ public class FileSystemDocumentLoader {
             throw illegalArgument("%s is not a directory", directoryPath);
         }
 
-        List<Document> documents = new ArrayList<>();
-
-        try (Stream<Path> files = Files.list(directoryPath)) {
-            files.filter(Files::isRegularFile)
-                    .forEach(file -> {
-                        try {
-                            Document document = loadDocument(file, documentParser);
-                            documents.add(document);
-                        } catch (Exception e) {
-                            log.warn("Failed to load document from " + file, e);
-                        }
-                    });
+        try (Stream<Path> pathStream = Files.list(directoryPath)) {
+            return loadDocuments(pathStream, (path) -> true, documentParser);
         } catch (IOException e) {
-            throw new RuntimeException(e);
+            throw new IllegalArgumentException(e);
         }
-
-        return documents;
     }
 
     /**
@@ -98,5 +89,147 @@ public class FileSystemDocumentLoader {
      */
     public static List<Document> loadDocuments(String directoryPath, DocumentParser documentParser) {
         return loadDocuments(Paths.get(directoryPath), documentParser);
+    }
+
+    /**
+     * Loads matching documents from the specified directory. Does not use recursion.
+     * Skips any documents that fail to load.
+     *
+     * @param directoryPath  The path to the directory with files.
+     * @param pathMatcher    Only documents matching the criteria set by the provided {@link PathMatcher} will be loaded.
+     *                       For example (remove "\" from glob pattern):
+     *                       <code>FileSystems.getDefault().getPathMatcher("glob:**\/*.txt")</code>.
+     * @param documentParser The parser to be used for parsing text from each file.
+     * @return list of documents
+     * @throws IllegalArgumentException If specified path is not a directory.
+     */
+    public static List<Document> loadDocuments(Path directoryPath,
+                                               PathMatcher pathMatcher,
+                                               DocumentParser documentParser) {
+        if (!isDirectory(directoryPath)) {
+            throw illegalArgument("%s is not a directory", directoryPath);
+        }
+
+        try (Stream<Path> pathStream = Files.list(directoryPath)) {
+            return loadDocuments(pathStream, pathMatcher, documentParser);
+        } catch (IOException e) {
+            throw new IllegalArgumentException(e);
+        }
+    }
+
+    /**
+     * Loads matching documents from the specified directory. Does not use recursion.
+     * Skips any documents that fail to load.
+     *
+     * @param directoryPath  The path to the directory with files.
+     * @param pathMatcher    Only documents matching the criteria set by the provided {@link PathMatcher} will be loaded.
+     *                       For example (remove "\" from glob pattern):
+     *                       <code>FileSystems.getDefault().getPathMatcher("glob:**\/*.txt")</code>
+     * @param documentParser The parser to be used for parsing text from each file.
+     * @return list of documents
+     * @throws IllegalArgumentException If specified path is not a directory.
+     */
+    public static List<Document> loadDocuments(String directoryPath,
+                                               PathMatcher pathMatcher,
+                                               DocumentParser documentParser) {
+        return loadDocuments(Paths.get(directoryPath), pathMatcher, documentParser);
+    }
+
+    /**
+     * Recursively loads documents from the specified directory and its subdirectories.
+     * Skips any documents that fail to load.
+     *
+     * @param directoryPath  The path to the directory with files.
+     * @param documentParser The parser to be used for parsing text from each file.
+     * @return list of documents
+     * @throws IllegalArgumentException If specified path is not a directory.
+     */
+    public static List<Document> loadDocumentsRecursively(Path directoryPath, DocumentParser documentParser) {
+        if (!isDirectory(directoryPath)) {
+            throw illegalArgument("%s is not a directory", directoryPath);
+        }
+
+        try (Stream<Path> pathStream = Files.walk(directoryPath)) {
+            return loadDocuments(pathStream, (path) -> true, documentParser);
+        } catch (IOException e) {
+            throw new IllegalArgumentException(e);
+        }
+    }
+
+    /**
+     * Recursively loads documents from the specified directory and its subdirectories.
+     * Skips any documents that fail to load.
+     *
+     * @param directoryPath  The path to the directory with files.
+     * @param documentParser The parser to be used for parsing text from each file.
+     * @return list of documents
+     * @throws IllegalArgumentException If specified path is not a directory.
+     */
+    public static List<Document> loadDocumentsRecursively(String directoryPath, DocumentParser documentParser) {
+        return loadDocumentsRecursively(Paths.get(directoryPath), documentParser);
+    }
+
+    /**
+     * Recursively loads matching documents from the specified directory and its subdirectories.
+     * Skips any documents that fail to load.
+     *
+     * @param directoryPath  The path to the directory with files.
+     * @param pathMatcher    Only documents matching the criteria set by the provided {@link PathMatcher} will be loaded.
+     *                       For example (remove "\" from glob pattern):
+     *                       <code>FileSystems.getDefault().getPathMatcher("glob:**\/*.txt")</code>
+     * @param documentParser The parser to be used for parsing text from each file.
+     * @return list of documents
+     * @throws IllegalArgumentException If specified path is not a directory.
+     */
+    public static List<Document> loadDocumentsRecursively(Path directoryPath,
+                                                          PathMatcher pathMatcher,
+                                                          DocumentParser documentParser) {
+        if (!isDirectory(directoryPath)) {
+            throw illegalArgument("%s is not a directory", directoryPath);
+        }
+
+        try (Stream<Path> pathStream = Files.walk(directoryPath)) {
+            return loadDocuments(pathStream, pathMatcher, documentParser);
+        } catch (IOException e) {
+            throw new IllegalArgumentException(e);
+        }
+    }
+
+    /**
+     * Recursively loads matching documents from the specified directory and its subdirectories.
+     * Skips any documents that fail to load.
+     *
+     * @param directoryPath  The path to the directory with files.
+     * @param pathMatcher    Only documents matching the criteria set by the provided {@link PathMatcher} will be loaded.
+     *                       For example (remove "\" from glob pattern):
+     *                       <code>FileSystems.getDefault().getPathMatcher("glob:**\/*.txt")</code>
+     * @param documentParser The parser to be used for parsing text from each file.
+     * @return list of documents
+     * @throws IllegalArgumentException If specified path is not a directory.
+     */
+    public static List<Document> loadDocumentsRecursively(String directoryPath,
+                                                          PathMatcher pathMatcher,
+                                                          DocumentParser documentParser) {
+        return loadDocumentsRecursively(Paths.get(directoryPath), pathMatcher, documentParser);
+    }
+
+    private static List<Document> loadDocuments(Stream<Path> pathStream,
+                                                PathMatcher pathMatcher,
+                                                DocumentParser documentParser) {
+        List<Document> documents = new ArrayList<>();
+
+        pathStream
+                .filter(Files::isRegularFile)
+                .filter(pathMatcher::matches)
+                .forEach(file -> {
+                    try {
+                        Document document = loadDocument(file, documentParser);
+                        documents.add(document);
+                    } catch (Exception e) {
+                        log.warn("Failed to load document from " + file, e);
+                    }
+                });
+
+        return documents;
     }
 }
--- langchain4j/src/test/java/dev/langchain4j/data/document/loader/FileSystemDocumentLoaderTest.java
+++ langchain4j/src/test/java/dev/langchain4j/data/document/loader/FileSystemDocumentLoaderTest.java
@@ -8,22 +8,20 @@ import org.junit.jupiter.api.Test;
 
 import java.io.InputStream;
 import java.net.URISyntaxException;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
+import java.nio.file.*;
 import java.util.List;
 import java.util.Set;
 
-import static dev.langchain4j.data.document.loader.FileSystemDocumentLoader.loadDocument;
-import static dev.langchain4j.data.document.loader.FileSystemDocumentLoader.loadDocuments;
+import static dev.langchain4j.data.document.loader.FileSystemDocumentLoader.*;
 import static java.util.stream.Collectors.toSet;
 
 class FileSystemDocumentLoaderTest implements WithAssertions {
+
     @Test
     void load_bad_file() {
         assertThatExceptionOfType(IllegalArgumentException.class)
-            .isThrownBy(() -> loadDocument(Paths.get("bad_file"), new TextDocumentParser()))
-            .withMessageContaining("bad_file is not a file");
+                .isThrownBy(() -> loadDocument(Paths.get("bad_file"), new TextDocumentParser()))
+                .withMessageContaining("bad_file is not a file");
 
         assertThatExceptionOfType(IllegalArgumentException.class)
                 .isThrownBy(() -> loadDocument(Paths.get("/"), new TextDocumentParser()))
@@ -51,14 +49,15 @@ class FileSystemDocumentLoaderTest implements WithAssertions {
     }
 
     @Test
-    void should_load_documents_from_directory_including_unknown_document_types() {
-        String userDir = System.getProperty("user.dir");
-        Path resourceDirectory = Paths.get(userDir, "langchain4j/src/test/resources");
-        if (!Files.exists(resourceDirectory)) {
-            resourceDirectory = Paths.get(userDir, "src/test/resources");
-        }
+    void should_load_documents_including_unknown_document_types() {
 
+        // given
+        Path resourceDirectory = resourceDirectory();
+
+        // when
         List<Document> documents = loadDocuments(resourceDirectory, new TextDocumentParser());
+
+        // then
         assertThat(documents).hasSize(4);
 
         Set<String> fileNames = documents.stream()
@@ -71,6 +70,7 @@ class FileSystemDocumentLoaderTest implements WithAssertions {
                 "test-file-utf8.txt"
         );
 
+        // when-then
         assertThat(loadDocuments(resourceDirectory.toString(), new TextDocumentParser()))
                 .isEqualTo(documents);
 
@@ -78,6 +78,7 @@ class FileSystemDocumentLoaderTest implements WithAssertions {
         DocumentParser failFirstParser = new DocumentParser() {
             private boolean first = true;
             private final DocumentParser parser = new TextDocumentParser();
+
             @Override
             public Document parse(InputStream inputStream) {
                 if (first) {
@@ -88,8 +89,103 @@ class FileSystemDocumentLoaderTest implements WithAssertions {
             }
         };
 
+        // when-then
         assertThat(loadDocuments(resourceDirectory, failFirstParser))
                 .hasSize(documents.size() - 1);
+    }
+
+    @Test
+    void should_load_matching_documents() {
+
+        // given
+        Path resourceDirectory = resourceDirectory();
+        PathMatcher pathMatcher = FileSystems.getDefault().getPathMatcher("glob:**/*.txt");
+
+        // when
+        List<Document> documents = loadDocuments(resourceDirectory, pathMatcher, new TextDocumentParser());
+
+        // then
+        assertThat(documents).hasSize(3);
+
+        Set<String> fileNames = documents.stream()
+                .map(document -> document.metadata("file_name"))
+                .collect(toSet());
+        assertThat(fileNames).containsExactlyInAnyOrder(
+                "miles-of-smiles-terms-of-use.txt",
+                "test-file-iso-8859-1.txt",
+                "test-file-utf8.txt"
+        );
+
+        // when-then
+        assertThat(loadDocuments(resourceDirectory.toString(), pathMatcher, new TextDocumentParser()))
+                .isEqualTo(documents);
+    }
+
+    @Test
+    void should_recursively_load_documents() {
+
+        // given
+        Path resourceDirectory = resourceDirectory();
+
+        // when
+        List<Document> documents = loadDocumentsRecursively(resourceDirectory, new TextDocumentParser());
+
+        // then
+        assertThat(documents).hasSize(6);
+
+        Set<String> fileNames = documents.stream()
+                .map(document -> document.metadata("file_name"))
+                .collect(toSet());
+        assertThat(fileNames).containsExactlyInAnyOrder(
+                "miles-of-smiles-terms-of-use.txt",
+                "test-file.banana",
+                "test-file-iso-8859-1.txt",
+                "test-file-utf8.txt",
+                "chefs-prompt-based-on-ingredients.txt",
+                "chefs-prompt-system-message.txt"
+        );
+
+        // when-then
+        assertThat(loadDocumentsRecursively(resourceDirectory.toString(), new TextDocumentParser()))
+                .isEqualTo(documents);
+    }
+
+    @Test
+    void should_recursively_load_matching_documents() {
+
+        // given
+        Path resourceDirectory = resourceDirectory();
+        PathMatcher pathMatcher = FileSystems.getDefault().getPathMatcher("glob:**/*.txt");
+
+        // when
+        List<Document> documents = loadDocumentsRecursively(resourceDirectory, pathMatcher, new TextDocumentParser());
+
+        // then
+        assertThat(documents).hasSize(5);
+
+        Set<String> fileNames = documents.stream()
+                .map(document -> document.metadata("file_name"))
+                .collect(toSet());
+        assertThat(fileNames).containsExactlyInAnyOrder(
+                "miles-of-smiles-terms-of-use.txt",
+                "test-file-iso-8859-1.txt",
+                "test-file-utf8.txt",
+                "chefs-prompt-based-on-ingredients.txt",
+                "chefs-prompt-system-message.txt"
+        );
+
+        // when-then
+        assertThat(loadDocumentsRecursively(resourceDirectory.toString(), pathMatcher, new TextDocumentParser()))
+                .isEqualTo(documents);
+    }
+
+    private static Path resourceDirectory() {
+        String userDir = System.getProperty("user.dir");
+        Path resourceDirectory = Paths.get(userDir, "langchain4j/src/test/resources");
+        if (Files.exists(resourceDirectory)) {
+            return resourceDirectory;
+        }
+        return Paths.get(userDir, "src/test/resources");
     }
 
     private Path toPath(String fileName) {
