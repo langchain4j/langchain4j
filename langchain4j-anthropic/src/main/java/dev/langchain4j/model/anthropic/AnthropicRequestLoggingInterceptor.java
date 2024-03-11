4fd823f5de5d290cb366e970baa825f353ea6c4e
LangChain4j <info@langchain4j.dev>
2024-03-11 15:31:38 +0100

    WIP: Anthropic

--- langchain4j-anthropic/src/main/java/dev/langchain4j/model/anthropic/AnthropicRequestLoggingInterceptor.java
+++ langchain4j-anthropic/src/main/java/dev/langchain4j/model/anthropic/AnthropicRequestLoggingInterceptor.java
@@ -8,14 +8,19 @@ import okhttp3.Response;
 import okio.Buffer;
 
 import java.io.IOException;
-import java.util.stream.Collectors;
+import java.util.HashSet;
+import java.util.Set;
 import java.util.stream.StreamSupport;
 
 import static dev.langchain4j.internal.Utils.isNullOrBlank;
-import static dev.langchain4j.model.anthropic.AnthropicApi.X_API_KEY;
+import static java.util.Arrays.asList;
+import static java.util.stream.Collectors.joining;
 
 @Slf4j
 class AnthropicRequestLoggingInterceptor implements Interceptor {
+
+    private static final Set<String> COMMON_SECRET_HEADERS =
+            new HashSet<>(asList("authorization", "x-api-key", "x-auth-token"));
 
     @Override
     public Response intercept(Chain chain) throws IOException {
@@ -34,23 +39,25 @@ class AnthropicRequestLoggingInterceptor implements Interceptor {
     }
 
     static String getHeaders(Headers headers) {
-        return StreamSupport.stream(headers.spliterator(), false).map(header -> {
-            String headerKey = header.component1();
-            String headerValue = header.component2();
-            if (headerKey.equals(X_API_KEY)) {
-                headerValue = maskApiKey(headerValue);
-            }
-            return String.format("[%s: %s]", headerKey, headerValue);
-        }).collect(Collectors.joining(", "));
+        return StreamSupport.stream(headers.spliterator(), false)
+                .map(header -> format(header.component1(), header.component2()))
+                .collect(joining(", "));
     }
 
-    static String maskApiKey(String apiKey) {
-        if (isNullOrBlank(apiKey)) {
-            return apiKey;
+    static String format(String headerKey, String headerValue) {
+        if (COMMON_SECRET_HEADERS.contains(headerKey.toLowerCase())) {
+            headerValue = maskSecretKey(headerValue);
+        }
+        return String.format("[%s: %s]", headerKey, headerValue);
+    }
+
+    static String maskSecretKey(String key) {
+        if (isNullOrBlank(key)) {
+            return key;
         }
 
-        if (apiKey.length() >= 7) {
-            return apiKey.substring(0, 5) + "..." + apiKey.substring(apiKey.length() - 2);
+        if (key.length() >= 7) {
+            return key.substring(0, 5) + "..." + key.substring(key.length() - 2);
         } else {
             return "..."; // to short to be masked
         }
