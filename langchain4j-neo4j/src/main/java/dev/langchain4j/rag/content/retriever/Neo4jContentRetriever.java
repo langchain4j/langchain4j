35bfb33935d02b060a0defc8394dd25fe60fe4b4
Siben Nayak <siben.nayak@gmail.com>
2024-03-12 23:29:30 +0530

    feat: added support for Neo4j content retriever

---
+++ langchain4j-neo4j/src/main/java/dev/langchain4j/rag/content/retriever/Neo4jContentRetriever.java
@@ -0,0 +1,72 @@
+package dev.langchain4j.rag.content.retriever;
+
+import dev.langchain4j.graph.neo4j.Neo4jGraph;
+import dev.langchain4j.model.chat.ChatLanguageModel;
+import dev.langchain4j.model.input.Prompt;
+import dev.langchain4j.model.input.PromptTemplate;
+import dev.langchain4j.rag.content.Content;
+import dev.langchain4j.rag.query.Query;
+import lombok.Builder;
+import org.neo4j.driver.Record;
+import org.neo4j.driver.internal.value.NodeValue;
+
+import java.util.List;
+import java.util.Map;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import static dev.langchain4j.internal.ValidationUtils.ensureNotNull;
+
+public class Neo4jContentRetriever implements ContentRetriever {
+
+    private static final PromptTemplate CYPHER_PROMPT_TEMPLATE = PromptTemplate.from("""
+            Based on the Neo4j graph schema below, write a Cypher query that would answer the user's question:
+            {{schema}}
+
+            Question: {{question}}
+            Cypher query:
+            """);
+
+    private static final Pattern BACKTICKS_PATTERN = Pattern.compile("```(.*?)```", Pattern.MULTILINE | Pattern.DOTALL);
+
+    private final Neo4jGraph graph;
+
+    private final ChatLanguageModel chatLanguageModel;
+
+    @Builder
+    public Neo4jContentRetriever(Neo4jGraph graph, ChatLanguageModel chatLanguageModel) {
+
+        this.graph = ensureNotNull(graph, "graph");
+        this.chatLanguageModel = ensureNotNull(chatLanguageModel, "chatLanguageModel");
+    }
+
+    @Override
+    public List<Content> retrieve(Query query) {
+
+        String question = query.text();
+        String schema = graph.getSchema();
+        String cypherQuery = generateCypherQuery(schema, question);
+        List<String> response = executeQuery(cypherQuery);
+        return response.stream().map(Content::from).toList();
+    }
+
+    private String generateCypherQuery(String schema, String question) {
+
+        Prompt cypherPrompt = CYPHER_PROMPT_TEMPLATE.apply(Map.of("schema", schema, "question", question));
+        String cypherQuery = chatLanguageModel.generate(cypherPrompt.text());
+        Matcher matcher = BACKTICKS_PATTERN.matcher(cypherQuery);
+        if (matcher.find()) {
+            return matcher.group(1);
+        }
+        return cypherQuery;
+    }
+
+    private List<String> executeQuery(String cypherQuery) {
+
+        List<Record> records = graph.executeRead(cypherQuery);
+        return records.stream()
+                .flatMap(r -> r.values().stream())
+                .map(value -> value instanceof NodeValue ? value.asMap().toString() : value.toString())
+                .toList();
+    }
+}
---
+++ langchain4j-neo4j/src/test/java/dev/langchain4j/rag/content/retriever/Neo4jContentRetrieverTest.java
@@ -0,0 +1,75 @@
+package dev.langchain4j.rag.content.retriever;
+
+import dev.langchain4j.graph.neo4j.Neo4jGraph;
+import dev.langchain4j.model.chat.ChatLanguageModel;
+import dev.langchain4j.rag.content.Content;
+import dev.langchain4j.rag.query.Query;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.neo4j.driver.AuthTokens;
+import org.neo4j.driver.Driver;
+import org.neo4j.driver.GraphDatabase;
+import org.testcontainers.containers.Neo4jContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.utility.DockerImageName;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.Mockito.when;
+
+@ExtendWith(MockitoExtension.class)
+class Neo4jContentRetrieverTest {
+
+    @Container
+    private static final Neo4jContainer<?> neo4jContainer = new Neo4jContainer<>(DockerImageName.parse("neo4j:5.16.0"))
+            .withoutAuthentication()
+            .withLabsPlugins("apoc");
+
+    private static Neo4jContentRetriever retriever;
+
+    @Mock
+    private static ChatLanguageModel chatLanguageModel;
+
+    @BeforeAll
+    static void beforeAll() {
+
+        neo4jContainer.start();
+        Driver driver = GraphDatabase.driver(neo4jContainer.getBoltUrl(), AuthTokens.none());
+        Neo4jGraph graph = Neo4jGraph.builder().driver(driver).build();
+        retriever = Neo4jContentRetriever.builder()
+                .graph(graph)
+                .chatLanguageModel(chatLanguageModel)
+                .build();
+    }
+
+    @Test
+    void shouldRetrieveContentWhenQueryIsValid() {
+        // Given
+        Query query = new Query("Who is the author of the book 'Dune'?");
+        when(chatLanguageModel.generate(anyString())).thenReturn("MATCH(book:Book {title: 'Dune'})<-[:WROTE]-(author:Person) RETURN author.name AS output");
+
+        // When
+        List<Content> contents = retriever.retrieve(query);
+
+        // Then
+        assertEquals(1, contents.size());
+    }
+
+    @Test
+    void shouldReturnEmptyListWhenQueryIsInvalid() {
+        // Given
+        Query query = new Query("Who is the author of the movie 'Dune'?");
+        when(chatLanguageModel.generate(anyString())).thenReturn("MATCH(movie:Movie {title: 'Dune'})<-[:WROTE]-(author:Person) RETURN author.name AS output");
+
+        // When
+        List<Content> contents = retriever.retrieve(query);
+
+        // Then
+        assertEquals(0, contents.size());
+    }
+}
