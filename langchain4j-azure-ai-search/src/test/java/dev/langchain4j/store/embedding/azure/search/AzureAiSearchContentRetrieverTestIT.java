cba5ced7b9a790a252a81fc27e885f95400698fa
Julien Dubois <julien.dubois@gmail.com>
2024-03-20 17:31:09 +0100

    Update langchain4j-azure-ai-search/src/test/java/dev/langchain4j/store/embedding/azure/search/AzureAiSearchContentRetrieverTestIT.java
    Co-authored-by: LangChain4j <langchain4j@gmail.com>

--- langchain4j-azure-ai-search/src/test/java/dev/langchain4j/store/embedding/azure/search/AzureAiSearchContentRetrieverTestIT.java
+++ langchain4j-azure-ai-search/src/test/java/dev/langchain4j/store/embedding/azure/search/AzureAiSearchContentRetrieverTestIT.java
@@ -181,7 +181,7 @@ public class AzureAiSearchContentRetrieverTestIT extends EmbeddingStoreIT {
     }
 
     @Test
-    void testAddEmbeddingsAndFindRelevantWithSemantic() {
+    void testAddEmbeddingsAndRetrieveRelevantWithHybridAndReranking() {
         String content1 = "Albert Camus (7 November 1913 – 4 January 1960) was a French philosopher, author, dramatist, journalist, world federalist, and political activist. He was the recipient of the 1957 Nobel Prize in Literature at the age of 44, the second-youngest recipient in history. His works include The Stranger, The Plague, The Myth of Sisyphus, The Fall, and The Rebel.\n" +
                 "\n" +
                 "Camus was born in Algeria during the French colonization, to pied-noir parents. He spent his childhood in a poor neighbourhood and later studied philosophy at the University of Algiers. He was in Paris when the Germans invaded France during World War II in 1940. Camus tried to flee but finally joined the French Resistance where he served as editor-in-chief at Combat, an outlawed newspaper. After the war, he was a celebrity figure and gave many lectures around the world. He married twice but had many extramarital affairs. Camus was politically active; he was part of the left that opposed Joseph Stalin and the Soviet Union because of their totalitarianism. Camus was a moralist and leaned towards anarcho-syndicalism. He was part of many organisations seeking European integration. During the Algerian War (1954–1962), he kept a neutral stance, advocating for a multicultural and pluralistic Algeria, a position that was rejected by most parties.\n" +
