74c7e99ee9c14f1ba9e05928c99b77ebcf82cd4c
Eric Deandrea <edeandrea@redhat.com>
2024-04-01 11:04:43 -0400

    Add getters to AnthropicCreateMessageRequest

--- langchain4j-anthropic/src/main/java/dev/langchain4j/model/anthropic/AnthropicCreateMessageRequest.java
+++ langchain4j-anthropic/src/main/java/dev/langchain4j/model/anthropic/AnthropicCreateMessageRequest.java
@@ -1,9 +1,15 @@
 package dev.langchain4j.model.anthropic;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.util.List;
 
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 @Builder
 public class AnthropicCreateMessageRequest {
 
