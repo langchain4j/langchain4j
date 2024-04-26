7ea34a4672a7ad548edcc044bc2f8d178411d373
jiangsier-xyz <126842484+jiangsier-xyz@users.noreply.github.com>
2024-04-26 10:52:36 +0800

    Test open sourced models.

--- langchain4j-dashscope/src/test/java/dev/langchain4j/model/dashscope/QwenTestHelper.java
+++ langchain4j-dashscope/src/test/java/dev/langchain4j/model/dashscope/QwenTestHelper.java
@@ -23,6 +23,12 @@ public class QwenTestHelper {
                 Arguments.of(QwenModelName.QWEN_PLUS),
                 Arguments.of(QwenModelName.QWEN_MAX),
                 Arguments.of(QwenModelName.QWEN_MAX_LONGCONTEXT),
+                Arguments.of(QwenModelName.QWEN_7B_CHAT),
+                Arguments.of(QwenModelName.QWEN_14B_CHAT),
+                Arguments.of(QwenModelName.QWEN_72B_CHAT),
+                Arguments.of(QwenModelName.QWEN1_5_7B_CHAT),
+                Arguments.of(QwenModelName.QWEN1_5_14B_CHAT),
+                Arguments.of(QwenModelName.QWEN1_5_32B_CHAT),
                 Arguments.of(QwenModelName.QWEN1_5_72B_CHAT)
         );
     }
