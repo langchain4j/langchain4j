a85a0d53f0aff5f1fba28129203b14ff60631637
Kais NEFFATI <nef.kais@gmail.com>
2024-04-29 22:16:20 +0200

    [BUG] Introduce parser supplier support in FileSystemDocumentLoader#loadDocuments #1026

--- document-parsers/langchain4j-document-parser-apache-tika/src/main/java/dev/langchain4j/data/document/parser/apache/tika/ApacheTikaDocumentParser.java
+++ document-parsers/langchain4j-document-parser-apache-tika/src/main/java/dev/langchain4j/data/document/parser/apache/tika/ApacheTikaDocumentParser.java
@@ -12,6 +12,7 @@ import org.apache.tika.sax.BodyContentHandler;
 import org.xml.sax.ContentHandler;
 
 import java.io.InputStream;
+import java.util.function.Supplier;
 
 import static dev.langchain4j.internal.Utils.getOrDefault;
 import static dev.langchain4j.internal.Utils.isNullOrBlank;
@@ -25,11 +26,15 @@ import static dev.langchain4j.internal.Utils.isNullOrBlank;
 public class ApacheTikaDocumentParser implements DocumentParser {
 
     private static final int NO_WRITE_LIMIT = -1;
+    public static final Supplier<Parser> DEFAULT_PARSER_SUPPLIER = AutoDetectParser::new;
+    public static final Supplier<Metadata> DEFAULT_METADATA_SUPPLIER = Metadata::new;
+    public static final Supplier<ParseContext> DEFAULT_PARSE_CONTEXT_SUPPLIER = ParseContext::new;
+    public static final Supplier<ContentHandler> DEFAULT_CONTENT_HANDLER_SUPPLIER = () -> new BodyContentHandler(NO_WRITE_LIMIT);
 
-    private final Parser parser;
-    private final ContentHandler contentHandler;
-    private final Metadata metadata;
-    private final ParseContext parseContext;
+    private final Supplier<Parser> parserSupplier;
+    private final Supplier<ContentHandler> contentHandlerSupplier;
+    private final Supplier<Metadata> metadataSupplier;
+    private final Supplier<ParseContext> parseContextSupplier;
 
     /**
      * Creates an instance of an {@code ApacheTikaDocumentParser} with the default Tika components.
@@ -37,7 +42,7 @@ public class ApacheTikaDocumentParser implements DocumentParser {
      * empty {@link Metadata} and empty {@link ParseContext}.
      */
     public ApacheTikaDocumentParser() {
-        this(null, null, null, null);
+        this((Supplier<Parser>) null, null, null, null);
     }
 
     /**
@@ -48,15 +53,33 @@ public class ApacheTikaDocumentParser implements DocumentParser {
      * @param contentHandler Tika content handler. Default: {@link BodyContentHandler} without write limit
      * @param metadata       Tika metadata. Default: empty {@link Metadata}
      * @param parseContext   Tika parse context. Default: empty {@link ParseContext}
+     * @deprecated Use the constructor with suppliers for Tika components if you intend to use this parser for multiple files.
      */
+    @Deprecated
     public ApacheTikaDocumentParser(Parser parser,
                                     ContentHandler contentHandler,
                                     Metadata metadata,
                                     ParseContext parseContext) {
-        this.parser = getOrDefault(parser, AutoDetectParser::new);
-        this.contentHandler = getOrDefault(contentHandler, () -> new BodyContentHandler(NO_WRITE_LIMIT));
-        this.metadata = getOrDefault(metadata, Metadata::new);
-        this.parseContext = getOrDefault(parseContext, ParseContext::new);
+        this(() -> parser, () -> contentHandler, () -> metadata, () -> parseContext);
+    }
+
+    /**
+     * Creates an instance of an {@code ApacheTikaDocumentParser} with the provided suppliers for Tika components.
+     * If some of the suppliers are not provided ({@code null}), the defaults will be used.
+     *
+     * @param parserSupplier         Supplier for Tika parser to use. Default: {@link AutoDetectParser}
+     * @param contentHandlerSupplier Supplier for Tika content handler. Default: {@link BodyContentHandler} without write limit
+     * @param metadataSupplier       Supplier for Tika metadata. Default: empty {@link Metadata}
+     * @param parseContextSupplier   Supplier for Tika parse context. Default: empty {@link ParseContext}
+     */
+    public ApacheTikaDocumentParser(Supplier<Parser> parserSupplier,
+                                    Supplier<ContentHandler> contentHandlerSupplier,
+                                    Supplier<Metadata> metadataSupplier,
+                                    Supplier<ParseContext> parseContextSupplier) {
+        this.parserSupplier = parserSupplier;
+        this.contentHandlerSupplier = contentHandlerSupplier;
+        this.metadataSupplier = metadataSupplier;
+        this.parseContextSupplier = parseContextSupplier;
     }
 
     // TODO allow automatically extract metadata (e.g. creator, last-author, created/modified timestamp, etc)
@@ -64,6 +87,11 @@ public class ApacheTikaDocumentParser implements DocumentParser {
     @Override
     public Document parse(InputStream inputStream) {
         try {
+            Parser parser = getOrDefault(parserSupplier, () -> DEFAULT_PARSER_SUPPLIER).get();
+            ContentHandler contentHandler = getOrDefault(contentHandlerSupplier, () -> DEFAULT_CONTENT_HANDLER_SUPPLIER).get();
+            Metadata metadata = getOrDefault(metadataSupplier, () -> DEFAULT_METADATA_SUPPLIER).get();
+            ParseContext parseContext = getOrDefault(parseContextSupplier, () -> DEFAULT_PARSE_CONTEXT_SUPPLIER).get();
+
             parser.parse(inputStream, contentHandler, metadata, parseContext);
             String text = contentHandler.toString();
 
