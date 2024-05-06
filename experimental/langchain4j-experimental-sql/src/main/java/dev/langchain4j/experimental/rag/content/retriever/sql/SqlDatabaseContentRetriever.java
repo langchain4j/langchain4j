d99af0af8e622222aeafc782b1b9d4deb1118d38
LangChain4j <info@langchain4j.dev>
2024-05-06 12:51:41 +0200

    Experimental: RAG: SQL database content retriever

---
+++ experimental/langchain4j-experimental-sql/src/main/java/dev/langchain4j/experimental/rag/content/retriever/sql/SqlDatabaseContentRetriever.java
@@ -0,0 +1,308 @@
+package dev.langchain4j.experimental.rag.content.retriever.sql;
+
+import dev.langchain4j.Experimental;
+import dev.langchain4j.data.message.AiMessage;
+import dev.langchain4j.data.message.ChatMessage;
+import dev.langchain4j.data.message.SystemMessage;
+import dev.langchain4j.data.message.UserMessage;
+import dev.langchain4j.model.chat.ChatLanguageModel;
+import dev.langchain4j.model.input.Prompt;
+import dev.langchain4j.model.input.PromptTemplate;
+import dev.langchain4j.rag.content.Content;
+import dev.langchain4j.rag.content.retriever.ContentRetriever;
+import dev.langchain4j.rag.query.Query;
+import lombok.Builder;
+import net.sf.jsqlparser.JSQLParserException;
+import net.sf.jsqlparser.parser.CCJSqlParserUtil;
+import net.sf.jsqlparser.statement.select.Select;
+
+import javax.sql.DataSource;
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import static dev.langchain4j.internal.Utils.getOrDefault;
+import static dev.langchain4j.internal.ValidationUtils.ensureNotNull;
+import static java.util.Collections.emptyList;
+import static java.util.Collections.singletonList;
+
+/**
+ * <b>
+ * WARNING! Although fun and exciting, this class is dangerous to use! Do not ever use this in production!
+ * The database user must have very limited READ-ONLY permissions!
+ * Although the generated SQL is somewhat validated (to ensure that the SQL is a SELECT statement) using JSqlParser,
+ * this class does not guarantee that the SQL will be harmless. Use it at your own risk!
+ * </b>
+ * <br>
+ * <br>
+ * Using the {@link DataSource} and the {@link ChatLanguageModel}, this {@link ContentRetriever}
+ * attempts to generate and execute SQL queries for given natural language queries.
+ * <br>
+ * Optionally, {@link #sqlDialect}, {@link #ddl}, {@link #promptTemplate}, and {@link #maxRetries} can be specified
+ * to customize the behavior. See the javadoc of the constructor for more details.
+ * Most methods can be overridden to customize the behavior further.
+ * <br>
+ * The default prompt template is not optimized, so it is advised to experiment and see what works best for your use case.
+ */
+@Experimental
+public class SqlDatabaseContentRetriever implements ContentRetriever {
+
+    private static final PromptTemplate DEFAULT_PROMPT_TEMPLATE = PromptTemplate.from(
+            "You are an expert in writing SQL queries.\n" +
+                    "You have access to a {{sqlDialect}} database with the following structure:\n" +
+                    "{{ddl}}\n" +
+                    "If a user asks a question that can be answered by querying this database, generate an SQL SELECT query.\n" +
+                    "Do not output anything else aside from a valid SQL statement!"
+    );
+
+    private final DataSource dataSource;
+    private final String sqlDialect;
+    private final String ddl;
+
+    private final PromptTemplate promptTemplate;
+    private final ChatLanguageModel chatLanguageModel;
+
+    private final int maxRetries;
+
+    /**
+     * Creates an instance of a {@code SqlDatabaseContentRetriever}.
+     *
+     * @param dataSource        The {@link DataSource} to be used for executing SQL queries. Mandatory parameter.
+     *                          <b>WARNING! The database user must have very limited READ-ONLY permissions!</b>
+     * @param sqlDialect        The SQL dialect, which will be provided to the LLM in the {@link SystemMessage}.
+     *                          Optional parameter. If not specified, it will be determined from the {@code DataSource}.
+     * @param ddl               The DDL of the tables, which will be provided to the LLM in the {@code SystemMessage}.
+     *                          Optional parameter. If not specified, it will be generated from the {@code DataSource}.
+     *                          <b>WARNING! In this case, all tables will be visible to the LLM!</b>
+     * @param promptTemplate    The {@link PromptTemplate} to be used for creating a {@code SystemMessage}.
+     *                          Optional parameter. Default: {@link #DEFAULT_PROMPT_TEMPLATE}.
+     * @param chatLanguageModel The {@link ChatLanguageModel} to be used for generating SQL queries. Mandatory parameter.
+     * @param maxRetries        The maximum number of retries to perform if the database cannot execute the generated SQL query.
+     *                          An error message will be sent back to the LLM to try correcting the query.
+     *                          Optional parameter. Default: 1.
+     */
+    @Builder
+    @Experimental
+    public SqlDatabaseContentRetriever(DataSource dataSource,
+                                       String sqlDialect,
+                                       String ddl,
+                                       PromptTemplate promptTemplate,
+                                       ChatLanguageModel chatLanguageModel,
+                                       Integer maxRetries) {
+        this.dataSource = ensureNotNull(dataSource, "dataSource");
+        this.sqlDialect = getOrDefault(sqlDialect, () -> getSqlDialect(dataSource));
+        this.ddl = getOrDefault(ddl, () -> getDDL(dataSource));
+        this.promptTemplate = getOrDefault(promptTemplate, DEFAULT_PROMPT_TEMPLATE);
+        this.chatLanguageModel = ensureNotNull(chatLanguageModel, "chatLanguageModel");
+        this.maxRetries = getOrDefault(maxRetries, 1);
+    }
+
+    // TODO provide a few rows of data for each table
+
+    public static String getSqlDialect(DataSource dataSource) {
+        try (Connection connection = dataSource.getConnection()) {
+            DatabaseMetaData metaData = connection.getMetaData();
+            return metaData.getDatabaseProductName();
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private static String getDDL(DataSource dataSource) {
+        StringBuilder ddl = new StringBuilder();
+
+        try (Connection connection = dataSource.getConnection()) {
+            DatabaseMetaData metaData = connection.getMetaData();
+
+            ResultSet tables = metaData.getTables(null, null, "%", new String[]{"TABLE"});
+
+            while (tables.next()) {
+                String tableName = tables.getString("TABLE_NAME");
+                String tableDDL = getTableDDL(metaData, tableName);
+                ddl.append(tableDDL).append("\n");
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+
+        return ddl.toString();
+    }
+
+    private static String getTableDDL(DatabaseMetaData metaData, String tableName) {
+        StringBuilder ddl = new StringBuilder();
+
+        try {
+            ResultSet columns = metaData.getColumns(null, null, tableName, null);
+            ResultSet pk = metaData.getPrimaryKeys(null, null, tableName);
+            ResultSet fks = metaData.getImportedKeys(null, null, tableName);
+
+            String primaryKeyColumn = "";
+            if (pk.next()) {
+                primaryKeyColumn = pk.getString("COLUMN_NAME");
+            }
+
+            ddl.append("CREATE TABLE ").append(tableName).append(" (\n");
+            while (columns.next()) {
+                String columnName = columns.getString("COLUMN_NAME");
+                String columnType = columns.getString("TYPE_NAME");
+                int size = columns.getInt("COLUMN_SIZE");
+                String nullable = columns.getString("IS_NULLABLE").equals("YES") ? " NULL" : " NOT NULL";
+                String columnDef = columns.getString("COLUMN_DEF") != null ? " DEFAULT " + columns.getString("COLUMN_DEF") : "";
+                String comment = columns.getString("REMARKS");
+
+                ddl.append("  ").append(columnName).append(" ").append(columnType).append("(").append(size).append(")").append(nullable).append(columnDef);
+                if (columnName.equals(primaryKeyColumn)) {
+                    ddl.append(" PRIMARY KEY");
+                }
+                ddl.append(",\n");
+
+                if (comment != null && !comment.isEmpty()) {
+                    ddl.append("  COMMENT ON COLUMN ").append(tableName).append(".").append(columnName).append(" IS '").append(comment).append("',\n");
+                }
+            }
+
+            while (fks.next()) {
+                String fkColumnName = fks.getString("FKCOLUMN_NAME");
+                String pkTableName = fks.getString("PKTABLE_NAME");
+                String pkColumnName = fks.getString("PKCOLUMN_NAME");
+                ddl.append("  FOREIGN KEY (").append(fkColumnName).append(") REFERENCES ").append(pkTableName).append("(").append(pkColumnName).append("),\n");
+            }
+
+            if (ddl.charAt(ddl.length() - 2) == ',') {
+                ddl.delete(ddl.length() - 2, ddl.length());
+            }
+
+            ddl.append(");\n");
+
+            ResultSet tableRemarks = metaData.getTables(null, null, tableName, null);
+            if (tableRemarks.next()) {
+                String tableComment = tableRemarks.getString("REMARKS");
+                if (tableComment != null && !tableComment.isEmpty()) {
+                    ddl.append("COMMENT ON TABLE ").append(tableName).append(" IS '").append(tableComment).append("';\n");
+                }
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+
+        return ddl.toString();
+    }
+
+    @Override
+    public List<Content> retrieve(Query naturalLanguageQuery) {
+
+        String sqlQuery = null;
+        String errorMessage = null;
+
+        int attemptsLeft = maxRetries + 1;
+        while (attemptsLeft > 0) {
+            attemptsLeft--;
+
+            sqlQuery = generateSqlQuery(naturalLanguageQuery, sqlQuery, errorMessage);
+
+            sqlQuery = clean(sqlQuery);
+
+            if (!isSelect(sqlQuery)) {
+                return emptyList();
+            }
+
+            try {
+                validate(sqlQuery);
+
+                try (Connection connection = dataSource.getConnection();
+                     Statement statement = connection.createStatement()) {
+
+                    String result = getResult(sqlQuery, statement);
+                    Content content = format(result, sqlQuery);
+                    return singletonList(content);
+                }
+            } catch (Exception e) {
+                errorMessage = e.getMessage();
+            }
+        }
+
+        return emptyList();
+    }
+
+    protected String generateSqlQuery(Query naturalLanguageQuery, String previousSqlQuery, String previousErrorMessage) {
+
+        List<ChatMessage> messages = new ArrayList<>();
+        messages.add(createSystemPrompt().toSystemMessage());
+        messages.add(UserMessage.from(naturalLanguageQuery.text()));
+
+        if (previousSqlQuery != null && previousErrorMessage != null) {
+            messages.add(AiMessage.from(previousSqlQuery));
+            messages.add(UserMessage.from(previousErrorMessage));
+        }
+
+        String sqlQuery = chatLanguageModel.generate(messages).content().text();
+
+
+        return sqlQuery;
+    }
+
+    protected Prompt createSystemPrompt() {
+        Map<String, Object> variables = new HashMap<>();
+        variables.put("sqlDialect", sqlDialect);
+        variables.put("ddl", ddl);
+        return promptTemplate.apply(variables);
+    }
+
+    protected String clean(String sqlQuery) {
+        if (sqlQuery.contains("```sql")) {
+            return sqlQuery.substring(sqlQuery.indexOf("```sql") + 6, sqlQuery.lastIndexOf("```"));
+        } else if (sqlQuery.contains("```")) {
+            return sqlQuery.substring(sqlQuery.indexOf("```") + 3, sqlQuery.lastIndexOf("```"));
+        }
+        return sqlQuery;
+    }
+
+    protected void validate(String sqlQuery) {
+
+    }
+
+    protected boolean isSelect(String sqlQuery) {
+        try {
+            net.sf.jsqlparser.statement.Statement statement = CCJSqlParserUtil.parse(sqlQuery);
+            return statement instanceof Select;
+        } catch (JSQLParserException e) {
+            return false;
+        }
+    }
+
+    protected String getResult(String sqlQuery, Statement statement) throws SQLException {
+        List<String> resultRows = new ArrayList<>();
+
+        try (ResultSet resultSet = statement.executeQuery(sqlQuery)) {
+            int columnCount = resultSet.getMetaData().getColumnCount();
+
+            // header
+            List<String> columnNames = new ArrayList<>();
+            for (int i = 1; i <= columnCount; i++) {
+                columnNames.add(resultSet.getMetaData().getColumnName(i));
+            }
+            resultRows.add(String.join(",", columnNames));
+
+            // rows
+            while (resultSet.next()) {
+                List<String> columnValues = new ArrayList<>();
+                for (int i = 1; i <= columnCount; i++) {
+                    String columnValue = resultSet.getObject(i).toString();
+                    if (columnValue.contains(",")) {
+                        columnValue = "\"" + columnValue + "\"";
+                    }
+                    columnValues.add(columnValue);
+                }
+                resultRows.add(String.join(",", columnValues));
+            }
+        }
+
+        return String.join("\n", resultRows);
+    }
+
+    private static Content format(String result, String sqlQuery) {
+        return Content.from(String.format("Result of executing '%s':\n%s", sqlQuery, result));
+    }
+}
