1e874fc8a340527b57c749b8ddd94ae6db8407da
Xiaopeng Dai <xpdai@amazon.com>
2024-05-09 13:10:31 -0700

    Cohere: Add CohereChatModel

--- langchain4j-cohere/src/main/java/dev/langchain4j/model/cohere/internal/api/BilledUnits.java
+++ langchain4j-cohere/src/main/java/dev/langchain4j/model/cohere/internal/api/BilledUnits.java
@@ -1,15 +1,10 @@
 package dev.langchain4j.model.cohere.internal.api;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
-import com.fasterxml.jackson.annotation.JsonInclude;
-import com.fasterxml.jackson.databind.PropertyNamingStrategies.SnakeCaseStrategy;
-import com.fasterxml.jackson.databind.annotation.JsonNaming;
-
-import static com.fasterxml.jackson.annotation.JsonInclude.Include.NON_NULL;
 
 @Getter
-@JsonInclude(NON_NULL)
-@JsonNaming(SnakeCaseStrategy.class)
+@AllArgsConstructor
 public class BilledUnits {
 
     private Integer searchUnits;
