5eb508347542c46078e9cb14dfe26fec3b654e9f
Martin7-1 <1754350460@qq.com>
2024-05-09 11:53:09 +0800

    Jackson Entity annotation

--- langchain4j-ollama/src/main/java/dev/langchain4j/model/ollama/ChatRequest.java
+++ langchain4j-ollama/src/main/java/dev/langchain4j/model/ollama/ChatRequest.java
@@ -1,5 +1,8 @@
 package dev.langchain4j.model.ollama;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.databind.PropertyNamingStrategies.SnakeCaseStrategy;
+import com.fasterxml.jackson.databind.annotation.JsonNaming;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -7,10 +10,14 @@ import lombok.NoArgsConstructor;
 
 import java.util.List;
 
+import static com.fasterxml.jackson.annotation.JsonInclude.Include.NON_NULL;
+
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder
+@JsonInclude(NON_NULL)
+@JsonNaming(SnakeCaseStrategy.class)
 class ChatRequest {
 
     private String model;
--- langchain4j-ollama/src/main/java/dev/langchain4j/model/ollama/EmbeddingRequest.java
+++ langchain4j-ollama/src/main/java/dev/langchain4j/model/ollama/EmbeddingRequest.java
@@ -1,14 +1,21 @@
 package dev.langchain4j.model.ollama;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.databind.PropertyNamingStrategies.SnakeCaseStrategy;
+import com.fasterxml.jackson.databind.annotation.JsonNaming;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import static com.fasterxml.jackson.annotation.JsonInclude.Include.NON_NULL;
+
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder
+@JsonInclude(NON_NULL)
+@JsonNaming(SnakeCaseStrategy.class)
 class EmbeddingRequest {
 
     private String model;
