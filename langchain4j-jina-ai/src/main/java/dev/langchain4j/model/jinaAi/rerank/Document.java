70d1b4a3b05d2e87749f4706d38701d253c6af56
Kevin Smeyers <109274963+One16-KS@users.noreply.github.com>
2024-05-22 14:55:02 +0200

    Adding jina as scoring model. Feature request: #974 (#1053)
    Module added according #973 in order to minimize conflicts.
    Implementation in line with cohere reranking
    
    <!-- Thank you so much for your contribution! -->
    <!-- Please fill in all the sections below. -->
    
    <!-- Please open the PR as a draft initially. Once it is reviewed and
    approved, we will ask you to add documentation and examples. -->
    <!-- Please note that PRs with breaking changes will be rejected. -->
    <!-- Please note that PRs without tests will be rejected. -->
    
    <!-- Please note that PRs will be reviewed based on the priority of the
    issues they address. -->
    <!-- We ask for your patience. We are doing our best to review your PR
    as quickly as possible. -->
    <!-- Please refrain from pinging and asking when it will be reviewed.
    Thank you for understanding! -->
    
    
    ## Issue
    https://github.com/langchain4j/langchain4j/issues/974
    
    
    ## Change
    
    - Adding Jina ai as a scoring model.
    - Default model: jina-reranker-v1-base-en
    - Default url: https://api.jina.ai/v1/
    - Used Jackson object mapper instead of Gson, as I found it it's
    langchain4j intent to move away from Gson
    - Tried to keep implementation in line with Cohere reranking module
    
    ## General checklist
    <!-- Please double-check the following points and mark them like this:
    [X] -->
    - [x] There are no breaking changes
    - [x] I have added unit and integration tests for my change
    - [x] I have manually run all the unit and integration tests in the
    module I have added/changed, and they are all green
    - [x] I have manually run all the unit and integration tests in the
    [core](https://github.com/langchain4j/langchain4j/tree/main/langchain4j-core)
    and
    [main](https://github.com/langchain4j/langchain4j/tree/main/langchain4j)
    modules, and they are all green
    <!-- Before adding documentation and example(s) (below), please wait
    until the PR is reviewed and approved. -->
    - [x] I have added/updated the
    [documentation](https://github.com/langchain4j/langchain4j/tree/main/docs/docs)
    - [ ] I have added an example in the [examples
    repo](https://github.com/langchain4j/langchain4j-examples) (only for
    "big" features)
    
    
    ## Checklist for adding new model integration
    <!-- Please double-check the following points and mark them like this:
    [X] -->
    - [x] I have added my new module in the
    [BOM](https://github.com/langchain4j/langchain4j/blob/main/langchain4j-bom/pom.xml)
    
    ---------
    
    Co-authored-by: Kevin Smeyers <kevin.smeyers@audlau.be>
    Co-authored-by: LangChain4j <langchain4j@gmail.com>

---
+++ langchain4j-jina-ai/src/main/java/dev/langchain4j/model/jinaAi/rerank/Document.java
@@ -0,0 +1,9 @@
+package dev.langchain4j.model.jinaAi.rerank;
+
+import lombok.Getter;
+
+@Getter
+public class Document {
+
+    private String text;
+}
---
+++ langchain4j-jina-ai/src/main/java/dev/langchain4j/model/jinaAi/rerank/RerankRequest.java
@@ -0,0 +1,20 @@
+package dev.langchain4j.model.jinaAi.rerank;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+class RerankRequest {
+
+    private String model;
+    private String query;
+    private List<String> documents;
+    private Boolean returnDocuments;
+}
