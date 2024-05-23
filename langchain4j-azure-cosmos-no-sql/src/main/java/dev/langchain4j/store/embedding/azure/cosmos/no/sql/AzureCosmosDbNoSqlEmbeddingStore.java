9e382d4f33cfc88f0eb91f8968f4b48c6ad3f7a3
Aayush Kataria <aayushkataria3011@gmail.com>
2024-05-23 02:06:11 -0700

    Adds an embedding store for Azure Cosmos DB for NoSQL (#1115)
    ## Issue
    This PR add supports for Azure Cosmos DB for NoSQL embedding store.
    
    
    ## Change
    - This PR adds an embedding store for Azure Cosmos DB for NoSql. The
    test cases and IT test case is also included.
    
    
    ## General checklist
    <!-- Please double-check the following points and mark them like this:
    [X] -->
    - [ ] There are no breaking changes
    - [x] I have added unit and integration tests for my change
    - [x] I have manually run all the unit and integration tests in the
    module I have added/changed, and they are all green
    - [ ] I have manually run all the unit and integration tests in the
    [core](https://github.com/langchain4j/langchain4j/tree/main/langchain4j-core)
    and
    [main](https://github.com/langchain4j/langchain4j/tree/main/langchain4j)
    modules, and they are all green
    <!-- Before adding documentation and example(s) (below), please wait
    until the PR is reviewed and approved. -->
    - [ ] I have added/updated the
    [documentation](https://github.com/langchain4j/langchain4j/tree/main/docs/docs)
    - [ ] I have added an example in the [examples
    repo](https://github.com/langchain4j/langchain4j-examples) (only for
    "big" features)
    
    
    ## Checklist for adding new model integration
    <!-- Please double-check the following points and mark them like this:
    [X] -->
    - [x] I have added my new module in the
    [BOM](https://github.com/langchain4j/langchain4j/blob/main/langchain4j-bom/pom.xml)
    
    
    ## Checklist for adding new embedding store integration
    <!-- Please double-check the following points and mark them like this:
    [X] -->
    - [x] I have added a `{NameOfIntegration}EmbeddingStoreIT` that extends
    from either `EmbeddingStoreIT` or `EmbeddingStoreWithFilteringIT`
    - [x] I have added my new module in the
    [BOM](https://github.com/langchain4j/langchain4j/blob/main/langchain4j-bom/pom.xml)
    
    
    ## Checklist for changing existing embedding store integration
    <!-- Please double-check the following points and mark them like this:
    [X] -->
    - [ ] I have manually verified that the
    `{NameOfIntegration}EmbeddingStore` works correctly with the data
    persisted using the latest released version of LangChain4j

---
+++ langchain4j-azure-cosmos-no-sql/src/main/java/dev/langchain4j/store/embedding/azure/cosmos/no/sql/AzureCosmosDbNoSqlEmbeddingStore.java
@@ -0,0 +1,171 @@
+package dev.langchain4j.store.embedding.azure.cosmos.no.sql;
+
+import com.azure.cosmos.CosmosClient;
+import com.azure.cosmos.CosmosContainer;
+import com.azure.cosmos.CosmosDatabase;
+import com.azure.cosmos.models.CosmosBulkOperations;
+import com.azure.cosmos.models.CosmosContainerProperties;
+import com.azure.cosmos.models.CosmosItemOperation;
+import com.azure.cosmos.models.CosmosQueryRequestOptions;
+import com.azure.cosmos.models.CosmosVectorIndexSpec;
+import com.azure.cosmos.models.CosmosVectorEmbeddingPolicy;
+import com.azure.cosmos.models.PartitionKey;
+import com.azure.cosmos.util.CosmosPagedIterable;
+import dev.langchain4j.data.embedding.Embedding;
+import dev.langchain4j.data.segment.TextSegment;
+import dev.langchain4j.store.embedding.EmbeddingMatch;
+import dev.langchain4j.store.embedding.EmbeddingStore;
+import lombok.Builder;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import static dev.langchain4j.internal.Utils.isNullOrBlank;
+import static dev.langchain4j.internal.Utils.isNullOrEmpty;
+import static dev.langchain4j.internal.Utils.randomUUID;
+import static dev.langchain4j.internal.ValidationUtils.ensureTrue;
+import static dev.langchain4j.store.embedding.azure.cosmos.no.sql.MappingUtils.toNoSqlDbDocument;
+import static java.util.Collections.singletonList;
+
+public class AzureCosmosDbNoSqlEmbeddingStore implements EmbeddingStore<TextSegment> {
+
+    private static final Logger log = LoggerFactory.getLogger(AzureCosmosDbNoSqlEmbeddingStore.class);
+
+    private final CosmosClient cosmosClient;
+    private final String databaseName;
+    private final String containerName;
+    private final CosmosVectorEmbeddingPolicy cosmosVectorEmbeddingPolicy;
+    private final List<CosmosVectorIndexSpec> cosmosVectorIndexes;
+    private final CosmosContainerProperties containerProperties;
+    private final String embeddingKey;
+    private final CosmosDatabase database;
+    private final CosmosContainer container;
+
+    // You can read more about vector search using AzureCosmosDBNoSQL here
+    // https://aka.ms/CosmosVectorSearch
+    @Builder
+    public AzureCosmosDbNoSqlEmbeddingStore(CosmosClient cosmosClient,
+                                            String databaseName,
+                                            String containerName,
+                                            CosmosVectorEmbeddingPolicy cosmosVectorEmbeddingPolicy,
+                                            List<CosmosVectorIndexSpec> cosmosVectorIndexes,
+                                            CosmosContainerProperties containerProperties) {
+        this.cosmosClient = cosmosClient;
+        this.databaseName = databaseName;
+        this.containerName = containerName;
+        this.cosmosVectorEmbeddingPolicy = cosmosVectorEmbeddingPolicy;
+        this.cosmosVectorIndexes = cosmosVectorIndexes;
+        this.containerProperties = containerProperties;
+
+        if (cosmosClient == null) {
+            throw new IllegalArgumentException("cosmosClient cannot be null or empty for Azure CosmosDB NoSql Embedding Store.");
+        }
+
+        if (isNullOrBlank(databaseName) || isNullOrBlank(containerName)) {
+            throw new IllegalArgumentException("databaseName and containerName needs to be provided.");
+        }
+
+        if (cosmosVectorEmbeddingPolicy == null || cosmosVectorEmbeddingPolicy.getVectorEmbeddings() == null ||
+                cosmosVectorEmbeddingPolicy.getVectorEmbeddings().isEmpty()) {
+            throw new IllegalArgumentException("cosmosVectorEmbeddingPolicy cannot be null or empty for Azure CosmosDB NoSql Embedding Store.");
+        }
+
+        if (cosmosVectorIndexes == null || cosmosVectorIndexes.isEmpty()) {
+            throw new IllegalArgumentException("cosmosVectorIndexes cannot be null or empty for Azure CosmosDB NoSql Embedding Store.");
+        }
+
+        this.cosmosClient.createDatabaseIfNotExists(this.databaseName);
+        this.database = this.cosmosClient.getDatabase(this.databaseName);
+
+        containerProperties.setVectorEmbeddingPolicy(this.cosmosVectorEmbeddingPolicy);
+        containerProperties.getIndexingPolicy().setVectorIndexes(this.cosmosVectorIndexes);
+
+        this.database.createContainerIfNotExists(this.containerProperties);
+        this.container = this.database.getContainer(this.containerName);
+
+        this.embeddingKey = this.cosmosVectorEmbeddingPolicy.getVectorEmbeddings().get(0).getPath().substring(1);
+    }
+
+    @Override
+    public String add(Embedding embedding) {
+        String id = randomUUID();
+        add(id, embedding);
+        return id;
+    }
+
+    @Override
+    public void add(String id, Embedding embedding) {
+        addInternal(id, embedding, null);
+    }
+
+    @Override
+    public String add(Embedding embedding, TextSegment textSegment) {
+        String id = randomUUID();
+        addInternal(id, embedding, textSegment);
+        return id;
+    }
+
+    @Override
+    public List<String> addAll(List<Embedding> embeddings) {
+        List<String> ids = embeddings.stream()
+                .map(ignored -> randomUUID())
+                .collect(Collectors.toList());
+        addAllInternal(ids, embeddings, null);
+        return ids;
+    }
+
+    @Override
+    public List<String> addAll(List<Embedding> embeddings, List<TextSegment> embedded) {
+        List<String> ids = embeddings.stream()
+                .map(ignored -> randomUUID())
+                .collect(Collectors.toList());
+        addAllInternal(ids, embeddings, embedded);
+        return ids;
+    }
+
+    @Override
+    public List<EmbeddingMatch<TextSegment>> findRelevant(Embedding referenceEmbedding, int maxResults, double minScore) {
+        String referenceEmbeddingString = referenceEmbedding.vectorAsList().stream()
+                .map(Object::toString)
+                .collect(Collectors.joining(","));
+
+        String query = String.format("SELECT TOP %d c.id, c.%s, c.text, c.metadata, VectorDistance(c.%s,[%s]) AS score FROM c ORDER By " +
+                "VectorDistance(c.%s,[%s])", maxResults, embeddingKey, embeddingKey, referenceEmbeddingString, embeddingKey, referenceEmbeddingString);
+
+        CosmosPagedIterable<AzureCosmosDbNoSqlMatchedDocument> results = this.container.queryItems(query,
+                new CosmosQueryRequestOptions(), AzureCosmosDbNoSqlMatchedDocument.class);
+
+        if (!results.stream().findAny().isPresent()) {
+            return new ArrayList<>();
+        }
+        return results.stream()
+                .map(MappingUtils::toEmbeddingMatch)
+                .collect(Collectors.toList());
+    }
+
+    private void addInternal(String id, Embedding embedding, TextSegment embedded) {
+        addAllInternal(singletonList(id), singletonList(embedding), embedded == null ? null : singletonList(embedded));
+    }
+
+    private void addAllInternal(List<String> ids, List<Embedding> embeddings, List<TextSegment> embedded) {
+        if (isNullOrEmpty(ids) || isNullOrEmpty(embeddings)) {
+            log.info("do not add empty embeddings to Azure CosmosDB NoSQL");
+            return;
+        }
+
+        ensureTrue(ids.size() == embeddings.size(), "ids size is not equal to embeddings size");
+        ensureTrue(embedded == null || embeddings.size() == embedded.size(), "embeddings size is not equal to embedded size");
+
+        List<CosmosItemOperation> operations = new ArrayList<>(ids.size());
+        for (int i = 0; i < ids.size(); i++) {
+            operations.add(CosmosBulkOperations.getCreateItemOperation(
+                    toNoSqlDbDocument(ids.get(i), embeddings.get(i), embedded == null ? null : embedded.get(i)),
+                    new PartitionKey(ids.get(i))));
+        }
+
+        this.container.executeBulkOperations(operations);
+    }
+}
