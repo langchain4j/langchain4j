ff35308a38560786a46b4e0f4c180bed6a06df34
LangChain4j <info@langchain4j.dev>
2024-06-03 17:00:52 +0200

    Revert "Azure OpenAI: test all embedding models in ITs"
    This reverts commit 150eba96

--- langchain4j-azure-open-ai/src/test/java/dev/langchain4j/model/azure/AzureOpenAiEmbeddingModelIT.java
+++ langchain4j-azure-open-ai/src/test/java/dev/langchain4j/model/azure/AzureOpenAiEmbeddingModelIT.java
@@ -6,43 +6,37 @@ import dev.langchain4j.model.embedding.EmbeddingModel;
 import dev.langchain4j.model.openai.OpenAiTokenizer;
 import dev.langchain4j.model.output.Response;
 import dev.langchain4j.model.output.TokenUsage;
-
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.CsvSource;
 import org.junit.jupiter.params.provider.EnumSource;
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.util.ArrayList;
 import java.util.List;
 
-import static dev.langchain4j.model.azure.AzureOpenAiModelName.*;
 import static dev.langchain4j.model.openai.OpenAiEmbeddingModelName.TEXT_EMBEDDING_ADA_002;
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class AzureOpenAiEmbeddingModelIT {
 
-    @ParameterizedTest
-    @CsvSource({
-            TEXT_EMBEDDING_ADA_002 + ",1536",
-            TEXT_EMBEDDING_3_SMALL + ",1536",
-            TEXT_EMBEDDING_3_LARGE + ",3072"
-    })
-    void should_embed_and_return_token_usage(String deploymentName, int expectedDimension) {
+    Logger logger = LoggerFactory.getLogger(AzureOpenAiEmbeddingModelIT.class);
 
-        EmbeddingModel model = AzureOpenAiEmbeddingModel.builder()
-                .endpoint(System.getenv("AZURE_OPENAI_ENDPOINT"))
-                .apiKey(System.getenv("AZURE_OPENAI_KEY"))
-                .deploymentName(deploymentName)
-                .tokenizer(new OpenAiTokenizer(deploymentName))
-                .logRequestsAndResponses(true)
-                .build();
+    EmbeddingModel model = AzureOpenAiEmbeddingModel.builder()
+            .endpoint(System.getenv("AZURE_OPENAI_ENDPOINT"))
+            .apiKey(System.getenv("AZURE_OPENAI_KEY"))
+            .deploymentName("text-embedding-ada-002")
+            .tokenizer(new OpenAiTokenizer(TEXT_EMBEDDING_ADA_002))
+            .logRequestsAndResponses(true)
+            .build();
+
+    @Test
+    void should_embed_and_return_token_usage() {
 
         Response<Embedding> response = model.embed("hello world");
+        logger.info(response.toString());
 
-        assertThat(response.content().vector()).hasSize(expectedDimension);
+        assertThat(response.content().vector()).hasSize(1536);
 
         TokenUsage tokenUsage = response.tokenUsage();
         assertThat(tokenUsage.inputTokenCount()).isEqualTo(2);
@@ -52,13 +46,8 @@ public class AzureOpenAiEmbeddingModelIT {
         assertThat(response.finishReason()).isNull();
     }
 
-    @ParameterizedTest
-    @CsvSource({
-            TEXT_EMBEDDING_ADA_002 + ",1536",
-            TEXT_EMBEDDING_3_SMALL + ",1536",
-            TEXT_EMBEDDING_3_LARGE + ",3072"
-    })
-    void should_embed_in_batches(String deploymentName, int expectedDimension) {
+    @Test
+    void should_embed_in_batches() {
 
         int batchSize = 16;
         int numberOfSegments = batchSize + 1;
@@ -68,18 +57,11 @@ public class AzureOpenAiEmbeddingModelIT {
             segments.add(TextSegment.from("text " + i));
         }
 
-        EmbeddingModel model = AzureOpenAiEmbeddingModel.builder()
-                .endpoint(System.getenv("AZURE_OPENAI_ENDPOINT"))
-                .apiKey(System.getenv("AZURE_OPENAI_KEY"))
-                .deploymentName(deploymentName)
-                .tokenizer(new OpenAiTokenizer(deploymentName))
-                .logRequestsAndResponses(true)
-                .build();
-
         Response<List<Embedding>> response = model.embedAll(segments);
+        System.out.println(response);
 
         assertThat(response.content()).hasSize(numberOfSegments);
-        assertThat(response.content().get(0).dimension()).isEqualTo(expectedDimension);
+        assertThat(response.content().get(0).dimension()).isEqualTo(1536);
 
         TokenUsage tokenUsage = response.tokenUsage();
         assertThat(tokenUsage.inputTokenCount()).isEqualTo(numberOfSegments * 3);
