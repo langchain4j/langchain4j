35e76cc7991649be38afa9fb48919f6b3b18bcd6
LangChain4j <info@langchain4j.dev>
2024-06-13 16:29:17 +0200

    Fix #1277 Trim LLM response string before parsing into Java types

--- langchain4j/src/main/java/dev/langchain4j/model/output/LocalDateTimeOutputParser.java
+++ langchain4j/src/main/java/dev/langchain4j/model/output/LocalDateTimeOutputParser.java
@@ -8,7 +8,7 @@ public class LocalDateTimeOutputParser implements OutputParser<LocalDateTime> {
 
     @Override
     public LocalDateTime parse(String string) {
-        return LocalDateTime.parse(string, ISO_LOCAL_DATE_TIME);
+        return LocalDateTime.parse(string.trim(), ISO_LOCAL_DATE_TIME);
     }
 
     @Override
