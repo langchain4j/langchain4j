b9ccc53bb23a53f44b07d81a8736eab35e4a20d3
François Barbe <f.barbe@lectra.com>
2024-06-14 19:55:11 +0200

    fix(TokenUsage): Token can't be null in AI Response
    Signed-off-by: François Barbe <f.barbe@lectra.com>
    
    #1294

--- langchain4j-azure-open-ai/src/main/java/dev/langchain4j/model/azure/AzureOpenAiLanguageModel.java
+++ langchain4j-azure-open-ai/src/main/java/dev/langchain4j/model/azure/AzureOpenAiLanguageModel.java
@@ -13,6 +13,7 @@ import dev.langchain4j.model.language.LanguageModel;
 import dev.langchain4j.model.language.TokenCountEstimator;
 import dev.langchain4j.model.output.FinishReason;
 import dev.langchain4j.model.output.Response;
+import dev.langchain4j.model.output.TokenUsage;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -232,7 +233,7 @@ public class AzureOpenAiLanguageModel implements LanguageModel, TokenCountEstima
             FinishReason exceptionFinishReason = contentFilterManagement(httpResponseException, "content_filter");
             return Response.from(
                     httpResponseException.getMessage(),
-                    null,
+                    new TokenUsage(),
                     exceptionFinishReason
             );
         }
