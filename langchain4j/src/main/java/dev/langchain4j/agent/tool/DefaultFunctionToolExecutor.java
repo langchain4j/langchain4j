fae22cb456f582573ae2d7e2895b437b6bdb9166
LangChain4j <info@langchain4j.dev>
2024-06-19 15:08:26 +0200

    WIP: Register tools programmatically

---
+++ langchain4j/src/main/java/dev/langchain4j/agent/tool/DefaultFunctionToolExecutor.java
@@ -0,0 +1,11 @@
+package dev.langchain4j.agent.tool;
+
+public class DefaultFunctionToolExecutor implements ToolExecutor {
+
+
+
+    @Override
+    public String execute(ToolExecutionRequest toolExecutionRequest, Object memoryId) {
+        return null;
+    }
+}
