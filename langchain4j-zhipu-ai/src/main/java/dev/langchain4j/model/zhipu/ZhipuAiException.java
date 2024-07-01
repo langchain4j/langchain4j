fc78f3fd77ace18bc94a4699d39312fbc2172292
二毛 <1402564807@qq.com>
2024-07-01 17:05:06 +0800

    chroe: add ChatModelListener support for ZhipuAi chat model and streaming chat model (#1378)
    ## Issue
    #199 
    
    ## Change
    
    add ChatModelListener support for ZhipuAi:
    - chat model
    - streaming chat model
    
    ## General checklist
    <!-- Please double-check the following points and mark them like this:
    [X] -->
    - [x] There are no breaking changes
    - [x] I have added unit and integration tests for my change
    - [x] I have manually run all the unit and integration tests in the
    module I have added/changed, and they are all green
    - [x] I have manually run all the unit and integration tests in the
    [core](https://github.com/langchain4j/langchain4j/tree/main/langchain4j-core)
    and
    [main](https://github.com/langchain4j/langchain4j/tree/main/langchain4j)
    modules, and they are all green
    <!-- Before adding documentation and example(s) (below), please wait
    until the PR is reviewed and approved. -->
    - [ ] I have added/updated the
    [documentation](https://github.com/langchain4j/langchain4j/tree/main/docs/docs)
    - [ ] I have added an example in the [examples
    repo](https://github.com/langchain4j/langchain4j-examples) (only for
    "big" features)
    - [ ] I have added/updated [Spring Boot
    starter(s)](https://github.com/langchain4j/langchain4j-spring) (if
    applicable)

---
+++ langchain4j-zhipu-ai/src/main/java/dev/langchain4j/model/zhipu/ZhipuAiException.java
@@ -0,0 +1,41 @@
+package dev.langchain4j.model.zhipu;
+
+import dev.langchain4j.model.zhipu.shared.ErrorResponse;
+import lombok.Getter;
+import okhttp3.ResponseBody;
+
+import java.io.IOException;
+
+@Getter
+public class ZhipuAiException extends RuntimeException {
+
+    /**
+     * error code see <a href="https://open.bigmodel.cn/dev/api#error-code-v3">error codes document</a>
+     */
+    private String code;
+    private String message;
+
+    public ZhipuAiException(String code, String message) {
+        super(message);
+        this.code = code;
+    }
+
+    public ZhipuAiException(okhttp3.Response response) {
+        try {
+            ResponseBody body = response.body();
+            if (body != null) {
+                ErrorResponse errorResponse = Json.fromJson(body.string(), ErrorResponse.class);
+                this.message = errorResponse.getError().get("message");
+                this.code = errorResponse.getError().get("code");
+            }
+        } catch (IOException ignored) {
+            this.message = "null";
+            this.code = String.valueOf(response.code());
+        }
+    }
+
+    public ZhipuAiException(String message) {
+        super(message);
+        this.message = message;
+    }
+}
