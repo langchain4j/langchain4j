e8f3599636535eef35692aafc335d3ce53bf7fc4
primoz <primoz.kokol@gmail.com>
2024-07-02 17:40:35 +0200

    https://github.com/langchain4j/langchain4j/issues/725
    PR 3 / 4: Makes it possible to use @Description on Enum values
    
    Side notes:
    1. This PR is dependent on PR 1 / 4.
    2. List<Enum> is still not supported - this will be tackled in PR 4 / 4
    
    Made it possible to annotate Enum values with @Description.
    
    This is how prompt looks like for enums without descriptions:
    ```
    Analyze sentiment of:
    |||This LaptopPro X15 is wicked fast and that 4K screen is a dream.|||
    You must answer strictly with one of these enums:
    POSITIVE
    NEUTRAL
    NEGATIVE
    ```
    
    This is how prompt looks like for enums with descriptions:
    ```
    Analyze weather forecast for:
    |||It will be cloudy and mostly rainy. No more rain early in the day but the sky remains overcast. Afternoon it is mostly cloudy. The sun will not be visible. The forecast has a moderate, 40% chance of Precipitation. Temperatures peaking at 17 °C.|||
    You must answer strictly with one of these enums:
    SUNNY - A clear day with bright sunlight and few or no clouds
    CLOUDY - The sky is covered with clouds with no rain, often creating a gray and overcast appearance
    RAINY - Precipitation in the form of rain, with cloudy skies and wet conditions
    SNOWY - Snowfall occurs, covering the ground in white and creating cold, wintry conditions
    ```
    
    Covered everything with unit and integrationt tests.
    
    Tested mainly with ˙gpt-3.5-turbo˙.
    
    - [X] There are no breaking changes
    - [X] I have added unit and integration tests for my change
    - [X] I have manually run all the unit and integration tests in the module I have added/changed, and they are all green
    - [X] I have manually run all the unit and integration tests in the [core](https://github.com/langchain4j/langchain4j/tree/main/langchain4j-core) and [main](https://github.com/langchain4j/langchain4j/tree/main/langchain4j) modules, and they are all green

--- langchain4j/src/main/java/dev/langchain4j/model/output/EnumOutputParser.java
+++ langchain4j/src/main/java/dev/langchain4j/model/output/EnumOutputParser.java
@@ -1,9 +1,15 @@
 package dev.langchain4j.model.output;
 
-import java.util.Arrays;
+import dev.langchain4j.model.output.structured.Description;
+
+import java.lang.reflect.Field;
+import java.util.Locale;
+import java.util.Optional;
 
 @SuppressWarnings("rawtypes")
 public class EnumOutputParser implements OutputParser<Enum> {
+
+    private static final boolean PRETTY = false;
 
     private final Class<? extends Enum> enumClass;
 
@@ -24,6 +30,48 @@ public class EnumOutputParser implements OutputParser<Enum> {
 
     @Override
     public String formatInstructions() {
-        return "one of " + Arrays.toString(enumClass.getEnumConstants());
+        try {
+            Enum[] enumConstants = enumClass.getEnumConstants();
+
+            if (enumConstants.length == 0) {
+                throw new IllegalArgumentException("Should be at least one enum constant defined.");
+            }
+
+            StringBuilder instruction = new StringBuilder();
+
+            // 'enums' keyword will hopefully make it clearer that
+            // no description should be included (if present)
+            instruction.append("one of these enums:");
+
+            for (Enum enumConstant : enumConstants) {
+                instruction.append("\n").append(enumConstant.name().toUpperCase(Locale.ROOT));
+                Optional<String> optionalEnumDescription = getEnumDescription(enumConstant);
+                optionalEnumDescription.ifPresent(description -> instruction.append(" - ").append(description));
+            }
+
+            return instruction.toString();
+        } catch (NoSuchFieldException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+
+    /**
+     * Returns Enum description (if present)
+     *
+     * @param enumConstant for which description should be returned
+     * @return description of the provided enum
+     */
+    private Optional<String> getEnumDescription(Enum enumConstant) throws NoSuchFieldException {
+        Field field = enumClass.getDeclaredField(enumConstant.name());
+
+        if (field.isAnnotationPresent(Description.class)) {
+            Description annotation = field.getAnnotation(Description.class);
+            if (annotation != null) {
+                return Optional.of(String.join(" ", annotation.value()));
+            }
+        }
+
+        return Optional.empty();
     }
 }
