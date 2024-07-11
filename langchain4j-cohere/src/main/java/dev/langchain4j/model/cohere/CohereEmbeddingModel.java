720a4c942c019689f9928b816304a775951b2922
Jeffin joffy <114131523+Apetun@users.noreply.github.com>
2024-07-11 18:54:20 +0530

    [FEATURE] CohereEmbeddingModel: implement batching

--- langchain4j-cohere/src/main/java/dev/langchain4j/model/cohere/CohereEmbeddingModel.java
+++ langchain4j-cohere/src/main/java/dev/langchain4j/model/cohere/CohereEmbeddingModel.java
@@ -9,6 +9,7 @@ import dev.langchain4j.model.output.TokenUsage;
 import lombok.Builder;
 
 import java.time.Duration;
+import java.util.ArrayList;
 import java.util.List;
 
 import static dev.langchain4j.internal.Utils.getOrDefault;
@@ -24,10 +25,12 @@ import static java.util.stream.Collectors.toList;
 public class CohereEmbeddingModel extends DimensionAwareEmbeddingModel {
 
     private static final String DEFAULT_BASE_URL = "https://api.cohere.ai/v1/";
+    private static final int DEFAULT_MAX_SEGMENTS_PER_BATCH = 96;
 
     private final CohereClient client;
     private final String modelName;
     private final String inputType;
+    private final int maxSegmentsPerBatch;
 
     @Builder
     public CohereEmbeddingModel(String baseUrl,
@@ -36,7 +39,8 @@ public class CohereEmbeddingModel extends DimensionAwareEmbeddingModel {
                                 String inputType,
                                 Duration timeout,
                                 Boolean logRequests,
-                                Boolean logResponses) {
+                                Boolean logResponses,
+                                Integer maxSegmentsPerBatch) {
         this.client = CohereClient.builder()
                 .baseUrl(getOrDefault(baseUrl, DEFAULT_BASE_URL))
                 .apiKey(ensureNotBlank(apiKey, "apiKey"))
@@ -46,6 +50,7 @@ public class CohereEmbeddingModel extends DimensionAwareEmbeddingModel {
                 .build();
         this.modelName = modelName;
         this.inputType = inputType;
+        this.maxSegmentsPerBatch = getOrDefault(maxSegmentsPerBatch, DEFAULT_MAX_SEGMENTS_PER_BATCH);
     }
 
     public static CohereEmbeddingModel withApiKey(String apiKey) {
@@ -59,15 +64,37 @@ public class CohereEmbeddingModel extends DimensionAwareEmbeddingModel {
                 .map(TextSegment::text)
                 .collect(toList());
 
-        EmbedRequest request = EmbedRequest.builder()
-                .texts(texts)
-                .inputType(inputType)
-                .model(modelName)
-                .build();
+        return embedTexts(texts);
+        
+    }
 
-        EmbedResponse response = this.client.embed(request);
 
-        return Response.from(getEmbeddings(response), getTokenUsage(response));
+    private Response<List<Embedding>> embedTexts(List<String> texts) {
+
+        List<Embedding> embeddings = new ArrayList<>();
+        Integer totalTokenUsage = 0;
+
+        for (int i = 0; i < texts.size(); i += maxSegmentsPerBatch) {
+
+            List<String> batch = texts.subList(i, Math.min(i + maxSegmentsPerBatch, texts.size()));
+
+            EmbedRequest request = EmbedRequest.builder()
+                    .texts(batch)
+                    .inputType(inputType)
+                    .model(modelName)
+                    .build();
+
+            EmbedResponse response = this.client.embed(request);
+            
+            embeddings.addAll(getEmbeddings(response));
+            totalTokenUsage += getTokenUsage(response);
+        }
+
+        return Response.from(
+                embeddings,
+                new TokenUsage(totalTokenUsage,0)
+        );
+
     }
 
     private static List<Embedding> getEmbeddings(EmbedResponse response) {
@@ -80,8 +107,8 @@ public class CohereEmbeddingModel extends DimensionAwareEmbeddingModel {
         if (response.getMeta() != null
                 && response.getMeta().getBilledUnits() != null
                 && response.getMeta().getBilledUnits().getInputTokens() != null) {
-            return new TokenUsage(response.getMeta().getBilledUnits().getInputTokens(), 0);
+            return response.getMeta().getBilledUnits().getInputTokens();
         }
-        return null;
+        return 0;
     }
 }
