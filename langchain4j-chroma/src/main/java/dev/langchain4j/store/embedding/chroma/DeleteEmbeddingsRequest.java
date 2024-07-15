d718a6c728c50a8e75b009c7cca7637c7bab4e08
Alexey Titov <33568148+Heezer@users.noreply.github.com>
2024-07-15 14:49:30 +0200

    [Feature] Implement embedding removal methods for Chroma embedding (#1457)
    ## Issue
    Closes #1422
    
    
    ## Change
    * Implemented #1422
    * Added logging support of request & response
    
    
    ## General checklist
    <!-- Please double-check the following points and mark them like this:
    [X] -->
    - [X] There are no breaking changes
    - [X] I have added unit and integration tests for my change
    - [X] I have manually run all the unit and integration tests in the
    module I have added/changed, and they are all green
    - [X] I have manually run all the unit and integration tests in the
    [core](https://github.com/langchain4j/langchain4j/tree/main/langchain4j-core)
    and
    [main](https://github.com/langchain4j/langchain4j/tree/main/langchain4j)
    modules, and they are all green
    <!-- Before adding documentation and example(s) (below), please wait
    until the PR is reviewed and approved. -->
    - [ ] I have added/updated the
    [documentation](https://github.com/langchain4j/langchain4j/tree/main/docs/docs)
    - [X] I have added an example in the [examples
    repo](https://github.com/langchain4j/langchain4j-examples) (only for
    "big" features)
    - [ ] I have added/updated [Spring Boot
    starter(s)](https://github.com/langchain4j/langchain4j-spring) (if
    applicable)
    
    ## Checklist for adding new model integration
    <!-- Please double-check the following points and mark them like this:
    [X] -->
    - [ ] I have added my new module in the
    [BOM](https://github.com/langchain4j/langchain4j/blob/main/langchain4j-bom/pom.xml)
    
    
    ## Checklist for adding new embedding store integration
    <!-- Please double-check the following points and mark them like this:
    [X] -->
    - [X] I have added a `{NameOfIntegration}EmbeddingStoreIT` that extends
    from either `EmbeddingStoreIT` or `EmbeddingStoreWithFilteringIT`
    - [ ] I have added my new module in the
    [BOM](https://github.com/langchain4j/langchain4j/blob/main/langchain4j-bom/pom.xml)
    
    
    ## Checklist for changing existing embedding store integration
    <!-- Please double-check the following points and mark them like this:
    [X] -->
    - [X] I have manually verified that the
    `{NameOfIntegration}EmbeddingStore` works correctly with the data
    persisted using the latest released version of LangChain4j

---
+++ langchain4j-chroma/src/main/java/dev/langchain4j/store/embedding/chroma/DeleteEmbeddingsRequest.java
@@ -0,0 +1,47 @@
+package dev.langchain4j.store.embedding.chroma;
+
+import java.util.List;
+import java.util.Map;
+
+public class DeleteEmbeddingsRequest {
+
+    private final List<String> ids;
+    private final Map<String, Object> where;
+
+    private DeleteEmbeddingsRequest(Builder builder) {
+        this.ids = builder.ids;
+        this.where = builder.where;
+    }
+
+    public static Builder builder() {
+        return new Builder();
+    }
+
+    public List<String> getIds() {
+        return ids;
+    }
+
+    public Map<String, Object> getWhere() {
+        return where;
+    }
+
+    public static class Builder {
+
+        private List<String> ids;
+        private Map<String, Object> where;
+
+        public Builder ids(List<String> ids) {
+            this.ids = ids;
+            return this;
+        }
+
+        public Builder where(Map<String, Object> where) {
+            this.where = where;
+            return this;
+        }
+
+        public DeleteEmbeddingsRequest build() {
+            return new DeleteEmbeddingsRequest(this);
+        }
+    }
+}
