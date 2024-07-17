ae5ee21c8f442633dcb7f3bf3c4f2af6b83931ee
Felipe Zambrin <zambrinf@gmail.com>
2024-07-16 21:08:32 -0300

    try with resources in tests

--- document-parsers/langchain4j-document-parser-apache-pdfbox/src/test/java/dev/langchain4j/data/document/parser/apache/pdfbox/ApachePdfBoxDocumentParserTest.java
+++ document-parsers/langchain4j-document-parser-apache-pdfbox/src/test/java/dev/langchain4j/data/document/parser/apache/pdfbox/ApachePdfBoxDocumentParserTest.java
@@ -1,10 +1,11 @@
 package dev.langchain4j.data.document.parser.apache.pdfbox;
 
-import dev.langchain4j.data.document.Document;
 import dev.langchain4j.data.document.BlankDocumentException;
+import dev.langchain4j.data.document.Document;
 import dev.langchain4j.data.document.DocumentParser;
 import org.junit.jupiter.api.Test;
 
+import java.io.IOException;
 import java.io.InputStream;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -14,27 +15,29 @@ class ApachePdfBoxDocumentParserTest {
 
     @Test
     void should_parse_pdf_file() {
+        try (InputStream inputStream = getClass().getClassLoader().getResourceAsStream("test-file.pdf")) {
+            DocumentParser parser = new ApachePdfBoxDocumentParser();
+            Document document = parser.parse(inputStream);
 
-        DocumentParser parser = new ApachePdfBoxDocumentParser();
-        InputStream inputStream = getClass().getClassLoader().getResourceAsStream("test-file.pdf");
-
-        Document document = parser.parse(inputStream);
-
-        assertThat(document.text()).isEqualToIgnoringWhitespace("test content");
-        assertThat(document.metadata().toMap())
-                .containsEntry("Author", "ljuba")
-                .containsEntry("Creator", "WPS Writer")
-                .containsEntry("CreationDate", "D:20230608171011+15'10'")
-                .containsEntry("SourceModified", "D:20230608171011+15'10'");
+            assertThat(document.text()).isEqualToIgnoringWhitespace("test content");
+            assertThat(document.metadata().toMap())
+                    .containsEntry("Author", "ljuba")
+                    .containsEntry("Creator", "WPS Writer")
+                    .containsEntry("CreationDate", "D:20230608171011+15'10'")
+                    .containsEntry("SourceModified", "D:20230608171011+15'10'");
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 
     @Test
     void should_throw_BlankDocumentException() {
-
-        DocumentParser parser = new ApachePdfBoxDocumentParser();
-        InputStream inputStream = getClass().getClassLoader().getResourceAsStream("blank-file.pdf");
-
-        assertThatThrownBy(() -> parser.parse(inputStream))
-                .isExactlyInstanceOf(BlankDocumentException.class);
+        try (InputStream inputStream = getClass().getClassLoader().getResourceAsStream("blank-file.pdf")) {
+            DocumentParser parser = new ApachePdfBoxDocumentParser();
+            assertThatThrownBy(() -> parser.parse(inputStream))
+                    .isExactlyInstanceOf(BlankDocumentException.class);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
