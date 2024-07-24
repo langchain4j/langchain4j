91a727d9eab9b6a624c31fe3af1508a00207d934
SridharNagaraj <snagaraj@gainsight.com>
2024-07-24 11:58:32 +0530

    Support schema for List, Set, Map and Array of objects

--- langchain4j/src/test/java/dev/langchain4j/agent/tool/DefaultToolExecutorTest.java
+++ langchain4j/src/test/java/dev/langchain4j/agent/tool/DefaultToolExecutorTest.java
@@ -308,7 +308,11 @@ class DefaultToolExecutorTest implements WithAssertions {
             assert arg != null;
             assert arg.length == 2;
             assert arg[0].getClass() == Person.class;
+            assert arg[0].name.equals("Klaus");
+            assert arg[0].age == 42;
             assert arg[1].getClass() == Person.class;
+            assert arg[1].name.equals("Peter");
+            assert arg[1].age == 43;
             return arg;
         }
 
@@ -317,6 +321,10 @@ class DefaultToolExecutorTest implements WithAssertions {
             assert personList != null;
             assert personList.size() == 2;
             personList.forEach(person -> { assert person.getClass() == Person.class;} );
+            assert personList.get(0).name.equals("Klaus");
+            assert personList.get(0).age == 42;
+            assert personList.get(1).name.equals("Peter");
+            assert personList.get(1).age == 43;
             return personList;
         }
 
@@ -325,6 +333,8 @@ class DefaultToolExecutorTest implements WithAssertions {
             assert personSet != null;
             assert personSet.size() == 2;
             personSet.forEach(person -> { assert person.getClass() == Person.class;});
+            assert personSet.stream().anyMatch(person -> person.name.equals("Klaus") && person.age == 42);
+            assert personSet.stream().anyMatch(person -> person.name.equals("Peter") && person.age == 43);
             return personSet;
         }
 
@@ -336,6 +346,10 @@ class DefaultToolExecutorTest implements WithAssertions {
                 assert id.getClass() == String.class;
                 assert person.getClass() == Person.class;
             });
+            assert idPersonMap.get("p1").name.equals("Klaus");
+            assert idPersonMap.get("p1").age == 42;
+            assert idPersonMap.get("p2").name.equals("Peter");
+            assert idPersonMap.get("p2").age == 43;
             return idPersonMap;
         }
     }
