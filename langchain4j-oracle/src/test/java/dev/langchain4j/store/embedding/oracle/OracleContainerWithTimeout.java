be3d19574f8dfc6a97d64472ed552e3e7e773700
Fernanda Meheust <fernanda.meheust@oracle.com>
2024-08-21 08:39:16 +0200

    Set waitStrategy startup timeout when startup timeout is changed.

--- langchain4j-oracle/src/test/java/dev/langchain4j/store/embedding/oracle/OracleContainerWithTimeout.java
+++ langchain4j-oracle/src/test/java/dev/langchain4j/store/embedding/oracle/OracleContainerWithTimeout.java
@@ -8,33 +8,17 @@ import java.time.Duration;
 
 public class OracleContainerWithTimeout extends OracleContainer {
 
-  private int startupTimeoutSeconds = 60;
-
-  private int connectTimeoutSeconds = 60;
-
   public OracleContainerWithTimeout(String dockerImageName) {
     this(DockerImageName.parse(dockerImageName));
   }
 
   public OracleContainerWithTimeout(DockerImageName dockerImageName) {
     super(dockerImageName);
-    waitingFor(
-        Wait
-            .forLogMessage(".*DATABASE IS READY TO USE!.*\\s", 1)
-            .withStartupTimeout(Duration.ofSeconds(startupTimeoutSeconds))
-    );
-    withConnectTimeoutSeconds(connectTimeoutSeconds);
   }
 
   @Override
   public OracleContainer withStartupTimeoutSeconds(int startupTimeoutSeconds) {
-    this.startupTimeoutSeconds = startupTimeoutSeconds;
+    this.waitStrategy.withStartupTimeout(Duration.ofSeconds(startupTimeoutSeconds));
     return super.withStartupTimeoutSeconds(startupTimeoutSeconds);
-  }
-
-  @Override
-  public OracleContainer withConnectTimeoutSeconds(int connectTimeoutSeconds) {
-    this.connectTimeoutSeconds = connectTimeoutSeconds;
-    return super.withConnectTimeoutSeconds(connectTimeoutSeconds);
   }
 }
