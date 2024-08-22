83951123a6d10f7eb4a3b0ada54bfab2a9a5500e
Fernanda Meheust <fernanda.meheust@oracle.com>
2024-08-22 11:00:54 +0200

    Removed OracleContainerWithTimeout and added missing import (#28)
    Merged directly without review because the missing import causes test class not to build.

--- langchain4j-oracle/src/test/java/dev/langchain4j/store/embedding/oracle/CommonTestOperations.java
+++ langchain4j-oracle/src/test/java/dev/langchain4j/store/embedding/oracle/CommonTestOperations.java
@@ -18,6 +18,7 @@ import java.sql.Connection;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.time.Duration;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Locale;
