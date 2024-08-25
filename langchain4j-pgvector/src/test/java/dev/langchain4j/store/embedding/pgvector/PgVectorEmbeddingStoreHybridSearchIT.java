07a39f30de529477120d5573644610083f7376a3
hrhrng <hrhrng@foxmail.com>
2024-08-25 19:27:30 +0800

    PgVectorEmbeddingStore supports mixed search

---
+++ langchain4j-pgvector/src/test/java/dev/langchain4j/store/embedding/pgvector/PgVectorEmbeddingStoreHybridSearchIT.java
@@ -0,0 +1,50 @@
+package dev.langchain4j.store.embedding.pgvector;
+
+import dev.langchain4j.data.segment.TextSegment;
+import dev.langchain4j.model.embedding.EmbeddingModel;
+import dev.langchain4j.model.embedding.onnx.allminilml6v2q.AllMiniLmL6V2QuantizedEmbeddingModel;
+import dev.langchain4j.store.embedding.EmbeddingStore;
+import dev.langchain4j.store.embedding.EmbeddingStoreWithFilteringIT;
+import org.junit.jupiter.api.BeforeEach;
+import org.testcontainers.containers.PostgreSQLContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
+
+/**
+ * @author xiaoyang
+ **/
+@Testcontainers
+public class PgVectorEmbeddingStoreHybridSearchIT extends EmbeddingStoreWithFilteringIT {
+
+    @Container
+    static PostgreSQLContainer<?> pgVector = new PostgreSQLContainer<>("pgvector/pgvector:pg15");
+
+    EmbeddingStore<TextSegment> embeddingStore;
+
+    EmbeddingModel embeddingModel = new AllMiniLmL6V2QuantizedEmbeddingModel();
+
+    @BeforeEach
+    void beforeEach() {
+        embeddingStore = PgVectorEmbeddingStore.builder()
+                .host(pgVector.getHost())
+                .port(pgVector.getFirstMappedPort())
+                .user("test")
+                .password("test")
+                .database("test")
+                .table("test")
+                .useFullTextIndex(true)
+                .dimension(384)
+                .dropTableFirst(true)
+                .build();
+    }
+
+    @Override
+    protected EmbeddingStore<TextSegment> embeddingStore() {
+        return embeddingStore;
+    }
+
+    @Override
+    protected EmbeddingModel embeddingModel() {
+        return embeddingModel;
+    }
+}
