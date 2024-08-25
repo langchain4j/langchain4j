b7d839ed90cc41a7b29ee2477b3a45d92ac4ab78
hrhrng <hrhrng@foxmail.com>
2024-08-26 00:55:45 +0800

    PgVectorEmbeddingStore supports mixed search

--- langchain4j-pgvector/src/test/java/dev/langchain4j/store/embedding/pgvector/PgVectorEmbeddingStoreHybridSearchIT.java
+++ langchain4j-pgvector/src/test/java/dev/langchain4j/store/embedding/pgvector/PgVectorEmbeddingStoreHybridSearchIT.java
@@ -16,9 +16,7 @@ import java.util.List;
 import static java.util.Arrays.asList;
 import static org.assertj.core.api.Assertions.assertThat;
 
-/**
- * @author xiaoyang
- **/
+
 @Testcontainers
 public class PgVectorEmbeddingStoreHybridSearchIT extends PgVectorEmbeddingStoreIT {
 
