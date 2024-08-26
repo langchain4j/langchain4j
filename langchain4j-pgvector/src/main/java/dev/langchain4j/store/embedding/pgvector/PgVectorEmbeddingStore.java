e38d8ac40a722860dc0c253638731d1f9a782ebd
hrhrng <hrhrng@foxmail.com>
2024-08-26 17:22:52 +0800

    PgVectorEmbeddingStore supports mixed search

--- langchain4j-pgvector/src/main/java/dev/langchain4j/store/embedding/pgvector/PgVectorEmbeddingStore.java
+++ langchain4j-pgvector/src/main/java/dev/langchain4j/store/embedding/pgvector/PgVectorEmbeddingStore.java
@@ -17,7 +17,6 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import javax.sql.DataSource;
-import java.math.BigDecimal;
 import java.sql.*;
 import java.util.*;
 import java.util.function.Consumer;
