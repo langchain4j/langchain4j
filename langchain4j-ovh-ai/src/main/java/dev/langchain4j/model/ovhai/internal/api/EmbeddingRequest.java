03cd29a9330dffbcf217a7c3abb97dc13a30f9f9
Stéphane Philippart <wildagsx@gmail.com>
2024-08-27 10:06:53 +0200

    feat: ✨ Enable batch embedding server side (#1643)
    ## Change
    As agreed I update the code to use the server side batch processing for
    OVHcloud AI embedding models.
    
    
    ## General checklist
    <!-- Please double-check the following points and mark them like this:
    [X] -->
    - [X] There are no breaking changes
    - [X] I have added unit and integration tests for my change
    - [X] I have manually run all the unit and integration tests in the
    module I have added/changed, and they are all green
    - [X] I have manually run all the unit and integration tests in the
    [core](https://github.com/langchain4j/langchain4j/tree/main/langchain4j-core)
    and
    [main](https://github.com/langchain4j/langchain4j/tree/main/langchain4j)
    modules, and they are all green
    <!-- Before adding documentation and example(s) (below), please wait
    until the PR is reviewed and approved. -->
    - [X] I have added/updated the
    [documentation](https://github.com/langchain4j/langchain4j/tree/main/docs/docs)
    - [X] I have added an example in the [examples
    repo](https://github.com/langchain4j/langchain4j-examples) (only for
    "big" features)
    - [X] I have added/updated [Spring Boot
    starter(s)](https://github.com/langchain4j/langchain4j-spring) (if
    applicable)

--- langchain4j-ovh-ai/src/main/java/dev/langchain4j/model/ovhai/internal/api/EmbeddingRequest.java
+++ langchain4j-ovh-ai/src/main/java/dev/langchain4j/model/ovhai/internal/api/EmbeddingRequest.java
@@ -1,6 +1,8 @@
 package dev.langchain4j.model.ovhai.internal.api;
 
 import java.util.List;
+
+import com.fasterxml.jackson.annotation.*;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -11,6 +13,6 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @Builder
 public class EmbeddingRequest {
-
+  @JsonValue
   private List<String> input;
 }
