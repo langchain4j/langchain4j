16331694ba8179bb9062b3e02a30acd230de3dd2
LangChain4j <info@langchain4j.dev>
2024-09-02 16:58:36 +0200

    OpenAI: migrate from Gson to Jackson

--- langchain4j-ollama/src/test/java/dev/langchain4j/model/ollama/OllamaImage.java
+++ langchain4j-ollama/src/test/java/dev/langchain4j/model/ollama/OllamaImage.java
@@ -13,7 +13,7 @@ public class OllamaImage {
 
     static final String BAKLLAVA_MODEL = "bakllava";
 
-    static final String TINY_DOLPHIN_MODEL = "tinydolphin";
+    static final String TINY_DOLPHIN_MODEL = "llama3.1:latest";
     static final String TOOL_MODEL = "mistral";
 
     static final String ALL_MINILM_MODEL = "all-minilm";
