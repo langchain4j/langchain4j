68496ab0cb1b11fa590a53c31b21c259acf4ad3f
jiangsier-xyz <126842484+jiangsier-xyz@users.noreply.github.com>
2024-09-03 11:42:46 +0800

    DashScope: Support ChatModelListener

--- langchain4j-dashscope/src/main/java/dev/langchain4j/model/dashscope/QwenChatModel.java
+++ langchain4j-dashscope/src/main/java/dev/langchain4j/model/dashscope/QwenChatModel.java
@@ -15,22 +15,29 @@ import dev.langchain4j.data.message.AiMessage;
 import dev.langchain4j.data.message.ChatMessage;
 import dev.langchain4j.internal.Utils;
 import dev.langchain4j.model.chat.ChatLanguageModel;
+import dev.langchain4j.model.chat.listener.*;
 import dev.langchain4j.model.dashscope.spi.QwenChatModelBuilderFactory;
 import dev.langchain4j.model.output.Response;
 import lombok.Builder;
+import lombok.extern.slf4j.Slf4j;
 
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
 
 import static com.alibaba.dashscope.aigc.conversation.ConversationParam.ResultFormat.MESSAGE;
 import static dev.langchain4j.internal.Utils.isNullOrEmpty;
 import static dev.langchain4j.model.dashscope.QwenHelper.*;
 import static dev.langchain4j.spi.ServiceHelper.loadFactories;
+import static java.util.Collections.emptyList;
 
 /**
  * Represents a Qwen language model with a chat completion interface.
  * More details are available <a href="https://help.aliyun.com/zh/dashscope/developer-reference/api-details">here</a>.
  */
+@Slf4j
 public class QwenChatModel implements ChatLanguageModel {
     private final String apiKey;
     private final String modelName;
@@ -45,6 +52,7 @@ public class QwenChatModel implements ChatLanguageModel {
     private final Generation generation;
     private final MultiModalConversation conv;
     private final boolean isMultimodalModel;
+    private final List<ChatModelListener> listeners;
 
     @Builder
     protected QwenChatModel(String baseUrl,
@@ -57,7 +65,8 @@ public class QwenChatModel implements ChatLanguageModel {
                             Float repetitionPenalty,
                             Float temperature,
                             List<String> stops,
-                            Integer maxTokens) {
+                            Integer maxTokens,
+                            List<ChatModelListener> listeners) {
         if (Utils.isNullOrBlank(apiKey)) {
             throw new IllegalArgumentException("DashScope api key must be defined. It can be generated here: https://dashscope.console.aliyun.com/apiKey");
         }
@@ -71,6 +80,7 @@ public class QwenChatModel implements ChatLanguageModel {
         this.temperature = temperature;
         this.stops = stops;
         this.maxTokens = maxTokens;
+        this.listeners = listeners == null ? emptyList() : new ArrayList<>(listeners);
         this.isMultimodalModel = QwenHelper.isMultimodalModel(modelName);
 
         if (Utils.isNullOrBlank(baseUrl)) {
@@ -109,40 +119,89 @@ public class QwenChatModel implements ChatLanguageModel {
     private Response<AiMessage> generateByNonMultimodalModel(List<ChatMessage> messages,
                                                              List<ToolSpecification> toolSpecifications,
                                                              ToolSpecification toolThatMustBeExecuted) {
+
+        GenerationParam.GenerationParamBuilder<?, ?> builder = GenerationParam.builder()
+                .apiKey(apiKey)
+                .model(modelName)
+                .topP(topP)
+                .topK(topK)
+                .enableSearch(enableSearch)
+                .seed(seed)
+                .repetitionPenalty(repetitionPenalty)
+                .temperature(temperature)
+                .maxTokens(maxTokens)
+                .messages(toQwenMessages(messages))
+                .resultFormat(MESSAGE);
+
+        if (stops != null) {
+            builder.stopStrings(stops);
+        }
+
+        if (!isNullOrEmpty(toolSpecifications)) {
+            builder.tools(toToolFunctions(toolSpecifications));
+        } else if (toolThatMustBeExecuted != null) {
+            builder.tools(toToolFunctions(Collections.singleton(toolThatMustBeExecuted)));
+            builder.toolChoice(toToolFunction(toolThatMustBeExecuted));
+        }
+
+        GenerationParam param = builder.build();
+
+        ChatModelRequest modelListenerRequest = createModelListenerRequest(param, messages, toolSpecifications);
+        Map<Object, Object> attributes = new ConcurrentHashMap<>();
+        ChatModelRequestContext requestContext = new ChatModelRequestContext(modelListenerRequest, attributes);
+        listeners.forEach(listener -> {
+            try {
+                listener.onRequest(requestContext);
+            } catch (Exception e) {
+                log.warn("Exception while calling model listener", e);
+            }
+        });
+
         try {
-            GenerationParam.GenerationParamBuilder<?, ?> builder = GenerationParam.builder()
-                    .apiKey(apiKey)
-                    .model(modelName)
-                    .topP(topP)
-                    .topK(topK)
-                    .enableSearch(enableSearch)
-                    .seed(seed)
-                    .repetitionPenalty(repetitionPenalty)
-                    .temperature(temperature)
-                    .maxTokens(maxTokens)
-                    .messages(toQwenMessages(messages))
-                    .resultFormat(MESSAGE);
-
-            if (stops != null) {
-                builder.stopStrings(stops);
-            }
-
-            if (!isNullOrEmpty(toolSpecifications)) {
-                builder.tools(toToolFunctions(toolSpecifications));
-            } else if (toolThatMustBeExecuted != null) {
-                builder.tools(toToolFunctions(Collections.singleton(toolThatMustBeExecuted)));
-                builder.toolChoice(toToolFunction(toolThatMustBeExecuted));
-            }
-
-            GenerationResult generationResult = generation.call(builder.build());
-
-            return Response.from(
-                    aiMessageFrom(generationResult),
-                    tokenUsageFrom(generationResult),
-                    finishReasonFrom(generationResult)
+            GenerationResult result = generation.call(param);
+            Response<AiMessage> response = Response.from(
+                    aiMessageFrom(result),
+                    tokenUsageFrom(result),
+                    finishReasonFrom(result)
             );
-        } catch (NoApiKeyException | InputRequiredException e) {
-            throw new RuntimeException(e);
+
+            ChatModelResponse modelListenerResponse = createModelListenerResponse(
+                    result.getRequestId(),
+                    param.getModel(),
+                    response
+            );
+            ChatModelResponseContext responseContext = new ChatModelResponseContext(
+                    modelListenerResponse,
+                    modelListenerRequest,
+                    attributes
+            );
+            listeners.forEach(listener -> {
+                try {
+                    listener.onResponse(responseContext);
+                } catch (Exception e) {
+                    log.warn("Exception while calling model listener", e);
+                }
+            });
+
+            return response;
+        } catch (NoApiKeyException | InputRequiredException | RuntimeException e) {
+            ChatModelErrorContext errorContext = new ChatModelErrorContext(
+                    e,
+                    modelListenerRequest,
+                    null,
+                    attributes
+            );
+
+            listeners.forEach(listener -> {
+                try {
+                    listener.onError(errorContext);
+                } catch (Exception e2) {
+                    log.warn("Exception while calling model listener", e2);
+                }
+            });
+
+            throw e instanceof RuntimeException ?
+                    (RuntimeException) e : new RuntimeException(e);
         }
     }
 
@@ -153,26 +212,73 @@ public class QwenChatModel implements ChatLanguageModel {
             throw new IllegalArgumentException("Tools are currently not supported by this model");
         }
 
-        try {
-            MultiModalConversationParam param = MultiModalConversationParam.builder()
-                    .apiKey(apiKey)
-                    .model(modelName)
-                    .topP(topP)
-                    .topK(topK)
-                    .enableSearch(enableSearch)
-                    .seed(seed)
-                    .temperature(temperature)
-                    .maxLength(maxTokens)
-                    .messages(toQwenMultiModalMessages(messages))
-                    .build();
+        MultiModalConversationParam param = MultiModalConversationParam.builder()
+                .apiKey(apiKey)
+                .model(modelName)
+                .topP(topP)
+                .topK(topK)
+                .enableSearch(enableSearch)
+                .seed(seed)
+                .temperature(temperature)
+                .maxLength(maxTokens)
+                .messages(toQwenMultiModalMessages(messages))
+                .build();
 
+        ChatModelRequest modelListenerRequest = createModelListenerRequest(param, messages, toolSpecifications);
+        Map<Object, Object> attributes = new ConcurrentHashMap<>();
+        ChatModelRequestContext requestContext = new ChatModelRequestContext(modelListenerRequest, attributes);
+        listeners.forEach(listener -> {
+            try {
+                listener.onRequest(requestContext);
+            } catch (Exception e) {
+                log.warn("Exception while calling model listener", e);
+            }
+        });
+
+        try {
             MultiModalConversationResult result = conv.call(param);
             String answer = answerFrom(result);
 
-            return Response.from(AiMessage.from(answer),
+            Response<AiMessage> response = Response.from(AiMessage.from(answer),
                     tokenUsageFrom(result), finishReasonFrom(result));
-        } catch (NoApiKeyException | UploadFileException e) {
-            throw new RuntimeException(e);
+
+            ChatModelResponse modelListenerResponse = createModelListenerResponse(
+                    result.getRequestId(),
+                    param.getModel(),
+                    response
+            );
+            ChatModelResponseContext responseContext = new ChatModelResponseContext(
+                    modelListenerResponse,
+                    modelListenerRequest,
+                    attributes
+            );
+            listeners.forEach(listener -> {
+                try {
+                    listener.onResponse(responseContext);
+                } catch (Exception e) {
+                    log.warn("Exception while calling model listener", e);
+                }
+            });
+
+            return response;
+        } catch (NoApiKeyException | UploadFileException | RuntimeException e) {
+            ChatModelErrorContext errorContext = new ChatModelErrorContext(
+                    e,
+                    modelListenerRequest,
+                    null,
+                    attributes
+            );
+
+            listeners.forEach(listener -> {
+                try {
+                    listener.onError(errorContext);
+                } catch (Exception e2) {
+                    log.warn("Exception while calling model listener", e2);
+                }
+            });
+
+            throw e instanceof RuntimeException ?
+                    (RuntimeException) e : new RuntimeException(e);
         }
     }
 
