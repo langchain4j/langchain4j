259c129dc88ce0f71f0b492380115a3cc3c90f3a
Guillaume Laforge <glaforge@gmail.com>
2024-09-05 10:23:27 +0200

    Gemini AI: improve error reporting and flags for Python code execution (#1708)
    ## Change
    
    - Error reporting now properly reflect when the input payload has
    problems, or when the server sends an error back.
    - Python code execution is more configurable, allows you to specify if
    you want to activate code execution or not, and if yes if you want the
    code executed and its output to appear in the response
    
    ## General checklist
    
    - [X] There are no breaking changes
    - [ ] I have added unit and integration tests for my change
    - [X] I have manually run all the unit and integration tests in the
    module I have added/changed, and they are all green
    - [ ] I have manually run all the unit and integration tests in the
    [core](https://github.com/langchain4j/langchain4j/tree/main/langchain4j-core)
    and
    [main](https://github.com/langchain4j/langchain4j/tree/main/langchain4j)
    modules, and they are all green
    <!-- Before adding documentation and example(s) (below), please wait
    until the PR is reviewed and approved. -->
    - [ ] I have added/updated the
    [documentation](https://github.com/langchain4j/langchain4j/tree/main/docs/docs)
    - [ ] I have added an example in the [examples
    repo](https://github.com/langchain4j/langchain4j-examples) (only for
    "big" features)
    - [ ] I have added/updated [Spring Boot
    starter(s)](https://github.com/langchain4j/langchain4j-spring) (if
    applicable)

--- langchain4j-google-ai-gemini/src/test/java/dev/langchain4j/model/googleai/GoogleAiGeminiChatModelIT.java
+++ langchain4j-google-ai-gemini/src/test/java/dev/langchain4j/model/googleai/GoogleAiGeminiChatModelIT.java
@@ -35,7 +35,6 @@ import static dev.langchain4j.internal.Utils.readBytes;
 import static dev.langchain4j.model.chat.request.ResponseFormatType.JSON;
 import static dev.langchain4j.model.chat.request.json.JsonIntegerSchema.JSON_INTEGER_SCHEMA;
 import static dev.langchain4j.model.chat.request.json.JsonStringSchema.JSON_STRING_SCHEMA;
-import static dev.langchain4j.model.googleai.GoogleAiGeminiChatModel.pythonCodeExecution;
 import static dev.langchain4j.model.googleai.GeminiHarmBlockThreshold.BLOCK_LOW_AND_ABOVE;
 import static dev.langchain4j.model.googleai.GeminiHarmCategory.HARM_CATEGORY_HARASSMENT;
 import static dev.langchain4j.model.googleai.GeminiHarmCategory.HARM_CATEGORY_HATE_SPEECH;
@@ -44,7 +43,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 public class GoogleAiGeminiChatModelIT {
 
-    private static final String GOOGLE_AI_GEMINI_API_KEY = System.getenv("GOOGLE_AI_GEMINI_API_KEY");
+    private static final String GOOGLE_AI_GEMINI_API_KEY = System.getenv("GEMINI_AI_KEY");
 
     private static final String CAT_IMAGE_URL = "https://upload.wikimedia.org/wikipedia/commons/e/e9/Felis_silvestris_silvestris_small_gradual_decrease_of_quality.png";
     private static final String MD_FILE_URL = "https://raw.githubusercontent.com/langchain4j/langchain4j/main/docs/docs/intro.md";
@@ -259,18 +258,23 @@ public class GoogleAiGeminiChatModelIT {
         GoogleAiGeminiChatModel gemini = GoogleAiGeminiChatModel.builder()
             .apiKey(GOOGLE_AI_GEMINI_API_KEY)
             .modelName("gemini-1.5-flash")
+            .logRequestsAndResponses(true)
+            .allowCodeExecution(true)
+            .includeCodeExecutionOutput(true)
             .build();
 
         // when
         Response<AiMessage> response = gemini.generate(
             singletonList(UserMessage.from(
                 "Calculate `fibonacci(13)`. " +
-                    "Write code in Python and execute it to get the result.")),
-            pythonCodeExecution()
+                    "Write code in Python and execute it to get the result."))
         );
 
         // then
-        assertThat(response.content().text()).containsIgnoringCase("233");
+        String text = response.content().text();
+        System.out.println("text = " + text);
+
+        assertThat(text).containsIgnoringCase("233");
     }
 
     @Test
