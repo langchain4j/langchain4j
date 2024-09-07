480907dd489b15ea5b01688a86a3a3579a28afc9
Martin7-1 <1754350460@qq.com>
2024-09-07 15:41:19 +0800

    raise the baseline to jdk17 and remove gson and lombok

--- langchain4j-vespa/src/main/java/dev/langchain4j/store/embedding/vespa/QueryResponse.java
+++ langchain4j-vespa/src/main/java/dev/langchain4j/store/embedding/vespa/QueryResponse.java
@@ -4,26 +4,26 @@ import java.util.List;
 
 class QueryResponse {
 
-  private RootNode root;
+    private RootNode root;
 
-  public RootNode getRoot() {
-    return root;
-  }
-
-  public void setRoot(RootNode root) {
-    this.root = root;
-  }
-
-  public static class RootNode {
-
-    private List<Record> children;
-
-    public List<Record> getChildren() {
-      return children;
+    public RootNode getRoot() {
+        return root;
     }
 
-    public void setChildren(List<Record> children) {
-      this.children = children;
+    public void setRoot(RootNode root) {
+        this.root = root;
     }
-  }
+
+    public static class RootNode {
+
+        private List<Record> children;
+
+        public List<Record> getChildren() {
+            return children;
+        }
+
+        public void setChildren(List<Record> children) {
+            this.children = children;
+        }
+    }
 }
