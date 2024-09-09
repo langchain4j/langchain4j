3cf556137907d0d9f8624b9ad1224d598192c208
LangChain4j <info@langchain4j.dev>
2024-09-09 13:53:42 +0200

    Judge0: migrate from Gson to Jackson (#1729)

--- code-execution-engines/langchain4j-code-execution-engine-judge0/src/main/java/dev/langchain4j/code/judge0/Json.java
+++ code-execution-engines/langchain4j-code-execution-engine-judge0/src/main/java/dev/langchain4j/code/judge0/Json.java
@@ -2,18 +2,16 @@ package dev.langchain4j.code.judge0;
 
 
 import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 import static com.fasterxml.jackson.databind.SerializationFeature.INDENT_OUTPUT;
 
 class Json {
+
     static final ObjectMapper OBJECT_MAPPER = new ObjectMapper()
             .enable(INDENT_OUTPUT)
             .disable(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES);
-
-    ;
 
     static String toJson(Object o) {
         try {
@@ -24,14 +22,6 @@ class Json {
     }
 
     static <T> T fromJson(String json, Class<T> type) {
-        try {
-            return OBJECT_MAPPER.readValue(json, type);
-        } catch (JsonProcessingException e) {
-            throw new RuntimeException(e);
-        }
-    }
-
-    static <T> T fromJson(String json, TypeReference<T> type) {
         try {
             return OBJECT_MAPPER.readValue(json, type);
         } catch (JsonProcessingException e) {
