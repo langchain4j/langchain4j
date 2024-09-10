7c5e3514863e31d703e0cf63a9be475212c47b31
ZYinNJU <1754350460@qq.com>
2024-09-10 19:49:53 +0800

    langchain4j-ollama get rid of lombok (#1658)
    ## Issue
    #1636 
    
    ## Change
    `langchain4j-ollama` get rid of `lombok`
    
    ## General checklist
    - [x] There are no breaking changes
    - [ ] I have added unit and integration tests for my change
    - [x] I have manually run all the unit and integration tests in the
    module I have added/changed, and they are all green
    - [x] I have manually run all the unit and integration tests in the
    [core](https://github.com/langchain4j/langchain4j/tree/main/langchain4j-core)
    and
    [main](https://github.com/langchain4j/langchain4j/tree/main/langchain4j)
    modules, and they are all green
    <!-- Before adding documentation and example(s) (below), please wait
    until the PR is reviewed and approved. -->
    - [ ] I have added/updated the
    [documentation](https://github.com/langchain4j/langchain4j/tree/main/docs/docs)
    - [ ] I have added an example in the [examples
    repo](https://github.com/langchain4j/langchain4j-examples) (only for
    "big" features)
    - [ ] I have added/updated [Spring Boot
    starter(s)](https://github.com/langchain4j/langchain4j-spring) (if
    applicable)

--- langchain4j-ollama/src/main/java/dev/langchain4j/model/ollama/OllamaLanguageModel.java
+++ langchain4j-ollama/src/main/java/dev/langchain4j/model/ollama/OllamaLanguageModel.java
@@ -4,7 +4,6 @@ import dev.langchain4j.model.language.LanguageModel;
 import dev.langchain4j.model.ollama.spi.OllamaLanguageModelBuilderFactory;
 import dev.langchain4j.model.output.Response;
 import dev.langchain4j.model.output.TokenUsage;
-import lombok.Builder;
 
 import java.time.Duration;
 import java.util.List;
@@ -29,7 +28,6 @@ public class OllamaLanguageModel implements LanguageModel {
     private final String format;
     private final Integer maxRetries;
 
-    @Builder
     public OllamaLanguageModel(String baseUrl,
                                String modelName,
                                Double temperature,
@@ -46,7 +44,7 @@ public class OllamaLanguageModel implements LanguageModel {
                                Boolean logRequests,
                                Boolean logResponses,
                                Map<String, String> customHeaders
-                               ) {
+    ) {
         this.client = OllamaClient.builder()
                 .baseUrl(baseUrl)
                 .timeout(getOrDefault(timeout, ofSeconds(60)))
@@ -69,6 +67,13 @@ public class OllamaLanguageModel implements LanguageModel {
         this.maxRetries = getOrDefault(maxRetries, 3);
     }
 
+    public static OllamaLanguageModelBuilder builder() {
+        for (OllamaLanguageModelBuilderFactory factory : loadFactories(OllamaLanguageModelBuilderFactory.class)) {
+            return factory.get();
+        }
+        return new OllamaLanguageModelBuilder();
+    }
+
     @Override
     public Response<String> generate(String prompt) {
 
@@ -88,17 +93,129 @@ public class OllamaLanguageModel implements LanguageModel {
         );
     }
 
-    public static OllamaLanguageModelBuilder builder() {
-        for (OllamaLanguageModelBuilderFactory factory : loadFactories(OllamaLanguageModelBuilderFactory.class)) {
-            return factory.get();
-        }
-        return new OllamaLanguageModelBuilder();
-    }
-
     public static class OllamaLanguageModelBuilder {
+
+        private String baseUrl;
+        private String modelName;
+        private Double temperature;
+        private Integer topK;
+        private Double topP;
+        private Double repeatPenalty;
+        private Integer seed;
+        private Integer numPredict;
+        private Integer numCtx;
+        private List<String> stop;
+        private String format;
+        private Duration timeout;
+        private Integer maxRetries;
+        private Boolean logRequests;
+        private Boolean logResponses;
+        private Map<String, String> customHeaders;
+
         public OllamaLanguageModelBuilder() {
             // This is public so it can be extended
             // By default with Lombok it becomes package private
+        }
+
+        public OllamaLanguageModelBuilder baseUrl(String baseUrl) {
+            this.baseUrl = baseUrl;
+            return this;
+        }
+
+        public OllamaLanguageModelBuilder modelName(String modelName) {
+            this.modelName = modelName;
+            return this;
+        }
+
+        public OllamaLanguageModelBuilder temperature(Double temperature) {
+            this.temperature = temperature;
+            return this;
+        }
+
+        public OllamaLanguageModelBuilder topK(Integer topK) {
+            this.topK = topK;
+            return this;
+        }
+
+        public OllamaLanguageModelBuilder topP(Double topP) {
+            this.topP = topP;
+            return this;
+        }
+
+        public OllamaLanguageModelBuilder repeatPenalty(Double repeatPenalty) {
+            this.repeatPenalty = repeatPenalty;
+            return this;
+        }
+
+        public OllamaLanguageModelBuilder seed(Integer seed) {
+            this.seed = seed;
+            return this;
+        }
+
+        public OllamaLanguageModelBuilder numPredict(Integer numPredict) {
+            this.numPredict = numPredict;
+            return this;
+        }
+
+        public OllamaLanguageModelBuilder numCtx(Integer numCtx) {
+            this.numCtx = numCtx;
+            return this;
+        }
+
+        public OllamaLanguageModelBuilder stop(List<String> stop) {
+            this.stop = stop;
+            return this;
+        }
+
+        public OllamaLanguageModelBuilder format(String format) {
+            this.format = format;
+            return this;
+        }
+
+        public OllamaLanguageModelBuilder timeout(Duration timeout) {
+            this.timeout = timeout;
+            return this;
+        }
+
+        public OllamaLanguageModelBuilder maxRetries(Integer maxRetries) {
+            this.maxRetries = maxRetries;
+            return this;
+        }
+
+        public OllamaLanguageModelBuilder logRequests(Boolean logRequests) {
+            this.logRequests = logRequests;
+            return this;
+        }
+
+        public OllamaLanguageModelBuilder logResponses(Boolean logResponses) {
+            this.logResponses = logResponses;
+            return this;
+        }
+
+        public OllamaLanguageModelBuilder customHeaders(Map<String, String> customHeaders) {
+            this.customHeaders = customHeaders;
+            return this;
+        }
+
+        public OllamaLanguageModel build() {
+            return new OllamaLanguageModel(
+                    baseUrl,
+                    modelName,
+                    temperature,
+                    topK,
+                    topP,
+                    repeatPenalty,
+                    seed,
+                    numPredict,
+                    numCtx,
+                    stop,
+                    format,
+                    timeout,
+                    maxRetries,
+                    logRequests,
+                    logResponses,
+                    customHeaders
+            );
         }
     }
 }
