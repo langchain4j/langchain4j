93187c000dcd8cf9c4b83f507d84fcb5bf806cb8
LangChain4j <info@langchain4j.dev>
2024-09-15 12:37:56 +0200

    added StreamingChatModelListenerIT

---
+++ langchain4j-open-ai/src/test/java/dev/langchain4j/model/openai/OpenAiStreamingChatModelListenerIT.java
@@ -0,0 +1,48 @@
+package dev.langchain4j.model.openai;
+
+import dev.ai4j.openai4j.OpenAiHttpException;
+import dev.langchain4j.model.chat.StreamingChatLanguageModel;
+import dev.langchain4j.model.chat.StreamingChatModelListenerIT;
+import dev.langchain4j.model.chat.listener.ChatModelListener;
+
+import static dev.langchain4j.model.openai.OpenAiChatModelName.GPT_4_O_MINI;
+import static java.util.Collections.singletonList;
+
+class OpenAiStreamingChatModelListenerIT extends StreamingChatModelListenerIT {
+
+    @Override
+    protected StreamingChatLanguageModel createModel(ChatModelListener listener) {
+        return OpenAiStreamingChatModel.builder()
+                .baseUrl(System.getenv("OPENAI_BASE_URL"))
+                .apiKey(System.getenv("OPENAI_API_KEY"))
+                .organizationId(System.getenv("OPENAI_ORGANIZATION_ID"))
+                .modelName(modelName())
+                .temperature(temperature())
+                .topP(topP())
+                .maxTokens(maxTokens())
+                .logRequests(true)
+                .logResponses(true)
+                .listeners(singletonList(listener))
+                .build();
+    }
+
+    @Override
+    protected String modelName() {
+        return GPT_4_O_MINI.toString();
+    }
+
+    @Override
+    protected StreamingChatLanguageModel createFailingModel(ChatModelListener listener) {
+        return OpenAiStreamingChatModel.builder()
+                .apiKey("banana")
+                .logRequests(true)
+                .logResponses(true)
+                .listeners(singletonList(listener))
+                .build();
+    }
+
+    @Override
+    protected Class<?> expectedExceptionClass() {
+        return OpenAiHttpException.class;
+    }
+}
