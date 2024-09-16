3471e84fa2aef62406f0ea9166e35beffb602853
LangChain4j <info@langchain4j.dev>
2024-09-16 14:14:18 +0200

    OpenAI: added maxCompletionTokens

--- langchain4j-open-ai/src/test/java/dev/langchain4j/model/openai/OpenAiChatModelIT.java
+++ langchain4j-open-ai/src/test/java/dev/langchain4j/model/openai/OpenAiChatModelIT.java
@@ -77,14 +77,14 @@ class OpenAiChatModelIT {
     void should_generate_answer_and_return_token_usage_and_finish_reason_length() {
 
         // given
-        int maxTokens = 1;
+        int maxCompletionTokens = 1;
 
         ChatLanguageModel model = OpenAiChatModel.builder()
                 .baseUrl(System.getenv("OPENAI_BASE_URL"))
                 .apiKey(System.getenv("OPENAI_API_KEY"))
                 .organizationId(System.getenv("OPENAI_ORGANIZATION_ID"))
                 .modelName(GPT_4_O_MINI)
-                .maxTokens(maxTokens)
+                .maxCompletionTokens(maxCompletionTokens)
                 .temperature(0.0)
                 .logRequests(true)
                 .logResponses(true)
@@ -100,7 +100,7 @@ class OpenAiChatModelIT {
 
         TokenUsage tokenUsage = response.tokenUsage();
         assertThat(tokenUsage.inputTokenCount()).isEqualTo(14);
-        assertThat(tokenUsage.outputTokenCount()).isLessThanOrEqualTo(maxTokens);
+        assertThat(tokenUsage.outputTokenCount()).isLessThanOrEqualTo(maxCompletionTokens);
         assertThat(tokenUsage.totalTokenCount())
                 .isEqualTo(tokenUsage.inputTokenCount() + tokenUsage.outputTokenCount());
 
