30a3865816b6ed9bc8c702852170bb10ca2bd661
Martin7-1 <1754350460@qq.com>
2024-09-20 20:52:46 +0800

    vearch get rid of lombok

--- langchain4j-vearch/src/main/java/dev/langchain4j/store/embedding/vearch/CreateSpaceRequest.java
+++ langchain4j-vearch/src/main/java/dev/langchain4j/store/embedding/vearch/CreateSpaceRequest.java
@@ -4,18 +4,12 @@ import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.databind.PropertyNamingStrategies.SnakeCaseStrategy;
 import com.fasterxml.jackson.databind.annotation.JsonNaming;
-import lombok.Builder;
-import lombok.Getter;
-import lombok.Setter;
 
 import java.util.List;
 import java.util.Map;
 
 import static com.fasterxml.jackson.annotation.JsonInclude.Include.NON_NULL;
 
-@Getter
-@Setter
-@Builder
 @JsonIgnoreProperties(ignoreUnknown = true)
 @JsonInclude(NON_NULL)
 @JsonNaming(SnakeCaseStrategy.class)
@@ -29,7 +23,6 @@ class CreateSpaceRequest {
     private List<ModelParam> models;
 
     CreateSpaceRequest() {
-
     }
 
     CreateSpaceRequest(String name, Integer partitionNum, Integer replicaNum, SpaceEngine engine, Map<String, SpacePropertyParam> properties, List<ModelParam> models) {
@@ -39,5 +32,73 @@ class CreateSpaceRequest {
         this.engine = engine;
         this.properties = properties;
         this.models = models;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public Integer getPartitionNum() {
+        return partitionNum;
+    }
+
+    public Integer getReplicaNum() {
+        return replicaNum;
+    }
+
+    public SpaceEngine getEngine() {
+        return engine;
+    }
+
+    public Map<String, SpacePropertyParam> getProperties() {
+        return properties;
+    }
+
+    public List<ModelParam> getModels() {
+        return models;
+    }
+
+    static class Builder {
+
+        private String name;
+        private Integer partitionNum;
+        private Integer replicaNum;
+        private SpaceEngine engine;
+        private Map<String, SpacePropertyParam> properties;
+        private List<ModelParam> models;
+
+        Builder name(String name) {
+            this.name = name;
+            return this;
+        }
+
+        Builder partitionNum(Integer partitionNum) {
+            this.partitionNum = partitionNum;
+            return this;
+        }
+
+        Builder replicaNum(Integer replicaNum) {
+            this.replicaNum = replicaNum;
+            return this;
+        }
+
+        Builder engine(SpaceEngine engine) {
+            this.engine = engine;
+            return this;
+        }
+
+        Builder properties(Map<String, SpacePropertyParam> properties) {
+            this.properties = properties;
+            return this;
+        }
+
+        Builder models(List<ModelParam> models) {
+            this.models = models;
+            return this;
+        }
+
+        CreateSpaceRequest build() {
+            return new CreateSpaceRequest(name, partitionNum, replicaNum, engine, properties, models);
+        }
     }
 }
