942aaeef22a576a2a6cfde5cb6dc19e4c0476ba3
Maxim Katcharov <maxim.katcharov@mongodb.com>
2024-10-04 12:36:26 -0600

    Rename to MongoDbEmbeddingStoreIT

--- langchain4j-mongodb-atlas/src/test/java/dev/langchain4j/store/embedding/mongodb/MongoDbEmbeddingStoreIT.java
+++ langchain4j-mongodb-atlas/src/test/java/dev/langchain4j/store/embedding/mongodb/MongoDbEmbeddingStoreIT.java
@@ -5,15 +5,10 @@ import dev.langchain4j.data.segment.TextSegment;
 import dev.langchain4j.model.embedding.EmbeddingModel;
 import dev.langchain4j.store.embedding.EmbeddingStore;
 import dev.langchain4j.store.embedding.EmbeddingStoreIT;
-import dev.langchain4j.store.embedding.EmbeddingStoreWithFilteringIT;
-import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
-import org.testcontainers.mongodb.MongoDBAtlasLocalContainer;
 
-import static dev.langchain4j.store.embedding.TestUtils.awaitUntilAsserted;
 import static dev.langchain4j.store.embedding.mongodb.MongoDbTestFixture.*;
-import static org.assertj.core.api.Assertions.assertThatNoException;
 
 public class MongoDbEmbeddingStoreIT extends EmbeddingStoreIT {
 
@@ -48,12 +43,5 @@ public class MongoDbEmbeddingStoreIT extends EmbeddingStoreIT {
     @AfterEach
     void afterEach() {
         helper.afterTests();
-    }
-
-
-    @Override
-    protected void ensureStoreIsReady() {
-        // to avoid "cannot query search index while in state INITIAL_SYNC" error
-        awaitUntilAsserted(() -> assertThatNoException().isThrownBy(this::getAllEmbeddings));
     }
 }
