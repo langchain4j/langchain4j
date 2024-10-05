e55175b0da84d20a36cadde21429415be6a3fb55
Hemanth <74316298+hsavasere@users.noreply.github.com>
2024-10-05 23:54:45 +0530

    Adding Tests for JacksonProcessingException class

--- langchain4j-jlama/src/main/java/dev/langchain4j/model/jlama/JacksonProcessingException.java
+++ langchain4j-jlama/src/main/java/dev/langchain4j/model/jlama/JacksonProcessingException.java
@@ -2,15 +2,7 @@ package dev.langchain4j.model.jlama;
 
 public class JacksonProcessingException extends RuntimeException {
 
-    public JacksonProcessingException(String message) {
-        super(message);
-    }
-
     public JacksonProcessingException(String message, Throwable cause) {
         super(message, cause);
-    }
-
-    public JacksonProcessingException(Throwable cause) {
-        super(cause);
     }
 }
