d74a8fc2803184ffb7644dc0b60121c4918c5f1e
Konstantin Pavlov <{ID}+{username}@users.noreply.github.com>
2024-10-25 17:17:24 +0300

    Add imports for fully qualified references to types
    https://docs.openrewrite.org/recipes/java/shortenfullyqualifiedtypereferences

--- langchain4j-core/src/main/java/dev/langchain4j/store/embedding/EmbeddingStoreIngestor.java
+++ langchain4j-core/src/main/java/dev/langchain4j/store/embedding/EmbeddingStoreIngestor.java
@@ -11,6 +11,7 @@ import dev.langchain4j.model.output.Response;
 import dev.langchain4j.spi.data.document.splitter.DocumentSplitterFactory;
 import dev.langchain4j.spi.model.embedding.EmbeddingModelFactory;
 import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.util.Collection;
 import java.util.List;
@@ -50,7 +51,7 @@ import static java.util.stream.Collectors.toList;
  */
 public class EmbeddingStoreIngestor {
 
-    private static final Logger log = org.slf4j.LoggerFactory.getLogger(EmbeddingStoreIngestor.class);
+    private static final Logger log = LoggerFactory.getLogger(EmbeddingStoreIngestor.class);
 
     private final DocumentTransformer documentTransformer;
     private final DocumentSplitter documentSplitter;
