2b657ad5839a387787530b7e4d87a7d44f14a746
astoisavljevic <astoisavljevic@jatheon.com>
2024-11-02 17:39:54 +0100

    Providing CohereEmbeddingModel (and Response) in AWS Bedrock code review changes CR changes #2

--- langchain4j-bedrock/src/main/java/dev/langchain4j/model/bedrock/BedrockCohereEmbeddingResponse.java
+++ langchain4j-bedrock/src/main/java/dev/langchain4j/model/bedrock/BedrockCohereEmbeddingResponse.java
@@ -2,17 +2,13 @@ package dev.langchain4j.model.bedrock;
 
 import dev.langchain4j.data.embedding.Embedding;
 import dev.langchain4j.model.bedrock.internal.BedrockEmbeddingResponse;
-import lombok.Getter;
-import lombok.Setter;
 
 import java.util.List;
 
 /**
  * Bedrock Cohere embedding response
  */
-@Getter
-@Setter
-public class BedrockCohereEmbeddingResponse implements BedrockEmbeddingResponse {
+class BedrockCohereEmbeddingResponse implements BedrockEmbeddingResponse {
 
     private String id;
 
@@ -25,6 +21,47 @@ public class BedrockCohereEmbeddingResponse implements BedrockEmbeddingResponse 
     @Override
     public Embedding toEmbedding() {
         return new Embedding(embedding);
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public List<String> getTexts() {
+        return texts;
+    }
+
+    public void setTexts(List<String> texts) {
+        this.texts = texts;
+    }
+
+    public void addText(String text) {
+        if (this.texts == null) {
+            this.texts = List.of(text);
+            return;
+        }
+        this.texts.add(text);
+    }
+
+    public float[] getEmbedding() {
+        return embedding;
+    }
+
+    public void setEmbedding(float[] embedding) {
+        this.embedding = embedding;
+    }
+
+    @Override
+    public int getInputTextTokenCount() {
+        return inputTextTokenCount;
+    }
+
+    public void setInputTextTokenCount(int inputTextTokenCount) {
+        this.inputTextTokenCount = inputTextTokenCount;
     }
 
 }
