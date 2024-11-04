77172820d9fa8a370c18ee5dd136b603ed0b3af1
jiangsier-xyz <126842484+jiangsier-xyz@users.noreply.github.com>
2024-11-04 21:41:45 +0800

    Fix: handle toolSpecification for  backwards-compatibility

--- langchain4j-dashscope/src/main/java/dev/langchain4j/model/dashscope/QwenHelper.java
+++ langchain4j-dashscope/src/main/java/dev/langchain4j/model/dashscope/QwenHelper.java
@@ -367,11 +367,12 @@ class QwenHelper {
         return ToolFunction.builder().function(functionDefinition).build();
     }
 
+    @SuppressWarnings("deprecation")
     private static JsonObject toParameters(ToolSpecification toolSpecification) {
         if (toolSpecification.parameters() != null) {
             return JsonUtils.toJsonObject(toMap(toolSpecification.parameters()));
-        } else if (toolSpecification.parameters() != null) {
-            return JsonUtils.toJsonObject(toolSpecification.parameters());
+        } else if (toolSpecification.toolParameters() != null) {
+            return JsonUtils.toJsonObject(toolSpecification.toolParameters());
         } else {
             return JsonUtils.toJsonObject(Collections.emptyMap());
         }
