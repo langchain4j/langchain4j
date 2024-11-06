b76a98e47069fc2e5bbd5a8f48bd70bea576f9a5
Omar <59146036+omarmahamid@users.noreply.github.com>
2024-11-06 12:31:49 +0200

    avoid NPE on GoogleCustomWebSearchEngine results (#2033)
    ## Issue
    Closes #2005
    
    ## Change
    
    ## General checklist
    - [x] There are no breaking changes
    - [ ] I have added unit and integration tests for my change
    - [x] I have manually run all the unit and integration tests in the
    module I have added/changed, and they are all green
    - [ ] I have manually run all the unit and integration tests in the
    [core](https://github.com/langchain4j/langchain4j/tree/main/langchain4j-core)
    and
    [main](https://github.com/langchain4j/langchain4j/tree/main/langchain4j)
    modules, and they are all green
    - [ ] I have added/updated the
    [documentation](https://github.com/langchain4j/langchain4j/tree/main/docs/docs)
    - [ ] I have added an example in the [examples
    repo](https://github.com/langchain4j/langchain4j-examples) (only for
    "big" features)
    - [ ] I have added/updated [Spring Boot
    starter(s)](https://github.com/langchain4j/langchain4j-spring) (if
    applicable)

--- langchain4j-core/src/main/java/dev/langchain4j/internal/Utils.java
+++ langchain4j-core/src/main/java/dev/langchain4j/internal/Utils.java
@@ -123,6 +123,15 @@ public class Utils {
   }
 
   /**
+   * Is the map object {@code null} or empty?
+   * @param map The iterable object to check.
+   * @return {@code true} if the map object is {@code null} or empty map, otherwise {@code false}.
+   * */
+  public static boolean isNullOrEmpty(Map<?, ?> map) {
+      return map == null || map.isEmpty();
+  }
+
+  /**
    * @deprecated Use {@link #isNullOrEmpty(Collection)} instead.
    * @param collection The collection to check.
    * @return {@code true} if the collection is {@code null} or empty, {@code false} otherwise.
