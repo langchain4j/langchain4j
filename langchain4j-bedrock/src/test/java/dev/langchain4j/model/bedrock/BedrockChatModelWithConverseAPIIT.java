e1ee202abfa0188720c502af1e8b4fbdc980d29b
Herbert Beckman <hbeckman@uolinc.com>
2024-11-17 01:06:45 -0300

    Add image content test

--- langchain4j-bedrock/src/test/java/dev/langchain4j/model/bedrock/BedrockChatModelWithConverseAPIIT.java
+++ langchain4j-bedrock/src/test/java/dev/langchain4j/model/bedrock/BedrockChatModelWithConverseAPIIT.java
@@ -1,6 +1,7 @@
 package dev.langchain4j.model.bedrock;
 
 import dev.langchain4j.data.message.AiMessage;
+import dev.langchain4j.data.message.ImageContent;
 import dev.langchain4j.data.message.SystemMessage;
 import dev.langchain4j.data.message.UserMessage;
 import dev.langchain4j.model.bedrock.converse.BedrockChatModel;
@@ -9,10 +10,15 @@ import dev.langchain4j.model.output.Response;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.condition.EnabledIfEnvironmentVariable;
 
+import java.util.Base64;
+
+import static dev.langchain4j.internal.Utils.readBytes;
 import static org.assertj.core.api.Assertions.assertThat;
 
 @EnabledIfEnvironmentVariable(named = "AWS_SECRET_ACCESS_KEY", matches = ".+")
 public class BedrockChatModelWithConverseAPIIT {
+
+    private static final String CAT_IMAGE_URL = "https://upload.wikimedia.org/wikipedia/commons/e/e9/Felis_silvestris_silvestris_small_gradual_decrease_of_quality.png";
 
     @Test
     void testBedrockChatModelWithDefaultConfig() {
@@ -38,6 +44,24 @@ public class BedrockChatModelWithConverseAPIIT {
         UserMessage userMessage = UserMessage.from("hi, how are you doing?");
 
         Response<AiMessage> response = bedrockChatModel.generate(systemMessage, userMessage);
+
+        assertThat(response).isNotNull();
+        assertThat(response.content().text()).isNotBlank();
+        assertThat(response.tokenUsage()).isNotNull();
+        assertThat(response.finishReason()).isIn(FinishReason.STOP, FinishReason.LENGTH);
+    }
+
+    @Test
+    void testBedrockChatModelWithDefaultConfigAndImageContent() {
+        BedrockChatModel bedrockChatModel = new BedrockChatModel();
+
+        assertThat(bedrockChatModel).isNotNull();
+
+        String base64Data = Base64.getEncoder().encodeToString(readBytes(CAT_IMAGE_URL));
+        ImageContent imageContent = ImageContent.from(base64Data, "image/png");
+        UserMessage userMessage = UserMessage.from(imageContent);
+
+        Response<AiMessage> response = bedrockChatModel.generate(userMessage);
 
         assertThat(response).isNotNull();
         assertThat(response.content().text()).isNotBlank();
