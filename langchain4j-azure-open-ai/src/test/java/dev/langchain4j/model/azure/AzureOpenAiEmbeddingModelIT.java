a2720cb6f121a3c3942632605ea87b967f1a5bb8
Julien Dubois <julien.dubois@gmail.com>
2024-11-20 16:09:13 +0100

    Use the small model instead, as before

--- langchain4j-azure-open-ai/src/test/java/dev/langchain4j/model/azure/AzureOpenAiEmbeddingModelIT.java
+++ langchain4j-azure-open-ai/src/test/java/dev/langchain4j/model/azure/AzureOpenAiEmbeddingModelIT.java
@@ -20,7 +20,7 @@ class AzureOpenAiEmbeddingModelIT {
     EmbeddingModel model = AzureOpenAiEmbeddingModel.builder()
             .endpoint(System.getenv("AZURE_OPENAI_ENDPOINT"))
             .apiKey(System.getenv("AZURE_OPENAI_KEY"))
-            .deploymentName("text-embedding-3-large")
+            .deploymentName("text-embedding-3-small")
             .logRequestsAndResponses(true)
             .build();
 
