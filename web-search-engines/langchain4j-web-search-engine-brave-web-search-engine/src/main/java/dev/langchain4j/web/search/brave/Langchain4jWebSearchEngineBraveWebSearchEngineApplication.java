1d8727b15076f06dabc92dc99ef06196c11c9b3a
suhassk-hash <sharmasuhas450@gmail.com>
2024-11-21 15:12:32 +0530

    Adding new module brave web search engine

---
+++ web-search-engines/langchain4j-web-search-engine-brave-web-search-engine/src/main/java/dev/langchain4j/web/search/brave/Langchain4jWebSearchEngineBraveWebSearchEngineApplication.java
@@ -0,0 +1,13 @@
+package dev.langchain4j.web.search.brave;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Langchain4jWebSearchEngineBraveWebSearchEngineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Langchain4jWebSearchEngineBraveWebSearchEngineApplication.class, args);
+	}
+
+}
---
+++ web-search-engines/langchain4j-web-search-engine-brave-web-search-engine/src/test/java/dev/langchain4j/web/search/brave/Langchain4jWebSearchEngineBraveWebSearchEngineApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.langchain4j.web.search.brave;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Langchain4jWebSearchEngineBraveWebSearchEngineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
