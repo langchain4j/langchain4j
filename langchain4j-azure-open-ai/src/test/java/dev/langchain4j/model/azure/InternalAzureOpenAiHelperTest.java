5ef07c5668eaa3dc75f6dac7dbd950deefcb3372
Julien Dubois <julien.dubois@gmail.com>
2024-11-22 16:04:37 +0100

    keep ToolParameters for a bit

--- langchain4j-azure-open-ai/src/test/java/dev/langchain4j/model/azure/InternalAzureOpenAiHelperTest.java
+++ langchain4j-azure-open-ai/src/test/java/dev/langchain4j/model/azure/InternalAzureOpenAiHelperTest.java
@@ -8,6 +8,7 @@ import com.azure.json.JsonOptions;
 import com.azure.json.JsonReader;
 import com.azure.json.implementation.DefaultJsonReader;
 import dev.langchain4j.agent.tool.ToolExecutionRequest;
+import dev.langchain4j.agent.tool.ToolParameters;
 import dev.langchain4j.agent.tool.ToolSpecification;
 import dev.langchain4j.data.message.AiMessage;
 import dev.langchain4j.data.message.ChatMessage;
@@ -92,7 +93,7 @@ class InternalAzureOpenAiHelperTest {
         toolSpecifications.add(ToolSpecification.builder()
                 .name("test-tool")
                 .description("test-description")
-                .parameters(JsonObjectSchema.builder().build())
+                .parameters(ToolParameters.builder().build())
                 .build());
 
         List<ChatCompletionsToolDefinition> tools = InternalAzureOpenAiHelper.toToolDefinitions(toolSpecifications);
