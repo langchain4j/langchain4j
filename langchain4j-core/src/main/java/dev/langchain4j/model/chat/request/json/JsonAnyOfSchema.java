7ab89c05b4e4ac1f025331c631b6f1654ec09e9f
Marek Svatos <svatos.marek.98@gmail.com>
2024-11-22 17:39:22 +0100

    Update openai4j version, JsonAnyOfSchema cleanup

--- langchain4j-core/src/main/java/dev/langchain4j/model/chat/request/json/JsonAnyOfSchema.java
+++ langchain4j-core/src/main/java/dev/langchain4j/model/chat/request/json/JsonAnyOfSchema.java
@@ -6,6 +6,7 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Objects;
 
+import static dev.langchain4j.internal.Utils.quoted;
 import static dev.langchain4j.internal.ValidationUtils.ensureNotEmpty;
 
 @Experimental
