b1b90015033315c776295add548ea0afa32b42b9
suhassk-hash <sharmasuhas450@gmail.com>
2024-11-24 03:43:32 +0530

    Remove old files and add new files

--- web-search-engines/langchain4j-web-search-engine-brave/src/main/java/dev/langchain4j/web/search/brave/BraveClient.java
+++
@@ -1,4 +0,0 @@
-package dev.langchain4j.web.search.brave;
-
-public class BraveClient {
-}
--- web-search-engines/langchain4j-web-search-engine-brave/src/main/java/dev/langchain4j/web/search/brave/BraveWebSearchRequest.java
+++
@@ -1,8 +0,0 @@
-package dev.langchain4j.web.search.brave;
-
-public class BraveWebSearchRequest {
-
-    private final String query;
-    private final String token;
-
-}
