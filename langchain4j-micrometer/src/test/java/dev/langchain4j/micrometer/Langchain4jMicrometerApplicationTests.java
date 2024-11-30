a73c6125ea5a43cd8b77c0ce39456b20154d53df
Sus4nne <victorisu@gmail.com>
2024-12-01 00:12:55 +0100

    Boilerplate code

---
+++ langchain4j-micrometer/src/test/java/dev/langchain4j/micrometer/Langchain4jMicrometerApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.langchain4j.micrometer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Langchain4jMicrometerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
