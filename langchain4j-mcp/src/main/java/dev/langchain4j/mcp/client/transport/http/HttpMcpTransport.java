9cba3883d39221c6f70dfa27701afbb82fd849ea
Jan Martiska <jmartisk@redhat.com>
2024-12-13 13:17:34 +0100

    Properly receive the MCP POST URL from the MCP server (#2267)
    Closes #2266

--- langchain4j-mcp/src/main/java/dev/langchain4j/mcp/client/transport/http/HttpMcpTransport.java
+++ langchain4j-mcp/src/main/java/dev/langchain4j/mcp/client/transport/http/HttpMcpTransport.java
@@ -1,5 +1,6 @@
 package dev.langchain4j.mcp.client.transport.http;
 
+import static dev.langchain4j.internal.Utils.getOrDefault;
 import static dev.langchain4j.internal.ValidationUtils.ensureNotNull;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
@@ -10,43 +11,48 @@ import dev.langchain4j.mcp.client.protocol.McpInitializeRequest;
 import dev.langchain4j.mcp.client.protocol.McpListToolsRequest;
 import dev.langchain4j.mcp.client.transport.McpTransport;
 import java.io.IOException;
+import java.net.URI;
 import java.time.Duration;
 import java.util.Map;
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.TimeUnit;
 import okhttp3.OkHttpClient;
 import okhttp3.Request;
 import okhttp3.RequestBody;
 import okhttp3.Response;
 import okhttp3.sse.EventSource;
 import okhttp3.sse.EventSources;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class HttpMcpTransport implements McpTransport {
 
     private final String sseUrl;
-    private final String postUrl;
     private final OkHttpClient client;
     private final boolean logResponses;
     private final boolean logRequests;
     private EventSource mcpSseEventListener;
     private static final ObjectMapper OBJECT_MAPPER = new ObjectMapper();
     private final Map<Long, CompletableFuture<JsonNode>> pendingOperations = new ConcurrentHashMap<>();
+    private static final Logger log = LoggerFactory.getLogger(HttpMcpTransport.class);
+
+    // this is obtained from the server after initializing the SSE channel
+    private volatile String postUrl;
 
     public HttpMcpTransport(Builder builder) {
         OkHttpClient.Builder httpClientBuilder = new OkHttpClient.Builder();
-        if (builder.timeout != null) {
-            httpClientBuilder.callTimeout(builder.timeout);
-            httpClientBuilder.connectTimeout(builder.timeout);
-            httpClientBuilder.readTimeout(builder.timeout);
-            httpClientBuilder.writeTimeout(builder.timeout);
-        }
+        Duration timeout = getOrDefault(builder.timeout, Duration.ofSeconds(60));
+        httpClientBuilder.callTimeout(timeout);
+        httpClientBuilder.connectTimeout(timeout);
+        httpClientBuilder.readTimeout(timeout);
+        httpClientBuilder.writeTimeout(timeout);
         this.logRequests = builder.logRequests;
         if (builder.logRequests) {
             httpClientBuilder.addInterceptor(new McpRequestLoggingInterceptor());
         }
         this.logResponses = builder.logResponses;
         sseUrl = ensureNotNull(builder.sseUrl, "Missing SSE endpoint URL");
-        postUrl = ensureNotNull(builder.postUrl, "Missing POST endpoint URL");
         client = httpClientBuilder.build();
     }
 
@@ -116,16 +122,28 @@ public class HttpMcpTransport implements McpTransport {
 
     private EventSource startSseChannel(boolean logResponses) {
         Request request = new Request.Builder().url(sseUrl).build();
-        CompletableFuture<Void> initializationFinished = new CompletableFuture<>();
+        CompletableFuture<String> initializationFinished = new CompletableFuture<>();
         SseEventListener listener = new SseEventListener(pendingOperations, logResponses, initializationFinished);
         EventSource eventSource = EventSources.createFactory(client).newEventSource(request, listener);
-        // wait for the SSE channel to be created, throw an exception if that failed
+        // wait for the SSE channel to be created, receive the POST url from the server, throw an exception if that
+        // failed
         try {
-            initializationFinished.get();
+            int timeout = client.callTimeoutMillis() > 0 ? client.callTimeoutMillis() : Integer.MAX_VALUE;
+            String relativePostUrl = initializationFinished.get(timeout, TimeUnit.MILLISECONDS);
+            postUrl = buildAbsolutePostUrl(relativePostUrl);
+            log.debug("Received the server's POST URL: " + postUrl);
         } catch (Exception e) {
             throw new RuntimeException(e);
         }
         return eventSource;
+    }
+
+    private String buildAbsolutePostUrl(String relativePostUrl) {
+        try {
+            return URI.create(this.sseUrl).resolve(relativePostUrl).toString();
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
     }
 
     @Override
@@ -141,18 +159,16 @@ public class HttpMcpTransport implements McpTransport {
     public static class Builder {
 
         private String sseUrl;
-        private String postUrl;
         private Duration timeout;
         private boolean logRequests = false;
         private boolean logResponses = false;
 
+        /**
+         * The initial URL where to connect to the server and request a SSE
+         * channel.
+         */
         public Builder sseUrl(String sseUrl) {
             this.sseUrl = sseUrl;
-            return this;
-        }
-
-        public Builder postUrl(String postUrl) {
-            this.postUrl = postUrl;
             return this;
         }
 
--- langchain4j-mcp/src/main/java/dev/langchain4j/mcp/client/transport/http/SseEventListener.java
+++ langchain4j-mcp/src/main/java/dev/langchain4j/mcp/client/transport/http/SseEventListener.java
@@ -17,7 +17,8 @@ public class SseEventListener extends EventSourceListener {
     private static final ObjectMapper OBJECT_MAPPER = new ObjectMapper();
     private static final Logger log = LoggerFactory.getLogger(SseEventListener.class);
     private final boolean logEvents;
-    private final CompletableFuture<Void> initializationFinished;
+    // this will contain the POST url for sending commands to the server
+    private final CompletableFuture<String> initializationFinished;
 
     public SseEventListener(
             Map<Long, CompletableFuture<JsonNode>> pendingOperations,
@@ -35,7 +36,13 @@ public class SseEventListener extends EventSourceListener {
 
     @Override
     public void onEvent(EventSource eventSource, String id, String type, String data) {
-        if (type.equals("message")) {
+        if (type.equals("endpoint")) {
+            if (initializationFinished.isDone()) {
+                log.warn("Received endpoint event after initialization");
+                return;
+            }
+            initializationFinished.complete(data);
+        } else if (type.equals("message")) {
             if (logEvents) {
                 log.debug("< {}", data);
             }
@@ -66,7 +73,6 @@ public class SseEventListener extends EventSourceListener {
 
     @Override
     public void onOpen(EventSource eventSource, Response response) {
-        initializationFinished.complete(null);
         log.debug("Connected to SSE channel at {}", response.request().url());
     }
 }
