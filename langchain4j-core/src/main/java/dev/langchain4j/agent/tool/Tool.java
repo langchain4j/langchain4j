aef98d04684640febef6625d3112222925b2cf03
Joshua Sperry <jrsperry@halosight.com>
2024-12-17 08:52:18 -0700

    reverted import order

--- langchain4j-core/src/main/java/dev/langchain4j/agent/tool/Tool.java
+++ langchain4j-core/src/main/java/dev/langchain4j/agent/tool/Tool.java
@@ -1,10 +1,10 @@
 package dev.langchain4j.agent.tool;
 
-import static java.lang.annotation.ElementType.METHOD;
-import static java.lang.annotation.RetentionPolicy.RUNTIME;
-
 import java.lang.annotation.Retention;
 import java.lang.annotation.Target;
+
+import static java.lang.annotation.ElementType.METHOD;
+import static java.lang.annotation.RetentionPolicy.RUNTIME;
 
 /**
  * Java methods annotated with {@code @Tool} are considered tools/functions that language model can execute/call.
