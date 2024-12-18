74f2eb3a95fcb61f93d519a1fb7b13fef18c6d24
ddobrin <ddobrin@google.com>
2024-12-18 17:57:44 -0500

    Fixes #2300 - Update langchain4j-jina module to support late chunking in long-context embedding models

--- langchain4j-jina/src/main/java/dev/langchain4j/model/jina/internal/client/JinaClient.java
+++ langchain4j-jina/src/main/java/dev/langchain4j/model/jina/internal/client/JinaClient.java
@@ -1,18 +1,17 @@
 package dev.langchain4j.model.jina.internal.client;
 
+import static com.fasterxml.jackson.databind.SerializationFeature.INDENT_OUTPUT;
+import static dev.langchain4j.internal.ValidationUtils.ensureNotBlank;
+
 import com.fasterxml.jackson.databind.ObjectMapper;
 import dev.langchain4j.internal.Utils;
 import dev.langchain4j.model.jina.internal.api.*;
+import java.io.IOException;
+import java.time.Duration;
 import lombok.Builder;
 import okhttp3.OkHttpClient;
 import retrofit2.Retrofit;
 import retrofit2.converter.jackson.JacksonConverterFactory;
-
-import java.io.IOException;
-import java.time.Duration;
-
-import static com.fasterxml.jackson.databind.SerializationFeature.INDENT_OUTPUT;
-import static dev.langchain4j.internal.ValidationUtils.ensureNotBlank;
 
 public class JinaClient {
 
@@ -22,7 +21,13 @@ public class JinaClient {
     private final String authorizationHeader;
 
     @Builder
-    JinaClient(String baseUrl, String apiKey, Duration timeout, boolean logRequests, boolean logResponses) {
+    JinaClient(
+            String baseUrl,
+            String apiKey,
+            Duration timeout,
+            boolean lateChunking,
+            boolean logRequests,
+            boolean logResponses) {
 
         OkHttpClient.Builder okHttpClientBuilder = new OkHttpClient.Builder()
                 .callTimeout(timeout)
@@ -49,8 +54,8 @@ public class JinaClient {
 
     public JinaEmbeddingResponse embed(JinaEmbeddingRequest request) {
         try {
-            retrofit2.Response<JinaEmbeddingResponse> retrofitResponse
-                    = jinaApi.embed(request, authorizationHeader).execute();
+            retrofit2.Response<JinaEmbeddingResponse> retrofitResponse =
+                    jinaApi.embed(request, authorizationHeader).execute();
             if (retrofitResponse.isSuccessful()) {
                 return retrofitResponse.body();
             } else {
@@ -63,8 +68,8 @@ public class JinaClient {
 
     public JinaRerankingResponse rerank(JinaRerankingRequest request) {
         try {
-            retrofit2.Response<JinaRerankingResponse> retrofitResponse
-                    = jinaApi.rerank(request, authorizationHeader).execute();
+            retrofit2.Response<JinaRerankingResponse> retrofitResponse =
+                    jinaApi.rerank(request, authorizationHeader).execute();
 
             if (retrofitResponse.isSuccessful()) {
                 return retrofitResponse.body();
