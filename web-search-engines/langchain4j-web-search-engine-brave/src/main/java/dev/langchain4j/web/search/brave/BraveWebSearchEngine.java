95fac426c48c883de437e7f01448efb4ba5ac506
suhas-koheda <sharmasuhas450@gmail.com>
2024-12-27 22:11:48 +0530

    Complete WebSearchEngine

--- web-search-engines/langchain4j-web-search-engine-brave/src/main/java/dev/langchain4j/web/search/brave/BraveWebSearchEngine.java
+++ web-search-engines/langchain4j-web-search-engine-brave/src/main/java/dev/langchain4j/web/search/brave/BraveWebSearchEngine.java
@@ -1,11 +1,17 @@
 package dev.langchain4j.web.search.brave;
 
 import dev.langchain4j.web.search.WebSearchEngine;
+import dev.langchain4j.web.search.WebSearchInformationResult;
+import dev.langchain4j.web.search.WebSearchOrganicResult;
 import dev.langchain4j.web.search.WebSearchRequest;
 import dev.langchain4j.web.search.WebSearchResults;
+import lombok.Builder;
 
+import java.net.URI;
 import java.time.Duration;
+
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 import static dev.langchain4j.internal.Utils.copyIfNotNull;
@@ -18,48 +24,45 @@ import static java.time.Duration.ofSeconds;
  * An implementation of a{@link WebSearchEngine} that uses
  * <a href="https://brave.com/search/api//">Brave Search API</a> for performing web searches.
  */
+@Builder
 class BraveWebSearchEngine implements WebSearchEngine {
     /**
      * Constructs a new BraveSearchEngine with the specified parameters.
      *
      * @param apiKey        the Brave Search API key to access the Brave Search API
-     *                      <p>The API key can be generated at <a href="https://brave.com/search/api/">here</a></p>
-     *
+     * <p>The API key can be generated at <a href="https://brave.com/search/api/">here</a></p>
      * @param count         the parameter that defines the number of web search responses
-     *
      * @param safeSearch    Filters search results for adult content:
-     *                      The following values are supported:
-     *                      - off: No filtering is done.
-     *                      - moderate: Filters explicit content, like images and videos, but allows adult domains in the search results.
-     *                      - strict: Drops all adult content from search results.
-     *
+     * The following values are supported:
+     * - off: No filtering is done.
+     * - moderate: Filters explicit content, like images and videos, but allows adult domains in the search results.
+     * - strict: Drops all adult content from search results.
      * @param resultFilter  Available result filter values are:
-     *                      - summarizer
-     *                      - web (set to default as web)
-     *                      - image
-     *                      Example result filter param:
-     *                      result_filter=discussions,videos returns only discussions and videos responses.
-     *                      Another example where only location results are required, set the result_filter param to result_filter=locations.
-     *
+     * - summarizer
+     * - web (set to default as web)
+     * - image
+     * Example result filter param:
+     * result_filter=discussions,videos returns only discussions and videos responses.
+     * Another example where only location results are required, set the result_filter param to result_filter=locations.
      * @param freshness     Filters search results by when they were discovered.
-     *                      The following values are supported:
-     *                      - pd: Discovered within the last 24 hours.
-     *                      - pw: Discovered within the last 7 days.
-     *                      - pm: Discovered within the last 31 days.
-     *                      - py: Discovered within the last 365 days.
-     *                      - YYYY-MM-DDtoYYYY-MM-DD: timeframe is also supported by specifying the date range
-     *                        e.g., 2022-04-01to2022-07-30.
-     *
+     * The following values are supported:
+     * - pd: Discovered within the last 24 hours.
+     * - pw: Discovered within the last 7 days.
+     * - pm: Discovered within the last 31 days.
+     * - py: Discovered within the last 365 days.
+     * - YYYY-MM-DDtoYYYY-MM-DD: timeframe is also supported by specifying the date range
+     * e.g., 2022-04-01to2022-07-30.
      */
 
     private static final String DEFAULT_BASE_URL = "https://api.search.brave.com/";
     private final String apiKey;
     private final BraveClient braveClient;
-    private final Map<String,Object> optionalParams;
+    private final Map<String, Object> optionalParams;
+
     /**
      * This constructor has a  Builder method for building Brave Search Client with the default parameters:
-     *      baseUrl : It acts as the base URL where the requests are sent - a trailing route /search is added by the retrofit while request is made by user
-     *      timeout : It is the maximum amount of time a network request is allowed to wait for a response from the server before it is considered a failure
+     * baseUrl : It acts as the base URL where the requests are sent - a trailing route /search is added by the retrofit while request is made by user
+     * timeout : It is the maximum amount of time a network request is allowed to wait for a response from the server before it is considered a failure
      */
 
     public BraveWebSearchEngine(String baseUrl,
@@ -78,22 +81,28 @@ class BraveWebSearchEngine implements WebSearchEngine {
     @Override
     public WebSearchResults search(final WebSearchRequest webSearchRequest) {
 
-            BraveWebSearchRequest request=BraveWebSearchRequest.builder()
-                    .apiKey(apiKey)
-                    .query(webSearchRequest.searchTerms())
-                    .optionalParams(optionalParams)
-                    .build();
-            BraveWebSearchResponse response=braveClient.search(request);
-            if (response.getWeb() != null && response.getWeb().getResults() != null) {
-            response.getWeb().getResults().forEach(result -> {
-                System.out.println("Title: " + result.getTitle());
-                System.out.println("URL: " + result.getUrl());
-                System.out.println("Description: " + result.getDescription());
-            });
-        }
+        BraveWebSearchRequest request = BraveWebSearchRequest.builder()
+                .apiKey(apiKey)
+                .query(webSearchRequest.searchTerms())
+                .optionalParams(optionalParams)
+                .build();
+        BraveWebSearchResponse response = braveClient.search(request);
 
-        //todo : refining the output response to find the web results
+        List<WebSearchOrganicResult> results = response.getWeb().getResults().stream()
+                .map(BraveWebSearchEngine::toOrganicResults)
+                .toList();
 
-        return null;
+        return WebSearchResults.from(WebSearchInformationResult.from((long)results.size()), results);
+    }
+
+    static WebSearchOrganicResult toOrganicResults(BraveWebSearchResponse.Web.Result response) {
+        return WebSearchOrganicResult.from(response.getTitle(),
+                URI.create(response.getUrl().trim().replace(" ","+")),
+                response.getDescription(),
+                null);
+    }
+
+    static WebSearchEngine withApiKey(String apiKey) {
+        return builder().apiKey(apiKey).build();
     }
 }
--- web-search-engines/langchain4j-web-search-engine-brave/src/test/java/dev/langchain4j/web/search/brave/BraveWebSearchEngineIT.java
+++ web-search-engines/langchain4j-web-search-engine-brave/src/test/java/dev/langchain4j/web/search/brave/BraveWebSearchEngineIT.java
@@ -10,22 +10,13 @@ import java.util.Map;
 
 public class BraveWebSearchEngineIT extends WebSearchEngineIT {
 
+    WebSearchEngine webSearchEngine = BraveWebSearchEngine.withApiKey(System.getenv("BRAVE_API_KEY"));
     @Override
     protected WebSearchEngine searchEngine() {
-        // Parameters for the BraveWebSearchEngine
-        String baseUrl = "https://api.search.brave.com";
-        String apiKey = ""; // Replace with your API key
-        Duration timeout = Duration.ofSeconds(10);
-        Integer count = WebSearchEngineIT.EXPECTED_MAX_RESULTS;
-        String safeSearch = "off";
-        String resultFilter = "web";
-        String freshness = "";
-        Map<String, Object> params = new HashMap<>();
-        params.put("count",count);
-        params.put("safeSearch",safeSearch);
-        params.put("resultFilter",resultFilter);
-        params.put("freshness",freshness);
-        return new BraveWebSearchEngine(baseUrl,apiKey,timeout,params);
+        return webSearchEngine;
     }
+
+
+
 }
 
