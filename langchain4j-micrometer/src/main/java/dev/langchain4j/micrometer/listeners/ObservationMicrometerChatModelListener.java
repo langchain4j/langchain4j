f929ad7cbd8f3eb35e44504e6f86339284ea721f
Sus4nne <victorisu@gmail.com>
2024-12-28 17:00:10 +0100

    Update ObservationMicrometerChatModelListener.java
    Added enums usage and refactor methods

--- langchain4j-micrometer/src/main/java/dev/langchain4j/micrometer/listeners/ObservationMicrometerChatModelListener.java
+++ langchain4j-micrometer/src/main/java/dev/langchain4j/micrometer/listeners/ObservationMicrometerChatModelListener.java
@@ -1,22 +1,24 @@
 package dev.langchain4j.micrometer.listeners;
 
+import dev.langchain4j.Experimental;
+import dev.langchain4j.micrometer.conventions.AiObservationAttributes;
 import dev.langchain4j.micrometer.conventions.AiObservationMetricAttributes;
 import dev.langchain4j.micrometer.conventions.AiObservationMetricNames;
+import dev.langchain4j.micrometer.conventions.AiOperationType;
 import dev.langchain4j.micrometer.conventions.AiProvider;
 import dev.langchain4j.micrometer.conventions.AiTokenType;
 import dev.langchain4j.model.chat.listener.ChatModelErrorContext;
 import dev.langchain4j.model.chat.listener.ChatModelListener;
-import dev.langchain4j.model.chat.listener.ChatModelRequest;
 import dev.langchain4j.model.chat.listener.ChatModelRequestContext;
 import dev.langchain4j.model.chat.listener.ChatModelResponseContext;
 import io.micrometer.core.instrument.Counter;
 import io.micrometer.core.instrument.MeterRegistry;
 import io.micrometer.observation.Observation;
 import io.micrometer.observation.ObservationRegistry;
-
-import java.util.Arrays;
+import java.util.Map;
 import java.util.concurrent.atomic.AtomicReference;
 
+@Experimental
 public class ObservationMicrometerChatModelListener implements ChatModelListener {
 
     private final MeterRegistry meterRegistry;
@@ -24,7 +26,8 @@ public class ObservationMicrometerChatModelListener implements ChatModelListener
     private final ObservationRegistry observationRegistry;
     private final AtomicReference<Observation.Scope> scope;
 
-    public ObservationMicrometerChatModelListener(final MeterRegistry meterRegistry, ObservationRegistry observationRegistry) {
+    public ObservationMicrometerChatModelListener(
+            final MeterRegistry meterRegistry, ObservationRegistry observationRegistry) {
         this.meterRegistry = meterRegistry;
         this.observationRegistry = observationRegistry;
         this.scope = new AtomicReference<>();
@@ -32,71 +35,39 @@ public class ObservationMicrometerChatModelListener implements ChatModelListener
 
     @Override
     public void onRequest(final ChatModelRequestContext requestContext) {
-        Observation observation = Observation.createNotStarted("gen_ai.client.operation.duration", observationRegistry)
-                .lowCardinalityKeyValue("gen_ai.operation.name", "chat")
-                .lowCardinalityKeyValue("gen_ai.system", "langchain4j")
-                .lowCardinalityKeyValue("gen_ai.request.model", requestContext.request().model())
-                .contextualName("GenAI operation duration")
-                .start();
-
+        setAiProvider(requestContext);
+        Observation observation = createObservation(requestContext);
         scope.set(observation.openScope());
-
-        addAiProvider(requestContext, observation);
-
         addRequestMetrics(requestContext);
     }
 
-    private void addAiProvider(ChatModelRequestContext requestContext, Observation observation) {
-        final ChatModelRequest request = requestContext.request();
-        System.out.println("Request classname: " + request.getClass().getSimpleName()); // Request classname: ChatModelRequest
-
-        // ChatRequest (will be used instead of ChatModelRequest in ChatModelRequestContext)
-        // OpenAiChatRequest extends ChatRequest
-        // OpenAiChatResponse extends ChatResponse
-
-        final String className = requestContext.request().getClass().getSimpleName();// ChatRequest
-        String providerClassNamePrefix = className.endsWith("ChatRequest")
-                ? className.substring(0, className.length() - "ChatRequest".length()).toLowerCase()
-                : className.toLowerCase();
-
-        Arrays.stream(AiProvider.values())
-                .filter(provider -> provider.name().toLowerCase().equals(providerClassNamePrefix))
-                .findFirst()
-                .ifPresent(provider -> {
-                    observation.lowCardinalityKeyValue("gen_ai.provider", provider.value());
-                    requestContext.attributes().put("gen_ai.provider", provider.value());
-                });
+    private void setAiProvider(ChatModelRequestContext requestContext) {
+        final AiProvider aiProvider =
+                AiProvider.fromClass(requestContext.request().getClass());
+        requestContext.attributes().put("gen_ai.provider", aiProvider.value());
     }
 
-    private void addAiProvider(ChatModelResponseContext responseContext, Observation observation) {
-        final ChatModelRequest response = responseContext.request();
-        System.out.println("Response classname: " + response.getClass().getSimpleName()); // Request classname: ChatModelRequest
-
-        // ChatRequest (will be used instead of ChatModelRequest in ChatModelRequestContext)
-        // OpenAiChatRequest extends ChatRequest
-        // OpenAiChatResponse extends ChatResponse
-
-        final String className = responseContext.request().getClass().getSimpleName();// ChatRequest
-        String providerClassNamePrefix = className.endsWith("ChatResponse")
-                ? className.substring(0, className.length() - "ChatResponse".length()).toLowerCase()
-                : className.toLowerCase();
-
-        Arrays.stream(AiProvider.values())
-                .filter(provider -> provider.name().toLowerCase().equals(providerClassNamePrefix))
-                .findFirst()
-                .ifPresent(provider -> {
-                    observation.lowCardinalityKeyValue("gen_ai.provider", provider.value());
-                    responseContext.attributes().put("gen_ai.provider", provider.value());
-                });
+    private Observation createObservation(ChatModelRequestContext requestContext) {
+        return Observation.createNotStarted(AiObservationMetricNames.OPERATION_DURATION.value(), observationRegistry)
+                .lowCardinalityKeyValue(AiObservationAttributes.AI_OPERATION_TYPE.value(), AiOperationType.CHAT.value())
+                .lowCardinalityKeyValue(
+                        AiObservationAttributes.AI_PROVIDER.value(), getSystemValue(requestContext.attributes()))
+                .lowCardinalityKeyValue(
+                        AiObservationAttributes.REQUEST_MODEL.value(),
+                        requestContext.request().model())
+                .contextualName("GenAI operation duration")
+                .start();
     }
 
     private void addRequestMetrics(ChatModelRequestContext requestContext) {
         if (requestContext.request() != null) {
             Counter.builder("langchain4j.chat.model.request")
                     .tag("gen_ai.operation.name", "chat")
-                    .tag("gen_ai.system", requestContext.attributes().get("gen_ai.provider") != null
-                            ? String.valueOf(requestContext.attributes().get("gen_ai.provider"))
-                            : "langchain4j")
+                    .tag(
+                            "gen_ai.system",
+                            requestContext.attributes().get("gen_ai.provider") != null
+                                    ? String.valueOf(requestContext.attributes().get("gen_ai.provider"))
+                                    : "langchain4j")
                     .tag("gen_ai.request.model", requestContext.request().model())
                     .description("The number of requests that were made to the chat model")
                     .register(meterRegistry)
@@ -106,77 +77,95 @@ public class ObservationMicrometerChatModelListener implements ChatModelListener
 
     @Override
     public void onResponse(final ChatModelResponseContext responseContext) {
-        Observation.Scope scope = this.scope.getAndSet(null);
-        if (scope != null) {
-            Observation observation = scope.getCurrentObservation();
-            if (observation != null) {
-                observation.lowCardinalityKeyValue("gen_ai.response.model", responseContext.response().model())
-                        .stop();
-                addAiProvider(responseContext, scope.getCurrentObservation());
-            }
-        }
-
+        handleResponseObservationScope(responseContext);
         addResponseMetrics(responseContext);
+    }
+
+    private void handleResponseObservationScope(ChatModelResponseContext responseContext) {
+        Observation.Scope currentScope = this.scope.getAndSet(null);
+        if (currentScope != null) {
+            Observation observation = currentScope.getCurrentObservation();
+            updateObservationWithResponse(observation, responseContext);
+            observation.stop();
+        }
+    }
+
+    private void updateObservationWithResponse(Observation observation, ChatModelResponseContext responseContext) {
+        observation.lowCardinalityKeyValue(
+                AiObservationAttributes.RESPONSE_MODEL.value(),
+                responseContext.response().model());
     }
 
     private void addResponseMetrics(ChatModelResponseContext responseContext) {
         if (responseContext.response().tokenUsage() != null) {
-            Counter.builder(AiObservationMetricNames.TOKEN_USAGE.value())
-                    .tag("gen_ai.operation.name", "chat")
-                    .tag("gen_ai.system", responseContext.attributes().get("gen_ai.provider") != null
-                            ? String.valueOf(responseContext.attributes().get("gen_ai.provider"))
-                            : "langchain4j")
-                    .tag("gen_ai.request.model", responseContext.request().model())
-                    .tag("gen_ai.response.model", responseContext.response().model())
-                    .tag(AiObservationMetricAttributes.TOKEN_TYPE.value(), AiTokenType.INPUT.value())
-                    .description(DESCRIPTION)
-                    .register(meterRegistry)
-                    .increment(responseContext.response().tokenUsage().inputTokenCount());
-
-            Counter.builder(AiObservationMetricNames.TOKEN_USAGE.value())
-                    .tag("gen_ai.operation.name", "chat")
-                    .tag("gen_ai.system", "langchain4j")
-                    .tag("gen_ai.request.model", responseContext.request().model())
-                    .tag("gen_ai.response.model", responseContext.response().model())
-                    .tag(AiObservationMetricAttributes.TOKEN_TYPE.value(), AiTokenType.OUTPUT.value())
-                    .description(DESCRIPTION)
-                    .register(meterRegistry)
-                    .increment(responseContext.response().tokenUsage().outputTokenCount());
-
-            Counter.builder(AiObservationMetricNames.TOKEN_USAGE.value())
-                    .tag("gen_ai.operation.name", "chat")
-                    .tag("gen_ai.system", "langchain4j")
-                    .tag("gen_ai.request.model", responseContext.request().model())
-                    .tag("gen_ai.response.model", responseContext.response().model())
-                    .tag(AiObservationMetricAttributes.TOKEN_TYPE.value(), AiTokenType.TOTAL.value())
-                    .description(DESCRIPTION)
-                    .register(meterRegistry)
-                    .increment(responseContext.response().tokenUsage().totalTokenCount());
+            addTokenUsageMetrics(responseContext);
         }
+    }
+
+    private void addTokenUsageMetrics(ChatModelResponseContext responseContext) {
+        addTokenMetric(
+                responseContext,
+                AiTokenType.INPUT,
+                responseContext.response().tokenUsage().inputTokenCount());
+        addTokenMetric(
+                responseContext,
+                AiTokenType.OUTPUT,
+                responseContext.response().tokenUsage().outputTokenCount());
+        addTokenMetric(
+                responseContext,
+                AiTokenType.TOTAL,
+                responseContext.response().tokenUsage().totalTokenCount());
+    }
+
+    private void addTokenMetric(ChatModelResponseContext responseContext, AiTokenType tokenType, int tokenCount) {
+        Counter.builder(AiObservationMetricNames.TOKEN_USAGE.value())
+                .tag("gen_ai.operation.name", "chat")
+                .tag("gen_ai.system", getSystemValue(responseContext.attributes()))
+                .tag("gen_ai.request.model", responseContext.request().model())
+                .tag("gen_ai.response.model", responseContext.response().model())
+                .tag(AiObservationMetricAttributes.TOKEN_TYPE.value(), tokenType.value())
+                .description(DESCRIPTION)
+                .register(meterRegistry)
+                .increment(tokenCount);
+    }
+
+    private String getSystemValue(Map<Object, Object> attributes) {
+        return attributes.get("gen_ai.provider") != null
+                ? String.valueOf(attributes.get("gen_ai.provider"))
+                : AiProvider.LANGCHAIN4J.value();
     }
 
     @Override
     public void onError(final ChatModelErrorContext errorContext) {
-        Observation.Scope scope = this.scope.getAndSet(null);
-        if (scope != null) {
-            Observation observation = scope.getCurrentObservation();
-            if (observation != null) {
-                observation.lowCardinalityKeyValue("gen_ai.request.model", errorContext.request().model())
-                        .lowCardinalityKeyValue("error.type", errorContext.error().getClass().getSimpleName());
-                observation.error(errorContext.error());
-
-                observation.stop();
-            }
-        }
-
-        addErrorMetrics(errorContext);
+        handleErrorObservationScope(errorContext);
+        addErrorMetric(errorContext);
     }
 
-    private void addErrorMetrics(ChatModelErrorContext errorContext) {
+    private void handleErrorObservationScope(ChatModelErrorContext errorContext) {
+        Observation.Scope currentScope = this.scope.getAndSet(null);
+        if (currentScope != null) {
+            Observation observation = currentScope.getCurrentObservation();
+            updateObservationWithError(observation, errorContext);
+            observation.error(errorContext.error());
+            observation.stop();
+        }
+    }
+
+    private void updateObservationWithError(Observation observation, ChatModelErrorContext errorContext) {
+        observation
+                .lowCardinalityKeyValue(
+                        AiObservationAttributes.REQUEST_MODEL.value(),
+                        errorContext.request().model())
+                .lowCardinalityKeyValue(
+                        AiObservationAttributes.ERROR_TYPE.value(),
+                        errorContext.error().getClass().getSimpleName());
+    }
+
+    private void addErrorMetric(ChatModelErrorContext errorContext) {
         if (errorContext.request() != null) {
             Counter.builder("langchain4j.chat.model.error")
                     .tag("gen_ai.operation.name", "chat")
-                    .tag("gen_ai.system", "langchain4j")
+                    .tag("gen_ai.system", getSystemValue(errorContext.attributes()))
                     .tag("gen_ai.request.model", errorContext.request().model())
                     .description("The number of errors that occurred in the chat model")
                     .register(meterRegistry)
