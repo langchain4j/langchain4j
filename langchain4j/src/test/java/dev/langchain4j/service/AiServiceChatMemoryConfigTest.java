9fa849d9a00fc8914ed90361dd7a5078229713fd
1814liyongbin <77563764+1814liyongbin@users.noreply.github.com>
2025-01-05 20:05:24 +0800

    Update langchain4j/src/test/java/dev/langchain4j/service/AiServiceChatMemoryConfigTest.java
    I noticed that other test classes in the project are also not declared with public. Is it because test classes should not be accessed externally, and therefore they are not set to public? Thank you for your advice.
    
    Co-authored-by: Alexey Titov <33568148+Heezer@users.noreply.github.com>

--- langchain4j/src/test/java/dev/langchain4j/service/AiServiceChatMemoryConfigTest.java
+++ langchain4j/src/test/java/dev/langchain4j/service/AiServiceChatMemoryConfigTest.java
@@ -17,7 +17,7 @@ import org.mockito.Spy;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 @ExtendWith(MockitoExtension.class)
-public class AiServiceChatMemoryConfigTest {
+class AiServiceChatMemoryConfigTest {
     @Spy
     ChatLanguageModel chatLanguageModel = ChatModelMock.thatAlwaysResponds("Berlin");
 
