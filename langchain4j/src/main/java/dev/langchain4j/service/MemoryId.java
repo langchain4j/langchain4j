9493b1ae6d4e2e182fad46c3e88fb068a387d0c1
liyongbin <2637230700@qq.com>
2025-01-06 09:32:08 +0800

    refactor(langchain4j):mvn spotless:apply for MemoryId

--- langchain4j/src/main/java/dev/langchain4j/service/MemoryId.java
+++ langchain4j/src/main/java/dev/langchain4j/service/MemoryId.java
@@ -1,12 +1,11 @@
 package dev.langchain4j.service;
 
-import dev.langchain4j.memory.chat.ChatMemoryProvider;
-
-import java.lang.annotation.Retention;
-import java.lang.annotation.Target;
-
 import static java.lang.annotation.ElementType.PARAMETER;
 import static java.lang.annotation.RetentionPolicy.RUNTIME;
+
+import dev.langchain4j.memory.chat.ChatMemoryProvider;
+import java.lang.annotation.Retention;
+import java.lang.annotation.Target;
 
 /**
  * The value of a method parameter annotated with @MemoryId will be used to find the memory belonging to that user/conversation.
@@ -17,6 +16,4 @@ import static java.lang.annotation.RetentionPolicy.RUNTIME;
  */
 @Retention(RUNTIME)
 @Target(PARAMETER)
-public @interface MemoryId {
-
-}
+public @interface MemoryId {}
