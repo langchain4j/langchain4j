d338c1de542af91370dd6a47c9c42d207667e8e4
Dmytro Liubarskyi <ljubarskij@gmail.com>
2025-01-14 19:14:43 +0100

    WIP

--- langchain4j-core/src/main/java/dev/langchain4j/model/chat/ListenableChatModel.java
+++ langchain4j-core/src/main/java/dev/langchain4j/model/chat/ListenableChatModel.java
@@ -14,6 +14,7 @@ import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
 
 /**
+ * // TODO reword
  * A decorator for {@link ChatLanguageModel} that provides event notifications through {@link ChatModelListener}s
  * during the lifecycle of LLM API requests.
  * <p>
