4c3784a3a271446e86e840091bdab60cfeb4b4d8
Jan Martiska <jmartisk@redhat.com>
2025-01-28 12:00:06 +0100

    MCP: support logging (#2458)
    Fixes https://github.com/langchain4j/langchain4j/issues/2457

---
+++ langchain4j-mcp/src/test/java/dev/langchain4j/mcp/client/integration/McpLoggingTestBase.java
@@ -0,0 +1,50 @@
+package dev.langchain4j.mcp.client.integration;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import dev.langchain4j.agent.tool.ToolExecutionRequest;
+import dev.langchain4j.mcp.client.McpClient;
+import dev.langchain4j.mcp.client.logging.McpLogLevel;
+import dev.langchain4j.mcp.client.logging.McpLogMessage;
+import java.time.Duration;
+import java.util.List;
+import java.util.concurrent.TimeoutException;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+public abstract class McpLoggingTestBase {
+
+    static McpClient mcpClient;
+    static TestMcpLogHandler logMessageHandler;
+
+    @BeforeEach
+    public void clearMessages() {
+        logMessageHandler.clearMessages();
+    }
+
+    @Test
+    public void receiveInfoLogMessage() throws TimeoutException {
+        String result = mcpClient.executeTool(
+                ToolExecutionRequest.builder().arguments("{}").name("info").build());
+        assertThat(result).isEqualTo("ok");
+        List<McpLogMessage> receivedMessages = logMessageHandler.waitForAtLeastOneMessageAndGet(Duration.ofSeconds(10));
+        assertThat(receivedMessages).hasSize(1);
+        McpLogMessage message = receivedMessages.get(0);
+        assertThat(message.level()).isEqualTo(McpLogLevel.INFO);
+        assertThat(message.logger()).isEqualTo("tool:info");
+        assertThat(message.data().asText()).isEqualTo("HELLO");
+    }
+
+    @Test
+    public void receiveDebugLogMessage() throws TimeoutException {
+        String result = mcpClient.executeTool(
+                ToolExecutionRequest.builder().arguments("{}").name("debug").build());
+        assertThat(result).isEqualTo("ok");
+        List<McpLogMessage> receivedMessages = logMessageHandler.waitForAtLeastOneMessageAndGet(Duration.ofSeconds(10));
+        assertThat(receivedMessages).hasSize(1);
+        McpLogMessage message = receivedMessages.get(0);
+        assertThat(message.level()).isEqualTo(McpLogLevel.DEBUG);
+        assertThat(message.logger()).isEqualTo("tool:debug");
+        assertThat(message.data().asText()).isEqualTo("HELLO DEBUG");
+    }
+}
