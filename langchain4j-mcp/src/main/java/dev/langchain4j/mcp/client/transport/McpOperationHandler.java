2b423f5efd343479eebf681999cdd32f98c56285
Jan Martiska <jmartisk@redhat.com>
2025-01-28 11:09:44 +0100

    Support for MCP logging

--- langchain4j-mcp/src/main/java/dev/langchain4j/mcp/client/transport/McpOperationHandler.java
+++ langchain4j-mcp/src/main/java/dev/langchain4j/mcp/client/transport/McpOperationHandler.java
@@ -1,9 +1,11 @@
 package dev.langchain4j.mcp.client.transport;
 
 import com.fasterxml.jackson.databind.JsonNode;
+import dev.langchain4j.mcp.client.logging.McpLogMessage;
 import dev.langchain4j.mcp.client.protocol.PingResponse;
 import java.util.Map;
 import java.util.concurrent.CompletableFuture;
+import java.util.function.Consumer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -18,10 +20,15 @@ public class McpOperationHandler {
     private final Map<Long, CompletableFuture<JsonNode>> pendingOperations;
     private static final Logger log = LoggerFactory.getLogger(McpOperationHandler.class);
     private final McpTransport transport;
+    private final Consumer<McpLogMessage> logMessageConsumer;
 
-    public McpOperationHandler(Map<Long, CompletableFuture<JsonNode>> pendingOperations, McpTransport transport) {
+    public McpOperationHandler(
+            Map<Long, CompletableFuture<JsonNode>> pendingOperations,
+            McpTransport transport,
+            Consumer<McpLogMessage> logMessageConsumer) {
         this.pendingOperations = pendingOperations;
         this.transport = transport;
+        this.logMessageConsumer = logMessageConsumer;
     }
 
     public void handle(JsonNode message) {
@@ -40,8 +47,17 @@ public class McpOperationHandler {
                 }
                 log.warn("Received response for unknown message id: {}", messageId);
             }
+        } else if (message.has("method") && message.get("method").asText().equals("notifications/message")) {
+            // this is a log message
+            if (message.has("params")) {
+                if (logMessageConsumer != null) {
+                    logMessageConsumer.accept(McpLogMessage.fromJson(message.get("params")));
+                }
+            } else {
+                log.warn("Received log message without params: {}", message);
+            }
         } else {
-            log.warn("Received message without id: {}", message);
+            log.warn("Received unknown message: {}", message);
         }
     }
 
