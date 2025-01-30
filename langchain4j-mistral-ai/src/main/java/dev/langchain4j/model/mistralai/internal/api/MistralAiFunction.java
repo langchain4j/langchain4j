26d5b9948c866c7b860e4b71dc3947b3040c569c
Emmanuel Hugonnet <ehsavoie@users.noreply.github.com>
2025-01-30 15:57:59 +0100

    Remove Lombok in LC4J-mistral-ai (#2214)
    ## Issue
    Contributes to https://github.com/langchain4j/langchain4j/issues/1636
    
    ## Change
    Remove Lombok in LC4J-mistral-ai
    
    Replaced Lombok annotations with manual builder methods for better
    control and readability.
    
    
    ## General checklist
    - [X] There are no breaking changes
    - [ ] I have added unit and integration tests for my change
    - [ ] I have manually run all the unit and integration tests in the
    module I have added/changed, and they are all green
    - [ ] I have manually run all the unit and integration tests in the
    [core](https://github.com/langchain4j/langchain4j/tree/main/langchain4j-core)
    and
    [main](https://github.com/langchain4j/langchain4j/tree/main/langchain4j)
    modules, and they are all green
    - [ ] I have added/updated the
    [documentation](https://github.com/langchain4j/langchain4j/tree/main/docs/docs)
    - [ ] I have added an example in the [examples
    repo](https://github.com/langchain4j/langchain4j-examples) (only for
    "big" features)
    - [ ] I have added/updated [Spring Boot
    starter(s)](https://github.com/langchain4j/langchain4j-spring) (if
    applicable)

--- langchain4j-mistral-ai/src/main/java/dev/langchain4j/model/mistralai/internal/api/MistralAiFunction.java
+++ langchain4j-mistral-ai/src/main/java/dev/langchain4j/model/mistralai/internal/api/MistralAiFunction.java
@@ -1,27 +1,115 @@
 package dev.langchain4j.model.mistralai.internal.api;
 
+import static com.fasterxml.jackson.annotation.JsonInclude.Include.NON_NULL;
+
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.databind.PropertyNamingStrategies.SnakeCaseStrategy;
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
 import com.fasterxml.jackson.databind.annotation.JsonNaming;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import com.fasterxml.jackson.databind.annotation.JsonPOJOBuilder;
+import java.util.Objects;
+import java.util.StringJoiner;
 
-import static com.fasterxml.jackson.annotation.JsonInclude.Include.NON_NULL;
-
-@Data
-@NoArgsConstructor
-@AllArgsConstructor
-@Builder
 @JsonInclude(NON_NULL)
 @JsonIgnoreProperties(ignoreUnknown = true)
 @JsonNaming(SnakeCaseStrategy.class)
+@JsonDeserialize(builder = MistralAiFunction.MistralAiFunctionBuilder.class)
 public class MistralAiFunction {
-
     private String name;
     private String description;
     private MistralAiParameters parameters;
 
+    private MistralAiFunction(MistralAiFunctionBuilder builder) {
+        this.name = builder.name;
+        this.description = builder.description;
+        this.parameters = builder.parameters;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public String getDescription() {
+        return this.description;
+    }
+
+    public MistralAiParameters getParameters() {
+        return this.parameters;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 3;
+        hash = 79 * hash + Objects.hashCode(this.name);
+        hash = 79 * hash + Objects.hashCode(this.description);
+        hash = 79 * hash + Objects.hashCode(this.parameters);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) return true;
+        if (obj == null || getClass() != obj.getClass()) return false;
+        final MistralAiFunction other = (MistralAiFunction) obj;
+        return Objects.equals(this.name, other.name)
+                && Objects.equals(this.description, other.description)
+                && Objects.equals(this.parameters, other.parameters);
+    }
+
+    @Override
+    public String toString() {
+        return new StringJoiner(", ", "MistralAiFunction [", "]")
+                .add("name=" + this.getName())
+                .add("description=" + this.getDescription())
+                .add("parameters=" + this.getParameters())
+                .toString();
+    }
+
+    public static MistralAiFunction.MistralAiFunctionBuilder builder() {
+        return new MistralAiFunction.MistralAiFunctionBuilder();
+    }
+
+    @JsonPOJOBuilder(withPrefix = "")
+    @JsonIgnoreProperties(ignoreUnknown = true)
+    @JsonNaming(SnakeCaseStrategy.class)
+    public static class MistralAiFunctionBuilder {
+        private String name;
+        private String description;
+        private MistralAiParameters parameters;
+
+        private MistralAiFunctionBuilder() {}
+
+        /**
+         * @return {@code this}.
+         */
+        public MistralAiFunctionBuilder name(String name) {
+            this.name = name;
+            return this;
+        }
+
+        /**
+         * @return {@code this}.
+         */
+        public MistralAiFunctionBuilder description(String description) {
+            this.description = description;
+            return this;
+        }
+
+        /**
+         * @return {@code this}.
+         */
+        public MistralAiFunctionBuilder parameters(MistralAiParameters parameters) {
+            this.parameters = parameters;
+            return this;
+        }
+
+        public MistralAiFunction build() {
+            return new MistralAiFunction(this);
+        }
+    }
 }
