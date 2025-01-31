c37f2c313508450b5fcf9736e57439c51a6350ce
Dmytro Liubarskyi <ljubarskij@gmail.com>
2025-01-31 18:04:27 +0100

    OpenAI: removed deprecated models (2)

--- langchain4j-open-ai/src/test/java/dev/langchain4j/model/openai/OpenAiStreamingChatModelIT.java
+++ langchain4j-open-ai/src/test/java/dev/langchain4j/model/openai/OpenAiStreamingChatModelIT.java
@@ -574,13 +574,10 @@ class OpenAiStreamingChatModelIT {
             value = OpenAiChatModelName.class,
             mode = EXCLUDE,
             names = {
-                "GPT_4_32K",
-                "GPT_4_32K_0613",
-                "O1",
+                "GPT_4_32K", // don't have access
+                "GPT_4_32K_0613", // don't have access
+                "O1", // don't have access
                 "O1_2024_12_17", // don't have access
-                "GPT_4_VISION_PREVIEW", // deprecated
-                "GPT_4_32K_0314", // deprecated
-                "GPT_4_0314" // deprecated
             })
     void should_support_all_model_names(OpenAiChatModelName modelName) {
 
