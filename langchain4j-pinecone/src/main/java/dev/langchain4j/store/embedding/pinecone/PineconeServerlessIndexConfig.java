57c6ee41de7e742b0036940bcc0d26026630a3c2
Omar <omar.taiseerr@gmail.com>
2025-01-31 14:11:36 +0200

    upgrade-pinecone-and-add-ability-to-protect-deletion-index (#2368)
    ## Issue
    Closes #https://github.com/langchain4j/langchain4j/issues/2367
    
    and support new feature introduced in pinecone 2.0 about deletion
    protection
    https://github.com/pinecone-io/pinecone-java-client/releases/tag/v2.0.0
    
    ## Change
    
    
    ## General checklist
    - [x] There are no breaking changes
    - [ ] I have added unit and/or integration tests for my change
    - [ ] The tests cover both positive and negative cases
    - [x] I have manually run all the unit and integration tests in the
    module I have added/changed, and they are all green
    - [x] I have manually run all the unit and integration tests in the
    [core](https://github.com/langchain4j/langchain4j/tree/main/langchain4j-core)
    and
    [main](https://github.com/langchain4j/langchain4j/tree/main/langchain4j)
    modules, and they are all green
    - [ ] I have added/updated the
    [documentation](https://github.com/langchain4j/langchain4j/tree/main/docs/docs)
    - [ ] I have added an example in the [examples
    repo](https://github.com/langchain4j/langchain4j-examples) (only for
    "big" features)
    - [ ] I have added/updated [Spring Boot
    starter(s)](https://github.com/langchain4j/langchain4j-spring) (if
    applicable)

--- langchain4j-pinecone/src/main/java/dev/langchain4j/store/embedding/pinecone/PineconeServerlessIndexConfig.java
+++ langchain4j-pinecone/src/main/java/dev/langchain4j/store/embedding/pinecone/PineconeServerlessIndexConfig.java
@@ -1,8 +1,8 @@
 package dev.langchain4j.store.embedding.pinecone;
 
 import io.pinecone.clients.Pinecone;
+import org.openapitools.db_control.client.model.DeletionProtection;
 
-import static dev.langchain4j.internal.Utils.getOrDefault;
 import static dev.langchain4j.internal.ValidationUtils.ensureNotNull;
 
 public class PineconeServerlessIndexConfig implements PineconeIndexConfig {
@@ -10,10 +10,24 @@ public class PineconeServerlessIndexConfig implements PineconeIndexConfig {
     private final Integer dimension;
     private final String cloud;
     private final String region;
+    private final DeletionProtection deletionProtection;
 
     PineconeServerlessIndexConfig(Integer dimension,
                                   String cloud,
                                   String region) {
+        this(
+                dimension,
+                cloud,
+                region,
+                DeletionProtection.ENABLED
+        );
+    }
+
+    PineconeServerlessIndexConfig(Integer dimension,
+                                  String cloud,
+                                  String region,
+                                  DeletionProtection deletionProtection){
+
         cloud = ensureNotNull(cloud, "cloud");
         region = ensureNotNull(region, "region");
         ensureNotNull(dimension, "dimension");
@@ -21,13 +35,14 @@ public class PineconeServerlessIndexConfig implements PineconeIndexConfig {
         this.dimension = dimension;
         this.cloud = cloud;
         this.region = region;
+        this.deletionProtection = deletionProtection;
     }
 
     @Override
     public void createIndex(Pinecone pinecone, String index) {
         ensureNotNull(pinecone, "pinecone");
         ensureNotNull(index, "index");
-        pinecone.createServerlessIndex(index, "cosine", dimension, cloud, region);
+        pinecone.createServerlessIndex(index, "cosine", dimension, cloud, region, deletionProtection);
     }
 
     public static Builder builder() {
@@ -39,6 +54,7 @@ public class PineconeServerlessIndexConfig implements PineconeIndexConfig {
         private Integer dimension;
         private String cloud;
         private String region;
+        private DeletionProtection deletionProtection;
 
         public Builder dimension(Integer dimension) {
             this.dimension = dimension;
@@ -55,8 +71,16 @@ public class PineconeServerlessIndexConfig implements PineconeIndexConfig {
             return this;
         }
 
+        public Builder deletionProtection(DeletionProtection deletionProtection) {
+            this.deletionProtection = deletionProtection;
+            return this;
+        }
+
         public PineconeServerlessIndexConfig build() {
-            return new PineconeServerlessIndexConfig(dimension, cloud, region);
+            if (deletionProtection == null) {
+                return new PineconeServerlessIndexConfig(dimension, cloud, region);
+            }
+            return new PineconeServerlessIndexConfig(dimension, cloud, region, deletionProtection);
         }
     }
 }
