b03d04b3d6a90dd20e203269df2c89b3463f0500
mariofusco <mario.fusco@gmail.com>
2025-02-04 11:32:03 +0100

    wip

--- langchain4j/src/main/java/dev/langchain4j/service/AiServiceContext.java
+++ langchain4j/src/main/java/dev/langchain4j/service/AiServiceContext.java
@@ -1,5 +1,6 @@
 package dev.langchain4j.service;
 
+import dev.langchain4j.data.message.ChatMessage;
 import dev.langchain4j.memory.ChatMemory;
 import dev.langchain4j.memory.chat.ChatMemoryProvider;
 import dev.langchain4j.memory.chat.StorelessChatMemory;
@@ -10,7 +11,8 @@ import dev.langchain4j.rag.RetrievalAugmentor;
 import dev.langchain4j.service.tool.ToolService;
 import dev.langchain4j.store.memory.chat.ChatMemoryStore;
 import dev.langchain4j.store.memory.chat.InMemoryChatMemoryStore;
-import dev.langchain4j.store.memory.chat.SingleEntryChatMemoryStore;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Optional;
 import java.util.function.Function;
 
@@ -76,5 +78,25 @@ public class AiServiceContext {
         }
         ChatMemory chatMemory = chatMemoryProvider.get(memoryId);
         return chatMemory instanceof StorelessChatMemory storeless ? storeless.withStore(contextStore) : chatMemory;
+    }
+
+    static class SingleEntryChatMemoryStore implements ChatMemoryStore {
+
+        List<ChatMessage> chatMessages = new ArrayList<>();
+
+        @Override
+        public List<ChatMessage> getMessages(final Object memoryId) {
+            return chatMessages;
+        }
+
+        @Override
+        public void updateMessages(final Object memoryId, final List<ChatMessage> messages) {
+            this.chatMessages = messages;
+        }
+
+        @Override
+        public void deleteMessages(final Object memoryId) {
+            chatMessages.clear();
+        }
     }
 }
