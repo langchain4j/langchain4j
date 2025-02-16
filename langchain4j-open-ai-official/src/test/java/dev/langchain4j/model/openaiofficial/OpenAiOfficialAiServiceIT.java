cb5c347d8679db7e753a2f98d94b62155cc8562a
Julien Dubois <julien.dubois@gmail.com>
2025-02-16 20:54:06 +0100

    Add OPEN_AI_CHAT_MODEL for test

--- langchain4j-open-ai-official/src/test/java/dev/langchain4j/model/openaiofficial/OpenAiOfficialAiServiceIT.java
+++ langchain4j-open-ai-official/src/test/java/dev/langchain4j/model/openaiofficial/OpenAiOfficialAiServiceIT.java
@@ -1,5 +1,6 @@
 package dev.langchain4j.model.openaiofficial;
 
+import static dev.langchain4j.model.openaiofficial.OpenAiOfficialChatModelIT.OPEN_AI_CHAT_MODEL;
 import static dev.langchain4j.model.openaiofficial.OpenAiOfficialChatModelIT.OPEN_AI_CHAT_MODEL_STRICT_SCHEMA;
 
 import dev.langchain4j.model.chat.ChatLanguageModel;
@@ -11,7 +12,7 @@ class OpenAiOfficialAiServiceIT extends AbstractAiServiceIT {
     @Override
     protected List<ChatLanguageModel> models() {
         return List.of(
-                // OPEN_AI_CHAT_MODEL, //TODO FIX this doesn't run reliably when generating JSON (as there is no schema)
+                OPEN_AI_CHAT_MODEL,
                 OPEN_AI_CHAT_MODEL_STRICT_SCHEMA);
     }
 }
