6db9bfb90be169f8d70ddc3670ee2616869335d0
tenpigs267 <126336487+tenpigs267@users.noreply.github.com>
2025-02-19 14:35:47 +0100

    add doc reference for BedrockModel capabilities

--- langchain4j-bedrock/src/test/java/dev/langchain4j/model/bedrock/TestedModelsWithConverseAPI.java
+++ langchain4j-bedrock/src/test/java/dev/langchain4j/model/bedrock/TestedModelsWithConverseAPI.java
@@ -4,6 +4,14 @@ import dev.langchain4j.model.chat.common.ChatLanguageModelCapabilities;
 
 public final class TestedModelsWithConverseAPI {
 
+    //JsonResponseFormat and JsonResponseFormatWithSchema are not yet supported by Bedrock ConverseAPI
+
+    // ToolChoice "only supported by Anthropic Claude 3 models and by Mistral AI Mistral Large" from
+    // https://docs.aws.amazon.com/bedrock/latest/APIReference/API_runtime_ToolChoice.html
+
+
+    // AWS_NOVA_MICRO doesn't support image as input parameters
+    // https://docs.aws.amazon.com/bedrock/latest/userguide/models-supported.html
     public static final ChatLanguageModelCapabilities AWS_NOVA_MICRO = ChatLanguageModelCapabilities.builder()
             .model(BedrockChatModel.builder()
                     .modelId("us.amazon.nova-micro-v1:0")
@@ -34,6 +42,8 @@ public final class TestedModelsWithConverseAPI {
             .assertExceptionType(false)
             .build();
 
+    // ToolChoice "only supported by Anthropic Claude 3 models and by Mistral AI Mistral Large" from
+    // https://docs.aws.amazon.com/bedrock/latest/APIReference/API_runtime_ToolChoice.html
     public static final ChatLanguageModelCapabilities CLAUDE_3_HAIKU = ChatLanguageModelCapabilities.builder()
             .model(BedrockChatModel.builder()
                     .modelId("anthropic.claude-3-haiku-20240307-v1:0")
@@ -43,6 +53,10 @@ public final class TestedModelsWithConverseAPI {
             .assertExceptionType(false)
             .build();
 
+    // ToolChoice "only supported by Anthropic Claude 3 models and by Mistral AI Mistral Large" from
+    // https://docs.aws.amazon.com/bedrock/latest/APIReference/API_runtime_ToolChoice.html
+    // MISTRAL_LARGE doesn't support image as input parameters
+    // https://docs.aws.amazon.com/bedrock/latest/userguide/models-supported.html
     public static final ChatLanguageModelCapabilities MISTRAL_LARGE = ChatLanguageModelCapabilities.builder()
             .model(BedrockChatModel.builder()
                     .modelId("mistral.mistral-large-2402-v1:0")
@@ -64,6 +78,8 @@ public final class TestedModelsWithConverseAPI {
             .assertExceptionType(false)
             .build();
 
+    // COHERE_COMMAND_R_PLUS doesn't support image as input parameters
+    // https://docs.aws.amazon.com/bedrock/latest/userguide/models-supported.html
     public static final ChatLanguageModelCapabilities COHERE_COMMAND_R_PLUS = ChatLanguageModelCapabilities.builder()
             .model(BedrockChatModel.builder()
                     .modelId("cohere.command-r-plus-v1:0")
@@ -76,6 +92,8 @@ public final class TestedModelsWithConverseAPI {
             .assertExceptionType(false)
             .build();
 
+    // AWS_NOVA_MICRO, COHERE_COMMAND_R_PLUS, AI_JAMBA_1_5_MINI, MISTRAL_LARGE doesn't support image as input parameters
+    // https://docs.aws.amazon.com/bedrock/latest/userguide/models-supported.html
     public static final ChatLanguageModelCapabilities AI_JAMBA_1_5_MINI = ChatLanguageModelCapabilities.builder()
             .model(BedrockChatModel.builder()
                     .modelId("ai21.jamba-1-5-mini-v1:0")
