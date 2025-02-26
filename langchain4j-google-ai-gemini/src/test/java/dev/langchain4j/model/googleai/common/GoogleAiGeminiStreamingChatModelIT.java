5f532c5eb039cb6877520a7ef924fd35ac01a8c0
Dmytro Liubarskyi <ljubarskij@gmail.com>
2025-02-26 18:59:00 +0100

    Remove old generate() API - Google AI Gemini

--- langchain4j-google-ai-gemini/src/test/java/dev/langchain4j/model/googleai/common/GoogleAiGeminiStreamingChatModelIT.java
+++ langchain4j-google-ai-gemini/src/test/java/dev/langchain4j/model/googleai/common/GoogleAiGeminiStreamingChatModelIT.java
@@ -1,28 +1,28 @@
 package dev.langchain4j.model.googleai.common;
 
-import dev.langchain4j.model.chat.ChatLanguageModel;
-import dev.langchain4j.model.chat.common.AbstractChatModelIT;
+import dev.langchain4j.model.chat.StreamingChatLanguageModel;
+import dev.langchain4j.model.chat.common.AbstractStreamingChatModelIT;
 import dev.langchain4j.model.chat.request.ChatRequestParameters;
-import dev.langchain4j.model.googleai.GoogleAiGeminiChatModel;
+import dev.langchain4j.model.googleai.GoogleAiGeminiStreamingChatModel;
 
 import java.util.List;
 
 import static dev.langchain4j.internal.Utils.getOrDefault;
 
-class GoogleAiGeminiChatModelIT extends AbstractChatModelIT {
+class GoogleAiGeminiStreamingChatModelIT extends AbstractStreamingChatModelIT {
 
     // TODO https://github.com/langchain4j/langchain4j/issues/2219
     // TODO https://github.com/langchain4j/langchain4j/issues/2220
 
-    static final GoogleAiGeminiChatModel GOOGLE_AI_GEMINI_CHAT_MODEL = GoogleAiGeminiChatModel.builder()
+    static final StreamingChatLanguageModel GOOGLE_AI_GEMINI_STREAMING_CHAT_MODEL = GoogleAiGeminiStreamingChatModel.builder()
             .apiKey(System.getenv("GOOGLE_AI_GEMINI_API_KEY"))
             .modelName("gemini-1.5-flash-8b")
             .build();
 
     @Override
-    protected List<ChatLanguageModel> models() {
+    protected List<StreamingChatLanguageModel> models() {
         return List.of(
-                GOOGLE_AI_GEMINI_CHAT_MODEL
+                GOOGLE_AI_GEMINI_STREAMING_CHAT_MODEL
                 // TODO add more model configs, see OpenAiChatModelIT
         );
     }
@@ -33,8 +33,8 @@ class GoogleAiGeminiChatModelIT extends AbstractChatModelIT {
     }
 
     @Override
-    protected ChatLanguageModel createModelWith(ChatRequestParameters parameters) {
-        return GoogleAiGeminiChatModel.builder()
+    protected StreamingChatLanguageModel createModelWith(ChatRequestParameters parameters) {
+        return GoogleAiGeminiStreamingChatModel.builder()
                 .apiKey(System.getenv("GOOGLE_AI_GEMINI_API_KEY"))
 
                 // TODO re-implement, support .defaultRequestParameters(ChatRequestParameters)
@@ -58,6 +58,21 @@ class GoogleAiGeminiChatModelIT extends AbstractChatModelIT {
 
     @Override
     protected boolean supportsDefaultRequestParameters() {
+        return false; // TODO implement
+    }
+
+    @Override
+    protected boolean supportsModelNameParameter() {
+        return false; // TODO implement
+    }
+
+    @Override
+    protected boolean supportsMaxOutputTokensParameter() {
+        return false; // TODO implement
+    }
+
+    @Override
+    protected boolean supportsStopSequencesParameter() {
         return false; // TODO implement
     }
 
@@ -88,6 +103,11 @@ class GoogleAiGeminiChatModelIT extends AbstractChatModelIT {
 
     protected boolean assertFinishReason() {
         return false; // TODO implement
+    }
+
+    @Override
+    protected boolean assertThreads() {
+        return false; // TODO fix
     }
 
     @Override
