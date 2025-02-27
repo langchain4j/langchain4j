4fdc9a1df5136ae6b6bcd3b19a9ffbd736611bd7
vga91 <giuseppe.villani@larus-ba.it>
2025-02-27 10:49:53 +0100

    applying spotless changes

--- langchain4j-neo4j/src/main/java/dev/langchain4j/rag/content/retriever/neo4j/Neo4jContentRetriever.java
+++ langchain4j-neo4j/src/main/java/dev/langchain4j/rag/content/retriever/neo4j/Neo4jContentRetriever.java
@@ -1,5 +1,8 @@
 package dev.langchain4j.rag.content.retriever.neo4j;
 
+import static dev.langchain4j.internal.Utils.getOrDefault;
+import static dev.langchain4j.internal.ValidationUtils.ensureNotNull;
+
 import dev.langchain4j.model.chat.ChatLanguageModel;
 import dev.langchain4j.model.input.Prompt;
 import dev.langchain4j.model.input.PromptTemplate;
@@ -7,18 +10,14 @@ import dev.langchain4j.rag.content.Content;
 import dev.langchain4j.rag.content.retriever.ContentRetriever;
 import dev.langchain4j.rag.query.Query;
 import dev.langchain4j.store.graph.neo4j.Neo4jGraph;
-import lombok.Builder;
-import org.neo4j.driver.Record;
-import org.neo4j.driver.types.Type;
-import org.neo4j.driver.types.TypeSystem;
-
 import java.util.List;
 import java.util.Map;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
-
-import static dev.langchain4j.internal.Utils.getOrDefault;
-import static dev.langchain4j.internal.ValidationUtils.ensureNotNull;
+import lombok.Builder;
+import org.neo4j.driver.Record;
+import org.neo4j.driver.types.Type;
+import org.neo4j.driver.types.TypeSystem;
 
 /**
  * A {@link ContentRetriever} that retrieves from an {@link Neo4jGraph}.
@@ -26,7 +25,8 @@ import static dev.langchain4j.internal.ValidationUtils.ensureNotNull;
  */
 public class Neo4jContentRetriever implements ContentRetriever {
 
-    private static final PromptTemplate DEFAULT_PROMPT_TEMPLATE = PromptTemplate.from("""
+    private static final PromptTemplate DEFAULT_PROMPT_TEMPLATE = PromptTemplate.from(
+            """
             Task:Generate Cypher statement to query a graph database.
             Instructions
             Use only the provided relationship types and properties in the schema.
@@ -34,7 +34,7 @@ public class Neo4jContentRetriever implements ContentRetriever {
 
             Schema:
             {{schema}}
-            
+
             {{examples}}
 
             Note: Do not include any explanations or apologies in your responses.
@@ -55,14 +55,13 @@ public class Neo4jContentRetriever implements ContentRetriever {
     private final String examples;
 
     @Builder
-    public Neo4jContentRetriever(Neo4jGraph graph, ChatLanguageModel chatLanguageModel, PromptTemplate promptTemplate, String examples) {
+    public Neo4jContentRetriever(
+            Neo4jGraph graph, ChatLanguageModel chatLanguageModel, PromptTemplate promptTemplate, String examples) {
 
         this.graph = ensureNotNull(graph, "graph");
         this.chatLanguageModel = ensureNotNull(chatLanguageModel, "chatLanguageModel");
         this.promptTemplate = getOrDefault(promptTemplate, DEFAULT_PROMPT_TEMPLATE);
-        this.examples = examples == null
-                ? ""
-                : "Cypher examples: " + examples;
+        this.examples = examples == null ? "" : "Cypher examples: " + examples;
     }
 
     @Override
--- langchain4j-neo4j/src/test/java/dev/langchain4j/rag/content/retriever/neo4j/Neo4jContentRetrieverIT.java
+++ langchain4j-neo4j/src/test/java/dev/langchain4j/rag/content/retriever/neo4j/Neo4jContentRetrieverIT.java
@@ -1,11 +1,20 @@
 package dev.langchain4j.rag.content.retriever.neo4j;
 
+import static dev.langchain4j.model.openai.OpenAiChatModelName.GPT_4_O_MINI;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.Mockito.when;
+
 import dev.langchain4j.internal.RetryUtils;
 import dev.langchain4j.model.chat.ChatLanguageModel;
 import dev.langchain4j.model.openai.OpenAiChatModel;
 import dev.langchain4j.rag.content.Content;
 import dev.langchain4j.rag.query.Query;
 import dev.langchain4j.store.graph.neo4j.Neo4jGraph;
+import java.net.URI;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.List;
 import org.junit.jupiter.api.*;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
@@ -17,23 +26,12 @@ import org.neo4j.driver.Session;
 import org.testcontainers.containers.Neo4jContainer;
 import org.testcontainers.junit.jupiter.Container;
 
-import java.net.URI;
-import java.nio.file.Files;
-import java.nio.file.Paths;
-import java.util.List;
-
-import static dev.langchain4j.model.openai.OpenAiChatModelName.GPT_4_O_MINI;
-import static org.assertj.core.api.Assertions.assertThat;
-import static org.mockito.ArgumentMatchers.anyString;
-import static org.mockito.Mockito.when;
-
 @ExtendWith(MockitoExtension.class)
 class Neo4jContentRetrieverIT {
 
     @Container
-    private static final Neo4jContainer<?> neo4jContainer = new Neo4jContainer<>("neo4j:latest")
-            .withoutAuthentication()
-            .withPlugins("apoc");
+    private static final Neo4jContainer<?> neo4jContainer =
+            new Neo4jContainer<>("neo4j:latest").withoutAuthentication().withPlugins("apoc");
 
     private Driver driver;
     private Neo4jGraph graph;
@@ -56,9 +54,7 @@ class Neo4jContentRetrieverIT {
             session.run("CREATE (book:Book {title: 'Dune'})<-[:WROTE]-(author:Person {name: 'Frank Herbert'})");
         }
 
-        graph = Neo4jGraph.builder()
-                .driver(driver)
-                .build();
+        graph = Neo4jGraph.builder().driver(driver).build();
 
         retriever = Neo4jContentRetriever.builder()
                 .graph(graph)
@@ -84,7 +80,8 @@ class Neo4jContentRetrieverIT {
     void shouldRetrieveContentWhenQueryIsValid() {
         // Given
         Query query = new Query("Who is the author of the book 'Dune'?");
-        when(chatLanguageModel.generate(anyString())).thenReturn("MATCH(book:Book {title: 'Dune'})<-[:WROTE]-(author:Person) RETURN author.name AS output");
+        when(chatLanguageModel.generate(anyString()))
+                .thenReturn("MATCH(book:Book {title: 'Dune'})<-[:WROTE]-(author:Person) RETURN author.name AS output");
 
         // When
         List<Content> contents = retriever.retrieve(query);
@@ -97,7 +94,9 @@ class Neo4jContentRetrieverIT {
     void shouldRetrieveContentWhenQueryIsValidAndResponseHasBackticks() {
         // Given
         Query query = new Query("Who is the author of the book 'Dune'?");
-        when(chatLanguageModel.generate(anyString())).thenReturn("```MATCH(book:Book {title: 'Dune'})<-[:WROTE]-(author:Person) RETURN author.name AS output```");
+        when(chatLanguageModel.generate(anyString()))
+                .thenReturn(
+                        "```MATCH(book:Book {title: 'Dune'})<-[:WROTE]-(author:Person) RETURN author.name AS output```");
 
         // When
         List<Content> contents = retriever.retrieve(query);
@@ -133,20 +132,22 @@ class Neo4jContentRetrieverIT {
         // Then
         assertThat(contents).hasSize(1);
     }
-    
+
     @Test
     void shouldRetrieveContentWhenQueryIsValidAndOpenAiChatModelIsUsedWithExample() throws Exception {
         // remove existing `Book` and `Person` entities
         driver.session().run("MATCH (n) DETACH DELETE n");
         // recreate Neo4jGraph instead of reuse `this.graph`, otherwise it remains the old one with (:Book), etc..
         final Neo4jGraph graphStreamer = Neo4jGraph.builder().driver(driver).build();
-        
-        URI resource =  getClass().getClassLoader().getResource("streamer_dataset.cypher")
+
+        URI resource = getClass()
+                .getClassLoader()
+                .getResource("streamer_dataset.cypher")
                 .toURI();
-        String datasetEntities = Files.readString( Paths.get(resource) );
+        String datasetEntities = Files.readString(Paths.get(resource));
 
         driver.session().executeWriteWithoutResult(tx -> {
-            for (String query: datasetEntities.split(";")) {
+            for (String query : datasetEntities.split(";")) {
                 System.out.println("query = " + query);
                 tx.run(query);
             }
@@ -162,12 +163,13 @@ class Neo4jContentRetrieverIT {
                 .logResponses(true)
                 .build();
 
-        String examples = """
+        String examples =
+                """
             # Which streamer has the most followers?
             MATCH (s:Stream)
             RETURN s.name AS streamer
             ORDER BY s.followers DESC LIMIT 1
-            
+
             # How many streamers are from Norway?
             MATCH (s:Stream)-[:HAS_LANGUAGE]->(:Language {{name: 'Norwegian'}})
             RETURN count(s) AS streamers
@@ -218,7 +220,9 @@ class Neo4jContentRetrieverIT {
     void shouldReturnEmptyListWhenQueryIsInvalid() {
         // Given
         Query query = new Query("Who is the author of the movie 'Dune'?");
-        when(chatLanguageModel.generate(anyString())).thenReturn("MATCH(movie:Movie {title: 'Dune'})<-[:WROTE]-(author:Person) RETURN author.name AS output");
+        when(chatLanguageModel.generate(anyString()))
+                .thenReturn(
+                        "MATCH(movie:Movie {title: 'Dune'})<-[:WROTE]-(author:Person) RETURN author.name AS output");
 
         // When
         List<Content> contents = retriever.retrieve(query);
