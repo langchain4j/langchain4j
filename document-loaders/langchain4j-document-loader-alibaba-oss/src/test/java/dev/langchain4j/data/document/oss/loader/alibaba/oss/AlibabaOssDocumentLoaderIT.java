bf2c4a8aae9ed527b843a39f58a7ff4860f52441
code2t <code2t@dev.org>
2025-03-07 15:07:38 +0800

    Update unit

--- document-loaders/langchain4j-document-loader-alibaba-oss/src/test/java/dev/langchain4j/data/document/oss/loader/alibaba/oss/AlibabaOssDocumentLoaderIT.java
+++ document-loaders/langchain4j-document-loader-alibaba-oss/src/test/java/dev/langchain4j/data/document/oss/loader/alibaba/oss/AlibabaOssDocumentLoaderIT.java
@@ -11,8 +11,9 @@ import dev.langchain4j.data.document.DocumentParser;
 import dev.langchain4j.data.document.loader.alibaba.oss.AlibabaOssCredentials;
 import dev.langchain4j.data.document.loader.alibaba.oss.AlibabaOssDocumentLoader;
 import dev.langchain4j.data.document.parser.TextDocumentParser;
-import java.io.File;
-import java.net.URL;
+import java.io.ByteArrayInputStream;
+import java.io.InputStream;
+import java.nio.charset.StandardCharsets;
 import java.util.List;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
@@ -36,11 +37,13 @@ class AlibabaOssDocumentLoaderIT {
     // test bucket info
     private static final String TEST_BUCKET = "test-by-reg";
     // test OSS file key info
-    private static final String TEST_KEY = "test-file-1.txt";
+    private static final String TEST_KEY_1 = "test-file-1.txt";
     private static final String TEST_KEY_2 = "test-directory/test-file-2.txt";
+    private static final String TEST_KEY_3 = "other_directory/file-3.txt";
     // test OSS file content info
-    private static final String TEST_CONTENT = "Hello, World!";
+    private static final String TEST_CONTENT_1 = "Hello, World!";
     private static final String TEST_CONTENT_2 = "Hello again!";
+    private static final String TEST_CONTENT_3 = "you can't load me";
 
     @BeforeAll
     static void beforeAll() {
@@ -60,9 +63,9 @@ class AlibabaOssDocumentLoaderIT {
     void beforeEach() {
         // clean exist data
         final ListObjectsV2Result listObjectsV2Result = ossClient.listObjectsV2(TEST_BUCKET);
-        listObjectsV2Result.getObjectSummaries().forEach(ossObjectSummary -> {
-            ossClient.deleteObject(TEST_BUCKET, ossObjectSummary.getKey());
-        });
+        listObjectsV2Result
+                .getObjectSummaries()
+                .forEach(ossObjectSummary -> ossClient.deleteObject(TEST_BUCKET, ossObjectSummary.getKey()));
     }
 
     @Test
@@ -75,29 +78,28 @@ class AlibabaOssDocumentLoaderIT {
 
     @Test
     void should_load_single_document() {
-
         // given
-        URL url = getClass().getClassLoader().getResource("test.txt");
-        ossClient.putObject(new PutObjectRequest(TEST_BUCKET, TEST_KEY, new File(url.getFile())));
+        InputStream inputStream = new ByteArrayInputStream(TEST_CONTENT_1.getBytes(StandardCharsets.UTF_8));
+        ossClient.putObject(new PutObjectRequest(TEST_BUCKET, TEST_KEY_1, inputStream));
 
         // when
-        Document document = loader.loadDocument(TEST_BUCKET, TEST_KEY, parser);
+        Document document = loader.loadDocument(TEST_BUCKET, TEST_KEY_1, parser);
 
         // then
-        assertThat(document.text()).isEqualTo(TEST_CONTENT);
+        assertThat(document.text()).isEqualTo(TEST_CONTENT_1);
         assertThat(document.metadata().toMap()).hasSize(1);
         assertThat(document.metadata().getString("source"))
-                .isEqualTo(String.format("oss://%s/%s", TEST_BUCKET, TEST_KEY));
+                .isEqualTo(String.format("oss://%s/%s", TEST_BUCKET, TEST_KEY_1));
     }
 
     @Test
     void should_load_multiple_documents() {
 
         // given
-        URL url = getClass().getClassLoader().getResource("test.txt");
-        URL url2 = getClass().getClassLoader().getResource("test2.txt");
-        ossClient.putObject(new PutObjectRequest(TEST_BUCKET, TEST_KEY, new File(url.getFile())));
-        ossClient.putObject(new PutObjectRequest(TEST_BUCKET, TEST_KEY_2, new File(url2.getFile())));
+        ossClient.putObject(new PutObjectRequest(
+                TEST_BUCKET, TEST_KEY_1, new ByteArrayInputStream(TEST_CONTENT_1.getBytes(StandardCharsets.UTF_8))));
+        ossClient.putObject(new PutObjectRequest(
+                TEST_BUCKET, TEST_KEY_2, new ByteArrayInputStream(TEST_CONTENT_2.getBytes(StandardCharsets.UTF_8))));
 
         // when
         List<Document> documents = loader.loadDocuments(TEST_BUCKET, parser);
@@ -110,25 +112,22 @@ class AlibabaOssDocumentLoaderIT {
         assertThat(documents.get(0).metadata().toMap())
                 .containsEntry("source", String.format("oss://%s/%s", TEST_BUCKET, TEST_KEY_2));
 
-        assertThat(documents.get(1).text()).isEqualTo(TEST_CONTENT);
+        assertThat(documents.get(1).text()).isEqualTo(TEST_CONTENT_1);
         assertThat(documents.get(1).metadata().toMap()).hasSize(1);
         assertThat(documents.get(1).metadata().toMap())
-                .containsEntry("source", String.format("oss://%s/%s", TEST_BUCKET, TEST_KEY));
+                .containsEntry("source", String.format("oss://%s/%s", TEST_BUCKET, TEST_KEY_1));
     }
 
     @Test
     void should_load_multiple_documents_with_prefix() {
 
         // given
-        URL otherUrl = getClass().getClassLoader().getResource("other.txt");
-        ossClient.putObject(
-                new PutObjectRequest(TEST_BUCKET, "other_directory/file.txt", new File(otherUrl.getFile())));
-
-        URL url = getClass().getClassLoader().getResource("test.txt");
-        ossClient.putObject(new PutObjectRequest(TEST_BUCKET, TEST_KEY, new File(url.getFile())));
-
-        URL url2 = getClass().getClassLoader().getResource("test2.txt");
-        ossClient.putObject(new PutObjectRequest(TEST_BUCKET, TEST_KEY_2, new File(url2.getFile())));
+        ossClient.putObject(new PutObjectRequest(
+                TEST_BUCKET, TEST_KEY_1, new ByteArrayInputStream(TEST_CONTENT_1.getBytes(StandardCharsets.UTF_8))));
+        ossClient.putObject(new PutObjectRequest(
+                TEST_BUCKET, TEST_KEY_2, new ByteArrayInputStream(TEST_CONTENT_2.getBytes(StandardCharsets.UTF_8))));
+        ossClient.putObject(new PutObjectRequest(
+                TEST_BUCKET, TEST_KEY_3, new ByteArrayInputStream(TEST_CONTENT_3.getBytes(StandardCharsets.UTF_8))));
 
         // when
         List<Document> documents = loader.loadDocuments(TEST_BUCKET, "test", parser);
@@ -136,6 +135,6 @@ class AlibabaOssDocumentLoaderIT {
         // then
         assertThat(documents).hasSize(2);
         assertThat(documents.get(0).text()).isEqualTo(TEST_CONTENT_2);
-        assertThat(documents.get(1).text()).isEqualTo(TEST_CONTENT);
+        assertThat(documents.get(1).text()).isEqualTo(TEST_CONTENT_1);
     }
 }
