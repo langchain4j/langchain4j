9130edd7572cbbce614ad7944087273353cac1ee
Jan Martiska <jmartisk@redhat.com>
2025-03-11 13:20:38 +0100

    MCP resources (#2656)
    <!--
    Thank you so much for your contribution!
    
    Please fill in all the sections below.
    Please open the PR as a draft initially. Once it is reviewed and
    approved, we will ask you to add documentation and examples.
    Please note that PRs with breaking changes or without tests will be
    rejected.
    
    Please note that PRs will be reviewed based on the priority of the
    issues they address.
    We ask for your patience. We are doing our best to review your PR as
    quickly as possible.
    Please refrain from pinging and asking when it will be reviewed. Thank
    you for understanding!
    -->
    
    ## Issue
    <!-- Please specify the ID of the issue this PR is addressing. For
    example: "Closes #1234" or "Fixes #1234" -->
    Partially fixes #2304 (only the resources part for now)
    
    ## Change
    Support for MCP resources, as per
    https://modelcontextprotocol.io/docs/concepts/resources
    
    
    ## General checklist
    <!-- Please double-check the following points and mark them like this:
    [X] -->
    - [x] There are no breaking changes
    - [x] I have added unit and/or integration tests for my change
    - [ ] The tests cover both positive and negative cases
    - [x] I have manually run all the unit and integration tests in the
    module I have added/changed, and they are all green
    - [x] I have manually run all the unit and integration tests in the
    [core](https://github.com/langchain4j/langchain4j/tree/main/langchain4j-core)
    and
    [main](https://github.com/langchain4j/langchain4j/tree/main/langchain4j)
    modules, and they are all green
    <!-- Before adding documentation and example(s) (below), please wait
    until the PR is reviewed and approved. -->
    - [x] I have added/updated the
    [documentation](https://github.com/langchain4j/langchain4j/tree/main/docs/docs)
    - [ ] I have added an example in the [examples
    repo](https://github.com/langchain4j/langchain4j-examples) (only for
    "big" features)
    - [ ] I have added/updated [Spring Boot
    starter(s)](https://github.com/langchain4j/langchain4j-spring) (if
    applicable)

---
+++ langchain4j-mcp/src/main/java/dev/langchain4j/mcp/client/protocol/ReadResourceParams.java
@@ -0,0 +1,14 @@
+package dev.langchain4j.mcp.client.protocol;
+
+public class ReadResourceParams {
+
+    private String uri;
+
+    public String getUri() {
+        return uri;
+    }
+
+    public void setUri(String uri) {
+        this.uri = uri;
+    }
+}
