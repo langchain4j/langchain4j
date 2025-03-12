70217c9e3816bb569bccccdfd4a2d34676c0488a
kpavlov <1517853+kpavlov@users.noreply.github.com>
2025-03-12 10:23:28 +0200

    Revert OpenAiChatModelListenerIT

--- langchain4j-open-ai/src/test/java/dev/langchain4j/model/openai/OpenAiChatModelListenerIT.java
+++ langchain4j-open-ai/src/test/java/dev/langchain4j/model/openai/OpenAiChatModelListenerIT.java
@@ -1,19 +1,16 @@
 package dev.langchain4j.model.openai;
 
-import static dev.langchain4j.model.openai.OpenAiChatModelName.GPT_4_O_MINI;
-import static java.util.Collections.singletonList;
-
 import dev.langchain4j.exception.HttpException;
 import dev.langchain4j.model.chat.ChatLanguageModel;
 import dev.langchain4j.model.chat.ChatModelListenerIT;
 import dev.langchain4j.model.chat.listener.ChatModelListener;
-import me.kpavlov.aimocks.openai.MockOpenai;
 import org.junit.jupiter.api.condition.EnabledIfEnvironmentVariable;
+
+import static dev.langchain4j.model.openai.OpenAiChatModelName.GPT_4_O_MINI;
+import static java.util.Collections.singletonList;
 
 @EnabledIfEnvironmentVariable(named = "OPENAI_API_KEY", matches = ".+")
 class OpenAiChatModelListenerIT extends ChatModelListenerIT {
-
-    private static final MockOpenai MOCK_OPENAI = new MockOpenai(0, true);
 
     @Override
     protected ChatLanguageModel createModel(ChatModelListener listener) {
@@ -38,20 +35,8 @@ class OpenAiChatModelListenerIT extends ChatModelListenerIT {
 
     @Override
     protected ChatLanguageModel createFailingModel(ChatModelListener listener) {
-
-        MOCK_OPENAI
-                .completion(request -> {
-                    request.seed(100500);
-                })
-                .responds(resp -> {
-                    resp.setFinishReason("banana");
-                });
-
         return OpenAiChatModel.builder()
                 .apiKey("banana")
-                .baseUrl("http://localhost:" + MOCK_OPENAI.port() + "/v1")
-                .modelName("foo")
-                .seed(100500)
                 .maxRetries(1)
                 .listeners(singletonList(listener))
                 .build();
