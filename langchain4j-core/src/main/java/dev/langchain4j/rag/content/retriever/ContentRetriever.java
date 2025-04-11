f820cb0ffddf545a251746e8792ff46b4f81e19a
Dmytro Liubarskyi <ljubarskij@gmail.com>
2025-04-11 13:18:45 +0200

    Removed Neo4j, as it was migrated to https://github.com/langchain4j/langchain4j-community

--- langchain4j-core/src/main/java/dev/langchain4j/rag/content/retriever/ContentRetriever.java
+++ langchain4j-core/src/main/java/dev/langchain4j/rag/content/retriever/ContentRetriever.java
@@ -13,11 +13,11 @@ import java.util.List;
  * The underlying data source can be virtually anything:
  * <pre>
  * - Embedding (vector) store (see {@link EmbeddingStoreContentRetriever})
- * - Full-text search engine (see {@code AzureAiSearchContentRetriever} in {@code langchain4j-azure-ai-search} module)
- * - Hybrid of vector and full-text search (see {@code AzureAiSearchContentRetriever} in {@code langchain4j-azure-ai-search} module)
+ * - Full-text search engine (see {@code AzureAiSearchContentRetriever} in the {@code langchain4j-azure-ai-search} module)
+ * - Hybrid of vector and full-text search (see {@code AzureAiSearchContentRetriever} in the {@code langchain4j-azure-ai-search} module)
  * - Web Search Engine (see {@link WebSearchContentRetriever})
- * - Knowledge graph (see {@code Neo4jContentRetriever} in {@code langchain4j-neo4j} module)
- * - SQL database (see {@code SqlDatabaseContentRetriever} in {@code langchain4j-experimental-sql} module)
+ * - Knowledge graph (see {@code Neo4jContentRetriever} in the {@code langchain4j-community-neo4j-retriever} module)
+ * - SQL database (see {@code SqlDatabaseContentRetriever} in the {@code langchain4j-experimental-sql} module)
  * - etc.
  * </pre>
  *
