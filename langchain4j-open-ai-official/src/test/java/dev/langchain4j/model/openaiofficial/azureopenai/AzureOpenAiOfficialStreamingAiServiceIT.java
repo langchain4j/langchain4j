3911bbea4814290a6fccba3e7ae69059c5c6b671
Dmytro Liubarskyi <ljubarskij@gmail.com>
2025-04-17 10:28:01 +0200

    WIP

--- langchain4j-open-ai-official/src/test/java/dev/langchain4j/model/openaiofficial/azureopenai/AzureOpenAiOfficialStreamingAiServiceIT.java
+++ langchain4j-open-ai-official/src/test/java/dev/langchain4j/model/openaiofficial/azureopenai/AzureOpenAiOfficialStreamingAiServiceIT.java
@@ -1,6 +1,10 @@
 package dev.langchain4j.model.openaiofficial.azureopenai;
 
 import dev.langchain4j.model.chat.StreamingChatModel;
+import dev.langchain4j.model.chat.response.ChatResponseMetadata;
+import dev.langchain4j.model.openaiofficial.OpenAiOfficialChatResponseMetadata;
+import dev.langchain4j.model.openaiofficial.OpenAiOfficialTokenUsage;
+import dev.langchain4j.model.output.TokenUsage;
 import dev.langchain4j.service.common.AbstractStreamingAiServiceIT;
 import java.util.List;
 import org.junit.jupiter.api.condition.EnabledIfEnvironmentVariable;
@@ -11,5 +15,15 @@ class AzureOpenAiOfficialStreamingAiServiceIT extends AbstractStreamingAiService
     @Override
     protected List<StreamingChatModel> models() {
         return InternalAzureOpenAiOfficialTestHelper.chatModelsStreamingNormalAndJsonStrict();
+    }
+
+    @Override
+    protected Class<? extends ChatResponseMetadata> chatResponseMetadataType() {
+        return OpenAiOfficialChatResponseMetadata.class;
+    }
+
+    @Override
+    protected Class<? extends TokenUsage> tokenUsageType() {
+        return OpenAiOfficialTokenUsage.class;
     }
 }
