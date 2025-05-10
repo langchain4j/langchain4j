8ac31d6aa037c692b6226e8ec318a16a6ca65c15
kpavlov <1517853+kpavlov@users.noreply.github.com>
2025-05-10 13:06:42 +0300

    Increase test timeout duration to 1500ms

--- langchain4j-vertex-ai-gemini/src/test/java/dev/langchain4j/model/vertexai/VertexAiGeminiChatModelErrorsTest.java
+++ langchain4j-vertex-ai-gemini/src/test/java/dev/langchain4j/model/vertexai/VertexAiGeminiChatModelErrorsTest.java
@@ -45,7 +45,7 @@ class VertexAiGeminiChatModelErrorsTest {
         Runtime.getRuntime().addShutdownHook(new Thread(MOCK::stop));
     }
 
-    public static final Duration TIMEOUT = Duration.ofMillis(700);
+    private static final Duration TIMEOUT = Duration.ofMillis(1500);
 
     private final String project = "proj12345";
     private final String location = "us-central1";
