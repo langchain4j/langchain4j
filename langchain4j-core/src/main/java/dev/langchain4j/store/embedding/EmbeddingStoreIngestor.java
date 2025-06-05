a52b4a6fa4a4164a0e5b932a63bb57183bbc0c68
minglu7 <1347866672@qq.com>
2025-06-05 09:24:06 +0800

    Fix logging message to reflect correct number of transformed text segments

--- langchain4j-core/src/main/java/dev/langchain4j/store/embedding/EmbeddingStoreIngestor.java
+++ langchain4j-core/src/main/java/dev/langchain4j/store/embedding/EmbeddingStoreIngestor.java
@@ -195,7 +195,7 @@ public class EmbeddingStoreIngestor {
         }
         if (textSegmentTransformer != null) {
             segments = textSegmentTransformer.transformAll(segments);
-            log.debug("Text segments were transformed into {} text segments", documents.size());
+            log.debug("Text segments were transformed into {} text segments", segments.size());
         }
 
         log.debug("Starting to embed {} text segments", segments.size());
