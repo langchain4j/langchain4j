31baa2af1c446de910252d38d7896d294730278b
Dmytro Liubarskyi <ljubarskij@gmail.com>
2025-07-02 11:30:53 +0200

    Update langchain4j-open-ai-official/src/main/java/dev/langchain4j/model/openaiofficial/AzureInternalOpenAiOfficialHelper.java

--- langchain4j-open-ai-official/src/main/java/dev/langchain4j/model/openaiofficial/AzureInternalOpenAiOfficialHelper.java
+++ langchain4j-open-ai-official/src/main/java/dev/langchain4j/model/openaiofficial/AzureInternalOpenAiOfficialHelper.java
@@ -5,7 +5,7 @@ import com.azure.identity.DefaultAzureCredentialBuilder;
 import com.openai.credential.BearerTokenCredential;
 import com.openai.credential.Credential;
 
-public class AzureInternalOpenAiOfficialHelper {
+class AzureInternalOpenAiOfficialHelper {
 
     static Credential getAzureCredential() {
         return BearerTokenCredential.create(AuthenticationUtil.getBearerTokenSupplier(
