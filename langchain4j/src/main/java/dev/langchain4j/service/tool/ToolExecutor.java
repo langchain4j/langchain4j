a48bffb00c5e1d4763f26e17d21ff4ecde7da629
Bill Burke <bburke@redhat.com>
2025-07-08 10:15:55 -0400

    Expose invoke on ToolExecutor

--- langchain4j/src/main/java/dev/langchain4j/service/tool/ToolExecutor.java
+++ langchain4j/src/main/java/dev/langchain4j/service/tool/ToolExecutor.java
@@ -16,4 +16,19 @@ public interface ToolExecutor {
      * @return The result of the tool execution.
      */
     String execute(ToolExecutionRequest toolExecutionRequest, Object memoryId);
+
+
+    /**
+     * Invokes a tool request, but does not marshal the result to a string.
+     *
+     * @param toolExecutionRequest The tool execution request. Contains tool name and arguments.
+     * @param memoryId              The ID of the chat memory. See {@link MemoryId} for more details.
+     * @throws Throwable Propagated exception from the reflection call on the tool method
+     * @return The tool execution result.
+     */
+    default Object invoke(ToolExecutionRequest toolExecutionRequest, Object memoryId) throws Throwable {
+        // Default implementation is for backward compatibility with the old ToolExecutor interface.
+        throw new UnsupportedOperationException("invoke is not supported by this tool executor");
+    }
+
 }
