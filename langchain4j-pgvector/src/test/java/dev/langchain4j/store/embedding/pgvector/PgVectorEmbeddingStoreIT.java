6a3dd29afeba8c7934cf7d77dfdf4adf0fac08a3
Dmytro Liubarskyi <ljubarskij@gmail.com>
2025-07-21 18:01:17 +0200

    PgVector: test ingesting multiple times

--- langchain4j-pgvector/src/test/java/dev/langchain4j/store/embedding/pgvector/PgVectorEmbeddingStoreIT.java
+++ langchain4j-pgvector/src/test/java/dev/langchain4j/store/embedding/pgvector/PgVectorEmbeddingStoreIT.java
@@ -1,15 +1,24 @@
 package dev.langchain4j.store.embedding.pgvector;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.testcontainers.shaded.org.apache.commons.lang3.RandomUtils.nextInt;
 
+import dev.langchain4j.data.document.Document;
+import dev.langchain4j.data.embedding.Embedding;
 import dev.langchain4j.data.segment.TextSegment;
 import dev.langchain4j.model.embedding.EmbeddingModel;
 import dev.langchain4j.model.embedding.onnx.allminilml6v2q.AllMiniLmL6V2QuantizedEmbeddingModel;
+import dev.langchain4j.model.output.Response;
+import dev.langchain4j.store.embedding.EmbeddingMatch;
 import dev.langchain4j.store.embedding.EmbeddingStore;
+import dev.langchain4j.store.embedding.EmbeddingStoreIngestor;
 import dev.langchain4j.store.embedding.EmbeddingStoreWithFilteringIT;
+import org.junit.jupiter.api.Test;
 import org.testcontainers.containers.PostgreSQLContainer;
 import org.testcontainers.junit.jupiter.Container;
 import org.testcontainers.junit.jupiter.Testcontainers;
+import org.w3c.dom.Text;
+import java.util.List;
 
 @Testcontainers
 class PgVectorEmbeddingStoreIT extends EmbeddingStoreWithFilteringIT {
@@ -20,6 +29,28 @@ class PgVectorEmbeddingStoreIT extends EmbeddingStoreWithFilteringIT {
     EmbeddingStore<TextSegment> embeddingStore;
 
     EmbeddingModel embeddingModel = new AllMiniLmL6V2QuantizedEmbeddingModel();
+
+    @Test
+    void should_ingest_multiple_times() {
+        assertThat(getAllEmbeddings()).isEmpty();
+
+        ingest(List.of(Document.from("one"), Document.from("two")), embeddingStore, embeddingModel);
+        assertThat(getAllEmbeddings()).hasSize(2);
+
+        ingest(List.of(Document.from("three"), Document.from("four")), embeddingStore, embeddingModel);
+        assertThat(getAllEmbeddings()).hasSize(4);
+
+        ingest(List.of(Document.from("five"), Document.from("six")), embeddingStore, embeddingModel);
+        assertThat(getAllEmbeddings()).hasSize(6);
+    }
+
+    void ingest(List<Document> documents, EmbeddingStore<TextSegment> embeddingStore, EmbeddingModel embeddingModel) {
+        EmbeddingStoreIngestor ingestor = EmbeddingStoreIngestor.builder()
+                .embeddingModel(embeddingModel)
+                .embeddingStore(embeddingStore)
+                .build();
+        ingestor.ingest(documents);
+    }
 
     @Override
     protected void ensureStoreIsReady() {
