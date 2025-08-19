8c9cb7d3efb9352b5a779b3735d2623181d81130
Dmytro Liubarskyi <ljubarskij@gmail.com>
2025-08-19 10:03:06 +0200

    added more info for debugging
    https://github.com/langchain4j/langchain4j/actions/runs/17056043652/job/48353816754

---
+++ langchain4j/src/test/java/dev/langchain4j/service/output/ParsingUtilsTest.java
@@ -0,0 +1,21 @@
+package dev.langchain4j.service.output;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.jupiter.api.Test;
+
+class ParsingUtilsTest {
+
+    @Test
+    void should_dump_problematic_text_in_base64() {
+
+        // given
+        String text = "invalid json";
+
+        // when
+        OutputParsingException e = ParsingUtils.outputParsingException(text, Object.class);
+
+        // then
+        assertThat(e).hasMessageContaining("aW52YWxpZCBqc29u");
+    }
+}
