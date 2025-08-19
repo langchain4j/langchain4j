b20dfeb7ed19e3331a0a97005b474a7cd46a694e
mariofusco <mario.fusco@gmail.com>
2025-08-19 21:52:13 +0200

    use VirtualThreadUtils

--- langchain4j-core/src/main/java/dev/langchain4j/internal/DefaultExecutorProvider.java
+++ langchain4j-core/src/main/java/dev/langchain4j/internal/DefaultExecutorProvider.java
@@ -1,11 +1,12 @@
 package dev.langchain4j.internal;
 
 import dev.langchain4j.Internal;
-import java.lang.reflect.Method;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.SynchronousQueue;
 import java.util.concurrent.ThreadPoolExecutor;
 import java.util.concurrent.TimeUnit;
+
+import static dev.langchain4j.internal.VirtualThreadUtils.createVirtualThreadExecutor;
 
 @Internal
 public class DefaultExecutorProvider {
@@ -16,44 +17,9 @@ public class DefaultExecutorProvider {
         return Holder.EXECUTOR_SERVICE;
     }
 
-
     private static class Holder {
-        private static final boolean VIRTUAL_THREADS_SUPPORTED = detectVirtualThreadSupport();
-        private static final ExecutorService EXECUTOR_SERVICE = createExecutorService(VIRTUAL_THREADS_SUPPORTED);
-
-        /**
-         * Creates an ExecutorService that uses virtual threads when supported, otherwise platform threads.
-         * @return An ExecutorService
-         */
-        private static ExecutorService createExecutorService(boolean useVirtualThreads) {
-            if (useVirtualThreads) {
-                return createVirtualThreadExecutorService();
-            } else {
-                return createPlatformThreadExecutorService();
-            }
-        }
-
-        private static boolean detectVirtualThreadSupport() {
-            try {
-                Class.forName("java.lang.Thread").getMethod("startVirtualThread", Runnable.class);
-                return true;
-            } catch (Exception e) {
-                return false;
-            }
-        }
-
-        private static ExecutorService createVirtualThreadExecutorService() {
-            try {
-                // Use reflection to access Executors.newVirtualThreadPerTaskExecutor()
-                Method newVirtualThreadPerTaskExecutorMethod =
-                        Class.forName("java.util.concurrent.Executors").getMethod("newVirtualThreadPerTaskExecutor");
-
-                return (ExecutorService) newVirtualThreadPerTaskExecutorMethod.invoke(null);
-            } catch (Exception e) {
-                // Fallback in case of unexpected error
-                return createPlatformThreadExecutorService();
-            }
-        }
+        private static final ExecutorService EXECUTOR_SERVICE =
+                createVirtualThreadExecutor(Holder::createPlatformThreadExecutorService);
 
         private static ExecutorService createPlatformThreadExecutorService() {
             return new ThreadPoolExecutor(
