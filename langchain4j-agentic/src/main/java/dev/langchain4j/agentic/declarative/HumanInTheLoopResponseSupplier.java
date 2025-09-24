a9df7c1eec2565364510830b0fbe149884be3310
Mario Fusco <mario.fusco@gmail.com>
2025-09-24 17:14:45 +0200

    Add Javadocs to declarative API annotations (#3747)
    <!--
    Thank you so much for your contribution!
    
    Please fill in all the sections below.
    Please open the PR as a draft initially. Once it is reviewed and
    approved, we will ask you to add documentation and examples.
    Please note that PRs with breaking changes or without tests will be
    rejected.
    
    Please note that PRs will be reviewed based on the priority of the
    issues they address.
    We ask for your patience. We are doing our best to review your PR as
    quickly as possible.
    Please refrain from pinging and asking when it will be reviewed. Thank
    you for understanding!
    -->
    
    ## Issue
    <!-- Please specify the ID of the issue this PR is addressing. For
    example: "Closes #1234" or "Fixes #1234" -->
    Closes #
    
    ## Change
    <!-- Please describe the changes you made. -->
    
    
    ## General checklist
    <!-- Please double-check the following points and mark them like this:
    [X] -->
    - [ ] There are no breaking changes (API, behaviour)
    - [ ] I have added unit and/or integration tests for my change
    - [ ] The tests cover both positive and negative cases
    - [ ] I have manually run all the unit and integration tests in the
    module I have added/changed, and they are all green
    - [ ] I have manually run all the unit and integration tests in the
    [core](https://github.com/langchain4j/langchain4j/tree/main/langchain4j-core)
    and
    [main](https://github.com/langchain4j/langchain4j/tree/main/langchain4j)
    modules, and they are all green
    <!-- Before adding documentation and example(s) (below), please wait
    until the PR is reviewed and approved. -->
    - [ ] I have added/updated the
    [documentation](https://github.com/langchain4j/langchain4j/tree/main/docs/docs)
    - [ ] I have added an example in the [examples
    repo](https://github.com/langchain4j/langchain4j-examples) (only for
    "big" features)
    - [ ] I have added/updated [Spring Boot
    starter(s)](https://github.com/langchain4j/langchain4j-spring) (if
    applicable)
    
    
    ## Checklist for adding new maven module
    <!-- Please double-check the following points and mark them like this:
    [X] -->
    - [ ] I have added my new module in the root `pom.xml` and
    `langchain4j-bom/pom.xml`
    
    
    ## Checklist for adding new embedding store integration
    <!-- Please double-check the following points and mark them like this:
    [X] -->
    - [ ] I have added a `{NameOfIntegration}EmbeddingStoreIT` that extends
    from either `EmbeddingStoreIT` or `EmbeddingStoreWithFilteringIT`
    - [ ] I have added a `{NameOfIntegration}EmbeddingStoreRemovalIT` that
    extends from `EmbeddingStoreWithRemovalIT`
    
    ## Checklist for changing existing embedding store integration
    <!-- Please double-check the following points and mark them like this:
    [X] -->
    - [ ] I have manually verified that the
    `{NameOfIntegration}EmbeddingStore` works correctly with the data
    persisted using the latest released version of LangChain4j

--- langchain4j-agentic/src/main/java/dev/langchain4j/agentic/declarative/HumanInTheLoopResponseSupplier.java
+++ langchain4j-agentic/src/main/java/dev/langchain4j/agentic/declarative/HumanInTheLoopResponseSupplier.java
@@ -6,6 +6,28 @@ import java.lang.annotation.Target;
 import static java.lang.annotation.ElementType.METHOD;
 import static java.lang.annotation.RetentionPolicy.RUNTIME;
 
+/**
+ * Marks a method as a supplier of response for human-in-the-loop agent.
+ * The annotated method must be static, with no arguments, and return a String response.
+ * <p>
+ * Example:
+ * <pre>
+ * {@code
+ *     public interface AudienceRetriever {
+ *
+ *         @HumanInTheLoop(description = "Generate a story based on the given topic", outputName = "audience", async = true)
+ *         static void request(@V("topic") String topic) {
+ *             request.set("Which audience for topic " + topic + "?");
+ *         }
+ *
+ *         @HumanInTheLoopResponseSupplier
+ *         static String response() {
+ *             return System.console().readLine();
+ *         }
+ *     }
+ * }
+ * </pre>
+ */
 @Retention(RUNTIME)
 @Target({METHOD})
 public @interface HumanInTheLoopResponseSupplier {
