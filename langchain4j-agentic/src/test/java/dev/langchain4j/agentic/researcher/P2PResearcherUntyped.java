4c41f0d85ebe1c9ee5981f092e9de132f63300ec
mariofusco <mario.fusco@gmail.com>
2025-10-03 16:16:37 +0200

    add untyped p2p agent

---
+++ langchain4j-agentic/src/test/java/dev/langchain4j/agentic/researcher/P2PResearcherUntyped.java
@@ -0,0 +1,56 @@
+package dev.langchain4j.agentic.researcher;
+
+import dev.langchain4j.agentic.AgenticServices;
+import dev.langchain4j.agentic.p2p.P2PAgent;
+
+import static dev.langchain4j.agentic.Models.baseModel;
+
+public class P2PResearcherUntyped {
+
+    public static void main(String[] args) {
+        ArxivCrawler arxivCrawler = new ArxivCrawler();
+
+        LiteratureAgent literatureAgent = AgenticServices.agentBuilder(LiteratureAgent.class)
+                .chatModel(baseModel())
+                .tools(arxivCrawler)
+                .outputName("researchFindings")
+                .build();
+        HypothesisAgent hypothesisAgent = AgenticServices.agentBuilder(HypothesisAgent.class)
+                .chatModel(baseModel())
+                .tools(arxivCrawler)
+                .outputName("hypothesis")
+                .build();
+        CriticAgent criticAgent = AgenticServices.agentBuilder(CriticAgent.class)
+                .chatModel(baseModel())
+                .tools(arxivCrawler)
+                .outputName("critique")
+                .build();
+        ValidationAgent validationAgent = AgenticServices.agentBuilder(ValidationAgent.class)
+                .chatModel(baseModel())
+                .tools(arxivCrawler)
+                .outputName("hypothesis")
+                .build();
+        ScorerAgent scorerAgent = AgenticServices.agentBuilder(ScorerAgent.class)
+                .chatModel(baseModel())
+                .tools(arxivCrawler)
+                .outputName("score")
+                .build();
+
+        P2PAgent researcher = AgenticServices.p2pBuilder()
+                .chatModel(baseModel())
+                .subAgents(literatureAgent, hypothesisAgent, criticAgent, validationAgent, scorerAgent)
+                .outputName("hypothesis")
+                .exitCondition( agenticScope -> {
+                    if (!agenticScope.hasState("score")) {
+                        return false;
+                    }
+                    double score = agenticScope.readState("score", 0.0);
+                    System.out.println("Current hypothesis score: " + score);
+                    return score >= 0.85;
+                })
+                .maxAgentsInvocations(10)
+                .build();
+
+        System.out.println(researcher.invoke("produce a research about black holes"));
+    }
+}
