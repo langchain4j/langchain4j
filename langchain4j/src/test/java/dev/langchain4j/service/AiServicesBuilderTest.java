50b3d4101478d6b7dfad1e0b9ceda8aa89fe544d
Oleksandr Klymenko <alexanderklmn@gmail.com>
2025-10-15 22:48:45 +0200

    test: Add validation tests and refactor AiServicesBuilderTest
    Co-authored-by: Oleksandr Klymenko <alexanderklmn@gmail.com>
    Signed-off-by: Oleksandr Klymenko <alexanderklmn@gmail.com>

--- langchain4j/src/test/java/dev/langchain4j/service/AiServicesBuilderTest.java
+++ langchain4j/src/test/java/dev/langchain4j/service/AiServicesBuilderTest.java
@@ -1,5 +1,6 @@
 package dev.langchain4j.service;
 
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 import static org.assertj.core.api.AssertionsForClassTypes.assertThatExceptionOfType;
 import static org.mockito.Mockito.mock;
 
@@ -11,10 +12,14 @@ import dev.langchain4j.rag.content.retriever.ContentRetriever;
 import org.junit.jupiter.api.Test;
 
 /**
- * Verify that the AIServices builder doesn't allow setting more than out of
+ * Verify that the AIServices builder doesn't allow setting more than one of
  * (retriever, contentRetriever, retrievalAugmentor).
  */
 class AiServicesBuilderTest {
+
+    interface TestService {
+        String chat(String userMessage);
+    }
 
     @Test
     void contentRetrieverAndRetrievalAugmentor() {
@@ -22,7 +27,7 @@ class AiServicesBuilderTest {
         RetrievalAugmentor retrievalAugmentor = mock(RetrievalAugmentor.class);
 
         assertThatExceptionOfType(IllegalConfigurationException.class).isThrownBy(() -> {
-            AiServices.builder(AiServices.class)
+            AiServices.builder(TestService.class)
                     .contentRetriever(contentRetriever)
                     .retrievalAugmentor(retrievalAugmentor)
                     .build();
@@ -35,7 +40,7 @@ class AiServicesBuilderTest {
         RetrievalAugmentor retrievalAugmentor = mock(RetrievalAugmentor.class);
 
         assertThatExceptionOfType(IllegalConfigurationException.class).isThrownBy(() -> {
-            AiServices.builder(AiServices.class)
+            AiServices.builder(TestService.class)
                     .retrievalAugmentor(retrievalAugmentor)
                     .contentRetriever(contentRetriever)
                     .build();
@@ -44,15 +49,7 @@ class AiServicesBuilderTest {
 
     @Test
     void should_raise_an_error_when_tools_are_classes() {
-
-        // given
-        interface Assistant {
-
-            String chat(String userMessage);
-        }
-
         class HelloWorld {
-
             @Tool("Say hello")
             void add(String name) {
                 System.out.printf("Hello %s!", name);
@@ -62,9 +59,39 @@ class AiServicesBuilderTest {
         ChatModel chatModel = ChatModelMock.thatAlwaysResponds("Hello there!");
 
         assertThatExceptionOfType(IllegalConfigurationException.class)
-                .isThrownBy(() -> AiServices.builder(Assistant.class)
+                .isThrownBy(() -> AiServices.builder(TestService.class)
                         .chatModel(chatModel)
                         .tools(HelloWorld.class)
                         .build());
+    }
+
+    @Test
+    void should_throw_when_chat_model_is_null() {
+        assertThatExceptionOfType(IllegalConfigurationException.class)
+                .isThrownBy(() ->
+                        AiServices.builder(TestService.class).chatModel(null).build())
+                .withMessageContaining("chatModel");
+    }
+
+    @Test
+    void should_throw_when_multiple_retrievers_set() {
+        ContentRetriever contentRetriever1 = mock(ContentRetriever.class);
+        ContentRetriever contentRetriever2 = mock(ContentRetriever.class);
+
+        assertThatExceptionOfType(IllegalConfigurationException.class)
+                .isThrownBy(() -> AiServices.builder(TestService.class)
+                        .contentRetriever(contentRetriever1)
+                        .contentRetriever(contentRetriever2)
+                        .build());
+    }
+
+    @Test
+    void should_allow_building_with_only_chat_model() {
+        ChatModel chatModel = ChatModelMock.thatAlwaysResponds("response");
+
+        TestService service =
+                AiServices.builder(TestService.class).chatModel(chatModel).build();
+
+        assertThat(service).isNotNull();
     }
 }
