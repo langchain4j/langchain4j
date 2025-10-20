04463d3f882ea7d97dd671fd6b98e6e36c4bea13
Mario Fusco <mario.fusco@gmail.com>
2025-10-20 18:15:32 +0200

    Update langchain4j-core/src/main/java/dev/langchain4j/invocation/LangChain4jManaged.java
    Co-authored-by: Dmytro Liubarskyi <ljubarskij@gmail.com>

--- langchain4j-core/src/main/java/dev/langchain4j/invocation/LangChain4jManaged.java
+++ langchain4j-core/src/main/java/dev/langchain4j/invocation/LangChain4jManaged.java
@@ -6,7 +6,7 @@ import dev.langchain4j.Internal;
  * A marker interface for components that are managed by LangChain4j framework.
  * <p>
  * Implementing this interface indicates that the component is internally managed by LangChain4j,
- * and doesn't require to be instatiated or passed around by the user.
+ * and doesn't require to be instatiated or passed around by the user or LLM.
  *
  * @since 1.8.0
  */
