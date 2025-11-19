580d5e468801eea3adedb5e14d12f001e45d0c42
Xin Wang <xinwang@apache.org>
2025-11-19 21:27:39 +0800

    Optimize tool hallucination handling (#3773)
    <!--
    Thank you so much for your contribution!
    
    Please fill in all the sections below.
    Please open the PR as a draft initially. Once it is reviewed and
    approved, we will ask you to add documentation and examples.
    Please note that PRs with breaking changes or without tests will be
    rejected.
    
    Please note that PRs will be reviewed based on the priority of the
    issues they address.
    We ask for your patience. We are doing our best to review your PR as
    quickly as possible.
    Please refrain from pinging and asking when it will be reviewed. Thank
    you for understanding!
    -->
    
    ## Issue
    <!-- Please specify the ID of the issue this PR is addressing. For
    example: "Closes #1234" or "Fixes #1234" -->
    Closes https://github.com/langchain4j/langchain4j/issues/3547
    
    ## Change
    <!-- Please describe the changes you made. -->
    Optimize tool hallucination handling:
    1. Support ToolHallucinationStrategy for Streaming Mode.
    2. Added a new HallucinatedToolNameStrategy `LET_LLM_TRY`
    
    
    ## General checklist
    <!-- Please double-check the following points and mark them like this:
    [X] -->
    - [x] There are no breaking changes (API, behaviour)
    - [x] I have added unit and/or integration tests for my change
    - [x] The tests cover both positive and negative cases
    - [x] I have manually run all the unit and integration tests in the
    module I have added/changed, and they are all green
    - [x] I have manually run all the unit and integration tests in the
    [core](https://github.com/langchain4j/langchain4j/tree/main/langchain4j-core)
    and
    [main](https://github.com/langchain4j/langchain4j/tree/main/langchain4j)
    modules, and they are all green
    <!-- Before adding documentation and example(s) (below), please wait
    until the PR is reviewed and approved. -->
    - [ ] I have added/updated the
    [documentation](https://github.com/langchain4j/langchain4j/tree/main/docs/docs)
    - [ ] I have added an example in the [examples
    repo](https://github.com/langchain4j/langchain4j-examples) (only for
    "big" features)
    - [ ] I have added/updated [Spring Boot
    starter(s)](https://github.com/langchain4j/langchain4j-spring) (if
    applicable)
    
    
    ## Checklist for adding new maven module
    <!-- Please double-check the following points and mark them like this:
    [X] -->
    - [ ] I have added my new module in the root `pom.xml` and
    `langchain4j-bom/pom.xml`
    
    
    ## Checklist for adding new embedding store integration
    <!-- Please double-check the following points and mark them like this:
    [X] -->
    - [ ] I have added a `{NameOfIntegration}EmbeddingStoreIT` that extends
    from either `EmbeddingStoreIT` or `EmbeddingStoreWithFilteringIT`
    - [ ] I have added a `{NameOfIntegration}EmbeddingStoreRemovalIT` that
    extends from `EmbeddingStoreWithRemovalIT`
    
    ## Checklist for changing existing embedding store integration
    <!-- Please double-check the following points and mark them like this:
    [X] -->
    - [ ] I have manually verified that the
    `{NameOfIntegration}EmbeddingStore` works correctly with the data
    persisted using the latest released version of LangChain4j
    
    ---------
    
    Co-authored-by: Dmytro Liubarskyi <ljubarskij@gmail.com>

---
+++ langchain4j/src/test/java/dev/langchain4j/service/tool/HallucinatedToolNameStrategyTest.java
@@ -0,0 +1,133 @@
+package dev.langchain4j.service.tool;
+
+import static dev.langchain4j.service.tool.HallucinatedToolNameStrategy.THROW_EXCEPTION;
+import static java.util.concurrent.TimeUnit.SECONDS;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.spy;
+import static org.mockito.Mockito.verify;
+
+import dev.langchain4j.agent.tool.P;
+import dev.langchain4j.agent.tool.Tool;
+import dev.langchain4j.agent.tool.ToolExecutionRequest;
+import dev.langchain4j.data.message.AiMessage;
+import dev.langchain4j.data.message.ToolExecutionResultMessage;
+import dev.langchain4j.memory.chat.MessageWindowChatMemory;
+import dev.langchain4j.model.chat.mock.StreamingChatModelMock;
+import dev.langchain4j.model.chat.response.ChatResponse;
+import dev.langchain4j.service.AiServices;
+import dev.langchain4j.service.TokenStream;
+import java.util.List;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.TimeoutException;
+import org.assertj.core.util.Lists;
+import org.junit.jupiter.api.Test;
+
+public class HallucinatedToolNameStrategyTest {
+
+    interface Assistant {
+
+        TokenStream chat(String userMessage);
+    }
+
+    @Test // HallucinatedToolNameStrategy.THROW_EXCEPTION
+    void should_throw_exception() throws ExecutionException, InterruptedException, TimeoutException {
+        // given
+        ToolExecutionRequest toolExecutionRequest = ToolExecutionRequest.builder()
+                .name("browser_use") // Mock the wrong tool name and arguments
+                .arguments("{\"arg0\":\"mock_action\"}")
+                .build();
+        AiMessage aiMessage = AiMessage.from("does not matter", List.of(toolExecutionRequest));
+        StreamingChatModelMock model = StreamingChatModelMock.thatAlwaysStreams(aiMessage);
+
+        Assistant assistant = AiServices.builder(Assistant.class)
+                .streamingChatModel(model)
+                .tools(new Calculator())
+                .chatMemory(MessageWindowChatMemory.withMaxMessages(10))
+                // Use the default tool hallucination strategy
+                .hallucinatedToolNameStrategy(THROW_EXCEPTION)
+                .build();
+
+        // when
+        TokenStream tokenStream = assistant.chat("does not matter");
+
+        CompletableFuture<Throwable> futureError = new CompletableFuture<>();
+        tokenStream
+                .onCompleteResponse(r -> futureError.completeExceptionally(
+                        new IllegalStateException("onCompleteResponse should not be called")))
+                .onError(futureError::complete)
+                .start();
+        Throwable error = futureError.get(30, SECONDS);
+
+        // then
+        assertThat(error)
+                .isExactlyInstanceOf(RuntimeException.class)
+                .hasMessageContaining("no such tool exists. Most likely, it is a hallucination");
+    }
+
+    @Test // custom HallucinatedToolNameStrategy
+    void should_execute_tool_without_interrupt() throws Exception {
+        ToolExecutionRequest toolExecutionRequest1 = ToolExecutionRequest.builder()
+                .name("browser_use")
+                .arguments("{\"arg0\":\"mock_action\"}")
+                .build();
+        AiMessage aiMessage1 = AiMessage.from("invoke tool..", Lists.list(toolExecutionRequest1));
+
+        ToolExecutionRequest toolExecutionRequest2 = ToolExecutionRequest.builder()
+                .name("squareRoot")
+                .arguments("{\"arg0\":\"4.859067984738941E17d\"}")
+                .build();
+        AiMessage aiMessage2 = AiMessage.from(
+                "Maybe the assistant made a mistake in the tool call. Let me check the tool parameters again.",
+                Lists.list(toolExecutionRequest2));
+
+        AiMessage aiMessage3 = AiMessage.from("6.97070153193991E8");
+
+        StreamingChatModelMock model = StreamingChatModelMock.thatAlwaysStreams(aiMessage1, aiMessage2, aiMessage3);
+        Calculator calculator = spy(new Calculator());
+
+        Assistant assistant = AiServices.builder(Assistant.class)
+                .streamingChatModel(model)
+                .tools(calculator)
+                .chatMemory(MessageWindowChatMemory.withMaxMessages(10))
+                // Use the custom HallucinatedToolNameStrategy let the LLM try to solve hallucination issues itself,
+                // instead of throwing a RuntimeException.
+                .hallucinatedToolNameStrategy(
+                        toolExecutionRequest -> ToolExecutionResultMessage.from(
+                                toolExecutionRequest,
+                                toolExecutionRequest.name()
+                                        + "' is not a tool. please check the tool specifications again and use available tools."))
+                .build();
+
+        StringBuilder answerBuilder = new StringBuilder();
+        CompletableFuture<String> futureAnswer = new CompletableFuture<>();
+        CompletableFuture<ChatResponse> futureResponse = new CompletableFuture<>();
+
+        String userMessage = "What is the square root of 485906798473894056 in scientific notation?";
+
+        assistant
+                .chat(userMessage)
+                .onPartialResponse(answerBuilder::append)
+                .onCompleteResponse(response -> {
+                    futureAnswer.complete(answerBuilder.toString());
+                    futureResponse.complete(response);
+                })
+                .onError(futureAnswer::completeExceptionally)
+                .start();
+
+        String answer = futureAnswer.get(30, SECONDS);
+        ChatResponse response = futureResponse.get(30, SECONDS);
+
+        verify(calculator).squareRoot(4.859067984738941E17d);
+        assertThat(answer).contains("6.97");
+        assertThat(response.aiMessage().text()).contains("6.97");
+    }
+
+    static class Calculator {
+
+        @Tool("calculates the square root of the provided number")
+        double squareRoot(@P("number to operate on") double number) {
+            return Math.sqrt(number);
+        }
+    }
+}
