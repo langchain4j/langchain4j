8147bf279f2ad4ef2d5db1bb170274385fae55c8
Dmytro Liubarskyi <ljubarskij@gmail.com>
2025-11-25 17:07:42 +0100

    Apply suggestions from code review

--- langchain4j-google-ai-gemini/src/test/java/dev/langchain4j/model/googleai/GeminiFilesIT.java
+++ langchain4j-google-ai-gemini/src/test/java/dev/langchain4j/model/googleai/GeminiFilesIT.java
@@ -11,6 +11,7 @@ import java.util.List;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.condition.EnabledIfEnvironmentVariable;
 import org.junit.jupiter.api.io.TempDir;
 
 @EnabledIfEnvironmentVariable(named = "GOOGLE_AI_GEMINI_API_KEY", matches = ".+")
