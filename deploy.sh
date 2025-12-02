ae6de7e912fb81fd1ae09590cfe1b3ad1d433bbb
Viacheslav Klimov <viacheslavklimov11@gmail.com>
2025-12-02 14:45:04 +0200

    [TB] Update distribution management to publish to public repo

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,4 @@
-mvn deploy -T36 \
+mvn deploy -T36 -e \
  -Daether.connector.http.maxConnectionsPerRoute=200 \
  -DskipTests -DskipITs -DskipAnthropicITs -DskipLocalAiITs -DskipMilvusITs \
  -DskipMongoDbAtlasITs -DskipOllamaITs -DskipVearchITs -DskipVertexAiGeminiITs -Djacoco.skip=true
