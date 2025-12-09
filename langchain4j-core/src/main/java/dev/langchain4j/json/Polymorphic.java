483bbb6b03a1f1e89b10647579fcef7ad7cc7b62
Donguk <yidonguk@gmail.com>
2025-12-10 08:47:44 +0900

    add annotations and test

--- langchain4j-core/src/main/java/dev/langchain4j/json/Polymorphic.java
+++ langchain4j-core/src/main/java/dev/langchain4j/json/Polymorphic.java
@@ -1,11 +1,10 @@
 package dev.langchain4j.json;
 
-import static java.lang.annotation.ElementType.TYPE;
-
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
+import static java.lang.annotation.ElementType.TYPE;
 @Retention(RetentionPolicy.RUNTIME)
 @Target(TYPE)
 public @interface Polymorphic {
