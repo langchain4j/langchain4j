eab1bd589462989427fdfa47f58b68d32a894c53
Abraham Wijnands <brambomail@gmail.com>
2025-12-11 12:13:56 +0000

    Test.

--- langchain4j-google-ai-gemini/src/main/java/dev/langchain4j/model/googleai/GoogleAiGeminiBatchChatModel.java
+++ langchain4j-google-ai-gemini/src/main/java/dev/langchain4j/model/googleai/GoogleAiGeminiBatchChatModel.java
@@ -248,7 +248,8 @@ public final class GoogleAiGeminiBatchChatModel {
                 return List.of();
             }
             return response.inlinedResponses().inlinedResponses().stream()
-                    .map(wrapper -> Json.convertValue(wrapper.response(), GeminiGenerateContentResponse.class))
+                    .map(BatchCreateResponse.InlinedResponseWrapper::response)
+//                    .map(wrapper -> Json.convertValue(wrapper.response(), GeminiGenerateContentResponse.class))
                     .map(chatModel::processResponse)
                     .toList();
         }
