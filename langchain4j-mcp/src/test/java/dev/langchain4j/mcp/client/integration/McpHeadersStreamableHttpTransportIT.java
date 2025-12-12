94c4f0bdee1570e50476a5cafa3a46a0bc81decc
dongfeng <dongfeng@51ddi.com>
2025-12-12 19:31:11 +0800

    Add MCP server restart and auto-reconnect test

--- langchain4j-mcp/src/test/java/dev/langchain4j/mcp/client/integration/McpHeadersStreamableHttpTransportIT.java
+++ langchain4j-mcp/src/test/java/dev/langchain4j/mcp/client/integration/McpHeadersStreamableHttpTransportIT.java
@@ -1,17 +1,20 @@
 package dev.langchain4j.mcp.client.integration;
 
-import dev.langchain4j.mcp.client.DefaultMcpClient;
-import dev.langchain4j.mcp.client.transport.http.StreamableHttpMcpTransport;
-import org.junit.jupiter.api.AfterAll;
-import org.junit.jupiter.api.BeforeAll;
+import static dev.langchain4j.mcp.client.integration.McpServerHelper.skipTestsIfJbangNotAvailable;
 
 import java.io.IOException;
 import java.time.Duration;
 import java.util.concurrent.TimeoutException;
-
-import static dev.langchain4j.mcp.client.integration.McpServerHelper.skipTestsIfJbangNotAvailable;
+import dev.langchain4j.mcp.client.DefaultMcpClient;
+import dev.langchain4j.mcp.client.transport.http.StreamableHttpMcpTransport;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 class McpHeadersStreamableHttpTransportIT extends McpHeadersTestBase {
+    private static final Logger log = LoggerFactory.getLogger(McpHeadersStreamableHttpTransportIT.class);
+
 
     @BeforeAll
     static void setup() throws IOException, InterruptedException, TimeoutException {
@@ -25,9 +28,17 @@ class McpHeadersStreamableHttpTransportIT extends McpHeadersTestBase {
                 .build();
         mcpClient = new DefaultMcpClient.Builder()
                 .transport(transport)
+                .autoHealthCheckInterval(Duration.ofSeconds(1))
                 .toolExecutionTimeout(Duration.ofSeconds(4))
                 .reconnectInterval(Duration.ofSeconds(1))
                 .build();
+        // waiting for mcpClient to be ready
+        Thread.sleep(5000);
+        destroyProcessTree(process);
+        Thread.sleep(5000);
+        process = startProcess();
+        // waiting for mcpClient to reconnect
+        Thread.sleep(5000);
     }
 
     @AfterAll
@@ -36,7 +47,16 @@ class McpHeadersStreamableHttpTransportIT extends McpHeadersTestBase {
             mcpClient.close();
         }
         if (process != null) {
-            process.destroyForcibly();
+            destroyProcessTree(process);
         }
+    }
+
+    static void destroyProcessTree(Process process) {
+        ProcessHandle handle = process.toHandle();
+        handle.descendants().forEach(ph -> {
+            log.info("Destroying child process: " + ph.pid());
+            ph.destroyForcibly();
+        });
+        process.destroyForcibly();
     }
 }
