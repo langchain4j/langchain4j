5acf12bed0535e80c0e80f4d1d3fd28c4ef0aec4
Sandra Ahlgrimm <sandra.kriemann@gmail.com>
2026-01-30 15:24:58 +0000

    Run spotless on Azure Cosmos DB Mongo vCore module

--- langchain4j-azure-cosmos-mongo-vcore/src/test/java/dev/langchain4j/store/embedding/azure/cosmos/mongo/vcore/AzureCosmosDbMongoVCoreEmbeddingStoreCompatibilityTest.java
+++ langchain4j-azure-cosmos-mongo-vcore/src/test/java/dev/langchain4j/store/embedding/azure/cosmos/mongo/vcore/AzureCosmosDbMongoVCoreEmbeddingStoreCompatibilityTest.java
@@ -1,14 +1,13 @@
 package dev.langchain4j.store.embedding.azure.cosmos.mongo.vcore;
 
-import com.mongodb.client.ListCollectionNamesIterable;
-import com.mongodb.client.MongoDatabase;
-import org.junit.jupiter.api.Test;
-
-import java.util.List;
-
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
+
+import com.mongodb.client.ListCollectionNamesIterable;
+import com.mongodb.client.MongoDatabase;
+import java.util.List;
+import org.junit.jupiter.api.Test;
 
 class AzureCosmosDbMongoVCoreEmbeddingStoreCompatibilityTest {
 
@@ -19,8 +18,10 @@ class AzureCosmosDbMongoVCoreEmbeddingStoreCompatibilityTest {
         when(iterable.spliterator()).thenReturn(List.of("foo", "bar").spliterator());
         when(database.listCollectionNames()).thenReturn(iterable);
 
-        assertThat(AzureCosmosDbMongoVCoreEmbeddingStore.collectionExists(database, "foo")).isTrue();
-        assertThat(AzureCosmosDbMongoVCoreEmbeddingStore.collectionExists(database, "baz")).isFalse();
+        assertThat(AzureCosmosDbMongoVCoreEmbeddingStore.collectionExists(database, "foo"))
+                .isTrue();
+        assertThat(AzureCosmosDbMongoVCoreEmbeddingStore.collectionExists(database, "baz"))
+                .isFalse();
     }
 
     @Test
