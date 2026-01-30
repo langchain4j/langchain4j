2c170dd4333c3144cf6397edecc64ac502776c8d
Laura Trotta <153528055+l-trotta@users.noreply.github.com>
2026-01-30 09:51:21 +0100

    Elasticsearch hybrid search implementation (#4069)
    ## Issue
    Closes #1666
    
    ## Changes
    
    This PR adds hybrid search and full text search capabilities to the
    langchain4j-elasticsearch module by following the same approach chosen
    by AzureAI search, creating a common abstract interface and adding an
    extension of `ContentRetriever` (as [discussed in the
    issue](https://github.com/langchain4j/langchain4j/issues/1666#issuecomment-2405520929),
    the third approach).
    
    In details:
    - Creates a common interface `AbstractElasticsearchEmbeddingStore`,
    makes `ElasticsearchEmbeddingStore` extend it.
    - Creates a new class `ElasticsearchContentRetriever` which extends
    `AbstractElasticsearchEmbeddingStore`
    - Adds new configurations for full text search and hybrid search
    (`ElasticsearchConfigurationFullText`,
    `ElasticsearchConfigurationHybrid`)
    
    Positives of this approach: no breaking changes,
    `ElasticsearchEmbeddingStore` keeps all previous functionalities and can
    also be used with the new configurations, also it's coherent with the
    other existing implementations of `ContentRetriever`.
    
    Negatives of this approach: `ElasticsearchEmbeddingStore` and
    `ElasticsearchContentRetriever` can basically be used interchangeably,
    but `ElasticsearchContentRetriever` is more high level and easier to
    use. Deprecating `ElasticsearchEmbeddingStore` in the future could be
    beneficial in term of usability.
    
    ## Additional Changes
    
    ### Bugfix
    
    Version 9.2 of Elasticsearch (server) introduces a breaking change: by
    default, vector fields are no longer returned by vector type queries for
    performance reasons ([full explanation and
    details](https://www.elastic.co/search-labs/blog/elasticsearch-exclude-vectors-from-source)).
    This means that using langchain4j with its hardcoded queries, users of
    the 9.2 server would have no way of making the Elasticsearch server
    return the full response complete with vectors (except recreating the
    index manually with the proper setting). To solve this:
    
    - The null check for the vector field that was present in method
    `toMatches` has been removed, since many vector search cases actually
    have no use for the vectors to be returned.
    - A new optional parameter `includeVectorResponse` has been added to
    `AbstractElasticsearchEmbeddingStore`, so that users can choose to
    include the vectors in the response.
    
    ### Telemetry
    
    To help us track usage of the langchain4j library, a user-agent header
    has been added to the ElasticsearchClient instance.
    
    
    ## Additional Info
    
    I'm open to discuss this approach and make changes if necessary of
    course (disclosure: I work for Elastic).
    
    
    ## General checklist
    <!-- Please double-check the following points and mark them like this:
    [X] -->
    - [x] There are no breaking changes (API, behaviour)
    - [x] I have added unit and/or integration tests for my change
    - [x] The tests cover both positive and negative cases
    - [x] I have manually run all the unit and integration tests in the
    module I have added/changed, and they are all green
    - [x] I have manually run all the unit and integration tests in the
    [core](https://github.com/langchain4j/langchain4j/tree/main/langchain4j-core)
    and
    [main](https://github.com/langchain4j/langchain4j/tree/main/langchain4j)
    modules, and they are all green
    <!-- Before adding documentation and example(s) (below), please wait
    until the PR is reviewed and approved. -->
    - [x] I have added/updated the
    [documentation](https://github.com/langchain4j/langchain4j/tree/main/docs/docs)
    - [ ] I have added an example in the [examples
    repo](https://github.com/langchain4j/langchain4j-examples) (only for
    "big" features)
    - [ ] I have added/updated [Spring Boot
    starter(s)](https://github.com/langchain4j/langchain4j-spring) (if
    applicable)
    
    ---------
    
    Co-authored-by: Dmytro Liubarskyi <ljubarskij@gmail.com>
    Co-authored-by: David Pilato <david@pilato.fr>

--- langchain4j-elasticsearch/src/main/java/dev/langchain4j/store/embedding/elasticsearch/ElasticsearchConfigurationHybrid.java
+++ langchain4j-elasticsearch/src/main/java/dev/langchain4j/store/embedding/elasticsearch/ElasticsearchConfigurationHybrid.java
@@ -2,30 +2,37 @@ package dev.langchain4j.store.embedding.elasticsearch;
 
 import co.elastic.clients.elasticsearch.ElasticsearchClient;
 import co.elastic.clients.elasticsearch._types.ElasticsearchException;
-import co.elastic.clients.elasticsearch._types.KnnQuery;
+import co.elastic.clients.elasticsearch._types.KnnRetriever;
+import co.elastic.clients.elasticsearch._types.Retriever;
+import co.elastic.clients.elasticsearch._types.query_dsl.MatchQuery;
 import co.elastic.clients.elasticsearch.core.SearchResponse;
 import co.elastic.clients.elasticsearch.core.search.SourceConfig;
 import dev.langchain4j.store.embedding.EmbeddingSearchRequest;
 import java.io.IOException;
+import java.util.List;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 /**
  * Represents an <a href="https://www.elastic.co/">Elasticsearch</a> index as an embedding store
- * using the approximate kNN query implementation.
+ * using rff to combine a kNN query and a full text search.
  *
- * @see <a href="https://www.elastic.co/guide/en/elasticsearch/reference/current/query-dsl-knn-query.html#knn-query-top-level-parameters">kNN query</a>
+ * @see <a href="https://www.elastic.co/search-labs/tutorials/search-tutorial/vector-search/hybrid-search">hybrid search</a>
+ * <br>
+ * Running hybrid search requires an elasticsearch paid license.
+ *
+ * @see <a href="https://www.elastic.co/subscriptions">subscriptions</a>
  */
-public class ElasticsearchConfigurationKnn extends ElasticsearchConfiguration {
-    private static final Logger log = LoggerFactory.getLogger(ElasticsearchConfigurationKnn.class);
+public class ElasticsearchConfigurationHybrid extends ElasticsearchConfiguration {
+    private static final Logger log = LoggerFactory.getLogger(ElasticsearchConfigurationHybrid.class);
     private final Integer numCandidates;
 
     public static class Builder {
         private Integer numCandidates;
         private boolean includeVectorResponse = false;
 
-        public ElasticsearchConfigurationKnn build() {
-            return new ElasticsearchConfigurationKnn(numCandidates, includeVectorResponse);
+        public ElasticsearchConfigurationHybrid build() {
+            return new ElasticsearchConfigurationHybrid(numCandidates, includeVectorResponse);
         }
 
         /**
@@ -53,11 +60,11 @@ public class ElasticsearchConfigurationKnn extends ElasticsearchConfiguration {
         }
     }
 
-    public static ElasticsearchConfigurationKnn.Builder builder() {
+    public static ElasticsearchConfigurationHybrid.Builder builder() {
         return new Builder();
     }
 
-    private ElasticsearchConfigurationKnn(final Integer numCandidates, final boolean includeVectorResponse) {
+    private ElasticsearchConfigurationHybrid(final Integer numCandidates, final boolean includeVectorResponse) {
         this.numCandidates = numCandidates;
         this.includeVectorResponse = includeVectorResponse;
     }
@@ -65,8 +72,27 @@ public class ElasticsearchConfigurationKnn extends ElasticsearchConfiguration {
     @Override
     SearchResponse<Document> vectorSearch(
             ElasticsearchClient client, String indexName, EmbeddingSearchRequest embeddingSearchRequest)
+            throws ElasticsearchException {
+        throw new UnsupportedOperationException("Hybrid configuration does not support vector search");
+    }
+
+    @Override
+    SearchResponse<Document> fullTextSearch(
+            final ElasticsearchClient client, final String indexName, final String textQuery)
+            throws ElasticsearchException {
+        throw new UnsupportedOperationException("Hybrid configuration does not support full text search");
+    }
+
+    @Override
+    SearchResponse<Document> hybridSearch(
+            final ElasticsearchClient client,
+            final String indexName,
+            final EmbeddingSearchRequest embeddingSearchRequest,
+            final String textQuery)
             throws ElasticsearchException, IOException {
-        KnnQuery.Builder krb = new KnnQuery.Builder()
+
+        // Building KNN part of the hybrid query
+        KnnRetriever.Builder krb = new KnnRetriever.Builder()
                 .field(VECTOR_FIELD)
                 .queryVector(embeddingSearchRequest.queryEmbedding().vectorAsList());
 
@@ -74,13 +100,23 @@ public class ElasticsearchConfigurationKnn extends ElasticsearchConfiguration {
             krb.filter(ElasticsearchMetadataFilterMapper.map(embeddingSearchRequest.filter()));
         }
 
+        // k and numCandidates are required in KnnRetriever, calculating default values similarly to how elasticsearch
+        // calculates them for KnnQuery
         if (numCandidates != null) {
             krb.numCandidates(numCandidates);
+            krb.k(Math.min(numCandidates, embeddingSearchRequest.maxResults()));
+        } else {
+            krb.numCandidates(embeddingSearchRequest.maxResults());
+            krb.k(embeddingSearchRequest.maxResults());
         }
 
-        KnnQuery knn = krb.build();
+        KnnRetriever knn = krb.build();
 
-        log.trace("Searching for embeddings in index [{}] with query [{}].", indexName, knn);
+        // Building full text part of the hybrid query
+        MatchQuery matchQuery =
+                new MatchQuery.Builder().field(TEXT_FIELD).query(textQuery).build();
+
+        log.trace("Searching for embeddings in index [{}] with hybrid query [{}], [{}].", indexName, knn, matchQuery);
 
         return client.search(
                 s -> s.source(sr -> {
@@ -90,26 +126,11 @@ public class ElasticsearchConfigurationKnn extends ElasticsearchConfiguration {
                             return new SourceConfig.Builder().filter(f -> f);
                         })
                         .index(indexName)
+                        .retriever(r -> r.rrf(rf -> rf.retrievers(List.of(
+                                Retriever.of(rt -> rt.standard(st -> st.query(matchQuery))),
+                                Retriever.of(rt -> rt.knn(knn))))))
                         .size(embeddingSearchRequest.maxResults())
-                        .query(q -> q.knn(knn))
                         .minScore(embeddingSearchRequest.minScore()),
                 Document.class);
-    }
-
-    @Override
-    SearchResponse<Document> fullTextSearch(
-            final ElasticsearchClient client, final String indexName, final String textQuery)
-            throws ElasticsearchException {
-        throw new UnsupportedOperationException("Knn configuration does not support full text search");
-    }
-
-    @Override
-    SearchResponse<Document> hybridSearch(
-            final ElasticsearchClient client,
-            final String indexName,
-            final EmbeddingSearchRequest embeddingSearchRequest,
-            final String textQuery)
-            throws ElasticsearchException {
-        throw new UnsupportedOperationException("Knn configuration does not support hybrid search");
     }
 }
--- langchain4j-elasticsearch/src/main/java/dev/langchain4j/store/embedding/elasticsearch/ElasticsearchConfigurationScript.java
+++ langchain4j-elasticsearch/src/main/java/dev/langchain4j/store/embedding/elasticsearch/ElasticsearchConfigurationScript.java
@@ -6,13 +6,13 @@ import co.elastic.clients.elasticsearch._types.query_dsl.Query;
 import co.elastic.clients.elasticsearch._types.query_dsl.ScriptScoreQuery;
 import co.elastic.clients.elasticsearch.core.SearchRequest;
 import co.elastic.clients.elasticsearch.core.SearchResponse;
+import co.elastic.clients.elasticsearch.core.search.SourceConfig;
 import co.elastic.clients.json.JsonData;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import dev.langchain4j.data.document.Metadata;
 import dev.langchain4j.store.embedding.EmbeddingSearchRequest;
 import dev.langchain4j.store.embedding.filter.Filter;
-
 import java.io.IOException;
 
 /**
@@ -21,14 +21,28 @@ import java.io.IOException;
  * <br>
  * Supports storing {@link Metadata} and filtering by it using {@link Filter}
  * (provided inside {@link EmbeddingSearchRequest}).
+ *
  * @see <a href="https://www.elastic.co/guide/en/elasticsearch/reference/current/query-dsl-script-score-query.html#vector-functions-cosine">vector-functions-cosine</a>
  */
 public class ElasticsearchConfigurationScript extends ElasticsearchConfiguration {
     private final ObjectMapper objectMapper = new ObjectMapper();
 
     public static class Builder {
+        private boolean includeVectorResponse = false;
+
+        /**
+         * Whether to include vector fields in the search response (from Elasticsearch 9.2).
+         *
+         * @param includeVectorResponse true to include vector fields, false otherwise
+         * @return the builder instance
+         */
+        public Builder includeVectorResponse(boolean includeVectorResponse) {
+            this.includeVectorResponse = includeVectorResponse;
+            return this;
+        }
+
         public ElasticsearchConfigurationScript build() {
-            return new ElasticsearchConfigurationScript();
+            return new ElasticsearchConfigurationScript(includeVectorResponse);
         }
     }
 
@@ -36,26 +50,50 @@ public class ElasticsearchConfigurationScript extends ElasticsearchConfiguration
         return new ElasticsearchConfigurationScript.Builder();
     }
 
-    private ElasticsearchConfigurationScript() {
-
+    private ElasticsearchConfigurationScript(final boolean includeVectorResponse) {
+        this.includeVectorResponse = includeVectorResponse;
     }
 
     @Override
-    SearchResponse<Document> internalSearch(ElasticsearchClient client,
-                                                   String indexName,
-                                                   EmbeddingSearchRequest embeddingSearchRequest) throws ElasticsearchException, IOException {
-        ScriptScoreQuery scriptScoreQuery = buildDefaultScriptScoreQuery(embeddingSearchRequest.queryEmbedding().vector(),
-                (float) embeddingSearchRequest.minScore(), embeddingSearchRequest.filter());
+    SearchResponse<Document> vectorSearch(
+            ElasticsearchClient client, String indexName, EmbeddingSearchRequest embeddingSearchRequest)
+            throws ElasticsearchException, IOException {
+        ScriptScoreQuery scriptScoreQuery = buildDefaultScriptScoreQuery(
+                embeddingSearchRequest.queryEmbedding().vector(),
+                (float) embeddingSearchRequest.minScore(),
+                embeddingSearchRequest.filter());
         return client.search(
-                SearchRequest.of(s -> s.index(indexName)
+                SearchRequest.of(s -> s.source(sr -> {
+                            if (includeVectorResponse) {
+                                return sr.filter(f -> f.excludeVectors(false));
+                            }
+                            return new SourceConfig.Builder().filter(f -> f);
+                        })
+                        .index(indexName)
                         .query(n -> n.scriptScore(scriptScoreQuery))
                         .size(embeddingSearchRequest.maxResults())),
-                Document.class
-        );
+                Document.class);
     }
 
-    private ScriptScoreQuery buildDefaultScriptScoreQuery(float[] vector, float minScore,
-                                                          Filter filter) throws JsonProcessingException {
+    @Override
+    SearchResponse<Document> fullTextSearch(
+            final ElasticsearchClient client, final String indexName, final String textQuery)
+            throws ElasticsearchException {
+        throw new UnsupportedOperationException("Script configuration does not support full text search");
+    }
+
+    @Override
+    SearchResponse<Document> hybridSearch(
+            final ElasticsearchClient client,
+            final String indexName,
+            final EmbeddingSearchRequest embeddingSearchRequest,
+            final String textQuery)
+            throws ElasticsearchException {
+        throw new UnsupportedOperationException("Script configuration does not support hybrid search");
+    }
+
+    private ScriptScoreQuery buildDefaultScriptScoreQuery(float[] vector, float minScore, Filter filter)
+            throws JsonProcessingException {
         JsonData queryVector = toJsonData(vector);
         Query query;
         if (filter == null) {
@@ -63,12 +101,9 @@ public class ElasticsearchConfigurationScript extends ElasticsearchConfiguration
         } else {
             query = ElasticsearchMetadataFilterMapper.map(filter);
         }
-        return ScriptScoreQuery.of(q -> q
-                .minScore(minScore)
-                .query(query)
-                .script(s -> s
-                        .source("(cosineSimilarity(params.query_vector, 'vector') + 1.0) / 2")
-                        .params("query_vector", queryVector)));
+        return ScriptScoreQuery.of(q -> q.minScore(minScore).query(query).script(s -> s.source(
+                        "(cosineSimilarity(params.query_vector, 'vector') + 1.0) / 2")
+                .params("query_vector", queryVector)));
     }
 
     private <T> JsonData toJsonData(T rawData) throws JsonProcessingException {
