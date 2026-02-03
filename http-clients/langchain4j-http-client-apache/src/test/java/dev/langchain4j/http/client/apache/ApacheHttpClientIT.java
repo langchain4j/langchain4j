f368ec0385e4a5deda56a55253413cc23f065e4d
Rui Yang <techoc@foxmail.com>
2026-02-03 22:40:36 +0800

    Add Apache HttpClient5 implementation for HTTP client (#4410)
    <!--
    Thank you so much for your contribution!
    
    Please fill in all the sections below.
    Please open the PR as a draft initially. Once it is reviewed and
    approved, we will ask you to add documentation and examples.
    Please note that PRs with breaking changes or without tests will be
    rejected.
    
    Please note that PRs will be reviewed based on the priority of the
    issues they address.
    We ask for your patience. We are doing our best to review your PR as
    quickly as possible.
    Please refrain from pinging and asking when it will be reviewed. Thank
    you for understanding!
    -->
    
    ## Issue
    <!-- Please specify the ID of the issue this PR is addressing. For
    example: "Closes #1234" or "Fixes #1234" -->
    Closes #3775
    
    ## Change
    <!-- Please describe the changes you made. -->
    Simply integrated Apache HttpClient 5 and modified the timeout in the
    HttpClientTimeoutIT class within the langchain4j-http-client test
    methods to 1 second. Since Apache HttpClient 5's asynchronous timeout
    cannot accurately implement a 250-millisecond timeout, the effect can
    only be achieved by increasing the timeout duration.
    
    ## General checklist
    <!-- Please double-check the following points and mark them like this:
    [X] -->
    - [x] There are no breaking changes (API, behaviour)
    - [x] I have added unit and/or integration tests for my change
    - [x] The tests cover both positive and negative cases
    - [x] I have manually run all the unit and integration tests in the
    module I have added/changed, and they are all green
    - [ ] I have manually run all the unit and integration tests in the
    [core](https://github.com/langchain4j/langchain4j/tree/main/langchain4j-core)
    and
    [main](https://github.com/langchain4j/langchain4j/tree/main/langchain4j)
    modules, and they are all green
    <!-- Before adding documentation and example(s) (below), please wait
    until the PR is reviewed and approved. -->
    - [ ] I have added/updated the
    [documentation](https://github.com/langchain4j/langchain4j/tree/main/docs/docs)
    - [ ] I have added an example in the [examples
    repo](https://github.com/langchain4j/langchain4j-examples) (only for
    "big" features)
    - [ ] I have added/updated [Spring Boot
    starter(s)](https://github.com/langchain4j/langchain4j-spring) (if
    applicable)
    
    
    ## Checklist for adding new maven module
    <!-- Please double-check the following points and mark them like this:
    [X] -->
    - [x] I have added my new module in the root `pom.xml` and
    `langchain4j-bom/pom.xml`
    
    
    ## Checklist for adding new embedding store integration
    <!-- Please double-check the following points and mark them like this:
    [X] -->
    - [ ] I have added a `{NameOfIntegration}EmbeddingStoreIT` that extends
    from either `EmbeddingStoreIT` or `EmbeddingStoreWithFilteringIT`
    - [ ] I have added a `{NameOfIntegration}EmbeddingStoreRemovalIT` that
    extends from `EmbeddingStoreWithRemovalIT`
    
    ## Checklist for changing existing embedding store integration
    <!-- Please double-check the following points and mark them like this:
    [X] -->
    - [ ] I have manually verified that the
    `{NameOfIntegration}EmbeddingStore` works correctly with the data
    persisted using the latest released version of LangChain4j
    
    ---------
    
    Co-authored-by: Dmytro Liubarskyi <ljubarskij@gmail.com>

---
+++ http-clients/langchain4j-http-client-apache/src/test/java/dev/langchain4j/http/client/apache/ApacheHttpClientIT.java
@@ -0,0 +1,15 @@
+package dev.langchain4j.http.client.apache;
+
+import dev.langchain4j.http.client.HttpClient;
+import dev.langchain4j.http.client.HttpClientIT;
+import java.util.List;
+import org.junit.jupiter.api.condition.EnabledIfEnvironmentVariable;
+
+@EnabledIfEnvironmentVariable(named = "OPENAI_API_KEY", matches = ".+")
+class ApacheHttpClientIT extends HttpClientIT {
+
+    @Override
+    protected List<HttpClient> clients() {
+        return List.of(ApacheHttpClient.builder().build());
+    }
+}
