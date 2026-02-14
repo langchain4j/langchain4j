e02bcdb5c4178cc645dee7eb12b33fc02934c5f5
Sus4nne <victorisu@gmail.com>
2026-02-14 16:14:03 +0100

    make the histogram work

--- langchain4j-micrometer-metrics/src/main/java/dev/langchain4j/micrometer/metrics/listeners/MicrometerMetricsChatModelListener.java
+++ langchain4j-micrometer-metrics/src/main/java/dev/langchain4j/micrometer/metrics/listeners/MicrometerMetricsChatModelListener.java
@@ -92,6 +92,9 @@ public class MicrometerMetricsChatModelListener implements ChatModelListener {
                 .tag(OTelGenAiAttributes.RESPONSE_MODEL.value(), getResponseModelName(responseContext))
                 .tag(OTelGenAiAttributes.TOKEN_TYPE.value(), tokenType.value())
                 .description(String.format("Measures %s tokens used", tokenType.value()))
+                .publishPercentileHistogram()
+                .minimumExpectedValue(1.0)
+                .maximumExpectedValue(67108864.0)
                 .serviceLevelObjectives(
                         1, 4, 16, 64, 256, 1024, 4096, 16384, 65536, 262144, 1048576, 4194304, 16777216, 67108864)
                 .register(meterRegistry)
