8f245c84607dee6bc26bb7386a689b6064d0423f
brunobat <brunobat@gmail.com>
2026-02-18 16:26:11 +0000

    Observation API listner implementation

--- langchain4j-observation/src/main/java/dev/langchain4j/observation/listeners/ObservationChatModelListener.java
+++ langchain4j-observation/src/main/java/dev/langchain4j/observation/listeners/ObservationChatModelListener.java
@@ -1,21 +1,41 @@
 package dev.langchain4j.observation.listeners;
 
+import static dev.langchain4j.observation.listeners.AttributeKeys.OPERATION_NAME;
+import static dev.langchain4j.observation.listeners.AttributeKeys.PROVIDER_NAME;
+import static dev.langchain4j.observation.listeners.AttributeKeys.REQUEST_MODEL;
+import static dev.langchain4j.observation.listeners.AttributeKeys.RESPONSE_MODEL;
+import static dev.langchain4j.observation.listeners.AttributeKeys.TOKEN_TYPE;
+import static dev.langchain4j.observation.listeners.AttributeKeys.TOKEN_USAGE;
+
 import dev.langchain4j.Experimental;
 import dev.langchain4j.model.chat.listener.ChatModelErrorContext;
 import dev.langchain4j.model.chat.listener.ChatModelListener;
 import dev.langchain4j.model.chat.listener.ChatModelRequestContext;
 import dev.langchain4j.model.chat.listener.ChatModelResponseContext;
+import io.micrometer.common.KeyValue;
+import io.micrometer.core.instrument.Counter;
+import io.micrometer.core.instrument.Meter.MeterProvider;
+import io.micrometer.core.instrument.MeterRegistry;
 import io.micrometer.observation.Observation;
 import io.micrometer.observation.ObservationRegistry;
 
 @Experimental
 public class ObservationChatModelListener implements ChatModelListener {
 
-    public static final String OBSERVATION_SCOPE_KEY = "micrometer.observation.scope";
-    private ObservationRegistry observationRegistry;
+    private static final String OBSERVATION_SCOPE_KEY = "micrometer.observation.scope";
+    private static final String OUTCOME_KEY = "outcome";
+    private static final String OUTCOME_SUCCESS = "SUCCESS";
+    private static final String OUTCOME_ERROR = "ERROR";
 
-    public ObservationChatModelListener(final ObservationRegistry observationRegistry) {
+    private final ObservationRegistry observationRegistry;
+    private final MeterProvider<Counter> tokenCounter;
+
+    public ObservationChatModelListener(final ObservationRegistry observationRegistry, final MeterRegistry meterRegistry) {
         this.observationRegistry = observationRegistry;
+        tokenCounter = Counter.builder(TOKEN_USAGE.value())
+                .description("Measures the quantity of used tokens")
+                .tag(OPERATION_NAME.value(), "chat")
+                .withRegistry(meterRegistry);
     }
 
     @Override
@@ -23,10 +43,10 @@ public class ObservationChatModelListener implements ChatModelListener {
         final Observation onRequest = Observation.createNotStarted("gen_ai.client.operation.duration",
                         () -> new ChatModelObservationContext(requestContext, null, null),
                         observationRegistry)
-                .lowCardinalityKeyValue("gen_ai.operation.name", "chat")
-                .lowCardinalityKeyValue("gen_ai.provider.name", requestContext.modelProvider().name())
-                .lowCardinalityKeyValue("gen_ai.request.model", requestContext.chatRequest().parameters().modelName())
-                .contextualName("Chat request duration")
+                .lowCardinalityKeyValue(OPERATION_NAME.value(), "chat")
+                .lowCardinalityKeyValue(PROVIDER_NAME.value(), requestContext.modelProvider().name())
+                .lowCardinalityKeyValue(REQUEST_MODEL.value(), requestContext.chatRequest().parameters().modelName())
+                .contextualName("GenAI request duration")
                 .start();
         final Observation.Scope scope = onRequest.openScope();
         requestContext.attributes().put(OBSERVATION_SCOPE_KEY, scope);
@@ -34,14 +54,44 @@ public class ObservationChatModelListener implements ChatModelListener {
 
     @Override
     public void onResponse(final ChatModelResponseContext responseContext) {
-        Observation.Scope currentScope = (Observation.Scope) responseContext.attributes().remove(OBSERVATION_SCOPE_KEY);
+        final Observation.Scope currentScope = (Observation.Scope) responseContext.attributes().remove(OBSERVATION_SCOPE_KEY);
         if (currentScope != null) {
-            Observation observation = currentScope.getCurrentObservation();
+            final Observation observation = currentScope.getCurrentObservation();
             try {
                 if (observation.getContext() instanceof ChatModelObservationContext chatModelObservationContext) {
                     chatModelObservationContext.setResponseContext(responseContext);
                 }
-                observation.lowCardinalityKeyValue("gen_ai.response.model", responseContext.chatResponse().metadata().modelName());
+
+                final String responseModelName = responseContext.chatResponse().metadata().modelName();
+                final Integer outputTokens = responseContext.chatResponse().tokenUsage().outputTokenCount();
+                final Integer inputTokens = responseContext.chatResponse().tokenUsage().inputTokenCount();
+                final KeyValue providerName = observation.getContextView().getLowCardinalityKeyValue(PROVIDER_NAME.value());
+
+                if (responseModelName != null) {
+                    observation.lowCardinalityKeyValue(RESPONSE_MODEL.value(), responseModelName);
+                    if (providerName != null && providerName.getValue() != null && outputTokens != null) {
+                        tokenCounter.withTags(
+                                        PROVIDER_NAME.value(), providerName.getValue(),
+                                        RESPONSE_MODEL.value(), responseModelName,
+                                        TOKEN_TYPE.value(), "input")
+                                .increment(inputTokens);
+
+                        tokenCounter.withTags(
+                                        PROVIDER_NAME.value(), providerName.getValue(),
+                                        RESPONSE_MODEL.value(), responseModelName,
+                                        TOKEN_TYPE.value(), "output")
+                                .increment(outputTokens);
+                    }
+                }
+
+                if (outputTokens != null) {
+                    observation.highCardinalityKeyValue("output_tokens", "" + outputTokens);
+                }
+
+                if (inputTokens != null) {
+                    observation.highCardinalityKeyValue("input_tokens", "" + inputTokens);
+                }
+                observation.lowCardinalityKeyValue(OUTCOME_KEY, OUTCOME_SUCCESS);
             } finally {
                 currentScope.close();
                 observation.stop();
@@ -51,14 +101,18 @@ public class ObservationChatModelListener implements ChatModelListener {
 
     @Override
     public void onError(final ChatModelErrorContext errorContext) {
-        Observation.Scope currentScope = (Observation.Scope) errorContext.attributes().remove(OBSERVATION_SCOPE_KEY);
+        final Observation.Scope currentScope = (Observation.Scope) errorContext.attributes().remove(OBSERVATION_SCOPE_KEY);
         if (currentScope != null) {
-            Observation observation = currentScope.getCurrentObservation();
+            final Observation observation = currentScope.getCurrentObservation();
             try {
                 if (observation.getContext() instanceof ChatModelObservationContext chatModelObservationContext) {
                     chatModelObservationContext.setErrorContext(errorContext);
                 }
-                observation.lowCardinalityKeyValue("gen_ai.response.model", responseContext.chatResponse().metadata().modelName())
+                final String modelName = errorContext.chatRequest().parameters().modelName();
+                if (modelName != null) {
+                    observation.lowCardinalityKeyValue(REQUEST_MODEL.value(), modelName);
+                }
+                observation.lowCardinalityKeyValue(OUTCOME_KEY, OUTCOME_ERROR);
                 observation.error(errorContext.error());
             } finally {
                 currentScope.close();
