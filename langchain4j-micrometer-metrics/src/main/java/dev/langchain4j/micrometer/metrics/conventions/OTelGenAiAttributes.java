898beedd4db2342f2c1afd41add19c48f30a486a
Susanne Pieterse <victorisu@gmail.com>
2026-02-23 13:41:34 +0100

    Feature/observability Metrics with Micrometer (#4556)
    <!--
    Thank you so much for your contribution!
    
    Please fill in all the sections below.
    Please open the PR as a draft initially. Once it is reviewed and
    approved, we will ask you to add documentation and examples.
    Please note that PRs with breaking changes or without tests will be
    rejected.
    
    Please note that PRs will be reviewed based on the priority of the
    issues they address.
    We ask for your patience. We are doing our best to review your PR as
    quickly as possible.
    Please refrain from pinging and asking when it will be reviewed. Thank
    you for understanding!
    -->
    
    ## Issue
    Closes https://github.com/langchain4j/langchain4j/issues/2329 partly
    It adds metrics for the chat model interaction only.
    
    ## Change
    This adds a specialised ChatModelListener which utilises a Micrometer
    Meterregistry to expose metrics about the ChatModelResponse/Request. It
    exposes metrics in line with the OpenTelemetry Semantic Conventions for
    Generative AI.
    
    ## General checklist
    <!-- Please double-check the following points and mark them like this:
    [X] -->
    - [x] There are no breaking changes (API, behaviour)
    - [x] I have added unit and/or integration tests for my change
    - [x] The tests cover both positive and negative cases
    - [x] I have manually run all the unit and integration tests in the
    module I have added/changed, and they are all green
    - [ ] I have manually run all the unit and integration tests in the
    [core](https://github.com/langchain4j/langchain4j/tree/main/langchain4j-core)
    and
    [main](https://github.com/langchain4j/langchain4j/tree/main/langchain4j)
    modules, and they are all green
    <!-- Before adding documentation and example(s) (below), please wait
    until the PR is reviewed and approved. -->
    - [x] I have added/updated the
    [documentation](https://github.com/langchain4j/langchain4j/tree/main/docs/docs)
    - [ ] I have added an example in the [examples
    repo](https://github.com/langchain4j/langchain4j-examples) (only for
    "big" features)
    - [ ] I have added/updated [Spring Boot
    starter(s)](https://github.com/langchain4j/langchain4j-spring) (if
    applicable) Will do later
    
    
    ## Checklist for adding new maven module
    <!-- Please double-check the following points and mark them like this:
    [X] -->
    - [x] I have added my new module in the root `pom.xml` and
    `langchain4j-bom/pom.xml`
    
    ---------
    
    Co-authored-by: Dmytro Liubarskyi <ljubarskij@gmail.com>

---
+++ langchain4j-micrometer-metrics/src/main/java/dev/langchain4j/micrometer/metrics/conventions/OTelGenAiAttributes.java
@@ -0,0 +1,96 @@
+package dev.langchain4j.micrometer.metrics.conventions;
+
+public enum OTelGenAiAttributes {
+    // GenAI General
+    /**
+     * The name of the operation being performed.
+     */
+    OPERATION_NAME("gen_ai.operation.name"),
+    /**
+     * The Generative AI provider as identified by the client or server instrumentation.
+     */
+    PROVIDER_NAME("gen_ai.provider.name"),
+
+    // Token
+    /**
+     * The type of token that is counted: input, output, total.
+     */
+    TOKEN_TYPE("gen_ai.token.type"),
+
+    // GenAI Request
+    /**
+     * The name of the model a request is being made to.
+     */
+    REQUEST_MODEL("gen_ai.request.model"),
+    /**
+     * The frequency penalty setting for the model request.
+     */
+    REQUEST_FREQUENCY_PENALTY("gen_ai.request.frequency_penalty"),
+    /**
+     * The maximum number of tokens the model generates for a request.
+     */
+    REQUEST_MAX_TOKENS("gen_ai.request.max_tokens"),
+    /**
+     * The presence penalty setting for the model request.
+     */
+    REQUEST_PRESENCE_PENALTY("gen_ai.request.presence_penalty"),
+    /**
+     * List of sequences that the model will use to stop generating further tokens.
+     */
+    REQUEST_STOP_SEQUENCES("gen_ai.request.stop_sequences"),
+    /**
+     * The temperature setting for the model request.
+     */
+    REQUEST_TEMPERATURE("gen_ai.request.temperature"),
+    /**
+     * The top_k sampling setting for the model request.
+     */
+    REQUEST_TOP_K("gen_ai.request.top_k"),
+    /**
+     * The top_p sampling setting for the model request.
+     */
+    REQUEST_TOP_P("gen_ai.request.top_p"),
+
+    // GenAI Response
+
+    /**
+     * Reasons the model stopped generating tokens, corresponding to each generation received.
+     */
+    RESPONSE_FINISH_REASONS("gen_ai.response.finish_reasons"),
+    /**
+     * The unique identifier for the AI response.
+     */
+    RESPONSE_ID("gen_ai.response.id"),
+    /**
+     * The name of the model that generated the response.
+     */
+    RESPONSE_MODEL("gen_ai.response.model"),
+
+    // Error
+
+    /**
+     * The type of error that occurred.
+     */
+    ERROR_TYPE("error.type"),
+
+    // GenAi server information
+    /**
+     * The GenAI server port.
+     */
+    SERVER_PORT("server.port"),
+
+    /**
+     * The GenAI server address.
+     */
+    SERVER_ADDRESS("server.address");
+
+    private final String value;
+
+    OTelGenAiAttributes(final String value) {
+        this.value = value;
+    }
+
+    public String value() {
+        return value;
+    }
+}
