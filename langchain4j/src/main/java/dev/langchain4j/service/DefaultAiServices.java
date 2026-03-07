6892b98be97dad339e2d0c62aba8c03b00ec280d
Titov, Alexey <a@koeg.de>
2026-03-07 14:10:00 +0100

    formatting

--- langchain4j/src/main/java/dev/langchain4j/service/DefaultAiServices.java
+++ langchain4j/src/main/java/dev/langchain4j/service/DefaultAiServices.java
@@ -738,12 +738,14 @@ class DefaultAiServices<T> extends AiServices<T> {
             prependTextContentsToUserMessage(userMessage, contents);
         }
 
-        return userMessage.contents().size() == contents.size() ? userMessage : userMessage.toBuilder().contents(contents).build();
+        return userMessage.contents().size() == contents.size()
+                ? userMessage
+                : userMessage.toBuilder().contents(contents).build();
     }
 
     private static void prependTextContentsToUserMessage(UserMessage userMessage, List<Content> contents) {
         List<Content> originalContent = userMessage.contents();
-        for (int i = originalContent.size()-1; i >= 0; i--) {
+        for (int i = originalContent.size() - 1; i >= 0; i--) {
             if (originalContent.get(i) instanceof TextContent textContent) {
                 contents.add(0, textContent);
             }
@@ -751,7 +753,7 @@ class DefaultAiServices<T> extends AiServices<T> {
     }
 
     private static boolean isMapOfContents(Object o) {
-        return o instanceof Map<?,?> map && map.values().stream().allMatch(Content.class::isInstance);
+        return o instanceof Map<?, ?> map && map.values().stream().allMatch(Content.class::isInstance);
     }
 
     private static boolean isListOfContents(Object o) {
