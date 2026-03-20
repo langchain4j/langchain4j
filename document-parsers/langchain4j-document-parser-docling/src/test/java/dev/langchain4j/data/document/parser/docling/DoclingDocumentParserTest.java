8595be3752345afed55462567791d17f85f3f0f6
lfelner22 <liamfelner22@gmail.com>
2026-03-20 17:11:36 -0400

    Add test class skeleton for DoclingDocumentParser

---
+++ document-parsers/langchain4j-document-parser-docling/src/test/java/dev/langchain4j/data/document/parser/docling/DoclingDocumentParserTest.java
@@ -0,0 +1,14 @@
+package dev.langchain4j.data.document.parser.docling;
+
+import dev.langchain4j.data.document.Document;
+import org.junit.jupiter.api.Test;
+
+import java.io.ByteArrayInputStream;
+import java.io.InputStream;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+class DoclingDocumentParserTest {
+    // Tests will be added incrementally
+}
