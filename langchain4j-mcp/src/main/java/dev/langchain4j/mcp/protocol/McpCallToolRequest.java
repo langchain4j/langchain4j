f0e73ae2a027dd278d97fc92de41bfa5c08bbe0a
Jan Martiska <wraychus@gmail.com>
2026-03-25 13:46:57 +0100

    Refactor MCP protocol messages to more closely reflect the MCP schema

--- langchain4j-mcp/src/main/java/dev/langchain4j/mcp/protocol/McpCallToolRequest.java
+++ langchain4j-mcp/src/main/java/dev/langchain4j/mcp/protocol/McpCallToolRequest.java
@@ -1,16 +1,14 @@
 package dev.langchain4j.mcp.protocol;
 
-import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.databind.node.ObjectNode;
 import dev.langchain4j.Internal;
-import java.util.HashMap;
 import java.util.Map;
 
+/**
+ * Corresponds to the {@code CallToolRequest} type from the MCP schema.
+ */
 @Internal
-public class McpCallToolRequest extends McpClientMessage {
-
-    @JsonInclude
-    private Map<String, Object> params;
+public class McpCallToolRequest extends McpClientRequest {
 
     public McpCallToolRequest(Long id, String toolName, ObjectNode arguments) {
         this(id, toolName, arguments, null);
@@ -18,15 +16,10 @@ public class McpCallToolRequest extends McpClientMessage {
 
     public McpCallToolRequest(Long id, String toolName, ObjectNode arguments, String progressToken) {
         super(id, McpClientMethod.TOOLS_CALL);
-        this.params = new HashMap<>();
-        this.params.put("name", toolName);
-        this.params.put("arguments", arguments);
+        McpCallToolParams params = new McpCallToolParams(toolName, arguments);
         if (progressToken != null) {
-            this.params.put("_meta", Map.of("progressToken", progressToken));
+            params.setMeta(Map.of("progressToken", progressToken));
         }
-    }
-
-    public Map<String, Object> getParams() {
-        return params;
+        setParams(params);
     }
 }
