dee5644111774b64518d3d2248747d474628f106
Rayan Ahmed <rayan12152007@gmail.com>
2026-04-17 13:51:43 -0400

    Implemented the default use of interaction event

---
+++ langchain4j-core/src/main/java/dev/langchain4j/observability/event/DefaultAiServiceInteractionEvent.java
@@ -0,0 +1,40 @@
+package dev.langchain4j.observatbility.event;
+
+import dev.langchain4j.invocation.InvocationContext;
+import dev.langchain4j.observability.api.event.AiServiceEvent;
+import dev.langchain4j.observability.api.event.AiServiceInteractionEvent;
+
+import java.util.List;
+
+import static dev.langchain4j.internal.ValidationUtils.ensureNotNull;
+
+public class DefaultAiServiceInteractionEvent implements AiServiceInteractionEvent {
+
+    private final InvocationContext invocationContext;
+
+    private final List<AiServiceEvent> events;
+
+    public DefaultAiServiceInteractionEvent(AiServiceInteractionEventBuilder builder){
+        this.invocationContext = ensureNotNull(bulider.invocationContext(), "invocationContext");
+        this.events = List.copyOf(ensureNotNull(builder.events(), "events"));
+    }
+
+    @Override 
+    public InvocationContext invocationContext(){
+        return invocationContext;
+    }
+
+    @Override 
+    public List<AiServiceEvent> events(){
+        return events;
+    }
+
+    @Override 
+    public String toString(){
+        return "DefaultAiServiceInteractionEvent{" + "invocationContext=" + invocationContext + ", events=" + events + "}";
+    }
+
+
+
+
+}
