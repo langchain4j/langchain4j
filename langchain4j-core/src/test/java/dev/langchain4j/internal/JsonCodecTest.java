8ff356245628cd2eb8d3df26ca342692682e5136
MoonlightAndSunlight <127007210+MoonlightAndSunlight@users.noreply.github.com>
2026-09-22 14:57:35 +0000

    style: put text blocks on the assignment line per spotless

--- langchain4j-core/src/test/java/dev/langchain4j/internal/JsonCodecTest.java
+++ langchain4j-core/src/test/java/dev/langchain4j/internal/JsonCodecTest.java
@@ -19,8 +19,7 @@ class JsonCodecTest {
 
     record Person(String name, int age) {}
 
-    private static final String PERSON_JSON =
-            """
+    private static final String PERSON_JSON = """
             {
                 "name": "Klaus",
                 "age": 42
@@ -63,8 +62,7 @@ class JsonCodecTest {
     void record_different_field_order(Json.JsonCodec codec) {
 
         // given
-        String json =
-                """
+        String json = """
                 {
                     "age": 42,
                     "name": "Klaus"
@@ -90,8 +88,7 @@ class JsonCodecTest {
     void should_fail_on_unknown_fields_by_default(Json.JsonCodec codec) {
 
         // given
-        String json =
-                """
+        String json = """
                 {
                     "name": "Klaus",
                     "age": 42,
@@ -118,8 +115,7 @@ class JsonCodecTest {
     void record_null_value(Json.JsonCodec codec) {
 
         // given
-        String json =
-                """
+        String json = """
                 {
                     "name": "Klaus",
                     "age": null
@@ -139,8 +135,7 @@ class JsonCodecTest {
     void record_wrong_type(Json.JsonCodec codec) {
 
         // given
-        String json =
-                """
+        String json = """
                 {
                     "name": "Klaus",
                     "age": "42"
@@ -160,8 +155,7 @@ class JsonCodecTest {
     void record_wrong_type_2(Json.JsonCodec codec) {
 
         // given
-        String json =
-                """
+        String json = """
                 {
                     "name": "Klaus",
                     "age": 42.0
@@ -185,8 +179,7 @@ class JsonCodecTest {
     void record_with_nested_record(Json.JsonCodec codec) {
 
         // given
-        String json =
-                """
+        String json = """
                 {
                     "name": "Klaus",
                     "address": {
@@ -239,8 +232,7 @@ class JsonCodecTest {
     void record_with_empty_collections(Json.JsonCodec codec) {
 
         // given
-        String json =
-                """
+        String json = """
                 {
                     "name": "Klaus",
                     "collection": [],
@@ -301,8 +293,7 @@ class JsonCodecTest {
     void record_with_optional_null(Json.JsonCodec codec) {
 
         // given
-        String json =
-                """
+        String json = """
                 {
                     "name": "Klaus",
                     "age": null
@@ -346,8 +337,7 @@ class JsonCodecTest {
     void record_with_validation(Json.JsonCodec codec) {
 
         // given
-        String json =
-                """
+        String json = """
                 {
                     "name": "Klaus",
                     "age": -1
